c27b64838a6ff09e77c042e581bee973d4c4f5fd
@ <@>
2012-03-14 16:36:18 +0100

    [MRG] stock_available_immediately from c2c-ecom-addons (lp:c2c-addons/6.1  rev 45.1.7)

--- stock_available_immediately/__openerp__.py
+++ stock_available_immediately/__openerp__.py
@@ -1,21 +1,19 @@
-# -*- coding: utf-8 -*-
+# -*- encoding: utf-8 -*-
 ##############################################################################
 #
-#    Author: Guewen Baconnier
-#    Copyright 2010-2012 Camptocamp SA
-#    Copyright (C) 2011 Akretion Sébastien BEAU <sebastien.beau@akretion.com>
+#    Author Guewen Baconnier. Copyright Camptocamp SA
 #
 #    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
 #    This program is distributed in the hope that it will be useful,
 #    but WITHOUT ANY WARRANTY; without even the implied warranty of
 #    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
+#    GNU General Public License for more details.
 #
-#    You should have received a copy of the GNU Affero General Public License
+#    You should have received a copy of the GNU General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
 ##############################################################################
@@ -24,18 +22,20 @@
 {
 	"name" : "Immediately Usable Stock Quantity",
 	"version" : "1.0",
-	"depends" : ["product", "stock", ],
+	"depends" : [
+	             "base",
+	             "product",
+				 "stock",
+				],
 	"author" : "Camptocamp",
-    "license": "AGPL-3",
-	"description": """
-Compute the immediately usable stock.
-Immediately usable is computed : Quantity on Hand - Outgoing Stock.
-""",
+	"description": """Compute the immediately usable stock. Immediately usable is computed : Real Stock - Outgoing Stock. """,
 	"website" : "http://tinyerp.com/module_account.html",
 	"category" : "Generic Modules/Stock",
 	"init_xml" : [],
 	"demo_xml" : [],
-	"update_xml" : ["product_view.xml", ],
+	"update_xml" : [
+	                "product_view.xml",
+	               ],
 	"active": False,
 	"installable": True
 }
