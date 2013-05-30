53778d4d998e9c5cbe004dd441fa1d9420673281
Lorenzo Battistini <lorenzo.battistini@agilebg.com>
2013-05-30 16:43:21 +0200

    [add] stock_optional_valuation - skeleton

---
+++ stock_optional_valuation/__openerp__.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#    
+#    Copyright (C) 2013 Agile Business Group sagl (<http://www.agilebg.com>)
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{
+    'name': "Stock optional valuation",
+    'version': '0.1',
+    'category': 'Warehouse Management',
+    'description': """
+You can choose which stock moves have to generate inventory valutanion accounting entries, by specifying it in the location form.
+""",
+    'author': 'Agile Business Group',
+    'website': 'http://www.agilebg.com',
+    'license': 'AGPL-3',
+    "depends" : ['stock'],
+    "data" : [
+        "stock_view.xml",
+        ],
+    "demo" : [],
+    'test': [
+        ],
+    "active": False,
+    "installable": True
+}
