3f83830efc3738b3c071e340dffd64568bc58fe1
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-09-05 15:09:14 +0200

    [ADD] button to open the reservation move

--- stock_reserve/__openerp__.py
+++ stock_reserve/__openerp__.py
@@ -31,10 +31,10 @@
 Stock Reserve
 =============
 
-Allows to create stock reservation on a product or a selection of
-products.  The reservations can be monitored and lifted on the product
-view.  Each reservation can have a validity date, once reached, the
-reservation is automatically lifted.
+Allows to create stock reservation on a product.
+
+Each reservation can have a validity date, once passed, the reservation
+is automatically lifted.
 
 """,
  'depends': ['stock',
