011c9c8ee9bad7cecb06e4a56bf279e0d8b3afc9
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-09-05 14:06:46 +0200

    [ADD] started a generic stock reservation module (stock_reserve), it will serve as a basis for the sale pre-book

---
+++ stock_reserve/__openerp__.py
@@ -0,0 +1,47 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    Author: Guewen Baconnier
+#    Copyright 2013 Camptocamp SA
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+{'name': 'Stock Reserve',
+ 'version': '0.1',
+ 'author': 'Camptocamp',
+ 'category': 'Warehouse',
+ 'license': 'AGPL-3',
+ 'complexity': 'normal',
+ 'images': [],
+ 'website': "http://www.camptocamp.com",
+ 'description': """
+Stock Reserve
+=============
+
+Allows to create stock reservation on a product or a selection of
+products.  The reservations can be monitored and lifter on the product
+view.  Each reservation can have a validity date, once reached, the
+reservation is automatically lifted.
+
+""",
+ 'depends': ['stock',
+             ],
+ 'demo': [],
+ 'data': [],
+ 'auto_install': False,
+ 'test': [],
+ 'installable': True,
+ }
