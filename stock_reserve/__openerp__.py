b65c6af19c0a961a99ef7fb7192b7b34f72a2664
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2013-09-10 08:40:51 +0200

    [IMP] description in __openerp__.py

--- stock_reserve/__openerp__.py
+++ stock_reserve/__openerp__.py
@@ -31,16 +31,16 @@
 Stock Reserve
 =============
 
-Allows to create stock reservation on a product.
+Allows to create stock reservations on products.
 
 Each reservation can have a validity date, once passed, the reservation
 is automatically lifted.
 
 The reserved products are substracted from the virtual stock. It means
-that if you reserved too many products and the virtual stock goes below
-the minimum, the orderpoint will be trigged and new purchase orders will
-be generated. It also implies that the max may be exceeded if the
-reservations are canceled.
+that if you reserved a quantity of products which bring the virtual
+stock below the minimum, the orderpoint will be triggered and new
+purchase orders will be generated. It also implies that the max may be
+exceeded if the reservations are canceled.
 
 """,
  'depends': ['stock',
