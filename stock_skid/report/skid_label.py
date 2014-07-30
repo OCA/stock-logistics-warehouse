26fd0e555f05296397568daa0c34ee774d4cafca
Vincent Vinet <vincent.vinet@savoirfairelinux.com>
2014-07-30 11:53:41 -0400

    wip on the label, shows empty for some reason

--- stock_skid/report/skid_label.py
+++ stock_skid/report/skid_label.py
@@ -17,3 +17,96 @@
 #
 ##############################################################################
 
+import time
+from collections import namedtuple
+
+from openerp.tools import DEFAULT_SERVER_DATETIME_FORMAT
+from openerp.report import report_sxw
+
+
+class SkidLabel(namedtuple("Label", "wizard label_data")):
+    @property
+    def production_lot_id(self):
+        return self.wizard.prodlot_id.name or ''
+
+    @property
+    def product(self):
+        return self.wizard.prodlot_id and self.wizard.prodlot_id.product_id
+
+    @property
+    def product_name(self):
+        return self.product and self.product.name
+
+    @property
+    def product_code(self):
+        return self.product and self.product.code
+
+    @property
+    def production_name(self):
+        return ''
+
+    @property
+    def bom_name(self):
+        return ''
+
+    @property
+    def machine(self):
+        return ''
+
+    @property
+    def shift(self):
+        return ''
+
+    @property
+    def quantity(self):
+        return self.label_data["product_qty"]
+
+    @property
+    def uom(self):
+        uom = self.product.uom_id
+        return uom and uom.name
+
+    @property
+    def supervisor(self):
+        return self.label_data["supervisor"]
+
+    @property
+    def packer_name(self):
+        return self.wizard.packer_name
+
+    @property
+    def date(self):
+        return time.strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+
+    @property
+    def company_name(self):
+        return self.label_data.get("company_name", "")
+
+
+class SkidLabelParser(report_sxw.rml_parse):
+    label_data = ()
+
+    def __init__(self, cr, uid, name, context):
+        super(SkidLabelParser, self).__init__(cr, uid, name, context=context)
+        if context and context.get("label_data"):
+            self.label_data = context["label_data"]
+
+        self.localcontext.update({
+            'labels': self.label_data,
+            'time': time,
+            'get_all_labels': self.get_all_labels,
+        })
+
+    def get_all_labels(self, objects):
+        import pdb; pdb.set_trace()
+        return [
+            SkidLabel(o, l)
+            for o, l in zip(objects, self.label_data)
+        ]
+
+
+report_sxw.report_sxw('report.stock_skid.report.skid_label',
+                      'stock.skid.reprint.wizard',
+                      'addons/stock_skid/report/skid_label.rml',
+                      parser=SkidLabelParser,
+                      header='internal')
--- stock_skid/wizard/stock_skid_reprint_wizard.py
+++ stock_skid/wizard/stock_skid_reprint_wizard.py
@@ -18,6 +18,7 @@
 ##############################################################################
 
 from openerp.osv import orm, fields
+from openerp.tools.translate import _
 
 
 class SkidReprintWizard(orm.TransientModel):
@@ -26,7 +27,54 @@ class SkidReprintWizard(orm.TransientModel):
     _columns = {
         "prodlot_id": fields.many2one("stock.production.lot", "Production Lot"),
         "location_id": fields.many2one("stock.location", "Location"),
+        "packer_name": fields.char('Packer', size=64),
     }
 
     def do_print(self, cr, uid, ids, context=None):
-        print cr, uid, ids, context
+        # We expect ids to be a list with one element
+        form = self.browse(cr, uid, ids[0], context=context)
+        inv_obj = self.pool["report.stock.inventory"]
+        user_obj = self.pool["res.users"]
+        supervisor = user_obj.browse(cr, uid, uid, context=context)
+        lot_info = [
+            lot for lot in inv_obj.read_group(
+                cr, uid, [
+                    # Do NOT filter on location_id, it messes up counts
+                    ('prodlot_id', '=', form.prodlot_id.id),
+                ],
+                fields=["product_qty", "location_id"],
+                groupby=["location_id"],
+                context=context,
+            )
+            if (lot["location_id"][0] == form.location_id.id and
+                lot["product_qty"] > 0)
+        ]
+        if not lot_info:
+            raise orm.except_orm(
+                _("No available lot"),
+                _("This production lot was not found at the requested"
+                  " location"),
+            )
+
+        label_data = [
+            {"product_qty": lot["product_qty"],
+             "supervisor": supervisor.name,
+             "company_name": supervisor.company_id.name,
+             }
+            for lot in lot_info
+        ]
+        ctx = context.copy()
+        ctx["label_data"] = label_data
+
+        return {
+            'type': 'ir.actions.report.xml',
+            'report_name': 'stock_skid.report.skid_label',
+            'context': ctx,
+            'datas': {
+                'model': self._name,
+                'id': ids and ids[0],
+                'ids': ids,
+                'report_type': 'pdf'
+            },
+            'nodestroy': True
+        }
