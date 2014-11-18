20f20fc7168f2ed5f9ce6bf2183599cfb7fc0858
gfcapalbo <giovanni@therp.nl>
2014-11-18 16:48:13 +0100

    [UPD] move out from unported to 8 for update
    [UPG] Upgraded to version 8, fixed references to new 8.0  views and  moved fields that were in  product.product to product.template
    
    [fix] remove duplicate view and correct view name
    
    [UPG][FIX] added outgoing field XML, that was in the base stock field in 7.0.
    
    [UPG] stock available immediately, corrected the calculation method of
    immediately_usable_qty to take in accountthe sign change in outgoing_qty
    (from negative to positive) in version 8.
    
    [FLAKE8]
    
    [FIX] renaming of a class, comment removing, useless code.
    
    [UPD] move out from unported to 8 for update
    
    [fix] remove duplicate view and correct view name
    
    [UPG][FIX] added outgoing field XML, that was in the base stock field in 7.0.
    
    [FIX] renaming of a class, comment removing, useless code.
    
    [fix] remove duplicate view and correct view name
    
    [UPG][FIX] added outgoing field XML, that was in the base stock field in 7.0.
    
    [UPD] move out from unported to 8 for update
    
    [fix] remove duplicate view and correct view name
    
    [UPG][FIX] added outgoing field XML, that was in the base stock field in 7.0.
    
    [FIX] renaming of a class, comment removing, useless code.
    
    [UPD] move out from unported to 8 for update
    
    [fix] remove duplicate view and correct view name
    
    [UPG][FIX] added outgoing field XML, that was in the base stock field in 7.0.
    
    [FIX] renaming of a class, comment removing, useless code.
    
    [UPD] move out from unported to 8 for update
    
    [fix] remove duplicate view and correct view name
    
    [UPG][FIX] added outgoing field XML, that was in the base stock field in 7.0.
    
    [FIX] renaming of a class, comment removing, useless code.
    
    [fix] remove duplicate view and correct view name
    
    [UPG][FIX] added outgoing field XML, that was in the base stock field in 7.0.
    
    [UPD] move out from unported to 8 for update
    
    [fix] remove duplicate view and correct view name
    
    [UPG][FIX] added outgoing field XML, that was in the base stock field in 7.0.
    
    [FIX] renaming of a class, comment removing, useless code.

--- stock_available_immediately/__openerp__.py
+++ stock_available_immediately/__openerp__.py
@@ -33,8 +33,8 @@ Immediately usable is computed : Quantity on Hand - Outgoing Stock.
 """,
     "website": "http://tinyerp.com/module_account.html",
     "category": "Generic Modules/Stock",
-    "data": ["product_view.xml", 
+    "data": ["product_view.xml",
              ],
     "active": False,
-    "installable": True
+    'installable': True
 }
