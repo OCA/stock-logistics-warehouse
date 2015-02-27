af0260318829de0b82370276650a29ade2788df2
Lionel Sausin (Numérigraphe) <ls@numerigraphe.com>
2015-02-27 13:43:56 +0100

    [ADD] stock_available
    Generic module to compute the stock quantity available to promise using several implementations.
    stock_available_immediatly is changed to become the first optional implementation.
    Cherry pick of commit 0b060f619fa5d60f9fb343afe1154acd5c730148 from the v7 branch
    
    [IMP] stock_available* uses new API
    
    [IMP] READMEs and TODOs
    
    Cherry-picked from v7 at 8add4bea7e91924983fb29966e14a1f0d4d5332e
    
    Conflicts:
    	__unported__/stock_available_mrp/__openerp__.py
    	stock_available/__openerp__.py
    	stock_available_immediately/__openerp__.py

--- stock_available_immediately/__openerp__.py
+++ stock_available_immediately/__openerp__.py
@@ -20,21 +20,12 @@
 #
 #
 
-
 {
-    "name": "Immediately Usable Stock Quantity",
-    "version": "1.0",
-    "depends": ["product", "stock", ],
+    "name": "Ignore planned receptions in quantity available to promise",
+    "version": "2.0",
+    "depends": ["stock_available"],
     "author": "Camptocamp",
     "license": "AGPL-3",
-    "description": """
-Compute the immediately usable stock.
-Immediately usable is computed : Quantity on Hand - Outgoing Stock.
-""",
-    "website": "http://tinyerp.com/module_account.html",
-    "category": "Generic Modules/Stock",
-    "data": ["product_view.xml",
-             ],
-    "active": False,
+    "category": "Hidden",
     'installable': True
 }
