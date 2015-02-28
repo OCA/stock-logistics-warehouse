b4c8ebd37edac6de31dd895a9d1b30992247ed80
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-02-28 16:44:51 +0300

    [#66647] Init

---
+++ stock_available_reserved_quantity/__openerp__.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Stock available reserved quantity',
+    'version': '1.0',
+    'author': 'Savoir-faire Linux',
+    'category': 'Warehouse',
+    'depends': [
+        'stock'
+    ],
+    'description': """\
+Stock available reserved quantity
+=================================
+
+Configuration
+=============
+
+Usage
+=====
+
+Credits
+=======
+
+Contributors
+------------
+
+* Loïc Faure-Lacroix <loic.lacroix@savoirfairelinux.com>
+
+Maintainer
+----------
+
+.. image:: http://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: http://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit http://odoo-community.org.""",
+    'license': 'AGPL-3',
+    'data': [
+    ]
+}
