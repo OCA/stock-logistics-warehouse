0aa87fb226d2da0e34e3d45bd67df55cb3c884f9
Loïc Faure-Lacroix <lamerstar@gmail.com>
2015-03-01 15:21:10 +0300

    [#66647] Added copyright

--- stock_available_reserved_quantity/report/report_stock.py
+++ stock_available_reserved_quantity/report/report_stock.py
@@ -1,11 +1,50 @@
+# -*- coding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    This module copyright (C) 2015 - Present Savoir-faire Linux
+#    (<http://www.savoirfairelinux.com>).
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as
+#    published by the Free Software Foundation, either version 3 of the
+#    License, or (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 from openerp.osv import fields, osv
 from openerp.tools.sql import drop_view_if_exists
 
 
 class stock_report_prodlots(osv.osv):
+
+    """
+    Override the stock.report.prodlots.
+
+    We have to substract and add the retained value on stock_move
+    that have a prodlot assigned to it. The prodlot objects should
+    have a retained_stock value which is used to calculate a fake
+    quantity of product.
+    """
+
     _inherit = "stock.report.prodlots"
 
     def init(self, cr):
+        """
+        Delete and replace the previous view by this one.
+
+        This view is an extension of the view in the module
+        stock of odoo.
+
+        file: odoo/addons/stock/report/report_stock.py
+        """
         drop_view_if_exists(cr, 'stock_report_prodlots')
         cr.execute("""
             create or replace view stock_report_prodlots as (
