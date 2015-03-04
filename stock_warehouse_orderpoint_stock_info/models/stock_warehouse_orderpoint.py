c5af47a4db778df44c4f235eb177ec0e79ff63ae
oihane <oihanecruce@gmail.com>
2015-03-04 12:17:34 +0100

    [MOD] removed innecessary data

--- stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
@@ -4,45 +4,24 @@
 ##############################################################################
 
 from openerp import models, fields, api
-from openerp.tools.float_utils import float_round
 
 
 class StockWarehouseOrderpoint(models.Model):
     _inherit = 'stock.warehouse.orderpoint'
 
     @api.one
+    def _product_available_qty(self):
+        self.product_location_qty = self.product_id.with_context(
+            location=self.location_id.id
+            )._product_available()[self.product_id.id]['qty_available']
+
+    @api.one
+    @api.depends('product_location_qty', 'product_min_qty')
     def _product_available(self):
-        domain_quant = [('location_id', '=', self.location_id.id),
-                        ('product_id', '=', self.product_id.id)]
-        quants = self.env['stock.quant'].read_group(
-            domain_quant, ['product_id', 'qty'], ['product_id'])
-        quants = dict(map(lambda x: (x['product_id'][0], x['qty']), quants))
-        self.product_location_qty = float_round(
-            quants.get(self.product_id.id, 0.0),
-            precision_rounding=self.product_uom.rounding)
+        self.available = self.product_location_qty > self.product_min_qty
 
     product_location_qty = fields.Float(
-        string='Quantity On Location', compute='_product_available')
-    product_qty_available = fields.Float(
-        string='Quantity On Hand', related='product_id.qty_available',
-        help="Current quantity of products.\n"
-             "In a context with a single Stock Location, this includes "
-             "goods stored at this Location, or any of its children.\n"
-             "In a context with a single Warehouse, this includes "
-             "goods stored in the Stock Location of this Warehouse, or any "
-             "of its children.\n"
-             "stored in the Stock Location of the Warehouse of this Shop, "
-             "or any of its children.\n"
-             "Otherwise, this includes goods stored in any Stock Location "
-             "with 'internal' type.")
-    product_virtual_available = fields.Float(
-        string='Forecast Quantity', related='product_id.virtual_available',
-        help="Forecast quantity (computed as Quantity On Hand "
-             "- Outgoing + Incoming)\n"
-             "In a context with a single Stock Location, this includes "
-             "goods stored in this location, or any of its children.\n"
-             "In a context with a single Warehouse, this includes "
-             "goods stored in the Stock Location of this Warehouse, or any "
-             "of its children.\n"
-             "Otherwise, this includes goods stored in any Stock Location "
-             "with 'internal' type.")
+        string='Quantity On Location', compute='_product_available_qty')
+    available = fields.Boolean(
+        string='Is enough product available?', compute='_product_available',
+        store=True)
