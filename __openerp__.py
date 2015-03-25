94a27a695445b6649aca1278a006846149d74f11
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-25 20:48:43 +0100

    [IMP] Expand AUTHORS

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,9 @@
     "depends": [
         "stock",
     ],
-    "author": "OdooMRP team",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
     "website": "http://www.odoomrp.com",
     "contributors": [
         "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
