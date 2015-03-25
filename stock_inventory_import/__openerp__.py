b97319348d891bdf950853dc2212c01a0e74534b
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-25 20:48:43 +0100

    [IMP] Expand AUTHORS

--- stock_inventory_import/__openerp__.py
+++ stock_inventory_import/__openerp__.py
@@ -1,4 +1,20 @@
-
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see http://www.gnu.org/licenses/.
+#
+##############################################################################
 {
     'name': 'Stock Inventory Import from CSV file',
     'version': "1.0",
@@ -10,7 +26,10 @@
     (if not defined, default inventory location will be used)
     You can also add a fourth column with Key "lot" to add a product lot.
     """,
-    'author': 'OdooMRP team',
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+
     'contributors': ["Daniel Campos <danielcampos@avanzosc.es>"],
     'website': "http://www.avanzosc.com",
     'depends': ['stock'],
