c1ea8e68de9fc75df2a95f3599d693726e1eadd4
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-03-25 21:35:06 +0100

    [IMP] Expand AUTHORS

--- stock_move_partner_info/__openerp__.py
+++ stock_move_partner_info/__openerp__.py
@@ -22,7 +22,9 @@
 {
     "name": "Stock Move Partner Info",
     "version": "1.0",
-    "author": "OdooMRP team",
+    "author": "OdooMRP team,"
+              "AvanzOSC,"
+              "Serv. Tecnol. Avanzados - Pedro M. Baeza",
     "website": "http://www.odoomrp.com",
     "category": "Warehouse Management",
     "depends": [
