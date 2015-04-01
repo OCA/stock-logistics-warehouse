3163e47a7189b87ea4108859cb1f831fa6c93c76
oihane <oihanecruce@gmail.com>
2015-04-01 13:57:38 +0200

    [IMP] <stock_inventory_import> File lines must be processed at least once

---
+++ stock_inventory_import/README.rst
@@ -0,0 +1,8 @@
+Stock Inventory Import from CSV file
+====================================
+
+Wizard to import Inventory from a CSV file
+The file must have at least 2 columns with "code" and "quantity" Head Keys.
+You can also add a third column with Key "location" to add product location
+(if not defined, default inventory location will be used)
+You can also add a fourth column with Key "lot" to add a product lot.
--- stock_inventory_import/__openerp__.py
+++ stock_inventory_import/__openerp__.py
@@ -16,25 +16,26 @@
 #
 ##############################################################################
 {
-    'name': 'Stock Inventory Import from CSV file',
-    'version': "1.0",
-    'category': "Generic Modules",
-    'description': """
-    Wizard to import Inventory from a CSV file
-    The file must have at least 2 columns with "code" and "quantity" Head Keys.
-    You can also add a third column with Key "location" to add product location
-    (if not defined, default inventory location will be used)
-    You can also add a fourth column with Key "lot" to add a product lot.
-    """,
-    "author": "OdooMRP team,"
-              "AvanzOSC,"
+    "name": "Stock Inventory Import from CSV file",
+    "version": "1.0",
+    "category": "Generic Modules",
+    "author": "OdooMRP team, "
+              "AvanzOSC, "
               "Serv. Tecnol. Avanzados - Pedro M. Baeza",
+    "contributors": [
+        "Daniel Campos <danielcampos@avanzosc.es>",
+        "Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>",
+        "Ana Juaristi <ajuaristio@gmail.com>",
 
-    'contributors': ["Daniel Campos <danielcampos@avanzosc.es>"],
-    'website': "http://www.avanzosc.com",
-    'depends': ['stock'],
-    'data': ['security/ir.model.access.csv',
-             'wizard/import_inventory_view.xml',
-             'views/inventory_view.xml'],
-    'installable': True,
+    ],
+    "website": "http://www.odoomrp.com",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "wizard/import_inventory_view.xml",
+        "views/inventory_view.xml",
+    ],
+    "installable": True,
 }
