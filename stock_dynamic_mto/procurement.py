93e9ffe169a4eb62d5de2f37771d934c964dfc3f
florian-dacosta <florian.dacosta@akretion.com>
2015-04-23 10:37:23 +0200

    fix copyright

--- stock_dynamic_mto/procurement.py
+++ stock_dynamic_mto/procurement.py
@@ -3,7 +3,7 @@
 #
 #    Module for OpenERP
 #    Copyright (C) 2015 Akretion (http://www.akretion.com). All Rights Reserved
-#    @author Benoît GUILLOT <benoit.guillot@akretion.com>
+#    @author Florian DA COSTA <florian.dacosta@akretion.com>
 #
 #    This program is free software: you can redistribute it and/or modify
 #    it under the terms of the GNU Affero General Public License as
