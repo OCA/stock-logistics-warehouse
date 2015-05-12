55b76a9f371b4cc24f6a286c2adbc99921cf8da7
Florian da Costa <florian.dacosta@akretion.com>
2015-05-12 18:57:17 +0200

    add test + use standard mts and mto rules

--- stock_mts_mto_rule/model/warehouse.py
+++ stock_mts_mto_rule/model/warehouse.py
@@ -27,28 +27,17 @@ class Warehouse(models.Model):
     _inherit = 'stock.warehouse'
 
     mto_mts_management = fields.Boolean(
-        'Use MTO+MTS rules')
+        'Use MTO+MTS rules',
+        help='If this new route is selected on product form view, a '
+             'purchase order will be created only if the virtual stock is '
+             'less than 0 else, the product will be taken from stocks')
     mts_mto_rule_id = fields.Many2one('procurement.rule',
                                       'MTO+MTS rule')
 
     @api.model
-    def _get_mts_mto_mto_rule(self, warehouse):
-        return {
-            'name': self._format_routename(warehouse, _('MTS+MTO : MTO')),
-            'route_id': False,
-        }
-
-    @api.model
-    def _get_mts_mto_mts_rule(self, warehouse):
-        return {
-            'name': self._format_routename(warehouse, _('MTS+MTO : MTS')),
-            'route_id': False,
-            'procure_method': 'make_to_stock',
-        }
-
-    @api.model
     def _get_mts_mto_rule(self, warehouse):
         route_model = self.env['stock.location.route']
+        pull_model = self.env['procurement.rule']
         try:
             mts_mto_route = self.env.ref(
                 'stock_mts_mto_rule.route_mto_mts')
@@ -60,55 +49,50 @@ class Warehouse(models.Model):
             raise exceptions.Warning(_(
                 'Can\'t find any generic MTS+MTO route.'))
 
+        if not warehouse.mto_pull_id:
+            raise exceptions.Warning(_(
+                'Can\'t find MTO Rule on the warehouse'))
+
+        mts_rules = pull_model.search(
+            [('location_src_id', '=', warehouse.lot_stock_id.id),
+             ('route_id', '=', warehouse.delivery_route_id.id)])
+        if not mts_rules:
+            raise exceptions.Warning(_(
+                'Can\'t find MTS Rule on the warehouse'))
         return {
             'name': self._format_routename(warehouse, _('MTS+MTO')),
             'route_id': mts_mto_route.id,
             'action': 'split_procurement',
+            'mto_rule_id': warehouse.mto_pull_id.id,
+            'mts_rule_id': mts_rules[0].id,
+            'warehouse_id': warehouse.id,
+            'location_id': warehouse.mto_pull_id.location_id.id,
+            'picking_type_id': warehouse.mto_pull_id.picking_type_id.id,
         }
-
-    @api.model
-    def create_mts_mto_rules(self, warehouse):
-        model_rule = warehouse.mto_pull_id
-        mts_pull_vals = self._get_mts_mto_mts_rule(warehouse)
-        mts_pull = model_rule.copy(mts_pull_vals)
-        mto_pull_vals = self._get_mts_mto_mto_rule(warehouse)
-        mto_pull_vals['mts_pull_rule_id'] = mts_pull.id
-        mto_pull = model_rule.copy(mto_pull_vals)
-
-        mts_mto_pull_vals = self._get_mts_mto_rule(warehouse)
-        mts_mto_pull_vals.update({'mts_rule_id': mts_pull.id,
-                                  'mto_rule_id': mto_pull.id})
-        mts_mto_pull = model_rule.copy(mts_mto_pull_vals)
-
-        res = {
-            'mts_rule_id': mts_pull.id,
-            'mto_rule_id': mto_pull.id,
-            'mts_mto_rule_id': mts_mto_pull.id,
-        }
-        return res
 
     @api.multi
     def create_routes(self, warehouse):
+        pull_model = self.env['procurement.rule']
         res = super(Warehouse, self).create_routes(warehouse)
         if warehouse.mto_mts_management:
-            vals = self.create_mts_mto_rules(warehouse)
-            res['mts_mto_rule_id'] = vals.get('mts_mto_rule_id', False)
+            mts_mto_pull_vals = self._get_mts_mto_rule(warehouse)
+            mts_mto_pull = pull_model.create(mts_mto_pull_vals)
+            res['mts_mto_rule_id'] = mts_mto_pull.id
         return res
 
     @api.multi
     def write(self, vals):
+        pull_model = self.env['procurement.rule']
         if 'mto_mts_management' in vals:
             if vals.get("mto_mts_management"):
                 for warehouse in self:
                     if not warehouse.mts_mto_rule_id:
-                        rule_vals = self.create_mts_mto_rules(warehouse)
-                        vals['mts_mto_rule_id'] = rule_vals.get(
-                            'mts_mto_rule_id', False)
+                        rule_vals = self._get_mts_mto_rule(warehouse)
+                        mts_mto_pull = pull_model.create(rule_vals)
+                        vals['mts_mto_rule_id'] = mts_mto_pull.id
             else:
                 for warehouse in self:
                     if warehouse.mts_mto_rule_id:
-                        warehouse.mts_mto_rule_id.mts_rule_id.unlink()
-                        warehouse.mts_mto_rule_id.mto_rule_id.unlink()
                         warehouse.mts_mto_rule_id.unlink()
         return super(Warehouse, self).write(vals)
 
@@ -117,7 +101,7 @@ class Warehouse(models.Model):
         all_routes = super(Warehouse, self).get_all_routes_for_wh(warehouse)
         if (
             warehouse.mto_mts_management and
-            warehouse.mts_mto_mto_rule_id.route_id
+            warehouse.mts_mto_rule_id.route_id
         ):
             all_routes += [warehouse.mts_mto_rule_id.route_id.id]
         return all_routes
@@ -127,14 +111,6 @@ class Warehouse(models.Model):
         res = super(Warehouse, self)._handle_renaming(warehouse, name, code)
 
         if warehouse.mts_mto_rule_id:
-            warehouse.mts_mto_rule_id.mts_rule_id.name = (
-                warehouse.mts_mto_rule_id.mts_rule_id.name.replace(
-                    warehouse.name, name, 1)
-            )
-            warehouse.mts_mto_rule_id.mto_rule_id.name = (
-                warehouse.mts_mto_rule_id.mto_rule_id.name.replace(
-                    warehouse.name, name, 1)
-            )
             warehouse.mts_mto_rule_id.name = (
                 warehouse.mts_mto_rule_id.name.replace(
                     warehouse.name, name, 1)
@@ -151,16 +127,11 @@ class Warehouse(models.Model):
 
         mts_mto_rule_id = warehouse.mts_mto_rule_id
         if new_delivery_step and mts_mto_rule_id:
-            model_rule = warehouse.mto_pull_id
-            rule_ids = [
-                mts_mto_rule_id.id,
-                mts_mto_rule_id.mts_rule_id.id,
-                mts_mto_rule_id.mto_rule_id.id
-            ]
             pull_model = self.env['procurement.rule']
-            vals = {
-                'location_id': model_rule.location_id.id,
-                'location_src_id': model_rule.location_src_id.id,
-            }
-            pull_model.write(rule_ids, vals)
+            warehouse.mts_mto_rule_id.location_id = (
+                warehouse.mto_pull_id.location_id)
+            mts_rules = pull_model.search(
+                [('location_src_id', '=', warehouse.lot_stock_id.id),
+                 ('route_id', '=', warehouse.delivery_route_id.id)])
+            warehouse.mts_mto_rule_id.mts_rule_id = mts_rules[0].id
         return res
