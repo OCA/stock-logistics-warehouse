ed065d3c3b93f6d5da99877aa0dea88fd45eeafa
Pedro M. Baeza <pedro.baeza@gmail.com>
2015-06-27 20:42:48 +0200

    [IMP] stock_inventory_preparation_filter: Preparation for OCA

---
+++ stock_inventory_preparation_filter/README.rst
@@ -0,0 +1,59 @@
+.. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
+
+========================================
+More preparation filters for inventories
+========================================
+
+Includes more options for making an inventory out of:
+
+* Multiple products.
+* Products of a category.
+* Multiple lots
+
+It also allows to make an inventory based on scanned products, adding a line
+with product code and quantity.
+
+Usage
+=====
+
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/153/8.0
+
+Bug Tracker
+===========
+
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/stock-logistics-warehouse/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed `feedback
+<https://github.com/OCA/
+stock-logistics-warehouse/issues/new?body=module:%20
+stock_inventory_preparation_filter%0Aversion:%20
+8.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+
+Credits
+=======
+
+Contributors
+------------
+
+* Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>
+* Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+
+Maintainer
+----------
+
+.. image:: https://odoo-community.org/logo.png
+   :alt: Odoo Community Association
+   :target: https://odoo-community.org
+
+This module is maintained by the OCA.
+
+OCA, or the Odoo Community Association, is a nonprofit organization whose
+mission is to support the collaborative development of Odoo features and
+promote its widespread use.
+
+To contribute to this module, please visit https://odoo-community.org.
---
+++ stock_inventory_preparation_filter/tests/test_stock_inventory_preparation_filter.py
@@ -0,0 +1,147 @@
+##############################################################################
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Affero General Public License as published
+#    by the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Affero General Public License for more details.
+#
+#    You should have received a copy of the GNU Affero General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+import openerp.tests.common as common
+
+
+class TestStockInventoryPreparationFilterCategories(common.TransactionCase):
+
+    def setUp(self):
+        super(TestStockInventoryPreparationFilterCategories, self).setUp()
+        self.inventory_model = self.env['stock.inventory']
+        # Create a category
+        self.category = self.env['product.category'].create(
+            {
+                'name': 'Category for inventory',
+                'type': 'normal',
+            })
+        # Create some products in the category
+        self.product1 = self.env['product.product'].create(
+            {
+                'name': 'Product for inventory 1',
+                'type': 'product',
+                'categ_id': self.category.id,
+                'default_code': 'PROD1',
+            }
+        )
+        self.product2 = self.env['product.product'].create(
+            {
+                'name': 'Product for inventory 2',
+                'type': 'product',
+                'categ_id': self.category.id,
+                'default_code': 'PROD2',
+            }
+        )
+        # And have some stock in a location
+        self.location = self.env['stock.location'].create(
+            {
+                'name': 'Inventory tests',
+                'usage': 'internal',
+            }
+        )
+        inventory = self.inventory_model.create(
+            {
+                'name': 'Product1 inventory',
+                'filter': 'product',
+                'line_ids': [
+                    (0, 0, {
+                        'product_id': self.product1.id,
+                        'product_uom_id': self.env.ref(
+                            "product.product_uom_unit").id,
+                        'product_qty': 2.0,
+                        'location_id': self.location.id,
+                    }),
+                    (0, 0, {
+                        'product_id': self.product2.id,
+                        'product_uom_id': self.env.ref(
+                            "product.product_uom_unit").id,
+                        'product_qty': 4.0,
+                        'location_id': self.location.id,
+                    }),
+                ],
+            })
+        inventory.action_done()
+
+    def test_inventory_category_filter(self):
+        inventory = self.inventory_model.create(
+            {
+                'name': 'Category inventory',
+                'filter': 'categories',
+                'location_id': self.location.id,
+                'categ_ids': [(6, 0, [self.category.id])],
+            }
+        )
+        inventory.prepare_inventory()
+        self.assertEqual(len(inventory.line_ids), 2)
+        line1 = inventory.line_ids[0]
+        self.assertEqual(line1.product_id, self.product1)
+        self.assertEqual(line1.theoretical_qty, 2.0)
+        self.assertEqual(line1.location_id, self.location)
+        line2 = inventory.line_ids[1]
+        self.assertEqual(line2.product_id, self.product2)
+        self.assertEqual(line2.theoretical_qty, 4.0)
+        self.assertEqual(line2.location_id, self.location)
+
+    def test_inventory_products_filter(self):
+        inventory = self.inventory_model.create(
+            {
+                'name': 'Products inventory',
+                'filter': 'products',
+                'location_id': self.location.id,
+                'product_ids': [(6, 0, [self.product1.id, self.product2.id])],
+            }
+        )
+        inventory.prepare_inventory()
+        self.assertEqual(len(inventory.line_ids), 2)
+        line1 = inventory.line_ids[0]
+        self.assertEqual(line1.product_id, self.product1)
+        self.assertEqual(line1.theoretical_qty, 2.0)
+        self.assertEqual(line1.location_id, self.location)
+        line2 = inventory.line_ids[1]
+        self.assertEqual(line2.product_id, self.product2)
+        self.assertEqual(line2.theoretical_qty, 4.0)
+        self.assertEqual(line2.location_id, self.location)
+
+    def test_inventory_empty_filter(self):
+        inventory = self.inventory_model.create(
+            {
+                'name': 'Products inventory',
+                'filter': 'empty',
+                'location_id': self.location.id,
+                'empty_line_ids': [
+                    (0, 0, {
+                        'product_code': 'PROD1',
+                        'product_qty': 3.0,
+                    }),
+                    (0, 0, {
+                        'product_code': 'PROD2',
+                        'product_qty': 7.0,
+                    }),
+                ],
+            }
+        )
+        inventory.prepare_inventory()
+        self.assertEqual(len(inventory.line_ids), 2)
+        line1 = inventory.line_ids[0]
+        self.assertEqual(line1.product_id, self.product1)
+        self.assertEqual(line1.theoretical_qty, 2.0)
+        self.assertEqual(line1.product_qty, 3.0)
+        self.assertEqual(line1.location_id, self.location)
+        line2 = inventory.line_ids[1]
+        self.assertEqual(line2.product_id, self.product2)
+        self.assertEqual(line2.theoretical_qty, 4.0)
+        self.assertEqual(line2.product_qty, 7.0)
+        self.assertEqual(line2.location_id, self.location)
