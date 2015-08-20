cf2000aad3fccd2cb3b3c2e4512434f24cec9299
Miku Laitinen <miku.laitinen@gmail.com>
2015-08-20 22:33:33 +0300

    Shortened the comment line

--- stock_reserve_sale/model/sale.py
+++ stock_reserve_sale/model/sale.py
@@ -237,8 +237,8 @@ class SaleOrderLine(models.Model):
     def unlink(self):
         """
         Force remove the reservations. This is necessary because even though
-        sale_line_id of stock.reservation has ondelete=cascade, the unlink method of
-        the reservations is never called.
+        sale_line_id of stock.reservation has ondelete=cascade, the unlink 
+        method of the reservations is never called.
         """
         self.reservation_ids.unlink()
         return super(SaleOrderLine, self).unlink()
