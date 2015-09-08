3cce4ff7e8fb8a29d49820efe919551daa94b239
Lionel Sausin <ls@numerigraphe.com>
2015-09-08 18:06:12 +0200

    Revert changes to computation of _immediately_usable_qty
    Commit 6c16913 changed the way we compute the immediately_usable_qty: instead of using the virtual stock, we used the sum of quants without reservations. But a quant may actually be reserved and still be available (for example it may be reserved for an internal move).
    Fixes https://github.com/OCA/stock-logistics-warehouse/issues/79
    
    Remove loop and use correct decorator
    
    Restore the features of stock_available_immediately
    
    The previous fix restored stock_available but then there was no way to exclude the incomming moves from the count. This belongs in stock_available_immediately, restoring it cleanly.
    This commit also takes care to respect the distinction between templates and variants, so it should fix https://github.com/OCA/stock-logistics-warehouse/issues/73 too.
    
    Restore the qty avail. to promise on variant treeview
    
    PEP8

--- stock_available/models/product_template.py
+++ stock_available/models/product_template.py
@@ -18,16 +18,25 @@
 #
 ##############################################################################
 
-{
-    'name': 'Stock available to promise',
-    'version': '8.0.2.0.0',
-    "author": u"Numérigraphe,Odoo Community Association (OCA)",
-    'category': 'Warehouse',
-    'depends': ['stock'],
-    'license': 'AGPL-3',
-    'data': [
-        'views/product_template_view.xml',
-        'views/product_product_view.xml',
-        'views/res_config_view.xml',
-    ],
-}
+from openerp import models, fields, api
+from openerp.addons import decimal_precision as dp
+
+
+class ProductTemplate(models.Model):
+    _inherit = 'product.template'
+
+    @api.one
+    @api.depends('virtual_available')
+    def _immediately_usable_qty(self):
+        """Compute the quantity using all the variants"""
+        self.immediately_usable_qty = sum(
+            [v.immediately_usable_qty for v in self.product_variant_ids])
+
+    immediately_usable_qty = fields.Float(
+        digits=dp.get_precision('Product Unit of Measure'),
+        compute='_immediately_usable_qty',
+        string='Available to promise',
+        help="Stock for this Product that can be safely proposed "
+             "for sale to Customers.\n"
+             "The definition of this value can be configured to suit "
+             "your needs")
