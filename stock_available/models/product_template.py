fe44a2cbe3169056bd9e703e656c6c84e8c71e04
Lionel Sausin <ls@numerigraphe.com>
2015-09-10 15:25:49 +0200

    PEP8

--- stock_available/models/product_template.py
+++ stock_available/models/product_template.py
@@ -25,15 +25,12 @@ from openerp.addons import decimal_precision as dp
 class ProductTemplate(models.Model):
     _inherit = 'product.template'
 
-
-
     @api.one
     @api.depends('virtual_available')
     def _immediately_usable_qty(self):
         """Compute the quantity using all the variants"""
         self.immediately_usable_qty = sum(
             [v.immediately_usable_qty for v in self.product_variant_ids])
-
 
     immediately_usable_qty = fields.Float(
         digits=dp.get_precision('Product Unit of Measure'),
