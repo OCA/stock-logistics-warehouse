d6bd36c16f995089664cae40ab244ed8c5a7f4ae
oihane <oihanecruce@gmail.com>
2015-10-14 18:51:46 +0200

    stock_inventory_import: Updated code

--- stock_inventory_import/models/stock_inventory.py
+++ stock_inventory_import/models/stock_inventory.py
@@ -1,4 +1,7 @@
-#
+# -*- coding: utf-8 -*-
+# (c) 2015 Oihane Crucelaegui - AvanzOSC
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
 from openerp import models, fields, api, exceptions, _
 
 
@@ -11,15 +14,17 @@ class StockInventory(models.Model):
         res.append(('file', _('By File')))
         return res
 
+    @api.multi
     @api.depends('import_lines')
     def _file_lines_processed(self):
-        processed = True
-        if self.import_lines:
-            processed = any((not line.fail or
-                             (line.fail and
-                              line.fail_reason != _('No processed')))
-                            for line in self.import_lines)
-        self.processed = processed
+        for record in self:
+            processed = True
+            if record.import_lines:
+                processed = any((not line.fail or
+                                 (line.fail and
+                                  line.fail_reason != _('No processed')))
+                                for line in record.import_lines)
+            record.processed = processed
 
     imported = fields.Boolean('Imported')
     import_lines = fields.One2many('stock.inventory.import.line',
@@ -30,16 +35,17 @@ class StockInventory(models.Model):
     processed = fields.Boolean(string='Has been processed at least once?',
                                compute='_file_lines_processed')
 
-    @api.one
+    @api.multi
     def process_import_lines(self):
         """Process Inventory Load lines."""
-        if not self.import_lines:
+        import_lines = self.mapped('import_lines')
+        if not import_lines:
             raise exceptions.Warning(_("There must be one line at least to "
                                        "process"))
         inventory_line_obj = self.env['stock.inventory.line']
         stk_lot_obj = self.env['stock.production.lot']
         product_obj = self.env['product.product']
-        for line in self.import_lines:
+        for line in import_lines:
             if line.fail:
                 if not line.product:
                     prod_lst = product_obj.search([('default_code', '=',
@@ -60,12 +66,13 @@ class StockInventory(models.Model):
                         lot = stk_lot_obj.create({'name': line.lot,
                                                   'product_id': product.id})
                         lot_id = lot.id
-                inventory_line_obj.create({'product_id': product.id,
-                                           'product_uom_id': product.uom_id.id,
-                                           'product_qty': line.quantity,
-                                           'inventory_id': self.id,
-                                           'location_id': line.location_id.id,
-                                           'prod_lot_id': lot_id})
+                inventory_line_obj.create({
+                    'product_id': product.id,
+                    'product_uom_id': product.uom_id.id,
+                    'product_qty': line.quantity,
+                    'inventory_id': line.inventory_id.id,
+                    'location_id': line.location_id.id,
+                    'prod_lot_id': lot_id})
                 line.write({'fail': False, 'fail_reason': _('Processed')})
         return True
 
