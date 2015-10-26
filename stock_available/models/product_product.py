df6b08c8ce16da6eee568f009eb1105e0e9b2e2f
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2015-10-26 13:56:24 +0100

    product_available improvements
    * fix the dependencies for the computed field
    
    * use api.multi instead of api.one to avoid calling
      super()._immediately_usable_qty in a loop (this improves perfs on a tree view
      display)

--- stock_available/models/product_product.py
+++ stock_available/models/product_product.py
@@ -14,7 +14,7 @@ class ProductProduct(models.Model):
     """
     _inherit = 'product.product'
 
-    @api.one
+    @api.multi
     @api.depends('virtual_available')
     def _immediately_usable_qty(self):
         """No-op implementation of the stock available to promise.
@@ -23,7 +23,8 @@ class ProductProduct(models.Model):
 
         Must be overridden by another module that actually implement
         computations."""
-        self.immediately_usable_qty = self.virtual_available
+        for prod in self:
+            prod.immediately_usable_qty = prod.virtual_available
 
     immediately_usable_qty = fields.Float(
         digits=dp.get_precision('Product Unit of Measure'),
