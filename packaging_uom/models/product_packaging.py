87784d765549418758198b300449a7c9a0c797fb
Laetitia Gangloff <laetitia.gangloff@acsone.eu>
2015-11-18 15:51:07 +0100

    correct product creation with packaging

--- packaging_uom/models/product_packaging.py
+++ packaging_uom/models/product_packaging.py
@@ -26,11 +26,19 @@ from openerp import api, fields, models
 class ProductPackaging(models.Model):
     _inherit = 'product.packaging'
 
+    @api.model
+    def _default_uom_categ_domain_id(self):
+        uom_id = self.env.context.get("get_uom_categ_from_uom")
+        if not uom_id:
+            return self.env['product.uom.categ']
+        uom = self.env['product.uom'].browse(uom_id)
+        return uom.category_id.id
+
     uom_id = fields.Many2one('product.uom', 'Unit of Measure', required=True,
                              help="It must be in the same category than "
                              "the default unit of measure.")
     uom_categ_domain_id = fields.Many2one(
-        related='product_tmpl_id.uom_id.category_id',
+        default=_default_uom_categ_domain_id,
         comodel_name='product.uom.categ')
     qty = fields.Float(compute="_compute_qty", store=True, readonly=True)
 
