17fb88754a8c5fd7cc7a32c93c2bdfd6414c4fd7
Dago Gallo <dago.gallo@clearcorp.co.cr>
2015-11-24 15:50:15 -0600

    [FIX] Fixed code not following OCA guidelines

--- stock_reserve_mrp/__openerp__.py
+++ stock_reserve_mrp/__openerp__.py
@@ -1,38 +1,18 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    Addons modules by CLEARCORP S.A.
-#    Copyright (C) 2009-TODAY CLEARCORP S.A. (<http://clearcorp.co.cr>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# © 2015 ClearCorp S.A.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Stock Reserve MRP',
-    'version': '1.0',
+    'version': '8.0.1.0.0',
     'category': 'Manufacturing',
     'sequence': 10,
-    'summary': 'This module adds an additional status to handle '
+    'summary': 'Automatic reservation of consumed products'
     'in production orders',
-    'author': 'ClearCorp',
+    'author': 'ClearCorp,Odoo Community Association (OCA)',
     'website': 'http://clearcorp.co.cr',
     'depends': ['mrp', 'stock_reserve'],
     'data': ['stock_reserve_mrp_view.xml'],
-    'test': [],
-    'demo': [],
     'installable': True,
     'auto_install': False,
     'application': False,
