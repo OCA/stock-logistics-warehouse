328a782e91dc8aa2284e27daf558111b595adf42
Loïc BELLIER <lb@numerigraphe.com>
2015-12-01 16:49:16 +0100

    [FIX]: PEP8 compliance + clonedagain remark fixes.

--- stock_lot_quantity/tests/test_lot_quantity.py
+++ stock_lot_quantity/tests/test_lot_quantity.py
@@ -8,18 +8,20 @@ class TestLotQuantity(TestStockCommon):
 
     @mute_logger('openerp.addons.base.ir.ir_model', 'openerp.models')
     def test_00_picking_create_and_verify_lot_quantity(self):
-        """ Make inventories to add stock with the same production lot on differents locations. Verify stock of lot."""
+        """  Verify stock pro production lot.
+             Make inventories to add stock with the same production lot
+             on different locations."""
         LotObj = self.env['stock.production.lot']
-        stock_location_shelf_1 = self.ModelDataObj.xmlid_to_res_id('stock.stock_location_components')
-        stock_location_shelf_2 = self.ModelDataObj.xmlid_to_res_id('stock.stock_location_14')
+        stock_location_shelf_1 = self.ref('stock.stock_location_components')
+        stock_location_shelf_2 = self.ref('stock.stock_location_14')
 
         # --------------------------------------------------------------
         # Create first inventory to add lot with quantity on location 1.
         # --------------------------------------------------------------
-        lot1_productB = LotObj.create({'name': 'Lot 1B', 'product_id': self.productB.id})
+        lot1_productB = LotObj.create({'name': 'Lot 1B',
+                                       'product_id': self.productB.id})
         inventory = self.InvObj.create({'name': 'Test Lot Quantity',
-                                        'product_id': self.productB.id,
-                                        'filter': 'product'})
+                                        'filter': 'none'})
         inventory.prepare_inventory()
         self.InvLineObj.create({
                     'inventory_id': inventory.id,
@@ -30,14 +32,14 @@ class TestLotQuantity(TestStockCommon):
                     'prod_lot_id': lot1_productB.id
                     })
         inventory.action_done()
-        self.assertEqual(lot1_productB.qty_available, 10, "Wrong qty available for lot")
+        self.assertEqual(lot1_productB.qty_available, 10,
+                         "Wrong qty available for lot")
 
         # ---------------------------------------------------------------
         # Create second inventory to add lot with quantity on location 1.
         # ---------------------------------------------------------------
         inventory = self.InvObj.create({'name': 'Test Lot Quantity 2',
-                                        'product_id': self.productB.id,
-                                        'filter': 'product'})
+                                        'filter': 'none'})
         inventory.prepare_inventory()
         self.InvLineObj.create({
                     'inventory_id': inventory.id,
@@ -48,4 +50,5 @@ class TestLotQuantity(TestStockCommon):
                     'prod_lot_id': lot1_productB.id
                     })
         inventory.action_done()
-        self.assertEqual(lot1_productB.qty_available, 30, "Wrong qty available for lot")
+        self.assertEqual(lot1_productB.qty_available, 30,
+                         "Wrong qty available for lot")
