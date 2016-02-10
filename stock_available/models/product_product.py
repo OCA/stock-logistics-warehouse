196443d7b51cee99d47a52e7d4f0a61fac87d3e4
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-02-10 11:41:48 +0530

    [9.0][PORT] Stock available

--- stock_available/models/product_product.py
+++ stock_available/models/product_product.py
@@ -1,28 +1,13 @@
-#-*- coding: utf-8 -*-
-##############################################################################
-#
-#    This module is copyright (C) 2014 Numérigraphe SARL. All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# -*- coding: utf-8 -*-
+# © 2014 Numérigraphe, Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp import models, fields, api
 from openerp.addons import decimal_precision as dp
 
 
 class ProductProduct(models.Model):
+
     """Add a field for the stock available to promise.
     Useful implementations need to be installed through the Settings menu or by
     installing one of the modules stock_available_*
