cad7b0eab78d165318537d906b7a002a788e5b70
jbeficent <jordi.ballester@eficent.com>
2016-02-24 00:34:24 +0100

    [FIX] pylint

--- stock_inventory_revaluation/wizards/stock_inventory_revaluation_mass_post.py
+++ stock_inventory_revaluation/wizards/stock_inventory_revaluation_mass_post.py
@@ -2,7 +2,8 @@
 # © 2015 Eficent Business and IT Consulting Services S.L.
 # - Jordi Ballester Alomar
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models, _, exceptions
+from openerp import api, models, _, exceptions
+
 
 class StockInventoryRevaluationMassPost(models.TransientModel):
 
