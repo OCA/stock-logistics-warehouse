99fb2ef0250b06ab368fa77a2d920f78bec0c729
Lionel Sausin & Loïc Bellier <ls@numerigraphe.com>
2016-03-21 17:12:39 +0100

    New module stock_inventory_lockdown
    This module is a feature extracted from stock_inventory_location.
    
    Ported to v8 and new API
    Adopted latest OCA standards
    Added tests

---
+++ stock_inventory_lockdown/__openerp__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# © 2013-2016 Numérigraphe SARL
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Inventory lock down",
+    "summary": "Lock down stock locations during inventories.",
+    "version": "8.0.1.0.0",
+    "depends": ["stock"],
+    "author": u"Numérigraphe,Odoo Community Association (OCA)",
+    "category": "Warehouse Management",
+    "images": [
+        "images/move_error.png",
+        "images/location_locked.png"],
+    'license': 'AGPL-3',
+    "installable": True,
+}
