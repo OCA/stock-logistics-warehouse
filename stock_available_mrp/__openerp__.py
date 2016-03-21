0df9d611aec2730184f2e5949c84146d45e0dd18
Cyril Gaudin <cyril.gaudin@camptocamp.com>
2016-03-21 10:45:31 +0100

    V9 migration modifications.
        * mrp_bom.name has been deleted.
        * mrp_bom_line.type moved to mrp_bom.type.
        * Fix missing group_mrp_user issue.
        * Change versions

--- stock_available_mrp/__openerp__.py
+++ stock_available_mrp/__openerp__.py
@@ -1,9 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2014 Numérigraphe SARL
+# © 2014 Numérigraphe SARL, Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Consider the production potential is available to promise',
-    'version': '8.0.3.1.1',
+    'version': '9.0.1.0.0',
     "author": u"Numérigraphe,"
               u"Odoo Community Association (OCA)",
     'category': 'Hidden',
