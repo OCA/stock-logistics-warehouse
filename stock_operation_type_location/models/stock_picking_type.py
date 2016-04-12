1c2a1f710d8fbe897969023f4eb925d6438e0137
mike <viriyananda.michael@gmail.com>
2016-04-12 11:52:24 +0700

    Create Unittest

--- stock_operation_type_location/models/stock_picking_type.py
+++ stock_operation_type_location/models/stock_picking_type.py
@@ -22,23 +22,3 @@ class StockPickingType(models.Model):
         relation='picking_type_dest_location_rel',
         column1='picking_type_id',
         column2='location_id')
-
-    @api.model
-    def fields_view_get(
-        self, view_id=None, view_type='form', toolbar=False, submenu=False
-    ):
-        res = super(StockPickingType, self).fields_view_get(
-            view_id=view_id, view_type=view_type,
-            toolbar=toolbar, submenu=submenu)
-        doc = etree.XML(res['arch'])
-
-        for node in doc.xpath("//field[@name='allowed_location_ids']"):
-            domain = "[('id', 'not in', [default_location_src_id])]"
-            node.set('domain', domain)
-
-        for node in doc.xpath("//field[@name='allowed_dest_location_ids']"):
-            domain = "[('id', 'not in', [default_location_dest_id])]"
-            node.set('domain', domain)
-
-        res['arch'] = etree.tostring(doc)
-        return res
--- stock_operation_type_location/tests/__init__.py
+++ stock_operation_type_location/tests/__init__.py
@@ -2,5 +2,4 @@
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from . import models
-from . import tests
+from . import test_onchange_location
