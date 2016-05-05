4cb49ab391f2c6f08cb08d47cf8291ef4ddc0b6c
mikelarre <mikelarregi@avanzosc.es>
2016-05-05 15:40:06 +0200

    added incomin, outgoing and forecast stock stock_warehouse_orderpoint_stock_info: Search orderpoint rules by product category

--- stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
@@ -11,9 +11,13 @@ class StockWarehouseOrderpoint(models.Model):
 
     @api.one
     def _product_available_qty(self):
-        self.product_location_qty = self.product_id.with_context(
+        product_available = self.product_id.with_context(
             location=self.location_id.id
-            )._product_available()[self.product_id.id]['qty_available']
+            )._product_available()[self.product_id.id]
+        self.product_location_qty = product_available['qty_available']
+        self.incoming_location_qty = product_available['incoming_qty']
+        self.outgoing_location_qty = product_available['outgoing_qty']
+        self.virtual_location_qty = product_available['virtual_available']
 
     @api.one
     @api.depends('product_location_qty', 'product_min_qty')
@@ -22,6 +26,16 @@ class StockWarehouseOrderpoint(models.Model):
 
     product_location_qty = fields.Float(
         string='Quantity On Location', compute='_product_available_qty')
+    incoming_location_qty = fields.Float(
+        string='Incoming On Location', compute='_product_available_qty')
+    outgoing_location_qty = fields.Float(
+        string='Outgoing On Location', compute='_product_available_qty')
+    virtual_location_qty = fields.Float(
+        string='Forecast On Location', compute='_product_available_qty')
+
     available = fields.Boolean(
         string='Is enough product available?', compute='_product_available',
         store=True)
+    product_category = fields.Many2one(string='Product Category',
+                                       related='product_id.categ_id',
+                                       store=True)
