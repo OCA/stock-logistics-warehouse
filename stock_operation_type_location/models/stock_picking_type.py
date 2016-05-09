ce95756a28d9440bd1177352d0acc35c89c235cb
mike <viriyananda.michael@gmail.com>
2016-05-09 12:20:11 +0700

    Fix error flake8

--- stock_operation_type_location/models/stock_picking_type.py
+++ stock_operation_type_location/models/stock_picking_type.py
@@ -2,8 +2,7 @@
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
-from lxml import etree
+from openerp import fields, models
 
 
 class StockPickingType(models.Model):
--- stock_operation_type_location/tests/test_onchange_location.py
+++ stock_operation_type_location/tests/test_onchange_location.py
@@ -2,10 +2,7 @@
 # © 2016 OpenSynergy Indonesia
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from lxml import etree
-
 from openerp.tests.common import TransactionCase
-from openerp.tools.safe_eval import safe_eval
 
 
 class TestOnchangeLocation(TransactionCase):
@@ -36,7 +33,7 @@ class TestOnchangeLocation(TransactionCase):
         sequence_id = self.obj_ir_sequence.create({
             'name': 'Sequence 1',
             'prefix': '/Test/',
-            'padding': 5        
+            'padding': 5
         })
 
         data_picking_type = {
@@ -64,7 +61,7 @@ class TestOnchangeLocation(TransactionCase):
         sequence_id = self.obj_ir_sequence.create({
             'name': 'Sequence 2',
             'prefix': '/Test/',
-            'padding': 5        
+            'padding': 5
         })
 
         data_picking_type = {
@@ -91,7 +88,7 @@ class TestOnchangeLocation(TransactionCase):
         sequence_id = self.obj_ir_sequence.create({
             'name': 'Sequence 3',
             'prefix': '/Test/',
-            'padding': 5        
+            'padding': 5
         })
 
         data_picking_type = {
@@ -108,7 +105,7 @@ class TestOnchangeLocation(TransactionCase):
 
     def _create_picking_type_3(self):
         data = self._prepare_picking_type_data_3()
-        picking_type= self.obj_picking_type.create(data)
+        picking_type = self.obj_picking_type.create(data)
 
         return picking_type
 
@@ -116,7 +113,7 @@ class TestOnchangeLocation(TransactionCase):
         sequence_id = self.obj_ir_sequence.create({
             'name': 'Sequence 4',
             'prefix': '/Test/',
-            'padding': 5        
+            'padding': 5
         })
 
         data_picking_type = {
@@ -132,7 +129,7 @@ class TestOnchangeLocation(TransactionCase):
 
     def _create_picking_type_4(self):
         data = self._prepare_picking_type_data_4()
-        picking_type= self.obj_picking_type.create(data)
+        picking_type = self.obj_picking_type.create(data)
 
         return picking_type
 
@@ -149,7 +146,7 @@ class TestOnchangeLocation(TransactionCase):
     def _create_picking(self, picking_type_id):
         data = self._prepare_picking_data(picking_type_id)
         picking = self.obj_stock_picking.create(data)
-        
+
         return picking
 
     def _prepare_move_data(self, picking_id, picking_type_id):
@@ -160,7 +157,7 @@ class TestOnchangeLocation(TransactionCase):
             'product_id': productA.id,
             'product_uom_qty': 1,
             'product_uom': productA.uom_id.id,
-            'picking_type_id':picking_type_id,
+            'picking_type_id': picking_type_id,
             'picking_id': picking_id,
             'location_id': self.supplier_location.id,
             'location_dest_id': self.stock_location.id
@@ -171,31 +168,31 @@ class TestOnchangeLocation(TransactionCase):
     def _create_move(self, picking_id, picking_type_id):
         data = self._prepare_move_data(picking_id, picking_type_id)
         move = self.obj_stock_move.create(data)
-        
+
         return move
 
     def test_onchange_picking_type_id_1(self):
-        #CONDITION:
-        #allowed_location_ids == True
-        #allowed_dest_location_ids == True
+        # CONDITION:
+        # allowed_location_ids == True
+        # allowed_dest_location_ids == True
 
-        #Create stock picking type
+        # Create stock picking type
         picking_type = self._create_picking_type()
-        #Check create stock picking type
+        # Check create stock picking type
         self.assertIsNotNone(picking_type)
-        #Create stock picking
+        # Create stock picking
         picking = self._create_picking(picking_type.id)
-        #Check create stock picking
+        # Check create stock picking
         self.assertIsNotNone(picking)
-        #Create stock move
+        # Create stock move
         move = self._create_move(picking.id, picking_type.id)
-        #Check create stock move
+        # Check create stock move
         self.assertIsNotNone(move)
-        #Check allowed_location_ids
+        # Check allowed_location_ids
         self.assertTrue(picking_type.allowed_location_ids)
-        #Check allowed_dest_location_ids
+        # Check allowed_dest_location_ids
         self.assertTrue(picking_type.allowed_dest_location_ids)
-        #Test Domain on Onchange 
+        # Test Domain on Onchange
         result = move.onchange_picking_type_id()
         default_location_src_id = self.obj_stock_location.search(
             [('id', '=', picking_type.default_location_src_id.id)]
@@ -205,35 +202,39 @@ class TestOnchangeLocation(TransactionCase):
         ).id
         location_ids = [x.id for x in picking_type.allowed_location_ids]
         location_ids.append(default_location_src_id)
-        dest_location_ids = [y.id for y in picking_type.allowed_dest_location_ids]
+        dest_location_ids = [
+            y.id for y in picking_type.allowed_dest_location_ids
+        ]
         dest_location_ids.append(default_location_dest_id)
         domain_location_id = [('id', 'in', location_ids)]
         domain_dest_location_id = [('id', 'in', dest_location_ids)]
         self.assertEqual(domain_location_id, result['domain']['location_id'])
-        self.assertEqual(domain_dest_location_id, result['domain']['location_dest_id'])
+        self.assertEqual(
+            domain_dest_location_id, result['domain']['location_dest_id']
+        )
 
     def test_onchange_picking_type_id_2(self):
-        #CONDITION:
-        #allowed_location_ids == True
-        #allowed_dest_location_ids == False
+        # CONDITION:
+        # allowed_location_ids == True
+        # allowed_dest_location_ids == False
 
-        #Create stock picking type
+        # Create stock picking type
         picking_type = self._create_picking_type_2()
-        #Check create stock picking type
+        # Check create stock picking type
         self.assertIsNotNone(picking_type)
-        #Create stock picking
+        # Create stock picking
         picking = self._create_picking(picking_type.id)
-        #Check create stock picking
+        # Check create stock picking
         self.assertIsNotNone(picking)
-        #Create stock move
+        # Create stock move
         move = self._create_move(picking.id, picking_type.id)
-        #Check create stock move
+        # Check create stock move
         self.assertIsNotNone(move)
-        #Check allowed_location_ids
+        # Check allowed_location_ids
         self.assertTrue(picking_type.allowed_location_ids)
-        #Check allowed_dest_location_ids
+        # Check allowed_dest_location_ids
         self.assertFalse(picking_type.allowed_dest_location_ids)
-        #Test Domain on Onchange 
+        # Test Domain on Onchange
         result = move.onchange_picking_type_id()
         default_location_src_id = self.obj_stock_location.search(
             [('id', '=', picking_type.default_location_src_id.id)]
@@ -243,66 +244,73 @@ class TestOnchangeLocation(TransactionCase):
         domain_location_id = [('id', 'in', location_ids)]
         domain_dest_location_id = []
         self.assertEqual(domain_location_id, result['domain']['location_id'])
-        self.assertEqual(domain_dest_location_id, result['domain']['location_dest_id'])
+        self.assertEqual(
+            domain_dest_location_id, result['domain']['location_dest_id']
+        )
 
     def test_onchange_picking_type_id_3(self):
-        #CONDITION:
-        #allowed_location_ids == False
-        #allowed_dest_location_ids == True
+        # CONDITION:
+        # allowed_location_ids == False
+        # allowed_dest_location_ids == True
 
-        #Create stock picking type
+        # Create stock picking type
         picking_type = self._create_picking_type_3()
-        #Check create stock picking type
+        # Check create stock picking type
         self.assertIsNotNone(picking_type)
-        #Create stock picking
+        # Create stock picking
         picking = self._create_picking(picking_type.id)
-        #Check create stock picking
+        # Check create stock picking
         self.assertIsNotNone(picking)
-        #Create stock move
+        # Create stock move
         move = self._create_move(picking.id, picking_type.id)
-        #Check create stock move
+        # Check create stock move
         self.assertIsNotNone(move)
-        #Check allowed_location_ids
+        # Check allowed_location_ids
         self.assertFalse(picking_type.allowed_location_ids)
-        #Check allowed_dest_location_ids
+        # Check allowed_dest_location_ids
         self.assertTrue(picking_type.allowed_dest_location_ids)
-        #Test Domain on Onchange 
+        # Test Domain on Onchange
         result = move.onchange_picking_type_id()
         default_location_dest_id = self.obj_stock_location.search(
             [('id', '=', picking_type.default_location_dest_id.id)]
         ).id
-        dest_location_ids = [y.id for y in picking_type.allowed_dest_location_ids]
+        dest_location_ids = [
+            y.id for y in picking_type.allowed_dest_location_ids
+        ]
         dest_location_ids.append(default_location_dest_id)
         domain_location_id = []
         domain_dest_location_id = [('id', 'in', dest_location_ids)]
         self.assertEqual(domain_location_id, result['domain']['location_id'])
-        self.assertEqual(domain_dest_location_id, result['domain']['location_dest_id'])
+        self.assertEqual(
+            domain_dest_location_id, result['domain']['location_dest_id']
+        )
 
     def test_onchange_picking_type_id_4(self):
-        #CONDITION:
-        #allowed_location_ids == False
-        #allowed_dest_location_ids == False
+        # CONDITION:
+        # allowed_location_ids == False
+        # allowed_dest_location_ids == False
 
-        #Create stock picking type
+        # Create stock picking type
         picking_type = self._create_picking_type_4()
-        #Check create stock picking type
+        # Check create stock picking type
         self.assertIsNotNone(picking_type)
-        #Create stock picking
+        # Create stock picking
         picking = self._create_picking(picking_type.id)
-        #Check create stock picking
+        # Check create stock picking
         self.assertIsNotNone(picking)
-        #Create stock move
+        # Create stock move
         move = self._create_move(picking.id, picking_type.id)
-        #Check create stock move
+        # Check create stock move
         self.assertIsNotNone(move)
-        #Check allowed_location_ids
+        # Check allowed_location_ids
         self.assertFalse(picking_type.allowed_location_ids)
-        #Check allowed_dest_location_ids
+        # Check allowed_dest_location_ids
         self.assertFalse(picking_type.allowed_dest_location_ids)
-        #Test Domain on Onchange 
+        # Test Domain on Onchange
         result = move.onchange_picking_type_id()
         domain_location_id = []
         domain_dest_location_id = []
         self.assertEqual(domain_location_id, result['domain']['location_id'])
-        self.assertEqual(domain_dest_location_id, result['domain']['location_dest_id'])
-
+        self.assertEqual(
+            domain_dest_location_id, result['domain']['location_dest_id']
+        )
