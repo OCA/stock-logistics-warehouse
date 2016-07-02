9eab5e04fdb96ca3e47d030d0eef223245cbea4a
Laurent Bélorgey <lb@lalieutenante.com>
2016-07-02 12:07:55 +0200

    [9.0][MIG] stock_manual_assign: Migration to 9.0
    * Fix uom issue
    * Convert move quantity in the default product uom to use the same uom as quant

--- stock_quant_manual_assign/__openerp__.py
+++ stock_quant_manual_assign/__openerp__.py
@@ -3,8 +3,8 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Stock - Manual assignment of quants",
-    "version": "8.0.1.0.0",
+    "name": "Stock - Manual Quant Assignment",
+    "version": "9.0.1.0.0",
     "category": "Warehouse Management",
     "license": "AGPL-3",
     "author": "OdooMRP team, "
@@ -12,12 +12,6 @@
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
     "website": "http://www.odoomrp.com",
-    "contributors": [
-        "Mikel Arregi <mikelarregi@avanzosc.es>",
-        "Ana Juaristi Olalde <anajuaristi@avanzosc.es>",
-        "Pedro Manuel Baeza Romero <pedro.baeza@serviciosbaeza.com>"
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-    ],
     "depends": [
         "stock",
     ],
@@ -25,5 +19,5 @@
         "wizard/assign_manual_quants_view.xml",
         "views/stock_move_view.xml",
     ],
-    "installable": False,
+    "installable": True,
 }
