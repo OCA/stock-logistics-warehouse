c1f5a17e07428b408e329c16d618361194307a2d
Laurent Bélorgey <lb@lalieutenante.com>
2016-07-03 19:25:05 +0200

    [FIX] stock_quant_manual_assign: change the name and comply with the maintainer tool template

--- stock_quant_manual_assign/__openerp__.py
+++ stock_quant_manual_assign/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
-    "name": "Stock - Manual assignment of quants",
+    "name": "Stock - Manual Quant Assignment",
     "version": "9.0.1.0.0",
     "category": "Warehouse Management",
     "license": "AGPL-3",
@@ -12,12 +12,6 @@
               "Serv. Tecnol. Avanzados - Pedro M. Baeza, "
               "Odoo Community Association (OCA)",
     "website": "http://www.odoomrp.com",
-    "contributors": [
-        "Mikel Arregi <mikelarregi@avanzosc.es>",
-        "Ana Juaristi Olalde <anajuaristi@avanzosc.es>",
-        "Pedro Manuel Baeza Romero <pedro.baeza@serviciosbaeza.com>"
-        "Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>",
-    ],
     "depends": [
         "stock",
     ],
