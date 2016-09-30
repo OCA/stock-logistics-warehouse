eea9ca6606cdab31c80051aa17c97e8573fc0e5a
jbeficent <jordi.ballester@eficent.com>
2016-09-30 18:02:40 +0200

    [FIX] flake 8

--- stock_account_quant_merge/tests/test_merge.py
+++ stock_account_quant_merge/tests/test_merge.py
@@ -3,7 +3,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from openerp.addons.stock.tests.common import TestStockCommon
-import time
 
 
 class TestMerge(TestStockCommon):
