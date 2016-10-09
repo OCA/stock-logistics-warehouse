39e7a7da07646eb95a4b4a8aad7055d2cd905ceb
Sylvain GARANCHER <sylvain.garancher@syleam.fr>
2016-10-09 17:23:38 +0200

    [MIG] Migrate stock_mts_mto_rule to v10.0

--- stock_mts_mto_rule/model/rule.py
+++ stock_mts_mto_rule/model/rule.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
-###############################################################################
-#
-#    Module for OpenERP
-#    Copyright (C) 2015 Akretion (http://www.akretion.com). All Rights Reserved
-#    @author Florian DA COSTA <florian.dacosta@akretion.com>
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-###############################################################################
-from openerp import models, api, fields
-from openerp.tools.translate import _
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import models, api, fields
+from odoo.tools.translate import _
 
 
 class ProcurementRule(models.Model):
