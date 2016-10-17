0ddc7fde37020faae7a2b32ac14f189d174790f0
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2016-10-17 16:02:21 +0200

    stock_account_quant_merge (#199)
    [ADD] stock_account_quant_merge

---
+++ stock_account_quant_merge/models/stock.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from openerp import models, api
+
+
+class StockQuant(models.Model):
+    _inherit = 'stock.quant'
+
+    @api.multi
+    def _mergeable_domain(self):
+        domain = super(StockQuant, self)._mergeable_domain()
+
+        if self.product_id.cost_method == 'real':
+            domain += [('cost', '=', self.cost),
+                       ('in_date', '=', self.in_date)]
+
+        return domain
