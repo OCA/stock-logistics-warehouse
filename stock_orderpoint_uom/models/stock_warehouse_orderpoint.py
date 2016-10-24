b9e9da98dd6c4a80f9b9bf004dd9c5b568c2b0c8
jbeficent <jordi.ballester@eficent.com>
2016-10-24 19:26:06 +0200

    [IMP] Can only procure on a UoM in the same dimension as the base UoM

--- stock_orderpoint_uom/models/stock_warehouse_orderpoint.py
+++ stock_orderpoint_uom/models/stock_warehouse_orderpoint.py
@@ -3,7 +3,8 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from openerp import api, fields, models, _
+from openerp.exceptions import Warning as UserError
 
 
 class StockWarehouseOrderpoint(models.Model):
@@ -18,3 +19,15 @@ class StockWarehouseOrderpoint(models.Model):
         for rec in self:
             if rec.procure_uom_id:
                 rec.procure_uom_id = False
+
+    @api.constrains('product_uom', 'procure_uom_id')
+    def _check_procure_uom(self):
+        if any(orderpoint.product_uom
+               and orderpoint.procure_uom_id
+               and orderpoint.product_uom.category_id
+                != orderpoint.procure_uom_id.category_id
+               for orderpoint in self):
+            raise UserError(_('Error: The product default Unit of Measure and '
+                              'the procurement Unit of Measure must be in '
+                              'the same category.'))
+        return True
