28bf22859e393c1f64b4d8fd8324a4021f9de67c
jbeficent <jordi.ballester@eficent.com>
2016-11-04 19:21:30 +0100

    [ADD] [9.0] [stock_quant_reserved_qty_uom]

---
+++ stock_quant_reserved_qty_uom/__openerp__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Stock Quant Reserved Qty UoM',
+    'version': '9.0.1.0.0',
+    'category': 'Inventory, Logistic, Storage',
+    'license': 'AGPL-3',
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    'depends': ['stock'],
+    'data': ['views/stock_quant_view.xml'],
+    'installable': True,
+}
