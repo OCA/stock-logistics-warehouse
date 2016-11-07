58b3191eeee772faf54841a934549b85dca3d432
jbeficent <jordi.ballester@eficent.com>
2016-11-07 13:41:42 +0100

    add OCA style field available cannot be a stored field. remove field 'available', as it was not correctly defined remove unused method

--- stock_warehouse_orderpoint_stock_info/__init__.py
+++ stock_warehouse_orderpoint_stock_info/__init__.py
@@ -1,6 +1,9 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2016 OdooMRP Team
+# Copyright 2016 AvanzOSC
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2016 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import models
