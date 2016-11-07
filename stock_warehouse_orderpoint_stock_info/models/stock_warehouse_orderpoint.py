d5d6d87b6edb16c7f721e16f9c610e61b9dad474
jbeficent <jordi.ballester@eficent.com>
2016-11-07 13:41:42 +0100

    add OCA style field available cannot be a stored field. remove field 'available', as it was not correctly defined remove unused method

--- stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
@@ -1,7 +1,10 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2016 OdooMRP Team
+# Copyright 2016 AvanzOSC
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2016 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp import models, fields, api
 
@@ -9,33 +12,29 @@ from openerp import models, fields, api
 class StockWarehouseOrderpoint(models.Model):
     _inherit = 'stock.warehouse.orderpoint'
 
-    @api.one
-    def _product_available_qty(self):
-        product_available = self.product_id.with_context(
-            location=self.location_id.id
-            )._product_available()[self.product_id.id]
-        self.product_location_qty = product_available['qty_available']
-        self.incoming_location_qty = product_available['incoming_qty']
-        self.outgoing_location_qty = product_available['outgoing_qty']
-        self.virtual_location_qty = product_available['virtual_available']
-
-    @api.one
-    @api.depends('product_location_qty', 'product_min_qty')
-    def _product_available(self):
-        self.available = self.product_location_qty > self.product_min_qty
+    @api.multi
+    def _compute_product_available_qty(self):
+        for rec in self:
+            product_available = rec.product_id.with_context(
+                location=rec.location_id.id
+                )._product_available()[rec.product_id.id]
+            rec.product_location_qty = product_available['qty_available']
+            rec.incoming_location_qty = product_available['incoming_qty']
+            rec.outgoing_location_qty = product_available['outgoing_qty']
+            rec.virtual_location_qty = product_available['virtual_available']
 
     product_location_qty = fields.Float(
-        string='Quantity On Location', compute='_product_available_qty')
+        string='Quantity On Location',
+        compute='_compute_product_available_qty')
     incoming_location_qty = fields.Float(
-        string='Incoming On Location', compute='_product_available_qty')
+        string='Incoming On Location',
+        compute='_compute_product_available_qty')
     outgoing_location_qty = fields.Float(
-        string='Outgoing On Location', compute='_product_available_qty')
+        string='Outgoing On Location',
+        compute='_compute_product_available_qty')
     virtual_location_qty = fields.Float(
-        string='Forecast On Location', compute='_product_available_qty')
-
-    available = fields.Boolean(
-        string='Is enough product available?', compute='_product_available',
-        store=True)
+        string='Forecast On Location',
+        compute='_compute_product_available_qty')
     product_category = fields.Many2one(string='Product Category',
                                        related='product_id.categ_id',
                                        store=True)
--- stock_warehouse_orderpoint_stock_info/tests/test_stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info/tests/test_stock_warehouse_orderpoint.py
@@ -1,7 +1,10 @@
-# -*- encoding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
+# -*- coding: utf-8 -*-
+# Copyright 2016 OdooMRP Team
+# Copyright 2016 AvanzOSC
+# Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
+# Copyright 2016 Serpent Consulting Services Pvt. Ltd.
+# Copyright 2016 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from openerp.tests import common
 
@@ -50,12 +53,12 @@ class TestStockWarehouseOrderpoint(common.TransactionCase):
     def create_orderpoint(self):
         """Create a Reordering rule for the product."""
         record = self.reordering_rule_model.create({
-                                'name': 'Reordering Rule',
-                                'product_id': self.product.id,
-                                'product_min_qty': '1',
-                                'product_max_qty': '5',
-                                'qty_multiple': '1',
-                                'location_id': self.dest_location.id,
+            'name': 'Reordering Rule',
+            'product_id': self.product.id,
+            'product_min_qty': '1',
+            'product_max_qty': '5',
+            'qty_multiple': '1',
+            'location_id': self.dest_location.id,
         })
         return record
 
@@ -74,27 +77,21 @@ class TestStockWarehouseOrderpoint(common.TransactionCase):
         return move
 
     def test_product_qty(self):
-        'Tests the product quantity in the Reordering rules'
-        # Checks the product availability
-        self.reordering_record._product_available()
-        self.assertFalse(self.reordering_record.available)
+        """Tests the product quantity in the Reordering rules"""
         # Create & process moves to test the product quantity
-        self.move = self.create_stock_move()
-        self.reordering_record._product_available_qty()
+        move = self.create_stock_move()
+        self.reordering_record.refresh()
         self.assertEqual(self.reordering_record.incoming_location_qty,
                          self.product.incoming_qty,
                          'Incoming Qty does not match')
         self.assertEqual(self.reordering_record.virtual_location_qty,
                          self.product.virtual_available,
                          'Virtual Qty does not match')
-        self.move.action_done()
-        self.reordering_record._product_available_qty()
+        move.action_done()
+        self.reordering_record.refresh()
         self.assertEqual(self.reordering_record.product_location_qty,
                          self.product.qty_available,
                          'Available Qty does not match')
         self.assertEqual(self.reordering_record.virtual_location_qty,
                          self.product.virtual_available,
                          'Virtual Qty does not match')
-        # Checks the product availability after moves processed
-        self.reordering_record._product_available()
-        self.assertTrue(self.reordering_record.available)
