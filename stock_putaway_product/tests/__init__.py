5b17baf1bc8b5e7597fb81cff68a4f4179e623fd
Carlos Dauden <carlos.dauden@tecnativa.com>
2016-11-07 11:21:44 +0100

    stock_putaway_product: Migration to v9.0 (#197)

---
+++ stock_putaway_product/tests/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# © 2016 Jos De Graeve - Apertoso N.V. <Jos.DeGraeve@apertoso.be>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import test_product_putaway
