6aeffbe0c68408a151d3a2646907671699100cf1
Atchuthan, Sodexis <atchuthan@sodexis.com>
2016-11-15 16:30:05 +0530

    [FIX] added test to increase coverall

--- stock_available_immediately/tests/test_stock_available_immediately.py
+++ stock_available_immediately/tests/test_stock_available_immediately.py
@@ -104,3 +104,7 @@ the variations in stock, both on product and template"""
         stockMoveOutA.action_done()
         compare_product_usable_qty(productA, 1)
         compare_product_usable_qty(templateAB, 4)
+
+        # Potential Qty is set as 0.0 by default
+        self.assertEquals(templateAB.potential_qty, 0.0)
+        self.assertEquals(productA.potential_qty, 0.0)
