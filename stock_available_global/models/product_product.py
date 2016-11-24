3e57ee2f3fd0bb5a462e71d7e697b3190e9b814d
Carlos Dauden <carlos.dauden@tecnativa.com>
2016-11-24 21:12:11 +0100

    [ADD] stock_available_global: Show stock for all companies

---
+++ stock_available_global/models/product_product.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016-2018 Tecnativa - Carlos Dauden
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from openerp import models
+
+
+class ProductProduct(models.Model):
+    _inherit = ['product.product', 'product.stock.available.mixin']
+    _name = 'product.product'
