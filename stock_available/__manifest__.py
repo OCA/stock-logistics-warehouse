4608331d01236ab16e66276107838595215a7897
SodexisTeam <dev@sodexis.com>
2016-11-25 12:20:18 +0530

    [10.0][MIG] stock_available & stock_available_immediately (#219)

--- stock_available/__manifest__.py
+++ stock_available/__manifest__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2014 Numérigraphe, Sodexis
+# Copyright 2014 Numérigraphe
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Stock available to promise',
-    'version': '9.0.1.1.0',
+    'version': '10.0.1.0.0',
     "author": "Numérigraphe, Sodexis, Odoo Community Association (OCA)",
     'category': 'Warehouse',
     'depends': ['stock'],
--- stock_available/models/product_template.py
+++ stock_available/models/product_template.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
-# © 2014 Numérigraphe, Sodexis
+# Copyright 2014 Numérigraphe
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, fields, api
-from openerp.addons import decimal_precision as dp
+from odoo import models, fields, api
+from odoo.addons import decimal_precision as dp
 
 
 class ProductTemplate(models.Model):
@@ -11,7 +12,7 @@ class ProductTemplate(models.Model):
 
     @api.multi
     @api.depends('product_variant_ids.immediately_usable_qty')
-    def _immediately_usable_qty(self):
+    def _compute_immediately_usable_qty(self):
         """No-op implementation of the stock available to promise.
 
         By default, available to promise = forecasted quantity.
@@ -24,11 +25,35 @@ class ProductTemplate(models.Model):
         for tmpl in self:
             tmpl.immediately_usable_qty = tmpl.virtual_available
 
+    @api.multi
+    @api.depends('product_variant_ids.potential_qty')
+    def _compute_potential_qty(self):
+        """Compute the potential as the max of all the variants's potential.
+
+        We can't add the potential of variants: if they share components we
+        may not be able to make all the variants.
+        So we set the arbitrary rule that we can promise up to the biggest
+        variant's potential.
+        """
+        for tmpl in self:
+            if not tmpl.product_variant_ids:
+                continue
+            tmpl.potential_qty = max(
+                [v.potential_qty for v in tmpl.product_variant_ids])
+
     immediately_usable_qty = fields.Float(
         digits=dp.get_precision('Product Unit of Measure'),
-        compute='_immediately_usable_qty',
+        compute='_compute_immediately_usable_qty',
         string='Available to promise',
         help="Stock for this Product that can be safely proposed "
              "for sale to Customers.\n"
              "The definition of this value can be configured to suit "
              "your needs")
+    potential_qty = fields.Float(
+        compute='_compute_potential_qty',
+        digits=dp.get_precision('Product Unit of Measure'),
+        string='Potential',
+        help="Quantity of this Product that could be produced using "
+             "the materials already at hand. "
+             "If the product has several variants, this will be the biggest "
+             "quantity that can be made for a any single variant.")
