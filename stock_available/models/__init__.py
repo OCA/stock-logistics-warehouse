f94ee2cdd39bca6adcf732667c2e0ad786129ec9
SodexisTeam <dev@sodexis.com>
2016-11-25 12:20:18 +0530

    [10.0][MIG] stock_available & stock_available_immediately (#219)

--- stock_available/models/__init__.py
+++ stock_available/models/__init__.py
@@ -1,4 +1,5 @@
-# Copyright 2014 Numérigraphe
+# -*- coding: utf-8 -*-
+# Copyright 2014 Camptocamp, Akretion, Numérigraphe
 # Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
