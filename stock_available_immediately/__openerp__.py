1d7028bafdd97d43a422ee94d7a49d8e6e88434d
SodexisTeam <dev@sodexis.com>
2016-11-25 12:20:18 +0530

    [10.0][MIG] stock_available & stock_available_immediately (#219)

--- stock_available_immediately/__openerp__.py
+++ stock_available_immediately/__openerp__.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2014 Camptocamp, Akretion, Numérigraphe, Sodexis
+# Copyright 2014 Camptocamp, Akretion, Numérigraphe
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Ignore planned receptions in quantity available to promise",
-    "version": "9.0.1.1.0",
+    "version": "10.0.1.0.0",
     "depends": ["stock_available"],
     "author": "Camptocamp,Sodexis,Odoo Community Association (OCA)",
     "license": "AGPL-3",
