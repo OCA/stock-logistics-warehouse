e1ea85e7fe7b79e110f9e76903b3b1ab4b685ccf
SodexisTeam <dev@sodexis.com>
2016-11-25 12:20:18 +0530

    [10.0][MIG] stock_available & stock_available_immediately (#219)

--- stock_available_immediately/product.py
+++ stock_available_immediately/product.py
@@ -1,8 +1,9 @@
 # -*- coding: utf-8 -*-
-# © 2014 Camptocamp, Akretion, Numérigraphe, Sodexis
+# Copyright 2014 Camptocamp, Akretion, Numérigraphe
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models
+from odoo import models, api
 
 
 class Product(models.Model):
@@ -11,10 +12,10 @@ class Product(models.Model):
 
     @api.multi
     @api.depends('virtual_available', 'incoming_qty')
-    def _immediately_usable_qty(self):
+    def _compute_immediately_usable_qty(self):
         """Ignore the incoming goods in the quantity available to promise
 
         This is the same implementation as for templates."""
-        super(ProductProduct, self)._immediately_usable_qty()
+        super(ProductProduct, self)._compute_immediately_usable_qty()
         for prod in self:
             prod.immediately_usable_qty -= prod.incoming_qty
