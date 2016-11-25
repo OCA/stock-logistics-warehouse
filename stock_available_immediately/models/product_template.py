ab79724adc5862c43f5cbde670ac8a3c636b285b
SodexisTeam <dev@sodexis.com>
2016-11-25 12:20:18 +0530

    [10.0][MIG] stock_available & stock_available_immediately (#219)

--- stock_available_immediately/models/product_template.py
+++ stock_available_immediately/models/product_template.py
@@ -1,24 +1,9 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    This module is copyright (C) 2014 Numérigraphe SARL. All Rights Reserved.
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
+# Copyright 2014 Camptocamp, Akretion, Numérigraphe
+# Copyright 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api
+from odoo import models, api
 
 
 class ProductTemplate(models.Model):
@@ -26,10 +11,10 @@ class ProductTemplate(models.Model):
 
     @api.multi
     @api.depends('virtual_available', 'incoming_qty')
-    def _immediately_usable_qty(self):
+    def _compute_immediately_usable_qty(self):
         """Ignore the incoming goods in the quantity available to promise
 
         This is the same implementation as for variants."""
-        super(ProductTemplate, self)._immediately_usable_qty()
+        super(ProductTemplate, self)._compute_immediately_usable_qty()
         for tmpl in self:
             tmpl.immediately_usable_qty -= tmpl.incoming_qty
