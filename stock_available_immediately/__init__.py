0f014950f1d1ff9e163dea08596d6aadc099452d
SodexisTeam <dev@sodexis.com>
2016-11-25 12:20:18 +0530

    [10.0][MIG] stock_available & stock_available_immediately (#219)

--- stock_available_immediately/__init__.py
+++ stock_available_immediately/__init__.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2014 Camptocamp, Akretion, Numérigraphe, Sodexis
+# Copyright 2014 Camptocamp, Akretion, Numérigraphe
+# Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import product_product
