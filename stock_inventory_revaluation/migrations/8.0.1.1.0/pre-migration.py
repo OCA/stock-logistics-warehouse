3ef5976b44aaad792809dff68efd627f3e37d1b5
jbeficent <jordi.ballester@eficent.com>
2016-12-15 13:27:08 +0100

    [FIX] move all to pre-migration

--- stock_inventory_revaluation/migrations/8.0.1.1.0/pre-migration.py
+++ stock_inventory_revaluation/migrations/8.0.1.1.0/pre-migration.py
@@ -30,7 +30,55 @@ def copy_account_move_id(cr):
         """)
 
 
+def set_revaluation_in_account_move(cr):
+
+    cr.execute("""SELECT column_name
+    FROM information_schema.columns
+    WHERE table_name='account_move' AND
+    column_name='stock_inventory_revaluation_id'""")
+    if not cr.fetchone():
+        cr.execute(
+            """
+            ALTER TABLE account_move ADD COLUMN stock_inventory_revaluation_id
+            integer;
+            COMMENT ON COLUMN account_move.stock_inventory_revaluation_id IS
+            'Stock Inventory Revaluation';
+            """)
+    cr.execute(
+        """
+        UPDATE account_move as am
+        SET stock_inventory_revaluation_id = sir.id
+        FROM stock_inventory_revaluation as sir
+        WHERE old_account_move_id = am.id
+        """)
+
+
+def set_revaluation_in_account_move_line(cr):
+    cr.execute("""SELECT column_name
+    FROM information_schema.columns
+    WHERE table_name='account_move_line' AND
+    column_name='stock_inventory_revaluation_id'""")
+    if not cr.fetchone():
+        cr.execute(
+            """
+            ALTER TABLE account_move_line
+            ADD COLUMN stock_inventory_revaluation_id
+            integer;
+            COMMENT ON COLUMN account_move_line.stock_inventory_revaluation_id
+            IS 'Stock Inventory Revaluation';
+            """)
+    cr.execute(
+        """
+        UPDATE account_move_line as aml
+        SET stock_inventory_revaluation_id = sir.id
+        FROM stock_inventory_revaluation as sir
+        WHERE old_account_move_id = aml.move_id
+        """)
+
+
 def migrate(cr, version):
     if not version:
         return
     copy_account_move_id(cr)
+    set_revaluation_in_account_move(cr)
+    set_revaluation_in_account_move_line(cr)
