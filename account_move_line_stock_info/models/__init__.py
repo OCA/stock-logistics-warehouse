75648edca04eb9f3c75925b01a31fba9dfc7e30b
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2016-12-20 11:35:05 +0100

    account_move_line_stock_info (#229)
    * [ADD] account_move_line_stock_info

---
+++ account_move_line_stock_info/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from . import account_move_line
+from . import stock_quant
+from . import stock_move
