4cac35f7f9e4fe8b88bf87d8c6dc99496ba30e93
jbeficent <jordi.ballester@eficent.com>
2016-12-20 11:17:31 +0100

    account_move_line_product

---
+++ account_move_line_product/__init__.py
@@ -0,0 +1,3 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
---
+++ account_move_line_product/__openerp__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Account Move Line Product",
+    "version": "8.0.1.0.0",
+    "author": "Eficent, Odoo Community Association (OCA)",
+    "website": "http://www.eficent.com",
+    "category": "Generic",
+    "depends": ["account"],
+    "description": """
+Account Move Line Product
+=========================
+    """,
+    "license": "AGPL-3",
+    "data": [
+        "views/account_move_line_view.xml",
+    ],
+    'installable': True,
+    'active': False,
+}
