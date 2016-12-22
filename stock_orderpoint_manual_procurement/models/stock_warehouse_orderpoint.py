65ebd9bf0781c83f53b367c3007e261e4ee58745
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2016-12-22 04:51:59 +0100

    stock_orderpoint_manual_procurement (#210)
    [ADD] stock_orderpoint_procure_uom

---
+++ stock_orderpoint_manual_procurement/models/stock_warehouse_orderpoint.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+from datetime import datetime
+from openerp.addons import decimal_precision as dp
+from openerp.tools import float_compare, float_round
+
+
+UNIT = dp.get_precision('Product Unit of Measure')
+
+
+class StockWarehouseOrderpoint(models.Model):
+    _inherit = 'stock.warehouse.orderpoint'
+
+    @api.multi
+    @api.depends("product_min_qty", "product_id", "qty_multiple")
+    def _compute_procure_recommended(self):
+        procurement_model = self.env['procurement.order']
+        for op in self:
+            op.procure_recommended_date = \
+                procurement_model._get_orderpoint_date_planned(
+                    op, datetime.today())
+            procure_recommended_qty = 0.0
+            prods = procurement_model._product_virtual_get(op)
+            if prods is None:
+                continue
+            if float_compare(prods, op.product_min_qty,
+                             precision_rounding=op.product_uom.rounding) < 0:
+                qty = max(op.product_min_qty, op.product_max_qty) - prods
+                reste = op.qty_multiple > 0 and qty % op.qty_multiple or 0.0
+                if float_compare(
+                        reste, 0.0,
+                        precision_rounding=op.product_uom.rounding) > 0:
+                    qty += op.qty_multiple - reste
+
+                if float_compare(
+                        qty, 0.0,
+                        precision_rounding=op.product_uom.rounding) <= 0:
+                    continue
+
+                qty -= op.subtract_procurements(op)
+
+                qty_rounded = float_round(
+                    qty, precision_rounding=op.product_uom.rounding)
+                if qty_rounded > 0:
+                    procure_recommended_qty = qty_rounded
+                if op.procure_uom_id:
+                    product_qty = op.procure_uom_id._compute_qty(
+                        op.product_id.uom_id.id, procure_recommended_qty,
+                        op.procure_uom_id.id)
+                else:
+                    product_qty = procure_recommended_qty
+
+                op.procure_recommended_qty = product_qty
+
+    procure_recommended_qty = fields.Float(
+        string='Procure recommendation',
+        compute="_compute_procure_recommended",
+        digits=UNIT)
+    procure_recommended_date = fields.Date(
+        string='Request Date',
+        compute="_compute_procure_recommended")
