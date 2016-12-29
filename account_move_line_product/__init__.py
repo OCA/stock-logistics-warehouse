4871bf8d13c2bbbdb79ff2fcb8809bbe50f47ee3
jbeficent <jordi.ballester@eficent.com>
2016-12-29 16:57:40 +0100

    migrate module 'account_move_line_product' to v9

--- account_move_line_product/__init__.py
+++ account_move_line_product/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent (http://www.eficent.com)
+# © 2016 Eficent Business and IT Consulting Services, S.L.
+#   (<http://www.eficent.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
