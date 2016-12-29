04508c2534eb2ac4e2c106517ad574d2e6cd4cd8
jbeficent <jordi.ballester@eficent.com>
2016-12-29 16:57:40 +0100

    migrate module 'account_move_line_product' to v9

--- account_move_line_product/__openerp__.py
+++ account_move_line_product/__openerp__.py
@@ -1,18 +1,16 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent (http://www.eficent.com)
+# © 2016 Eficent Business and IT Consulting Services, S.L.
+#   (<http://www.eficent.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account Move Line Product",
-    "version": "8.0.1.0.0",
+    "version": "9.0.1.0.0",
+    "summary": "Displays the product in the journal entries and items",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
     "category": "Generic",
     "depends": ["account"],
-    "description": """
-Account Move Line Product
-=========================
-    """,
     "license": "AGPL-3",
     "data": [
         "views/account_move_line_view.xml",
