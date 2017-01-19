1f6e993585d0fca4213cbca396cd706237457217
Giovanni Francesco Capalbo <giovanni@therp.nl>
2017-01-19 14:17:42 +0100

    Last fix, simplified

--- stock_reorder_forecast/models/procurement_order.py
+++ stock_reorder_forecast/models/procurement_order.py
@@ -8,42 +8,9 @@ class ProcurementOrder(models.Model):
     _inherit = 'procurement.order'
 
     #disable making PO's from procurement orders
-    
-    # this would defer the creation of PO but it would be triggered anyway on
-    # scheduler we need to process run without actually creating the PO..
-    """
+
     @api.model
-    def create(self, vals):
-        if vals['origin'][:2] == 'SO':
-            res = super(ProcurementOrder, self.with_context(
-                procurement_autorun_defer=True, 
-                tracking_disable=False,
-                mail_create_nosubscribe=True,
-                mail_create_nolog=True,
-                mail_notrack=True, 
-                )).create(vals)
-            return self
-        return super(ProcurementOrder, self).create(vals)
-    """
-
-    @api.multi
-    def run(self):
-        for procurement in self:
-            if ((procurement.rule_id and procurement.rule_id.action == 'buy') 
-                    or procurement.origin[:2] == 'SO'):
-                # Skip po will trigger in make_po to skip PO creation for buy
-                # products and return the procurement.id as if they where
-                # processed, without hchanging the state though, because our
-                # query also relies on procurements to create orders.
-                return self.with_context(skip_po=True)._run(procurement)
-        return super(ProcurementOrder, self).run()
-   
-    # don't create PO's if in skip mode
-
-    @api.multi
-    def make_po(self):
-        for procurement in self:
-            if self.context.get('skip_po', False):
-                return procurement.id
-            else:
-                return super(ProcurementOrder, self).make_po()
+    def _run(self, procurement):
+        if (procurement.rule_id and procurement.rule_id.action == 'buy'):
+            return True
+        return super(ProcurementOrder, self)._run(procurement)
