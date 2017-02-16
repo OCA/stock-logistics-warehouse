799518ea0f8cf26970cb016308fc4b78880c74fc
lreficent <lois.rilo@eficent.com>
2017-02-16 17:12:02 +0100

    [IMP] tests

--- stock_inventory_discrepancy/tests/test_inventory_discrepancy.py
+++ stock_inventory_discrepancy/tests/test_inventory_discrepancy.py
@@ -4,6 +4,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from openerp.tests.common import TransactionCase
+from openerp.exceptions import UserError
 
 
 class TestInventoryDiscrepancy(TransactionCase):
@@ -14,6 +15,7 @@ class TestInventoryDiscrepancy(TransactionCase):
         self.obj_inventory = self.env['stock.inventory']
         self.obj_product = self.env['product.product']
         self.obj_warehouse = self.env['stock.warehouse']
+        self.obj_upd_qty_wizard = self.env['stock.change.product.qty']
 
         self.product1 = self.obj_product.create({
             'name': 'Test Product 1',
@@ -42,11 +44,18 @@ class TestInventoryDiscrepancy(TransactionCase):
         group_inventory_all = self.env.ref(
             'stock_inventory_discrepancy.'
             'group_stock_inventory_validation_always')
+        self.manager = self.env['res.users'].create({
+            'name': 'Test Manager',
+            'login': 'manager',
+            'email': 'test.manager@example.com',
+            'groups_id': [(6, 0, [group_stock_man.id, group_inventory_all.id])]
+        })
+        group_stock_user = self.env.ref('stock.group_stock_user')
         self.user = self.env['res.users'].create({
             'name': 'Test User',
             'login': 'user',
             'email': 'test.user@example.com',
-            'groups_id': [(6, 0, [group_stock_man.id, group_inventory_all.id])]
+            'groups_id': [(6, 0, [group_stock_user.id])]
         })
 
         starting_inv = self.obj_inventory.create({
@@ -120,13 +129,13 @@ class TestInventoryDiscrepancy(TransactionCase):
                          'Testing Inventory wrongly configurated')
         self.assertEqual(inventory.line_ids.discrepancy_threshold, 0.1,
                          'Threshold wrongly computed in Inventory Line.')
-        inventory.action_done()
+        inventory.with_context({'normal_view': True}).action_done()
         self.assertEqual(inventory.over_discrepancy_line_count, 1,
                          'Computation of over-discrepancies failed.')
         self.assertEqual(inventory.state, 'pending',
                          'Inventory Adjustment not changing to Pending to '
                          'Approve.')
-        inventory.sudo(self.user).action_force_done()
+        inventory.sudo(self.manager).action_force_done()
         self.assertEqual(inventory.state, 'done',
                          'Forcing the validation of the inventory adjustment '
                          'not working properly.')
@@ -149,3 +158,16 @@ class TestInventoryDiscrepancy(TransactionCase):
         })
         self.assertEqual(inventory.line_ids.discrepancy_threshold, 0.2,
                          'Threshold wrongly computed in Inventory Line.')
+
+    def test_update_qty_user_error(self):
+        """Test if a user error raises when a stock user tries to update the
+        qty for a product and the correction is a discrepancy over the
+        threshold."""
+        upd_qty = self.obj_upd_qty_wizard.create({
+            'product_id': self.product1.id,
+            'product_tmpl_id': self.product1.product_tmpl_id.id,
+            'new_quantity': 10.0,
+            'location_id': self.test_loc.id,
+        })
+        with self.assertRaises(UserError):
+            upd_qty.sudo(self.user).change_product_qty()
