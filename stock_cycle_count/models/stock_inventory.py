2290db09cdba3ab9ca88967a5187d646b24d3519
lreficent <lois.rilo@eficent.com>
2017-02-17 14:48:08 +0100

    [ADD] stock_cycle_count

---
+++ stock_cycle_count/models/stock_inventory.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models
+
+
+class StockInventory(models.Model):
+    _inherit = 'stock.inventory'
+
+    @api.one
+    def _compute_inventory_accuracy(self):
+        total_qty = sum(self.line_ids.mapped('theoretical_qty'))
+        abs_discrepancy = sum(self.line_ids.mapped(
+            lambda x: abs(x.discrepancy_qty)))
+        if total_qty:
+            self.inventory_accuracy = 100 * (total_qty - abs_discrepancy) / \
+                total_qty
+        if not self.line_ids and self.state == 'done':
+            self.inventory_accuracy = 100.0
+
+    cycle_count_id = fields.Many2one(comodel_name='stock.cycle.count',
+                                     string='Stock Cycle Count',
+                                     ondelete='cascade')
+    inventory_accuracy = fields.Float(string='Accuracy',
+                                      compute=_compute_inventory_accuracy,
+                                      digits=(3, 2))
+
+    @api.multi
+    def action_done(self):
+        if self.cycle_count_id:
+            self.cycle_count_id.state = 'done'
+        return super(StockInventory, self).action_done()
