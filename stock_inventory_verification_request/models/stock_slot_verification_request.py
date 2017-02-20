f18722b0a97905a9c3b56e5441ee212c60c613bf
lreficent <lois.rilo@eficent.com>
2017-02-20 11:33:04 +0100

    [IMP]: stock_inventory_verification_request:  * Add tests.  * Several minor fixes.  * Update README.

--- stock_inventory_verification_request/models/stock_slot_verification_request.py
+++ stock_inventory_verification_request/models/stock_slot_verification_request.py
@@ -3,7 +3,7 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models, api
+from openerp import api, fields, models
 
 
 class SlotVerificationRequest(models.Model):
