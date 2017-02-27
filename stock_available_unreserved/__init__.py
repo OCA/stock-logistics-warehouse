1d6f01f59eeb57e75b49e1a8bf602b59fc995932
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2017-02-27 11:29:19 +0100

    new module 'stock_available_unreserved' (#206)
    * [ADD] new module 'stock_available_unreserved'

---
+++ stock_available_unreserved/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import models
