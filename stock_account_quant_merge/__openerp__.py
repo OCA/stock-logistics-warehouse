e308fee124c26100816e22164841b10ac3418e8e
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2017-03-03 12:07:48 +0100

    [9.0][mig][stock_account_quant_merge] (#256)
    * stock_account_quant_merge (#199)
    
    [ADD] stock_account_quant_merge
    
    * OCA Transbot updated translations from Transifex
    
    * [9.0][stock_account_quant_merge] Migration to 9.0

---
+++ stock_account_quant_merge/__openerp__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# © 2016-17 Eficent Business and IT Consulting Services S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Account - Quant merge",
+    "version": "9.0.1.0.0",
+    "depends": [
+        "stock_account",
+        "stock_quant_merge"
+    ],
+    "author": "Eficent,"
+              "Odoo Community Association (OCA)",
+    "website": "http://www.eficent.com",
+    "category": "Warehouse Management",
+    "installable": True,
+    "license": "AGPL-3",
+    "images": [],
+}
