4e2dcba532d5f36f8ccabbacf2dc92d0e9bb0175
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-03-09 17:07:44 +0100

    [MIG] packaging_uom, purchase_packaging, sale_packaging: Migrated to 10.0

---
+++ packaging_uom/__manifest__.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Packaging UOM",
+    "version": "10.0.1.0.0",
+    "author": 'ACSONE SA/NV, '
+              'Odoo Community Association (OCA)',
+    "category": "Warehouse",
+    "website": "http://www.acsone.eu",
+    'summary': "Use uom in package",
+    "depends": ["product",
+                ],
+    "data": ["views/product_packaging_views.xml",
+             ],
+    "license": "AGPL-3",
+    "installable": True,
+}
