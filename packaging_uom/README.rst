5c637074088e719f29adeef4ade2690a8a6dbd63
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-03-09 17:07:44 +0100

    [MIG] packaging_uom, purchase_packaging, sale_packaging: Migrated to 10.0

--- packaging_uom/README.rst
+++ packaging_uom/README.rst
@@ -1,5 +1,6 @@
 .. image:: https://img.shields.io/badge/licence-AGPL--3-blue.svg
-    :alt: License: AGPL-3
+   :target: http://www.gnu.org/licenses/agpl-3.0-standalone.html
+   :alt: License: AGPL-3
 
 Packaging UOM
 =============
@@ -25,21 +26,25 @@ To configure this module, you need to:
 Usage
 =====
 
-For further information, please visit:
-
-* https://www.odoo.com/forum/help-1
+.. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
+   :alt: Try me on Runbot
+   :target: https://runbot.odoo-community.org/runbot/153/10.0
 
 Bug Tracker
 ===========
 
-Bugs are tracked on `GitHub Issues <https://github.com/OCA/stock-logistics-warehouse/issues>`_.
-In case of trouble, please check there if your issue has already been reported.
-If you spotted it first, help us smashing it by providing a detailed and welcomed feedback
-`here <https://github.com/OCA/stock-logistics-warehouse/issues/new?body=module:%20packaging_extended%0Aversion:%208.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
-
+Bugs are tracked on `GitHub Issues
+<https://github.com/OCA/stock-logistics-warehouse/issues>`_. In case of trouble, please
+check there if your issue has already been reported. If you spotted it first,
+help us smashing it by providing a detailed and welcomed feedback.
 
 Credits
 =======
+
+Images
+------
+
+* Odoo Community Association: `Icon <https://github.com/OCA/maintainer-tools/blob/master/template/module/static/description/icon.svg>`_.
 
 Contributors
 ------------
--- packaging_uom/models/product_packaging.py
+++ packaging_uom/models/product_packaging.py
@@ -1,26 +1,7 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    Authors: Laetitia Gangloff
-#    Copyright (c) 2015 Acsone SA/NV (http://www.acsone.eu)
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
-
-from openerp import api, fields, models
+# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, fields, models
 
 
 class ProductPackaging(models.Model):
@@ -34,13 +15,22 @@ class ProductPackaging(models.Model):
         uom = self.env['product.uom'].browse(uom_id)
         return uom.category_id.id
 
-    uom_id = fields.Many2one('product.uom', 'Unit of Measure', required=True,
-                             help="It must be in the same category than "
-                             "the default unit of measure.")
+    uom_id = fields.Many2one(
+        'product.uom',
+        'Unit of Measure',
+        required=True,
+        help="It must be in the same category than "
+             "the default unit of measure."
+    )
     uom_categ_domain_id = fields.Many2one(
         default=_default_uom_categ_domain_id,
-        comodel_name='product.uom.categ')
-    qty = fields.Float(compute="_compute_qty", store=True, readonly=True)
+        comodel_name='product.uom.categ'
+    )
+    qty = fields.Float(
+        compute="_compute_qty",
+        store=True,
+        readonly=True
+    )
 
     @api.one
     @api.depends('uom_id', 'product_tmpl_id.uom_id')
@@ -49,7 +39,7 @@ class ProductPackaging(models.Model):
         Compute the quantity by package based on uom
         """
         if self.uom_id and self.product_tmpl_id:
-            self.qty = self.env['product.uom']._compute_qty_obj(
-                self.uom_id, 1, self.product_tmpl_id.uom_id)
+            self.qty = self.uom_id._compute_quantity(
+                1, to_unit=self.product_tmpl_id.uom_id)
         else:
             self.qty = 0
