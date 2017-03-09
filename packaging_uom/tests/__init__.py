9a8e93970440846249afdd32700c1e3db853b79b
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2017-03-09 17:07:44 +0100

    [MIG] packaging_uom, purchase_packaging, sale_packaging: Migrated to 10.0

--- packaging_uom/tests/__init__.py
+++ packaging_uom/tests/__init__.py
@@ -1,3 +1,5 @@
 # -*- coding: utf-8 -*-
+# Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from . import test_packaging
+from . import test_product_packaging
