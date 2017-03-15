767b5111ea11a05ec8140da1fbfb74c94799a8f0
lreficent <lois.rilo@eficent.com>
2017-03-15 17:32:05 +0100

    [10.0][MIG] stock_orderpoint_manual_procurement

--- stock_orderpoint_manual_procurement/tests/test_stock_orderpoint_manual_procurement.py
+++ stock_orderpoint_manual_procurement/tests/test_stock_orderpoint_manual_procurement.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests import common
+from odoo.tests import common
 
 
 class TestStockWarehouseOrderpoint(common.TransactionCase):
