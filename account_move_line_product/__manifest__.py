17ac49c0f5b2209824cc49a9e2362f33922cda15
lreficent <lois.rilo@eficent.com>
2017-03-16 10:56:32 +0100

    [MIG] account_move_line_product to v10

--- account_move_line_product/__manifest__.py
+++ account_move_line_product/__manifest__.py
@@ -1,11 +1,11 @@
 # -*- coding: utf-8 -*-
-# © 2016 Eficent Business and IT Consulting Services, S.L.
+# © 2016-17 Eficent Business and IT Consulting Services, S.L.
 #   (<http://www.eficent.com>)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Account Move Line Product",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "summary": "Displays the product in the journal entries and items",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "http://www.eficent.com",
