eb13d7a09bec162ec190108e1cae4eaf74a8251b
lreficent <lois.rilo@eficent.com>
2017-03-16 11:52:48 +0100

    [MIG]  account_move_line_stock_info to v10

--- account_move_line_stock_info/models/account_move_line.py
+++ account_move_line_stock_info/models/account_move_line.py
@@ -2,11 +2,12 @@
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class AccountMoveLine(models.Model):
     _inherit = 'account.move.line'
 
-    stock_move_id = fields.Many2one(comodel_name='stock.move',
-                                    string='Stock Move', copy=False)
+    stock_move_id = fields.Many2one(
+        comodel_name='stock.move', string='Stock Move', copy=False,
+        index=True)
