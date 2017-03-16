e45da5948dcff56dbbab877ee841b8f6127ddcc4
lreficent <lois.rilo@eficent.com>
2017-03-16 11:52:48 +0100

    [MIG]  account_move_line_stock_info to v10

--- account_move_line_stock_info/__openerp__.py
+++ account_move_line_stock_info/__openerp__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Move Line Stock Move",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "depends": [
         "stock_account"
     ],
