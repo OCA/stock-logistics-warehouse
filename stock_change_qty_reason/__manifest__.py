56c4043cf759ab0203271d25a27053442e614b75
Meyomesse <meyomesse.gilles@gmail.com>
2017-05-26 09:43:42 +0200

    [MIG] stock_change_qty_reason

--- stock_change_qty_reason/__manifest__.py
+++ stock_change_qty_reason/__manifest__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
-# © 2016 ACSONE SA/NV (<http://acsone.eu>)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     'name': "Stock Change Quantity Reason",
     'summary': """
@@ -8,7 +8,7 @@
     'author': 'ACSONE SA/NV, Odoo Community Association (OCA)',
     'website': "http://acsone.eu",
     'category': 'Warehouse Management',
-    'version': '8.0.1.0.0',
+    'version': '10.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'stock',
@@ -16,4 +16,5 @@
     'data': [
         'wizard/stock_product_change_qty.xml'
     ],
+    'installable': True,
 }
