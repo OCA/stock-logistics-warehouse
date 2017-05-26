37be78b0780731d2dc7ec645c9f127979f931974
Meyomesse <meyomesse.gilles@gmail.com>
2017-05-26 09:43:42 +0200

    [MIG] stock_change_qty_reason

--- stock_change_qty_reason/tests/test_stock_change_qty_reason.py
+++ stock_change_qty_reason/tests/test_stock_change_qty_reason.py
@@ -1,54 +1,68 @@
 # -*- coding: utf-8 -*-
-# © 2016  Denis Roussel, Acsone SA/NV (http://www.acsone.eu)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import openerp.tests.common as common
+from odoo.tests.common import SavepointCase
 
 
-class TestStockQuantityChangeReason(common.TransactionCase):
+class TestStockQuantityChangeReason(SavepointCase):
+
+    @classmethod
+    def setUpClass(cls):
+        super(TestStockQuantityChangeReason, cls).setUpClass()
+
+        # MODELS
+        cls.product_product_model = cls.env['product.product']
+        cls.product_category_model = cls.env['product.category']
+        cls.wizard_model = cls.env['stock.change.product.qty']
+
+        # INSTANCES
+        cls.category = cls.product_category_model.create({
+            'name': 'Physical (test)'})
+
+    def _create_product(self, name):
+        return self.product_product_model.create({
+            'name': name,
+            'categ_id': self.category.id})
 
     def _product_change_qty(self, product, new_qty, reason):
-        wizard_model = self.env['stock.change.product.qty']
-        wizard = wizard_model.create({'product_id': product.id,
-                                      'new_quantity': new_qty,
-                                      'reason': reason})
+        wizard = self.wizard_model.create({'product_id': product.id,
+                                           'new_quantity': new_qty,
+                                           'reason': reason})
         wizard.change_product_qty()
 
     def test_product_change_qty(self):
         """ Check product quantity update move reason is well set
         """
 
-        # set product in each period
-        product2 = self.env.ref('product.product_product_37')
-        product3 = self.env.ref('product.product_product_38')
-        product4 = self.env.ref('product.product_product_39')
-        product5 = self.env.ref('product.product_product_40')
-        product6 = self.env.ref('product.product_product_41')
+        # create products
+        product2 = self._create_product('product_product_2')
+        product3 = self._create_product('product_product_3')
+        product4 = self._create_product('product_product_4')
+        product5 = self._create_product('product_product_5')
+        product6 = self._create_product('product_product_6')
 
-        self._product_change_qty(product2, 10, 'product_37_reason')
-        self._product_change_qty(product3, 0, 'product_38_reason')
-        self._product_change_qty(product4, 0, 'product_39_reason')
-        self._product_change_qty(product5, 10, 'product_40_reason')
-        self._product_change_qty(product6, 0, 'product_41_reason')
+        # update qty on hand and add reason
+        self._product_change_qty(product2, 10, 'product_2_reason')
+        self._product_change_qty(product3, 0, 'product_3_reason')
+        self._product_change_qty(product4, 0, 'product_4_reason')
+        self._product_change_qty(product5, 10, 'product_5_reason')
+        self._product_change_qty(product6, 0, 'product_6_reason')
 
+        # check stock moves created
         move2 = self.env['stock.move'].search([('product_id', '=',
-                                                product2.id)], limit=1,
-                                              order='id desc')
+                                                product2.id)])
         move3 = self.env['stock.move'].search([('product_id', '=',
-                                                product3.id)],
-                                              limit=1, order='id desc')
+                                                product3.id)])
         move4 = self.env['stock.move'].search([('product_id', '=',
-                                                product4.id)],
-                                              limit=1, order='id desc')
+                                                product4.id)])
         move5 = self.env['stock.move'].search([('product_id', '=',
-                                                product5.id)],
-                                              limit=1, order='id desc')
+                                                product5.id)])
         move6 = self.env['stock.move'].search([('product_id', '=',
-                                                product6.id)],
-                                              limit=1, order='id desc')
+                                                product6.id)])
 
-        self.assertEqual(move2.origin, 'product_37_reason')
+        self.assertEqual(move2.origin, 'product_2_reason')
         self.assertFalse(move3)
         self.assertFalse(move4)
-        self.assertEqual(move5.origin, 'product_40_reason')
+        self.assertEqual(move5.origin, 'product_5_reason')
         self.assertFalse(move6)
