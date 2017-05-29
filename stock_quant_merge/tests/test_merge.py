74de6e7356a8bda9e3b10d1ae129258f1ed56866
Lois Rilo <lois.rilo@eficent.com>
2017-05-30 01:43:49 +0200

    [10.0][MIG] stock_quant_merge (#265)

--- stock_quant_merge/tests/test_merge.py
+++ stock_quant_merge/tests/test_merge.py
@@ -2,7 +2,7 @@
 # © 2015 Numérigraphe SARL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp.tests.common import TransactionCase
+from odoo.tests.common import TransactionCase
 
 
 class TestMerge(TransactionCase):
