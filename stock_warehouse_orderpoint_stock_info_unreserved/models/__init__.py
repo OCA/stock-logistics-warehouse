c07cd580e59198e7deb4e57d2f87b7dfcaac8089
Jordi Ballester <jordi.ballester@eficent.com>
2017-05-30 14:43:13 +0200

    [ADD] new module 'stock_warehouse_orderpoint_stock_info_unreserved'

---
+++ stock_warehouse_orderpoint_stock_info_unreserved/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2016 Eficent Business and IT Consulting Services, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import stock_warehouse_orderpoint
