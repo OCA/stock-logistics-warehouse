90e39aa5c15ff51063dce870bebe2401c670be06
Jordi Ballester <jordi.ballester@eficent.com>
2017-05-30 14:39:21 +0200

    [ADD] new module 'stock_demand_estimate'

---
+++ stock_demand_estimate/models/stock_demand_estimate.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+# © 2016 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# © 2016 Aleph Objects, Inc. (https://www.alephobjects.com/)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from openerp import api, fields, models, _
+import openerp.addons.decimal_precision as dp
+from openerp.exceptions import Warning as UserError
+
+
+class StockDemandEstimate(models.Model):
+    _name = 'stock.demand.estimate'
+    _description = 'Stock Demand Estimate Line'
+
+    @api.multi
+    @api.depends('product_id', 'product_uom', 'product_uom_qty')
+    def _compute_product_qty(self):
+        for rec in self:
+            if rec.product_uom:
+                rec.product_qty = rec.product_uom._compute_qty(
+                    rec.product_id.uom_id.id, rec.product_uom_qty,
+                    rec.product_uom.id)
+
+    def _set_product_qty(self):
+        raise UserError(_('The requested operation cannot be '
+                          'processed because of a programming error '
+                          'setting the `product_qty` field instead '
+                          'of the `product_uom_qty`.'))
+
+    @api.multi
+    def _compute_daily_qty(self):
+        for rec in self:
+            rec.daily_qty = rec.product_qty / rec.period_id.days
+
+    period_id = fields.Many2one(
+        comodel_name="stock.demand.estimate.period",
+        string="Estimating Period",
+        required=True)
+    product_id = fields.Many2one(comodel_name="product.product",
+                                 string="Product", required=True)
+    product_uom = fields.Many2one(comodel_name="product.uom",
+                                  string="Unit of measure")
+    location_id = fields.Many2one(comodel_name="stock.location",
+                                  string="Location", required=True)
+    product_uom_qty = fields.Float(
+        string="Quantity",
+        digits_compute=dp.get_precision('Product Unit of Measure'))
+    product_qty = fields.Float('Real Quantity', compute='_compute_product_qty',
+                               inverse='_set_product_qty', digits=0,
+                               store=True,
+                               help='Quantity in the default UoM of the '
+                                    'product', readonly=True)
+    daily_qty = fields.Float(string='Quantity / Day',
+                             compute='_compute_daily_qty')
+    company_id = fields.Many2one(
+        comodel_name='res.company', string='Company', required=True,
+        default=lambda self: self.env['res.company']._company_default_get(
+            'stock.demand.estimate'))
+
+    @api.multi
+    def name_get(self):
+        res = []
+        for rec in self:
+            name = "%s - %s - %s" % (rec.period_id.name, rec.product_id.name,
+                                     rec.location_id.name)
+            res.append((rec.id, name))
+        return res
+
+    @api.model
+    def get_quantity_by_date_range(self, date_from, date_to):
+        # Check if the dates overlap with the period
+        period_date_from = fields.Date.from_string(self.period_id.date_from)
+        period_date_to = fields.Date.from_string(self.period_id.date_to)
+
+        if date_from <= period_date_to and period_date_from <= date_to:
+            overlap_date_from = max(period_date_from, date_from)
+            overlap_date_to = min(period_date_to, date_to)
+            days = (abs(overlap_date_to-overlap_date_from)).days + 1
+            return days * self.daily_qty
+        return 0.0
+
