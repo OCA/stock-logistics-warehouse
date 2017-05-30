ece6fca776fcaf9b87c045b8f63184a0f52e6ca0
Jordi Ballester <jordi.ballester@eficent.com>
2017-05-30 16:50:47 +0200

    [IMP] stock_demand_estimate: replace demand estimate periods
    with date range

--- stock_demand_estimate/wizards/stock_demand_estimate_wizard.py
+++ stock_demand_estimate/wizards/stock_demand_estimate_wizard.py
@@ -8,21 +8,16 @@ from openerp import api, fields, models, _
 import openerp.addons.decimal_precision as dp
 from openerp.exceptions import Warning as UserError
 
-_PERIOD_SELECTION = [
-    ('monthly', 'Monthly'),
-    ('weekly', 'Weekly')
-]
-
 
 class StockDemandEstimateSheet(models.TransientModel):
     _name = 'stock.demand.estimate.sheet'
     _description = 'Stock Demand Estimate Sheet'
 
-    def _default_date_from(self):
-        return self.env.context.get('date_from', False)
+    def _default_date_start(self):
+        return self.env.context.get('date_start', False)
 
-    def _default_date_to(self):
-        return self.env.context.get('date_to', False)
+    def _default_date_end(self):
+        return self.env.context.get('date_end', False)
 
     def _default_location_id(self):
         location_id = self.env.context.get('location_id', False)
@@ -32,13 +27,24 @@ class StockDemandEstimateSheet(models.TransientModel):
             return False
 
     def _default_estimate_ids(self):
-        date_from = self.env.context.get('date_from', False)
-        date_to = self.env.context.get('date_to', False)
-        location_id = self.env.context.get('location_id', False)
-        product_ids = self.env.context.get('product_ids', False)
-        domain = [('date_from', '>=', date_from),
-                  ('date_to', '<=', date_to)]
-        periods = self.env['stock.demand.estimate.period'].search(
+        date_start = self.env.context.get('default_date_start', False)
+        date_end = self.env.context.get('default_date_end', False)
+        date_range_type_id = self.env.context.get('default_date_range_type_id',
+                                                  False)
+        location_id = self.env.context.get('default_location_id', False)
+        product_ids = self.env.context.get('default_product_ids', False)
+        domain = [('type_id', '=', date_range_type_id), '|', '&',
+                  ('date_start', '>=', date_start),
+                  ('date_start', '<=', date_end),
+                  '&',
+                  ('date_end', '>=', date_start),
+                  ('date_end', '<=', date_end)]
+        periods = self.env['date.range'].search(
+            domain)
+        domain = [('type_id', '=', date_range_type_id),
+                  ('date_start', '<=', date_start),
+                  ('date_end', '>=', date_start)]
+        periods |= self.env['date.range'].search(
             domain)
         products = self.env['product.product'].browse(product_ids)
 
@@ -52,13 +58,13 @@ class StockDemandEstimateSheet(models.TransientModel):
             for period in periods:
                 estimates = self.env['stock.demand.estimate'].search(
                     [('product_id', '=', product.id),
-                     ('period_id', '=', period.id),
+                     ('date_range_id', '=', period.id),
                      ('location_id', '=', location_id)])
                 if estimates:
                     lines.append((0, 0, {
                         'value_x': period.name,
                         'value_y': name_y,
-                        'period_id': period.id,
+                        'date_range_id': period.id,
                         'product_id': product.id,
                         'product_uom': estimates[0].product_uom.id,
                         'location_id': location_id,
@@ -69,7 +75,7 @@ class StockDemandEstimateSheet(models.TransientModel):
                     lines.append((0, 0, {
                         'value_x': period.name,
                         'value_y': name_y,
-                        'period_id': period.id,
+                        'date_range_id': period.id,
                         'product_id': product.id,
                         'product_uom': product.uom_id.id,
                         'location_id': location_id,
@@ -77,13 +83,13 @@ class StockDemandEstimateSheet(models.TransientModel):
                     }))
         return lines
 
-    date_from = fields.Date(string="Date From", readonly=True,
-                            default=_default_date_from)
-    date_to = fields.Date(string="Date From", readonly=True,
-                          default=_default_date_to)
+    date_start = fields.Date(string="Date From", readonly=True)
+    date_end = fields.Date(string="Date From", readonly=True)
+    date_range_type_id = fields.Many2one(string='Date Range Type',
+                                         comodel_name='date.range.type',
+                                         readonly=True)
     location_id = fields.Many2one(comodel_name="stock.location",
-                                  string="Location", readonly=True,
-                                  default=_default_location_id)
+                                  string="Location", readonly=True)
     line_ids = fields.Many2many(
         string="Estimates",
         comodel_name='stock.demand.estimate.sheet.line',
@@ -93,7 +99,7 @@ class StockDemandEstimateSheet(models.TransientModel):
     @api.model
     def _prepare_estimate_data(self, line):
         return {
-            'period_id': line.period_id.id,
+            'date_range_id': line.date_range_id.id,
             'product_id': line.product_id.id,
             'location_id': line.location_id.id,
             'product_uom_qty': line.product_uom_qty,
@@ -129,7 +135,7 @@ class StockDemandEstimateSheetLine(models.TransientModel):
     _description = 'Stock Demand Estimate Sheet Line'
 
     estimate_id = fields.Many2one(comodel_name='stock.demand.estimate')
-    period_id = fields.Many2one(
+    date_range_id = fields.Many2one(
         comodel_name='stock.demand.estimate.period',
         string='Period')
     location_id = fields.Many2one(comodel_name='stock.location',
@@ -146,8 +152,11 @@ class DemandEstimateWizard(models.TransientModel):
     _name = 'stock.demand.estimate.wizard'
     _description = 'Stock Demand Estimate Wizard'
 
-    date_from = fields.Date(string="Date From", required=True)
-    date_to = fields.Date(string="Date To", required=True)
+    date_start = fields.Date(string="Date From", required=True)
+    date_end = fields.Date(string="Date To", required=True)
+    date_range_type_id = fields.Many2one(string='Date Range Type',
+                                         comodel_name='date.range.type',
+                                         required=True)
     location_id = fields.Many2one(comodel_name="stock.location",
                                   string="Location", required=True)
     product_ids = fields.Many2many(
@@ -158,8 +167,9 @@ class DemandEstimateWizard(models.TransientModel):
     def _prepare_demand_estimate_sheet(self):
         self.ensure_one()
         return {
-            'date_from': self.date_from,
-            'date_to': self.date_to,
+            'date_start': self.date_start,
+            'date_end': self.date_end,
+            'date_range_type_id': self.date_range_type_id.id,
             'location_id': self.location_id.id,
         }
 
@@ -170,10 +180,11 @@ class DemandEstimateWizard(models.TransientModel):
             raise UserError(_('You must select at lease one product.'))
 
         context = {
-            'date_from': self.date_from,
-            'date_to': self.date_to,
-            'location_id': self.location_id.id,
-            'product_ids': self.product_ids.ids
+            'default_date_start': self.date_start,
+            'default_date_end': self.date_end,
+            'default_date_range_type_id': self.date_range_type_id.id,
+            'default_location_id': self.location_id.id,
+            'default_product_ids': self.product_ids.ids
         }
         res = {
             'context': context,
