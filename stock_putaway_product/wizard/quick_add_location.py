87c382e7fcb8aef6e6ea7e60496cc495cdf64f94
David Beal <david.beal@akretion.com>
2017-06-13 17:42:33 +0200

    FIX misc comments

--- stock_putaway_product/wizard/quick_add_location.py
+++ stock_putaway_product/wizard/quick_add_location.py
@@ -2,7 +2,6 @@
 # © 2017 David BEAL @ Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from collections import defaultdict
 from openerp import _, models, api, fields
 
 
@@ -10,11 +9,11 @@ class QuickAddProductLocation(models.TransientModel):
     _name = "quick.add.product.location"
 
     def _default_putaway(self):
-        # overridable method
-        putaway = self.env.ref(
-            'stock_putaway_product.product_putaway_per_product_wh')
-        if putaway:
-            return putaway.id
+        """ Overridable method
+        """
+        return self.env.ref(
+            'stock_putaway_product.product_putaway_per_product_wh',
+            raise_if_not_found=False)
 
     def __default_putaway(self):
         return self._default_putaway()
@@ -38,22 +37,26 @@ class QuickAddProductLocation(models.TransientModel):
     @api.multi
     def apply_location(self):
         self.ensure_one()
-        products = defaultdict(list)
+        products = {}
         for product in self.product_ids:
-            products[product.product_tmpl_id.id].append(product.id)
-        for template_id in products:
-            products_with_location = self.env['product.product'].browse(
-                products[template_id])
-            products_with_location.write({
+            products.setdefault(product.product_tmpl_id,
+                                self.env['product.product'].browse())
+            products[product.product_tmpl_id] |= product
+        # Remove existing putaway locations
+        self.env['stock.product.putaway.strategy'].search(
+            [('putaway_id', '=', self.putaway_id.id),
+             ('product_product_id', 'in', self.product_ids._ids)]).unlink()
+        for template in products:
+            products[template].write({
                 'product_putaway_ids': [(0, 0, {
                     'putaway_id': self.putaway_id.id,
-                    'product_template_id': template_id,
+                    'product_template_id': template.id,
                     'fixed_location_id': self.fixed_location_id.id})]})
         product_names = ', '.join([x.name for x in self.product_ids])
         return {
-            'name': _("Updated Products: %r" % product_names),
+            'name': _("Updated products putaway: %s" % product_names),
             'type': 'ir.actions.act_window',
             'res_id': self.env.ref('product.product_normal_action').id,
-            'view_mode': 'tree',
+            'view_mode': 'tree,form',
             'res_model': 'product.product',
         }
