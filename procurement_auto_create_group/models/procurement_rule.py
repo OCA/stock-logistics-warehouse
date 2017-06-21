16b364c3bf6c623a42383d4095a758049b083d26
Jordi Ballester <jordi.ballester@eficent.com>
2017-06-21 10:19:22 +0200

    auto_create_group should be false when group_propagation_option is not 'propagate'

--- procurement_auto_create_group/models/procurement_rule.py
+++ procurement_auto_create_group/models/procurement_rule.py
@@ -1,10 +1,15 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import fields, models
+from openerp import api, fields, models
 
 
 class ProcurementRule(models.Model):
     _inherit = 'procurement.rule'
 
     auto_create_group = fields.Boolean(string='Auto-create Procurement Group')
+
+    @api.onchange('group_propagation_option')
+    def _onchange_group_propagation_option(self):
+        if self.group_propagation_option != 'propagate':
+            self.auto_create_group = False
