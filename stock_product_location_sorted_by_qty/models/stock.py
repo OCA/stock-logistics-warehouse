2b141bca1decfbe6568456044a92a1ab4327bbad
chafique-delli <chafique.delli@akretion.com>
2017-06-27 18:44:13 +0200

    refactor code in name_get method uing the read_group() function

--- stock_product_location_sorted_by_qty/models/stock.py
+++ stock_product_location_sorted_by_qty/models/stock.py
@@ -14,8 +14,8 @@ class StockLocation(models.Model):
         context = self.env.context
         res = super(StockLocation, self).name_search(
             name=name, args=args, operator=operator, limit=limit)
-        filtered_loc_list = []
-        if context.get('stock_change_product_quantity', False):
+        ordered_loc_list = []
+        if context.get('stock_change_product_quantity'):
             domain = [
                 ('qty', '>', 0),
                 ('product_id', '=', context.get('default_product_id', False)),
@@ -25,25 +25,36 @@ class StockLocation(models.Model):
                 domain, ['location_id', 'qty'], 'location_id',
                 orderby='qty desc')
             for elm in quants:
-                filtered_loc_list.append(elm['location_id'])
+                ordered_loc_list.append(elm['location_id'])
                 if elm['location_id'] in res:
-                    index = res.index(elm['location_id'])
-                    del res[index]
-            res = filtered_loc_list + res
+                    res.remove(elm['location_id'])
+            res = ordered_loc_list + res
         return res
 
     @api.multi
     def name_get(self):
         res = super(StockLocation, self).name_get()
-        if self._context.get('stock_change_product_quantity', False):
-            names = []
+        context = self.env.context
+        if context.get('stock_change_product_quantity'):
+            new_res = []
+            ordered_name_loc_list = []
+            domain = [
+                ('qty', '>', 0),
+                ('product_id', '=', context.get('default_product_id', False)),
+                ('location_id', 'in', self.ids)
+            ]
+            quants = self.env['stock.quant'].with_context(
+                stock_change_product_quantity=False).read_group(
+                    domain, ['location_id', 'qty'], 'location_id',
+                    orderby='qty desc')
+            for quant in quants:
+                full_name_loc = (
+                    quant['location_id'][0],
+                    quant['location_id'][1] + ' (%s)' % quant['qty'])
+                ordered_name_loc_list.append(full_name_loc)
+                if quant['location_id'] in res:
+                    res.remove(quant['location_id'])
             for elm in res:
-                quants = self.env['stock.quant'].search([
-                    ('location_id', '=', elm[0]),
-                    ('product_id', '=', self._context.get(
-                        'default_product_id', False))
-                ])
-                qty = sum([quant.qty for quant in quants])
-                names.append((elm[0], elm[1] + ' (%s)' % qty))
-            res = names
+                new_res.append((elm[0], elm[1] + ' (0)'))
+            res = ordered_name_loc_list + new_res
         return res
