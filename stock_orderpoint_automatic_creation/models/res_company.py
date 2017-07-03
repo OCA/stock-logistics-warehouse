aeaef06320e4acb5a69f64ab80032875b8707ca6
cubells <vicent@vcubells.net>
2017-07-03 13:04:27 +0200

    Add precision and improve constrains method

--- stock_orderpoint_automatic_creation/models/res_company.py
+++ stock_orderpoint_automatic_creation/models/res_company.py
@@ -3,18 +3,23 @@
 # Copyright 2017 Tecnativa - Vicent Cubells
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, exceptions, fields, models
+from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
+from odoo.addons import decimal_precision as dp
+from odoo.tools import float_compare
 
 
 class ResCompany(models.Model):
     _inherit = 'res.company'
 
-    orderpoint_product_max_qty = fields.Integer(
+    orderpoint_product_max_qty = fields.Float(
         string='Max. product quantity',
+        digits=dp.get_precision('Product Unit of Measure'),
         help='Default orderpoint Max. product quantity',
     )
-    orderpoint_product_min_qty = fields.Integer(
+    orderpoint_product_min_qty = fields.Float(
         string='Min. product quantity',
+        digits=dp.get_precision('Product Unit of Measure'),
         help='Default orderpoint Max. product quantity',
     )
     create_orderpoints = fields.Boolean(
@@ -25,8 +30,9 @@ class ResCompany(models.Model):
     @api.multi
     @api.constrains('orderpoint_product_max_qty', 'orderpoint_product_min_qty')
     def _check_orderpoint_product_qty(self):
-        for orderpoint in self:
-            if orderpoint.orderpoint_product_max_qty < 0 \
-                    or orderpoint.orderpoint_product_min_qty < 0:
-                raise exceptions.Warning(
+        for company in self:
+            if float_compare(company.orderpoint_product_max_qty, 0.0,
+                             2) < 0 or float_compare(
+                    company.orderpoint_product_min_qty, 0.0, 2) < 0:
+                raise ValidationError(
                     _('Orderpoint product quantity cannot be negative'))
