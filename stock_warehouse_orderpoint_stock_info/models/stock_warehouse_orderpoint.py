fce0f341c5b188066c07533d45d9784818abe932
mreficent <miquel.raich@eficent.com>
2017-07-18 17:30:14 +0200

    [MIG] stock_warehouse_orderpoint_stock_info: Migration to 10.0

--- stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info/models/stock_warehouse_orderpoint.py
@@ -3,10 +3,11 @@
 # Copyright 2016 AvanzOSC
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
-# Copyright 2016 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2016-17 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from odoo import api, fields, models
+from collections import defaultdict
 
 
 class StockWarehouseOrderpoint(models.Model):
@@ -14,14 +15,21 @@ class StockWarehouseOrderpoint(models.Model):
 
     @api.multi
     def _compute_product_available_qty(self):
-        for rec in self:
-            product_available = rec.product_id.with_context(
-                location=rec.location_id.id
-                )._product_available()[rec.product_id.id]
-            rec.product_location_qty = product_available['qty_available']
-            rec.incoming_location_qty = product_available['incoming_qty']
-            rec.outgoing_location_qty = product_available['outgoing_qty']
-            rec.virtual_location_qty = product_available['virtual_available']
+        op_by_loc = defaultdict(lambda: self.env['stock.warehouse.orderpoint'])
+        for order in self:
+            op_by_loc[order.location_id] |= order
+        for location_id, order_in_loc in op_by_loc.items():
+            products = order_in_loc.mapped('product_id').with_context(
+                location=location_id.id)._compute_quantities_dict(
+                    lot_id=self.env.context.get('lot_id'),
+                    owner_id=self.env.context.get('owner_id'),
+                    package_id=self.env.context.get('package_id'))
+            for order in order_in_loc:
+                product = products[order.product_id.id]
+                order.product_location_qty = product['qty_available']
+                order.incoming_location_qty = product['incoming_qty']
+                order.outgoing_location_qty = product['outgoing_qty']
+                order.virtual_location_qty = product['virtual_available']
 
     product_location_qty = fields.Float(
         string='Quantity On Location',
@@ -37,4 +45,4 @@ class StockWarehouseOrderpoint(models.Model):
         compute='_compute_product_available_qty')
     product_category = fields.Many2one(string='Product Category',
                                        related='product_id.categ_id',
-                                       store=True)
+                                       store=True, readonly=True)
