41dce6b3ef020f4eab4fb0eeb98916e8eb767ed3
mreficent <miquel.raich@eficent.com>
2017-07-19 17:56:33 +0200

    [FIX] stock_orderpoint_generator (fields don't exist)

--- stock_orderpoint_generator/tests/test_orderpoint_generator.py
+++ stock_orderpoint_generator/tests/test_orderpoint_generator.py
@@ -2,7 +2,7 @@
 # © 2016 Cyril Gaudin (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from openerp.exceptions import UserError
-from openerp.tests import TransactionCase
+from openerp.tests.common import TransactionCase
 
 
 class TestOrderpointGenerator(TransactionCase):
