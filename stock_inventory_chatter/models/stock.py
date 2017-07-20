3dc281bf1ceafe67f5c71f2ee40a3b162a5572c8
mreficent <miquel.raich@eficent.com>
2017-07-20 10:51:02 +0200

    [MIG] stock_inventory_chatter: Migration to 10.0

--- stock_inventory_chatter/models/stock.py
+++ stock_inventory_chatter/models/stock.py
@@ -3,7 +3,7 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models
+from odoo import api, fields, models
 
 
 class StockInventory(models.Model):
