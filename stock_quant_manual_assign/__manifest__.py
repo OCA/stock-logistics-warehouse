e88bf5907d007adb17e6a16b8fa2940201cfaed9
Fanha Giang <fanha99@hotmail.com>
2017-07-26 15:29:25 +0700

    [10.0][MIG] stock_quant_manual_assign: Migration to 10.0

--- stock_quant_manual_assign/__manifest__.py
+++ stock_quant_manual_assign/__manifest__.py
@@ -1,19 +1,20 @@
 # -*- coding: utf-8 -*-
-# (c) 2015 Mikel Arregi - AvanzOSC
-# (c) 2016 Tecnativa - Pedro M. Baeza
-# (c) 2017 Eficent - Jordi Ballester
+# Copyright 2015 Mikel Arregi - AvanzOSC
+# Copyright 2016 Tecnativa - Pedro M. Baeza
+# Copyright 2017 Eficent - Jordi Ballester
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 {
     "name": "Stock - Manual Quant Assignment",
-    "version": "9.0.1.0.0",
+    "version": "10.0.1.0.0",
     "category": "Warehouse Management",
     "license": "AGPL-3",
     "author": "AvanzOSC, "
               "Tecnativa, "
               "Eficent, "
+              "Fanha Giang, "
               "Odoo Community Association (OCA)",
-    "website": "http://www.odoomrp.com",
+    "website": "https://odoo-community.org",
     "depends": [
         "stock",
     ],
