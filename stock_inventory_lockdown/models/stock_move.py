b27042d416a993a41bdb6302e5e4893075b94add
mreficent <miquel.raich@eficent.com>
2017-07-27 17:29:51 +0200

    [MIG] stock_inventory_lockdown: Migration to 10.0

--- stock_inventory_lockdown/models/stock_move.py
+++ stock_inventory_lockdown/models/stock_move.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 # © 2016 Numérigraphe SARL
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from openerp import models, api, _
-from openerp.exceptions import ValidationError
+from odoo import api, models, _
+from odoo.exceptions import ValidationError
 
 
 class StockMove(models.Model):
@@ -11,9 +13,7 @@ class StockMove(models.Model):
 
     @api.constrains('location_dest_id', 'location_id', 'state')
     def _check_locked_location(self):
-        for move in self:
-            if move.state == 'draft':
-                continue
+        for move in self.filtered(lambda m: m.state != 'draft'):
             locked_location_ids = self.env[
                 'stock.inventory']._get_locations_open_inventories(
                 [move.location_dest_id.id, move.location_id.id])
