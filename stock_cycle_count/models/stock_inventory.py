090d412246e75c2062314673d5dfe7626f5fb236
lreficent <lois.rilo@eficent.com>
2017-08-24 10:47:04 +0200

    [10.0][MIG] stock_cycle_count

--- stock_cycle_count/models/stock_inventory.py
+++ stock_cycle_count/models/stock_inventory.py
@@ -3,8 +3,8 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from openerp import api, fields, models, _
-from openerp.exceptions import UserError
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
 
 PERCENT = 100.0
 
@@ -29,9 +29,9 @@ class StockInventory(models.Model):
 
     cycle_count_id = fields.Many2one(
         comodel_name='stock.cycle.count', string='Stock Cycle Count',
-        ondelete='cascade', readonly=True)
+        ondelete='restrict', readonly=True)
     inventory_accuracy = fields.Float(
-        string='Accuracy', compute=_compute_inventory_accuracy,
+        string='Accuracy', compute='_compute_inventory_accuracy',
         digits=(3, 2), store=True, group_operator="avg")
 
     @api.multi
