64fffa8a354714921f55e7acf34a2c01aaf8d8bb
mreficent <miquel.raich@eficent.com>
2017-09-06 18:00:37 +0200

    [MIG] stock_warehouse_orderpoint_stock_info_unreserved: Migration to 10.0 [IMP] fast compute _product_available() method is computed only once by location [FIX] stock_available: Some affectations

--- stock_warehouse_orderpoint_stock_info_unreserved/__manifest__.py
+++ stock_warehouse_orderpoint_stock_info_unreserved/__manifest__.py
@@ -3,15 +3,15 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Reordering rules stock info unreserved",
-    "version": "9.0.1.0.0",
+    "name": "Stock Warehouse Orderpoint Stock Info Unreserved",
+    "version": "10.0.1.0.0",
     "depends": [
         "stock_warehouse_orderpoint_stock_info",
         "stock_available_unreserved"
     ],
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
-    "website": "http://www.eficent.com",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
     "license": "AGPL-3",
     "data": [
--- stock_warehouse_orderpoint_stock_info_unreserved/models/stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info_unreserved/models/stock_warehouse_orderpoint.py
@@ -2,7 +2,8 @@
 # Copyright 2016 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import models, fields, api
+from odoo import api, fields, models
+from collections import defaultdict
 
 
 class StockWarehouseOrderpoint(models.Model):
@@ -11,12 +12,16 @@ class StockWarehouseOrderpoint(models.Model):
     @api.multi
     def _compute_product_available_qty(self):
         super(StockWarehouseOrderpoint, self)._compute_product_available_qty()
-        for rec in self:
-            product_available = rec.product_id.with_context(
-                location=rec.location_id.id
-                )._product_available()[rec.product_id.id]
-            rec.product_location_qty_available_not_res = \
-                product_available['qty_available_not_res']
+        op_by_loc = defaultdict(lambda: self.env['stock.warehouse.orderpoint'])
+        for order in self:
+            op_by_loc[order.location_id] |= order
+        for location_id, order_in_loc in op_by_loc.items():
+            products = order_in_loc.mapped('product_id').with_context(
+                location=location_id.id)._compute_qty_available_not_res()
+            for order in order_in_loc:
+                product = products[order.product_id.id]
+                order.product_location_qty_available_not_res = \
+                    product['qty_available_not_res']
 
     product_location_qty_available_not_res = fields.Float(
         string='Quantity On Location (Unreserved)',
