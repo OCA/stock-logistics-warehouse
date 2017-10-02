cf1e1b13409a4d7a356749e0eb9c3917242701d5
Denis Roussel <denis.roussel@acsone.eu>
2017-10-02 11:01:58 +0200

    stock_location_restrict_procurement_group : Rename module

---
+++ setup/stock_location_restrict_procurement_group/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    setup_requires=[
+        'setuptools-odoo',
+        'openupgradelib'
+    ],
+    odoo_addon=True,
+)
--- stock_location_restrict_procurement_group/__manifest__.py
+++ stock_location_restrict_procurement_group/__manifest__.py
@@ -3,11 +3,11 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Stock Location Restrict Group',
+    'name': 'Stock Location Restrict Procurement Group',
     'summary': """
         Allows to restrict location to a dedicated procurement group
         (e.g. : For orders waiting delivery)""",
-    'version': '10.0.1.0.0',
+    'version': '10.0.1.1.0',
     'license': 'AGPL-3',
     'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
     'website': 'https://acsone.eu',
@@ -17,4 +17,8 @@
         'views/stock_move.xml',
         'views/stock_location.xml',
     ],
+    'external_dependencies': {
+        'python': ['openupgradelib'],
+    },
+    'pre_init_hook': 'pre_init_hook',
 }
