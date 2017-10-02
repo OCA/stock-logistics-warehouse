12f1cca1ba7759f13b987df4f2613b3254bbd4b4
Cédric Pigeon <cedric.pigeon@acsone.eu>
2017-10-02 22:34:17 +0200

    [FIX] fix orderpoint compute with purchase multiple

--- purchase_packaging/models/procurement_order.py
+++ purchase_packaging/models/procurement_order.py
@@ -2,6 +2,7 @@
 # Copyright 2015-2017 ACSONE SA/NV (<http://acsone.eu>)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
+from odoo.tools import float_compare
 
 
 class ProcurementOrder(models.Model):
@@ -27,4 +28,39 @@ class ProcurementOrder(models.Model):
                     qty = self.product_uom._compute_quantity(
                         self.product_qty, new_uom_id)
                     res['product_qty'] = max(qty, seller.min_qty)
+        return res
+
+
+class Orderpoint(models.Model):
+    _inherit = 'stock.warehouse.orderpoint'
+
+    @api.multi
+    def subtract_procurements_from_orderpoints(self):
+        # In this method we need to access the purchase order line quantity
+        # to correctly evaluate the forecast.
+        # Imagine a product with a minimum rule of 4 units and a purchase
+        # multiple of 12. The first run will generate a procurement for 4 Pc
+        # but a purchase for 12 units.
+        # Let's change the minimum rule to 5 units.
+        # The standard subtract_procurements_from_orderpoints will return 4
+        # and Odoo will create a procurement for 1 unit which will trigger a
+        # purchase of 12 due to the multiple. So the original purchase will
+        # be increased to 24 units which is wrong.
+        # This override will return 12 and no additionnal procurement will be
+        # created
+        res = super(Orderpoint, self).subtract_procurements_from_orderpoints()
+        for orderpoint in self:
+            procs = self.env['procurement.order'].search(
+                [('orderpoint_id', '=', orderpoint.id),
+                 ('state', 'not in', ['cancel', 'done'])])
+            if procs:
+                po_lines = procs.mapped('purchase_line_id').filtered(
+                    lambda x: x.state == 'draft')
+                if po_lines:
+                    qty = sum([line.product_qty for line in po_lines])
+                    precision = orderpoint.product_uom.rounding
+                    if float_compare(
+                            qty, res[orderpoint.id],
+                            precision_rounding=precision) >= 0:
+                        res[orderpoint.id] = qty
         return res
