b0e06e049d6d29e32aed0503f1134547456a6b04
Jordi Ballester <jordi.ballester@eficent.com>
2017-11-13 13:15:41 +0100

    add stock_request in 11.0

---
+++ stock_request_purchase/__manifest__.py
@@ -0,0 +1,24 @@
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Stock Request Purchase",
+    "summary": "Internal request for stock",
+    "version": "11.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://github.com/stock-logistics-warehouse",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "category": "Warehouse Management",
+    "depends": [
+        "stock_request",
+        "purchase",
+        "purchase_procurement_run_buy_hook"
+    ],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock_request_views.xml",
+        "views/purchase_order_views.xml",
+    ],
+    "installable": True,
+}
