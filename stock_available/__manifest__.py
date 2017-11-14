de44676e010f3846de0f50fc2dd96c0e747ddfb9
Kiko Peiro <francisco.peiro@factorlibre.com>
2017-11-14 17:24:13 +0100

    [MIG] stock_available: Migration to 11.0

--- stock_available/__manifest__.py
+++ stock_available/__manifest__.py
@@ -1,11 +1,10 @@
-# -*- coding: utf-8 -*-
 # Copyright 2014 Numérigraphe
 # Copyright 2016 Sodexis
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Stock available to promise',
-    'version': '10.0.1.0.1',
+    'version': '11.0.1.0.0',
     "author": "Numérigraphe, Sodexis, Odoo Community Association (OCA)",
     'category': 'Warehouse',
     'depends': ['stock'],
@@ -13,7 +12,7 @@
     'data': [
         'views/product_template_view.xml',
         'views/product_product_view.xml',
-        'views/res_config_view.xml',
+        'views/res_config_settings_views.xml',
     ],
     'installable': True,
 }
