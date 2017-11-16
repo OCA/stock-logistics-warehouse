6aec05a3bd703419109f9b43ae186f5c5c99c027
lreficent <lois.rilo@eficent.com>
2017-11-16 11:16:19 +0100

    [10.0][FIX] stock_inventory_lockdown: locking unneded locations.

--- stock_inventory_lockdown/tests/test_stock_inventory_lockdown.py
+++ stock_inventory_lockdown/tests/test_stock_inventory_lockdown.py
@@ -10,10 +10,14 @@ from odoo.addons.stock.tests.common import TestStockCommon
 class StockInventoryLocationTest(TestStockCommon):
     def setUp(self):
         super(StockInventoryLocationTest, self).setUp()
-        # Make a new location
+        # Make a new location with a parent and a child.
+        self.new_parent_location = self.env['stock.location'].create(
+            {'name': 'Test parent location',
+             'usage': 'internal'})
         self.new_location = self.env['stock.location'].create(
             {'name': 'Test location',
-             'usage': 'internal'})
+             'usage': 'internal',
+             'location_id': self.new_parent_location.id})
         self.new_sublocation = self.env['stock.location'].create(
             {'name': 'Test sublocation',
              'usage': 'internal',
@@ -23,6 +27,10 @@ class StockInventoryLocationTest(TestStockCommon):
             {'location_id': self.new_location.id,
              'product_id': self.productA.id,
              'qty': 10.0})
+        self.env['stock.quant'].create(
+            {'location_id': self.new_parent_location.id,
+             'product_id': self.productB.id,
+             'qty': 5.0})
         # Prepare an inventory
         self.inventory = self.env['stock.inventory'].create(
             {'name': 'Lock down location',
@@ -30,6 +38,16 @@ class StockInventoryLocationTest(TestStockCommon):
              'location_id': self.new_location.id})
         self.inventory.prepare_inventory()
         self.assertTrue(self.inventory.line_ids, 'The inventory is empty.')
+
+    def create_stock_move(self, product, origin_id=False, dest_id=False):
+        return self.env['stock.move'].create({
+            'name': 'Test move lock down',
+            'product_id': product.id,
+            'product_uom_qty': 10.0,
+            'product_uom': product.uom_id.id,
+            'location_id': origin_id or self.supplier_location,
+            'location_dest_id': dest_id or self.customer_location,
+        })
 
     def test_update_parent_location(self):
         """Updating the parent of a location is OK if no inv. in progress."""
@@ -73,14 +91,37 @@ class StockInventoryLocationTest(TestStockCommon):
             location=line.location_id.id).qty_available, 22.0)
 
     def test_move(self):
-        """Stock move must be forbidden during inventory"""
-        move = self.env['stock.move'].create({
-            'name': 'Test move lock down',
-            'product_id': self.productA.id,
-            'product_uom_qty': 10.0,
-            'product_uom': self.productA.uom_id.id,
-            'location_id': self.inventory.location_id.id,
-            'location_dest_id': self.customer_location
-            })
+        """Stock moves must be forbidden during inventory from/to inventoried
+        location."""
+        move1 = self.create_stock_move(
+            self.productA, origin_id=self.inventory.location_id.id)
+        move1.action_confirm()
         with self.assertRaises(ValidationError):
-            move.action_done()
+            move1.action_assign()
+            move1.action_done()
+        move2 = self.create_stock_move(
+            self.productA, dest_id=self.inventory.location_id.id)
+        with self.assertRaises(ValidationError):
+            move2.action_confirm()
+            move2.action_assign()
+            move2.action_done()
+
+    def test_move_reserved_quants(self):
+        """Shipping stock should be allowed or not depending on reserved
+        quants' locations.
+        * move1: quants are fetched from the parent location.
+        * move2: quants are fetched from 'new location' which is being
+        inventoried."""
+        move1 = self.create_stock_move(
+            self.productB, self.new_parent_location.id)
+        move1.action_confirm()
+        move1.action_assign()
+        move1.action_done()
+        self.assertEqual(
+            move1.state, 'done', 'Move has not been completed')
+        move2 = self.create_stock_move(
+            self.productA, self.new_parent_location.id)
+        move2.action_confirm()
+        with self.assertRaises(ValidationError):
+            move2.action_assign()
+            move2.action_done()
