fea7ce8524f7ecb10b5e3f256773f1d1f6794fbb
Fanha Giang <fanha99@hotmail.com>
2017-12-28 19:21:24 +0700

    11.0-mig-stock_quant_manual_assign

--- stock_quant_manual_assign/__init__.py
+++ stock_quant_manual_assign/__init__.py
@@ -2,5 +2,4 @@
 ##############################################################################
 # For copyright and license notices, see __openerp__.py file in root directory
 ##############################################################################
-from . import models
 from . import wizard
--- stock_quant_manual_assign/__manifest__.py
+++ stock_quant_manual_assign/__manifest__.py
@@ -6,7 +6,7 @@
 
 {
     "name": "Stock - Manual Quant Assignment",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "category": "Warehouse Management",
     "license": "AGPL-3",
     "author": "AvanzOSC, "
