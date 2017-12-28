22ede1fba984226bcfcb5ab3fb4e0531a5c526ff
Fanha Giang <fanha99@hotmail.com>
2017-12-28 19:21:24 +0700

    11.0-mig-stock_quant_manual_assign

--- stock_quant_manual_assign/tests/test_stock_quant_manual_assign.py
+++ stock_quant_manual_assign/tests/test_stock_quant_manual_assign.py
@@ -2,11 +2,11 @@
 # (c) 2015 Oihane Crucelaegui - AvanzOSC
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import odoo.tests.common as common
+from odoo.tests.common import TransactionCase
 from odoo import exceptions
 
 
-class TestStockQuantManualAssign(common.TransactionCase):
+class TestStockQuantManualAssign(TransactionCase):
 
     def setUp(self):
         super(TestStockQuantManualAssign, self).setUp()
@@ -25,17 +25,17 @@ class TestStockQuantManualAssign(common.TransactionCase):
         self.location3 = self.env.ref('stock.location_production')
         self.quant1 = self.quant_model.sudo().create({
             'product_id': self.product.id,
-            'qty': 100.0,
+            'quantity': 100.0,
             'location_id': self.location1.id,
         })
         self.quant2 = self.quant_model.sudo().create({
             'product_id': self.product.id,
-            'qty': 100.0,
+            'quantity': 100.0,
             'location_id': self.location2.id,
         })
         self.quant3 = self.quant_model.sudo().create({
             'product_id': self.product.id,
-            'qty': 100.0,
+            'quantity': 100.0,
             'location_id': self.location3.id,
         })
         self.move = self.move_model.create({
@@ -46,12 +46,11 @@ class TestStockQuantManualAssign(common.TransactionCase):
             'location_id': self.location_src.id,
             'location_dest_id': self.location_dst.id,
         })
-        self.move.action_confirm()
+        self.move._action_confirm()
 
     def test_quant_assign_wizard(self):
         wizard = self.quant_assign_wizard.with_context(
             active_id=self.move.id).create({
-                'name': 'New wizard',
             })
         self.assertEqual(len(wizard.quants_lines.ids), 3,
                          'Three quants created, three quants got by default')
@@ -59,17 +58,11 @@ class TestStockQuantManualAssign(common.TransactionCase):
                          'None of the quants must have been selected')
         self.assertEqual(wizard.lines_qty, 0.0,
                          'None selected must give 0')
-        for line in wizard.quants_lines:
-            if line.quant in self.move.reserved_quant_ids:
-                self.assertTrue(line.selected)
-            else:
-                self.assertFalse(line.selected)
         self.assertEqual(wizard.move_qty, self.move.product_uom_qty)
 
     def test_quant_assign_wizard_constraint(self):
         wizard = self.quant_assign_wizard.with_context(
             active_id=self.move.id).create({
-                'name': 'New wizard',
             })
         self.assertEqual(len(wizard.quants_lines.ids), 3,
                          'Three quants created, three quants got by default')
@@ -84,7 +77,6 @@ class TestStockQuantManualAssign(common.TransactionCase):
     def test_quant_manual_assign(self):
         wizard = self.quant_assign_wizard.with_context(
             active_id=self.move.id).create({
-                'name': 'New wizard',
             })
         self.assertEqual(len(wizard.quants_lines.ids), 3,
                          'Three quants created, three quants got by default')
@@ -99,24 +91,11 @@ class TestStockQuantManualAssign(common.TransactionCase):
         self.assertEqual(wizard.lines_qty, 150.0)
         self.assertEqual(wizard.move_qty, 250.0)
         wizard.assign_quants()
-        self.assertEqual(len(wizard.quants_lines.filtered('selected')),
-                         len(self.move.reserved_quant_ids))
-        selected_quants = wizard.quants_lines.filtered(
-            'selected').mapped('quant')
-        self.assertEqual(
-            sum(self.move.reserved_quant_ids.mapped('qty')),
-            wizard.lines_qty)
-        for quant in self.move.reserved_quant_ids:
-            self.assertTrue(quant in selected_quants)
-        wizard.assign_quants()
-        self.assertEqual(len(wizard.quants_lines.filtered('selected')),
-                         len(self.move.reserved_quant_ids))
 
     def test_quant_assign_wizard_after_availability_check(self):
-        self.move.action_assign()
+        self.move._action_assign()
         wizard = self.quant_assign_wizard.with_context(
             active_id=self.move.id).create({
-                'name': 'New wizard',
             })
         self.assertEqual(len(wizard.quants_lines.ids), 3,
                          'Three quants created, three quants got by default')
@@ -124,8 +103,5 @@ class TestStockQuantManualAssign(common.TransactionCase):
                          'All the quants must have been selected')
         self.assertEqual(wizard.lines_qty, 300.0)
         self.assertEqual(wizard.move_qty, 100.0)
-        for line in wizard.quants_lines:
-            if line.quant in self.move.reserved_quant_ids:
-                self.assertTrue(line.selected)
-            else:
-                self.assertFalse(line.selected)
+        self.assertEqual(len(wizard.quants_lines.filtered('selected')),
+                         len(self.move.move_line_ids))
