f53705d905404ae39689240bdea8d3208fcbaa55
Fanha Giang <fanha99@hotmail.com>
2017-12-28 19:21:24 +0700

    11.0-mig-stock_quant_manual_assign

--- stock_quant_manual_assign/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
-from . import stock_picking
