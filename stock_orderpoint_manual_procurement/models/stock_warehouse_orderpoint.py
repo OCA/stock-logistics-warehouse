8353b807706ae45f1d45e9b5680d6adb65379d62
lreficent <lois.rilo@eficent.com>
2018-01-02 13:50:56 +0100

    [10.0][REW] remove dependency with stock_orderpoint_uom

--- stock_orderpoint_manual_procurement/models/stock_warehouse_orderpoint.py
+++ stock_orderpoint_manual_procurement/models/stock_warehouse_orderpoint.py
@@ -26,40 +26,39 @@ class StockWarehouseOrderpoint(models.Model):
     )
 
     @api.multi
+    def _get_procure_recommended_qty(self, virtual_qty, op_qtys):
+        self.ensure_one()
+        procure_recommended_qty = 0.0
+        qty = max(self.product_min_qty, self.product_max_qty) - virtual_qty
+        remainder = \
+            self.qty_multiple > 0 and qty % self.qty_multiple or 0.0
+        if float_compare(
+                remainder, 0.0,
+                precision_rounding=self.product_uom.rounding) > 0:
+            qty += self.qty_multiple - remainder
+
+        if float_compare(
+                qty, 0.0,
+                precision_rounding=self.product_uom.rounding) <= 0:
+            return procure_recommended_qty
+
+        qty -= op_qtys[self.id]
+        qty_rounded = float_round(
+            qty, precision_rounding=self.product_uom.rounding)
+        if qty_rounded > 0:
+            procure_recommended_qty = qty_rounded
+        return procure_recommended_qty
+
+    @api.multi
     @api.depends("product_min_qty", "product_id", "qty_multiple")
     def _compute_procure_recommended(self):
         op_qtys = self.subtract_procurements_from_orderpoints()
         for op in self:
             op.procure_recommended_date = op._get_date_planned(
                 datetime.today())
-            procure_recommended_qty = 0.0
             virtual_qty = op.with_context(
                 location=op.location_id.id).product_id.virtual_available
             if float_compare(virtual_qty, op.product_min_qty,
                              precision_rounding=op.product_uom.rounding) < 0:
-                qty = max(op.product_min_qty, op.product_max_qty) - virtual_qty
-                remainder = \
-                    op.qty_multiple > 0 and qty % op.qty_multiple or 0.0
-                if float_compare(
-                        remainder, 0.0,
-                        precision_rounding=op.product_uom.rounding) > 0:
-                    qty += op.qty_multiple - remainder
-
-                if float_compare(
-                        qty, 0.0,
-                        precision_rounding=op.product_uom.rounding) <= 0:
-                    continue
-
-                qty -= op_qtys[op.id]
-                qty_rounded = float_round(
-                    qty, precision_rounding=op.product_uom.rounding)
-                if qty_rounded > 0:
-                    procure_recommended_qty = qty_rounded
-                if op.procure_uom_id:
-                    product_qty = op.procure_uom_id._compute_qty(
-                        op.product_id.uom_id.id, procure_recommended_qty,
-                        op.procure_uom_id.id)
-                else:
-                    product_qty = procure_recommended_qty
-
-                op.procure_recommended_qty = product_qty
+                op.procure_recommended_qty = op._get_procure_recommended_qty(
+                    virtual_qty, op_qtys)
