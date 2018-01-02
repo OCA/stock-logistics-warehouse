fcb467129fd979a64c18f9f94a512148260d49e6
lreficent <lois.rilo@eficent.com>
2018-01-02 13:53:01 +0100

    [10.0][ADD] stock_orderpoint_manual_procurement_uom

---
+++ stock_orderpoint_manual_procurement_uom/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Stock Orderpoint Manual Procurement UoM",
+    "summary": "Glue module for stock_orderpoint_uom and "
+               "stock_orderpoint_manual_procurement",
+    "version": "10.0.1.0.0",
+    "author": "Eficent, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "depends": [
+        "stock_orderpoint_uom",
+        "stock_orderpoint_manual_procurement",
+    ],
+    "data": [],
+    "license": "AGPL-3",
+    'installable': True,
+    'application': False,
+    'auto-install': True,
+}
---
+++ stock_orderpoint_manual_procurement_uom/wizards/make_procurement_orderpoint.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class MakeProcurementOrderpoint(models.TransientModel):
+    _inherit = 'make.procurement.orderpoint'
+
+    @api.model
+    def _prepare_item(self, orderpoint):
+        vals = super(MakeProcurementOrderpoint, self)._prepare_item(orderpoint)
+        if orderpoint.procure_uom_id:
+            product_uom = orderpoint.procure_uom_id
+            vals['uom_id'] = product_uom.id
+        return vals
+
+
+class MakeProcurementOrderpointItem(models.TransientModel):
+    _inherit = 'make.procurement.orderpoint.item'
+
+    @api.multi
+    @api.onchange('uom_id')
+    def onchange_uom_id(self):
+        for rec in self:
+            uom = rec.orderpoint_id.procure_uom_id or \
+                rec.orderpoint_id.product_uom
+            rec.qty = uom._compute_quantity(
+                rec.orderpoint_id.procure_recommended_qty,
+                rec.uom_id)
