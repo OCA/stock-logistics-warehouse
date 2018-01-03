c5fd741bf156ce4233332a047eb6e32b5ff13c50
lreficent <lois.rilo@eficent.com>
2018-01-03 12:06:56 +0100

    [10.0][IMP] fix api issues and clean up code

--- stock_cycle_count/models/stock_cycle_count_rule.py
+++ stock_cycle_count/models/stock_cycle_count_rule.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models, _
-from odoo.exceptions import UserError
+from odoo.exceptions import UserError, ValidationError
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
 from datetime import timedelta, datetime
 
@@ -13,9 +13,10 @@ class StockCycleCountRule(models.Model):
     _name = 'stock.cycle.count.rule'
     _description = "Stock Cycle Counts Rules"
 
-    @api.one
-    def _compute_currency(self):
-        self.currency_id = self.env.user.company_id.currency_id
+    @api.multi
+    def _compute_currency_id(self):
+        for rec in self:
+            rec.currency_id = self.env.user.company_id.currency_id
 
     @api.model
     def _selection_rule_types(self):
@@ -25,26 +26,27 @@ class StockCycleCountRule(models.Model):
             ('accuracy', _('Minimum Accuracy')),
             ('zero', _('Zero Confirmation'))]
 
-    @api.one
+    @api.multi
     @api.constrains('rule_type', 'warehouse_ids')
     def _check_zero_rule(self):
-        if self.rule_type == 'zero' and len(self.warehouse_ids) > 1:
-            raise UserError(
-                _('Zero confirmation rules can only have one warehouse '
-                  'assigned.')
-            )
-        if self.rule_type == 'zero':
-            zero_rule = self.search([
-                ('rule_type', '=', 'zero'),
-                ('warehouse_ids', '=', self.warehouse_ids.id)])
-            if len(zero_rule) > 1:
-                raise UserError(
-                    _('You can only have one zero confirmation rule per '
-                      'warehouse.')
+        for rec in self:
+            if rec.rule_type == 'zero' and len(rec.warehouse_ids) > 1:
+                raise ValidationError(
+                    _('Zero confirmation rules can only have one warehouse '
+                      'assigned.')
                 )
+            if rec.rule_type == 'zero':
+                zero_rule = self.search([
+                    ('rule_type', '=', 'zero'),
+                    ('warehouse_ids', '=', rec.warehouse_ids.id)])
+                if len(zero_rule) > 1:
+                    raise ValidationError(
+                        _('You can only have one zero confirmation rule per '
+                          'warehouse.')
+                    )
 
     @api.onchange('rule_type')
-    def _get_rule_description(self):
+    def _compute_rule_description(self):
         if self.rule_type == 'periodic':
             self.rule_description = _('Ensures that at least a defined number '
                                       'of counts in a given period will '
@@ -66,17 +68,19 @@ class StockCycleCountRule(models.Model):
         else:
             self.rule_description = _('(No description provided.)')
 
+    @api.multi
     @api.constrains('periodic_qty_per_period', 'periodic_count_period')
     def _check_negative_periodic(self):
-        if self.periodic_qty_per_period < 1:
-            raise UserError(
-                _('You cannot define a negative or null number of counts per '
-                  'period.')
-            )
-        if self.periodic_count_period < 0:
-            raise UserError(
-                _('You cannot define a negative period.')
-            )
+        for rec in self:
+            if rec.periodic_qty_per_period < 1:
+                raise ValidationError(
+                    _('You cannot define a negative or null number of counts '
+                      'per period.')
+                )
+            if rec.periodic_count_period < 0:
+                raise ValidationError(
+                    _('You cannot define a negative period.')
+                )
 
     @api.onchange('location_ids')
     def _get_warehouses(self):
@@ -87,36 +91,45 @@ class StockCycleCountRule(models.Model):
         wh_ids = list(set(wh_ids))
         self.warehouse_ids = self.env['stock.warehouse'].browse(wh_ids)
 
-    name = fields.Char('Name', required=True)
-    rule_type = fields.Selection(selection="_selection_rule_types",
-                                 string='Type of rule',
-                                 required=True)
-    rule_description = fields.Char(string='Rule Description',
-                                   compute='_get_rule_description')
-    active = fields.Boolean(string='Active', default=True)
-    periodic_qty_per_period = fields.Integer(string='Counts per period',
-                                             default=1)
+    name = fields.Char(required=True)
+    rule_type = fields.Selection(
+        selection="_selection_rule_types",
+        string='Type of rule', required=True,
+    )
+    rule_description = fields.Char(
+        string='Rule Description', compute='_compute_rule_description',
+    )
+    active = fields.Boolean(default=True)
+    periodic_qty_per_period = fields.Integer(
+        string='Counts per period', default=1,
+    )
     periodic_count_period = fields.Integer(string='Period in days')
     turnover_inventory_value_threshold = fields.Float(
-        string='Turnover Inventory Value Threshold')
-    currency_id = fields.Many2one(comodel_name='res.currency',
-                                  string='Currency',
-                                  compute='_compute_currency')
-    accuracy_threshold = fields.Float(string='Minimum Accuracy Threshold',
-                                      digits=(3, 2))
+        string='Turnover Inventory Value Threshold',
+    )
+    currency_id = fields.Many2one(
+        comodel_name='res.currency', string='Currency',
+        compute='_compute_currency_id',
+    )
+    accuracy_threshold = fields.Float(
+        string='Minimum Accuracy Threshold', digits=(3, 2),
+    )
     apply_in = fields.Selection(
         string='Apply this rule in:',
         selection=[('warehouse', 'Selected warehouses'),
                    ('location', 'Selected Location Zones.')],
-        default='warehouse')
+        default='warehouse',
+    )
     warehouse_ids = fields.Many2many(
         comodel_name='stock.warehouse',
         relation='warehouse_cycle_count_rule_rel', column1='rule_id',
-        column2='warehouse_id', string='Warehouses where applied')
+        column2='warehouse_id', string='Warehouses where applied',
+    )
     location_ids = fields.Many2many(
         comodel_name='stock.location',
         relation='location_cycle_count_rule_rel', column1='rule_id',
-        column2='location_id', string='Zones where applied')
+        column2='location_id', string='Zones where applied',
+    )
 
     def compute_rule(self, locs):
         if self.rule_type == 'periodic':
@@ -211,8 +224,9 @@ class StockCycleCountRule(models.Model):
                 cycle_counts.append(cycle_count)
         return cycle_counts
 
-    @api.model
+    @api.multi
     def _compute_rule_accuracy(self, locs):
+        self.ensure_one()
         cycle_counts = []
         for loc in locs:
             if loc.loc_accuracy < self.accuracy_threshold:
