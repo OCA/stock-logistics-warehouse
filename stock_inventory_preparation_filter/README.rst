62e14ce0fe0615058e6f2b37b89aacde667c4917
David <david.vidal@tecnativa.com>
2018-01-04 20:18:07 +0100

    [MIG] stock_inventory_preparation_filter: Migration to 10.0

--- stock_inventory_preparation_filter/README.rst
+++ stock_inventory_preparation_filter/README.rst
@@ -20,7 +20,7 @@ Usage
 
 .. image:: https://odoo-community.org/website/image/ir.attachment/5784_f2813bd/datas
    :alt: Try me on Runbot
-   :target: https://runbot.odoo-community.org/runbot/153/8.0
+   :target: https://runbot.odoo-community.org/runbot/153/10.0
 
 Bug Tracker
 ===========
@@ -28,11 +28,7 @@ Bug Tracker
 Bugs are tracked on `GitHub Issues
 <https://github.com/OCA/stock-logistics-warehouse/issues>`_. In case of trouble, please
 check there if your issue has already been reported. If you spotted it first,
-help us smashing it by providing a detailed and welcomed `feedback
-<https://github.com/OCA/
-stock-logistics-warehouse/issues/new?body=module:%20
-stock_inventory_preparation_filter%0Aversion:%20
-8.0%0A%0A**Steps%20to%20reproduce**%0A-%20...%0A%0A**Current%20behavior**%0A%0A**Expected%20behavior**>`_.
+help us smash it by providing detailed and welcomed feedback.
 
 Credits
 =======
@@ -41,7 +37,8 @@ Contributors
 ------------
 
 * Oihane Crucelaegui <oihanecrucelaegi@avanzosc.es>
-* Pedro M. Baeza <pedro.baeza@serviciosbaeza.com>
+* Pedro M. Baeza <pedro.baeza@tecnativa.com>
+* David Vidal <david.vidal@tecnativa.com>
 
 Maintainer
 ----------
--- stock_inventory_preparation_filter/tests/test_stock_inventory_preparation_filter.py
+++ stock_inventory_preparation_filter/tests/test_stock_inventory_preparation_filter.py
@@ -1,89 +1,101 @@
-##############################################################################
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as published
-#    by the Free Software Foundation, either version 3 of the License, or
-#    (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-import openerp.tests.common as common
+# -*- coding: utf-8 -*-
+# Copyright 2015 AvanzOSC - Oihane Crucelaegi
+# Copyright 2015 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.tests import common
 
 
+@common.at_install(False)
+@common.post_install(True)
 class TestStockInventoryPreparationFilterCategories(common.TransactionCase):
 
     def setUp(self):
         super(TestStockInventoryPreparationFilterCategories, self).setUp()
         self.inventory_model = self.env['stock.inventory']
-        # Create a category
-        self.category = self.env['product.category'].create(
-            {
-                'name': 'Category for inventory',
-                'type': 'normal',
-            })
+        # Create some categories
+        self.category = self.env['product.category'].create({
+            'name': 'Category for inventory',
+            'type': 'normal',
+        })
+        self.category2 = self.env['product.category'].create({
+            'name': 'Category for inventory 2',
+            'type': 'normal',
+        })
         # Create some products in the category
-        self.product1 = self.env['product.product'].create(
-            {
-                'name': 'Product for inventory 1',
-                'type': 'product',
-                'categ_id': self.category.id,
-                'default_code': 'PROD1',
-            }
-        )
-        self.product2 = self.env['product.product'].create(
-            {
-                'name': 'Product for inventory 2',
-                'type': 'product',
-                'categ_id': self.category.id,
-                'default_code': 'PROD2',
-            }
-        )
+        self.product1 = self.env['product.product'].create({
+            'name': 'Product for inventory 1',
+            'type': 'product',
+            'categ_id': self.category.id,
+            'default_code': 'PROD1-TEST',
+        })
+        self.product2 = self.env['product.product'].create({
+            'name': 'Product for inventory 2',
+            'type': 'product',
+            'categ_id': self.category.id,
+            'default_code': 'PROD2-TEST',
+        })
+        self.product3 = self.env['product.product'].create({
+            'name': 'Product for inventory 3',
+            'type': 'product',
+            'categ_id': self.category.id,
+            'default_code': 'PROD3-TEST',
+        })
+        self.product_lot = self.env['product.product'].create({
+            'name': 'Product for inventory with lots',
+            'type': 'product',
+            'categ_id': self.category2.id,
+        })
+        self.lot = self.env['stock.production.lot'].create({
+            'name': 'Lot test',
+            'product_id': self.product_lot.id,
+        })
+        # Add user to lot tracking group
+        self.env.user.groups_id = [
+            (4, self.env.ref('stock.group_production_lot').id),
+        ]
         # And have some stock in a location
-        self.location = self.env['stock.location'].create(
-            {
-                'name': 'Inventory tests',
-                'usage': 'internal',
-            }
-        )
-        inventory = self.inventory_model.create(
-            {
-                'name': 'Product1 inventory',
-                'filter': 'product',
-                'line_ids': [
-                    (0, 0, {
-                        'product_id': self.product1.id,
-                        'product_uom_id': self.env.ref(
-                            "product.product_uom_unit").id,
-                        'product_qty': 2.0,
-                        'location_id': self.location.id,
-                    }),
-                    (0, 0, {
-                        'product_id': self.product2.id,
-                        'product_uom_id': self.env.ref(
-                            "product.product_uom_unit").id,
-                        'product_qty': 4.0,
-                        'location_id': self.location.id,
-                    }),
-                ],
-            })
+        self.location = self.env['stock.location'].create({
+            'name': 'Inventory tests',
+            'usage': 'internal',
+        })
+        inventory = self.inventory_model.create({
+            'name': 'Product1 inventory',
+            'filter': 'product',
+            'line_ids': [
+                (0, 0, {
+                    'product_id': self.product1.id,
+                    'product_uom_id': self.env.ref(
+                        "product.product_uom_unit").id,
+                    'product_qty': 2.0,
+                    'location_id': self.location.id,
+                }),
+                (0, 0, {
+                    'product_id': self.product2.id,
+                    'product_uom_id': self.env.ref(
+                        "product.product_uom_unit").id,
+                    'product_qty': 4.0,
+                    'location_id': self.location.id,
+                }),
+                (0, 0, {
+                    'product_id': self.product_lot.id,
+                    'product_uom_id': self.env.ref(
+                        "product.product_uom_unit").id,
+                    'product_qty': 6.0,
+                    'location_id': self.location.id,
+                    'prod_lot_id': self.lot.id,
+                }),
+            ],
+        })
         inventory.action_done()
 
     def test_inventory_category_filter(self):
-        inventory = self.inventory_model.create(
-            {
-                'name': 'Category inventory',
-                'filter': 'categories',
-                'location_id': self.location.id,
-                'categ_ids': [(6, 0, [self.category.id])],
-            }
-        )
+        inventory = self.inventory_model.create({
+            'name': 'Category inventory',
+            'filter': 'categories',
+            'location_id': self.location.id,
+            'categ_ids': [(6, 0, [self.category.id])],
+        })
         inventory.prepare_inventory()
         self.assertEqual(len(inventory.line_ids), 2)
         line1 = inventory.line_ids[0]
@@ -115,26 +127,49 @@ class TestStockInventoryPreparationFilterCategories(common.TransactionCase):
         self.assertEqual(line2.theoretical_qty, 4.0)
         self.assertEqual(line2.location_id, self.location)
 
-    def test_inventory_empty_filter(self):
+    def test_inventory_lots_filter(self):
         inventory = self.inventory_model.create(
             {
                 'name': 'Products inventory',
-                'filter': 'empty',
+                'filter': 'lots',
                 'location_id': self.location.id,
-                'empty_line_ids': [
-                    (0, 0, {
-                        'product_code': 'PROD1',
-                        'product_qty': 3.0,
-                    }),
-                    (0, 0, {
-                        'product_code': 'PROD2',
-                        'product_qty': 7.0,
-                    }),
-                ],
+                'lot_ids': [(6, 0, [self.lot.id, ])],
             }
         )
         inventory.prepare_inventory()
-        self.assertEqual(len(inventory.line_ids), 2)
+        self.assertEqual(len(inventory.line_ids), 1)
+        line1 = inventory.line_ids[0]
+        self.assertEqual(line1.product_id, self.product_lot)
+        self.assertEqual(line1.prod_lot_id, self.lot)
+        self.assertEqual(line1.theoretical_qty, 6.0)
+        self.assertEqual(line1.location_id, self.location)
+
+    def test_inventory_empty_filter(self):
+        inventory = self.inventory_model.create({
+            'name': 'Products inventory',
+            'filter': 'empty',
+            'location_id': self.location.id,
+            'empty_line_ids': [
+                (0, 0, {
+                    'product_code': 'PROD1-TEST',
+                    'product_qty': 3.0,
+                }),
+                (0, 0, {
+                    'product_code': 'PROD2-TEST',
+                    'product_qty': 7.0,
+                }),
+                (0, 0, {
+                    'product_code': 'PROD3-TEST',
+                    'product_qty': 5.0,
+                }),
+                (0, 0, {
+                    'product_code': 'UNEXISTING-CODE',
+                    'product_qty': 0.0,
+                }),
+            ],
+        })
+        inventory.prepare_inventory()
+        self.assertEqual(len(inventory.line_ids), 3)
         line1 = inventory.line_ids[0]
         self.assertEqual(line1.product_id, self.product1)
         self.assertEqual(line1.theoretical_qty, 2.0)
@@ -145,3 +180,8 @@ class TestStockInventoryPreparationFilterCategories(common.TransactionCase):
         self.assertEqual(line2.theoretical_qty, 4.0)
         self.assertEqual(line2.product_qty, 7.0)
         self.assertEqual(line2.location_id, self.location)
+        line3 = inventory.line_ids[2]
+        self.assertEqual(line3.product_id, self.product3)
+        self.assertEqual(line3.theoretical_qty, 0.0)
+        self.assertEqual(line3.product_qty, 5.0)
+        self.assertEqual(line3.location_id, self.location)
