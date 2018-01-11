24694947e71f01fb1754d5a18f4183e39ffc993c
lreficent <lois.rilo@eficent.com>
2018-01-11 12:27:15 +0100

    [10.0][MIG] procurement_auto_create_group

--- procurement_auto_create_group/models/procurement_rule.py
+++ procurement_auto_create_group/models/procurement_rule.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-from openerp import api, fields, models
+
+from odoo import api, fields, models
 
 
 class ProcurementRule(models.Model):
