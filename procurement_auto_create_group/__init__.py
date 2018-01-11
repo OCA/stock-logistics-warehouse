8f13b7bcc147a7fd0e9516a1559e874fe2885961
lreficent <lois.rilo@eficent.com>
2018-01-11 12:27:15 +0100

    [10.0][MIG] procurement_auto_create_group

--- procurement_auto_create_group/__init__.py
+++ procurement_auto_create_group/__init__.py
@@ -1,5 +1,4 @@
 # # -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
--- procurement_auto_create_group/tests/__init__.py
+++ procurement_auto_create_group/tests/__init__.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-# © 2017 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_auto_create
