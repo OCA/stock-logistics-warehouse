07e279935ffedfe8c029efc26ab9dce46e0e7156
mpanarin <m.panarin@mobilunity.com>
2018-02-14 14:29:41 +0200

    [MIG] stock_warehouse_orderpoint_stock_info: Migration to Odoo 11

--- stock_warehouse_orderpoint_stock_info/__manifest__.py
+++ stock_warehouse_orderpoint_stock_info/__manifest__.py
@@ -1,14 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 OdooMRP Team
 # Copyright 2016 AvanzOSC
 # Copyright 2016 Pedro M. Baeza <pedro.baeza@tecnativa.com>
 # Copyright 2016 Serpent Consulting Services Pvt. Ltd.
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Stock Warehouse Orderpoint Stock Info",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "depends": [
         "stock",
     ],
@@ -16,12 +16,10 @@
               "AvanzOSC, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
-    "website": "http://www.odoomrp.com",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
     "license": "AGPL-3",
     "data": [
         "views/stock_warehouse_orderpoint_view.xml",
     ],
-    "installable": True,
-    "application": False,
 }
