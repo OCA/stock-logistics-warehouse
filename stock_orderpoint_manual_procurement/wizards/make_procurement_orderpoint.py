358d30af2f2b30d71cb67d1eaec389fc924f2565
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-02-27 21:08:34 +0530

    [MIG] stock_orderpoint_manual_procurement: Migration to 11.0

--- stock_orderpoint_manual_procurement/wizards/make_procurement_orderpoint.py
+++ stock_orderpoint_manual_procurement/wizards/make_procurement_orderpoint.py
@@ -1,10 +1,9 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016-17 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models, _
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError, ValidationError
 
 
 class MakeProcurementOrderpoint(models.TransientModel):
@@ -20,7 +19,6 @@ class MakeProcurementOrderpoint(models.TransientModel):
         return {
             'qty': orderpoint.procure_recommended_qty,
             'uom_id': orderpoint.product_uom.id,
-            'date_planned': orderpoint.procure_recommended_date,
             'orderpoint_id': orderpoint.id,
             'product_id': orderpoint.product_id.id,
             'warehouse_id': orderpoint.warehouse_id.id,
@@ -29,8 +27,7 @@ class MakeProcurementOrderpoint(models.TransientModel):
 
     @api.model
     def default_get(self, fields):
-        res = super(MakeProcurementOrderpoint, self).default_get(
-            fields)
+        res = super(MakeProcurementOrderpoint, self).default_get(fields)
         orderpoint_obj = self.env['stock.warehouse.orderpoint']
         orderpoint_ids = self.env.context['active_ids'] or []
         active_model = self.env.context['active_model']
@@ -49,20 +46,30 @@ class MakeProcurementOrderpoint(models.TransientModel):
     @api.multi
     def make_procurement(self):
         self.ensure_one()
-        res = []
+        errors = []
         for item in self.item_ids:
-            data = item._prepare_procurement()
-            procurement = self.env['procurement.order'].create(data)
-            res.append(procurement.id)
+            if not item.qty:
+                raise ValidationError(_("Quantity must be positive."))
+            if not item.orderpoint_id:
+                raise ValidationError(_("No reordering rule found!"))
+            values = item.orderpoint_id._prepare_procurement_values(item.qty)
+            # Run procurement
+            try:
+                self.env['procurement.group'].run(
+                    item.orderpoint_id.product_id,
+                    item.qty,
+                    item.orderpoint_id.product_uom,
+                    item.orderpoint_id.location_id,
+                    item.orderpoint_id.name,
+                    item.orderpoint_id.name,
+                    values
+                )
+            except UserError as error:
+                    errors.append(error.name)
+            if errors:
+                raise UserError('\n'.join(errors))
 
-        return {
-            'name': _('Created Procurements'),
-            'domain': [('id', 'in', res)],
-            'view_type': 'form',
-            'view_mode': 'tree,form',
-            'res_model': 'procurement.order',
-            'type': 'ir.actions.act_window',
-        }
+        return {'type': 'ir.actions.act_window_close'}
 
 
 class MakeProcurementOrderpointItem(models.TransientModel):
@@ -76,39 +83,21 @@ class MakeProcurementOrderpointItem(models.TransientModel):
     qty = fields.Float(string='Quantity', required=True)
 
     uom_id = fields.Many2one(string='Unit of Measure',
-                             comodel_name='product.uom', required=True)
-    date_planned = fields.Date(string='Planned Date', required=True)
+                             comodel_name='product.uom')
+    date_planned = fields.Date(string='Planned Date', required=False)
 
     orderpoint_id = fields.Many2one(string='Reordering rule',
                                     comodel_name='stock.warehouse.orderpoint',
-                                    required=True, readonly=True)
+                                    readonly=False)
     product_id = fields.Many2one(string='Product',
                                  comodel_name='product.product',
-                                 required=True, readonly=True)
+                                 readonly=True)
     warehouse_id = fields.Many2one(string='Warehouse',
                                    comodel_name='stock.warehouse',
                                    readonly=True)
     location_id = fields.Many2one(string='Location',
                                   comodel_name='stock.location',
                                   readonly=True)
-
-    @api.multi
-    def _prepare_procurement(self):
-        if not self.qty:
-            raise ValidationError(_("Quantity must be positive."))
-        return {
-            'name': self.orderpoint_id.name,
-            'date_planned': self.date_planned,
-            'product_id': self.product_id.id,
-            'product_qty': self.qty,
-            'product_uom': self.uom_id.id,
-            'warehouse_id': self.warehouse_id.id,
-            'location_id': self.location_id.id,
-            'company_id': self.orderpoint_id.company_id.id,
-            'orderpoint_id': self.orderpoint_id.id,
-            'origin': self.orderpoint_id.name,
-            'group_id': self.orderpoint_id.group_id.id,
-        }
 
     @api.multi
     @api.onchange('uom_id')
