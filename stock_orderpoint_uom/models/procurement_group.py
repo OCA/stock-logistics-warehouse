23d41f184309f7b6ae5e610ae7f55375ed0e1d16
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-02-27 21:21:23 +0530

    [MIG] stock_orderpoint_uom: Migration to 11.0

---
+++ stock_orderpoint_uom/models/procurement_group.py
@@ -0,0 +1,20 @@
+# Copyright (C) 2018 - TODAY, Open Source Integrators
+#   (http://www.opensourceintegrators.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ProcurementGroup(models.Model):
+    _inherit = "procurement.group"
+
+    @api.model
+    def run(self, product_id, product_qty, product_uom, location_id, name,
+            origin, values):
+        if 'orderpoint_id' in values:
+            orderpoint = values.get('orderpoint_id')
+            product_qty = orderpoint.product_uom._compute_quantity(
+                product_qty, orderpoint.procure_uom_id)
+        return super(ProcurementGroup, self).run(product_id, product_qty,
+                                                 product_uom, location_id,
+                                                 name, origin, values)
