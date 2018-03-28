723293be4b2b1b6ede82d22c61f94a17057b9cd4
Denis Roussel <denis.roussel@acsone.eu>
2018-03-28 14:07:36 +0200

    [10.0][FIX] purchase_packaging: Fix wrong domain

--- purchase_packaging/__manifest__.py
+++ purchase_packaging/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Purchase Packaging",
-    "version": "10.0.1.0.2",
+    "version": "10.0.1.0.3",
     "author": 'ACSONE SA/NV, '
               'Odoo Community Association (OCA)',
     "category": "Warehouse",
