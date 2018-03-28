2bf4b0e49d22d5416bbc9e72f66f7943e6a3614e
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2018-03-28 21:11:22 +0200

    fix test
    and apply suggested changes in the api.onchange specification

--- stock_quant_partner_info/models/stock_quant.py
+++ stock_quant_partner_info/models/stock_quant.py
@@ -12,14 +12,16 @@ class StockQuant(models.Model):
         comodel_name='res.partner', compute='_compute_partner_id',
         string='Partner', store=True)
 
-    @api.depends('location_id', 'history_ids')
+    @api.depends('history_ids.picking_id.partner_id.commercial_partner_id',
+                 'history_ids.location_id.usage',
+                 'history_ids.date')
     def _compute_partner_id(self):
         for quant in self:
             moves = quant.history_ids.sorted(key=lambda m: m.date)
             dest_partner = False
             if moves:
                 move = moves[-1]
-                if move.location_id in ('customer', 'supplier'):
+                if move.location_id.usage in ('customer', 'supplier'):
                     picking = move.picking_id
                     dest_partner = picking.partner_id.commercial_partner_id
             quant.dest_partner_id = dest_partner
