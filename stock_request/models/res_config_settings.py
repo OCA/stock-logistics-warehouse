f1015b744b466b3e85e049ba951ca3d0b6cd2c7e
Enric Tobella <etobella@creublanca.es>
2018-04-09 10:32:58 +0200

    [IMP] stock_request: Add stock request order

---
+++ stock_request/models/res_config_settings.py
@@ -0,0 +1,14 @@
+# Copyright 2018 Creu Blanca
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = 'res.config.settings'
+
+    group_stock_request_order = fields.Boolean(
+        implied_group='stock_request.group_stock_request_order')
+
+    module_stock_request_purchase = fields.Boolean(
+        string='Stock requests for purchase')
