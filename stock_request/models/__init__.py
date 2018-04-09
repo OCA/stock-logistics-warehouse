52cc35c469473fc30dcd61bc4aa84ac3ecb2d977
Enric Tobella <etobella@creublanca.es>
2018-04-09 10:32:58 +0200

    [IMP] stock_request: Add stock request order

--- stock_request/models/__init__.py
+++ stock_request/models/__init__.py
@@ -1,7 +1,9 @@
 
 from . import stock_request
 from . import stock_request_allocation
+from . import stock_request_order
 from . import stock_move
 from . import stock_picking
 from . import procurement_rule
 from . import stock_move_line
+from . import res_config_settings
