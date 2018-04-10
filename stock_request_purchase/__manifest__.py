6a62c07d668d23d03d737d8bef5757079763a9a4
Enric Tobella <etobella@creublanca.es>
2018-04-10 17:54:27 +0200

    [IMP] stock_request_purchase: Add Stock request orders. Change logo

--- stock_request_purchase/__manifest__.py
+++ stock_request_purchase/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Request Purchase",
     "summary": "Internal request for stock",
-    "version": "11.0.1.1.0",
+    "version": "11.0.2.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/stock-logistics-warehouse",
     "author": "Eficent, "
@@ -17,6 +17,7 @@
     "data": [
         "security/ir.model.access.csv",
         "views/stock_request_views.xml",
+        "views/stock_request_order_views.xml",
         "views/purchase_order_views.xml",
     ],
     "installable": True,
