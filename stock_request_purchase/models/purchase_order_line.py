59c85080ad54dbe16d671321d7a037889df57c03
mreficent <miquel.raich@eficent.com>
2018-04-11 17:21:15 +0200

    [FIX] Various fixes: - Error with the sequence number. - Visible texts that should be in uppercases. - order_id should only be visible if group_stock_request_order   option is enabled. - adds more tests - adds consistency between models company-wise

--- stock_request_purchase/models/purchase_order_line.py
+++ stock_request_purchase/models/purchase_order_line.py
@@ -1,7 +1,8 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import api, fields, models
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
 
 
 class PurchaseOrderLine(models.Model):
@@ -21,3 +22,12 @@ class PurchaseOrderLine(models.Model):
                     'requested_product_uom_qty': request.product_qty,
                 }) for request in self.stock_request_ids]
         return res
+
+    @api.constrains('stock_request_ids')
+    def _check_purchase_company_constrains(self):
+        if any(any(req.company_id != pol.company_id for
+                   req in pol.stock_request_ids) for pol in self):
+                raise ValidationError(
+                    _('You cannot link a purchase order line '
+                      'to a stock request that belongs to '
+                      'another company.'))
