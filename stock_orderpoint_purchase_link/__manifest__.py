a2619564a72f5489040fef810c8c8cc9cda94ac6
Bhavesh Odedra <bodedra@ursainfosystems.com>
2018-05-04 19:21:57 +0530

    Revert "[Add] ddmrp dependancy"
    This reverts commit 134ba388e121772221f7e49a6793912e13817249.

--- stock_orderpoint_purchase_link/__manifest__.py
+++ stock_orderpoint_purchase_link/__manifest__.py
@@ -10,7 +10,7 @@
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
-    "depends": ["stock", "purchase", "ddmrp"],
+    "depends": ["stock", "purchase"],
     "data": [
     ],
     "installable": True,
