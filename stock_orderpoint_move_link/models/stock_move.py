df26debbc7a1a7cb6c2f402b3e3ec25c1c8316d9
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-05-16 01:47:07 +0200

    add stock_orderpoint_move_link and stock_orderpoint_purchase_link

---
+++ stock_orderpoint_move_link/models/stock_move.py
@@ -0,0 +1,25 @@
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class StockMove(models.Model):
+    _inherit = 'stock.move'
+
+    orderpoint_ids = fields.Many2many(
+        comodel_name='stock.warehouse.orderpoint',
+        string='Linked Reordering Rules',
+    )
+
+    def _prepare_procurement_values(self):
+        res = super(StockMove, self)._prepare_procurement_values()
+        if self.orderpoint_ids:
+            res['orderpoint_ids'] = self.orderpoint_ids
+        return res
+
+    def _merge_moves_fields(self):
+        res = super(StockMove, self)._merge_moves_fields()
+        res['orderpoint_ids'] = [(4, m.id)
+                                 for m in self.mapped('orderpoint_ids')]
+        return res
