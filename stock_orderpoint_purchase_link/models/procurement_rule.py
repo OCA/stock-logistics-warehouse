b1c61973e2b37bfa272aac93a5f04c47bdff8731
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-05-16 01:47:07 +0200

    add stock_orderpoint_move_link and stock_orderpoint_purchase_link

---
+++ stock_orderpoint_purchase_link/models/procurement_rule.py
@@ -0,0 +1,31 @@
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import models
+
+
+class ProcurementRule(models.Model):
+    _inherit = 'procurement.rule'
+
+    def _prepare_purchase_order_line(self, product_id, product_qty,
+                                     product_uom, values, po, supplier):
+        vals = super(ProcurementRule, self)._prepare_purchase_order_line(
+            product_id, product_qty, product_uom, values, po, supplier)
+        # If the procurement was run directly by a reordering rule.
+        if 'orderpoint_id' in values:
+            vals['orderpoint_ids'] = [
+                (4, values['orderpoint_id'].id)]
+        # If the procurement was run by a stock move.
+        elif 'orderpoint_ids' in values:
+            vals['orderpoint_ids'] = [(4, o.id)
+                                      for o in values['orderpoint_ids']]
+        return vals
+
+    def _update_purchase_order_line(self, product_id, product_qty, product_uom,
+                                    values, line, partner):
+        vals = super(ProcurementRule, self)._update_purchase_order_line(
+            product_id, product_qty, product_uom, values, line, partner)
+        if 'orderpoint_id' in values:
+            vals['orderpoint_ids'] = [
+                (4, values['orderpoint_id'].id)]
+        return vals
