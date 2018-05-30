ba7ff47f62b0d15546e3457671f2a893046d1ad7
Lois Rilo <lois.rilo@eficent.com>
2018-05-30 16:24:48 +0200

    [11.0] stock_orderpoint_purchase_link: add corresponding field to PO lines on orderpoint

--- stock_orderpoint_purchase_link/models/procurement_rule.py
+++ stock_orderpoint_purchase_link/models/procurement_rule.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import models
