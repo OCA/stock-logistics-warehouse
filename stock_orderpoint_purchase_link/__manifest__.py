a631cb20f208bf733f64a791a962dd3e1836e708
Lois Rilo <lois.rilo@eficent.com>
2018-05-30 16:24:48 +0200

    [11.0] stock_orderpoint_purchase_link: add corresponding field to PO lines on orderpoint

--- stock_orderpoint_purchase_link/__manifest__.py
+++ stock_orderpoint_purchase_link/__manifest__.py
@@ -1,10 +1,10 @@
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Stock Orderpoint Purchase Link",
     "summary": "Link Reordering rules to purchase orders",
-    "version": "11.0.1.0.0",
+    "version": "11.0.1.0.1",
     "license": "LGPL-3",
     "website": "https://github.com/stock-logistics-warehouse",
     "author": "Eficent, "
