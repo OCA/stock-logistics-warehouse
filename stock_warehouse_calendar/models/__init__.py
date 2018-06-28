889ad73cf4ac2ae22c70e8455a4a3b56520291aa
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2018-06-28 19:02:11 +0200

    11.0 - add stock_warehouse_calendar

---
+++ stock_warehouse_calendar/models/__init__.py
@@ -0,0 +1,2 @@
+from . import stock_warehouse
+from . import procurement_rule
---
+++ stock_warehouse_calendar/models/stock_warehouse.py
@@ -0,0 +1,11 @@
+# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class StockWarehouse(models.Model):
+    _inherit = 'stock.warehouse'
+
+    calendar_id = fields.Many2one('resource.calendar',
+                                  'Working Hours')
