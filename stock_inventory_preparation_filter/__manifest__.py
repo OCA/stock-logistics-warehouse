03d6afd7ace5a48d3683484fd51502af6cfd1608
xavierjimenez <xavier.jimenez@qubiq.es>
2018-06-28 09:10:09 +0200

    [MIG][11.0] stock_inventory_preparation_filter

--- stock_inventory_preparation_filter/__manifest__.py
+++ stock_inventory_preparation_filter/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2015 AvanzOSC - Oihane Crucelaegi
 # Copyright 2015 Tecnativa - Pedro M. Baeza
 # Copyright 2017 Tecnativa - David Vidal
@@ -6,7 +5,7 @@
 
 {
     "name": "Extended Inventory Preparation Filters",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "depends": [
         "stock",
     ],
