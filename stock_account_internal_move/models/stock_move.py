2f288ac473eaacf3764ef0731d7de43408fe16e8
Artem Kostyuk <a.kostyuk@mobilunity.com>
2018-07-20 18:07:48 +0300

    [REF] Split valuation accounts on locations

--- stock_account_internal_move/models/stock_move.py
+++ stock_account_internal_move/models/stock_move.py
@@ -1,7 +1,7 @@
 # Copyright (C) 2018 by Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-from odoo import _, api, models
-from odoo.exceptions import ValidationError
+from odoo import api, models
+from odoo.tools import float_round
 
 
 class StockMove(models.Model):
@@ -10,30 +10,43 @@ class StockMove(models.Model):
     @api.multi
     def _action_done(self):
         res = super()._action_done()
-        for move in self:
-            if not (move.location_id.usage == 'internal'
-                    and move.location_dest_id.usage == 'internal'):
+
+        for move in res:
+            # we're customizing behavior on moves between internal locations
+            # only, thus ensuring that we don't clash w/ account moves
+            # created in `stock_account`
+            if not (move.location_id.usage
+                    == move.location_dest_id.usage
+                    == 'internal'):
                 continue
 
-            location_src = move.location_id
-            location_dest = move.location_dest_id
-            if location_src.force_accounting_entries \
-                    != location_dest.force_accounting_entries:
-                raise ValidationError(_(
-                    'Could not generate a journal entry.'
-                    ' You need to either force or disable accounting on both'
-                    ' locations in order to do that.'))
-            if not location_src.force_accounting_entries:
-                # `force_accounting_entries` is the same as on dest location
-                continue
+            location_from = move.location_id
+            location_to = move.location_dest_id
 
-            # checks above along w/ the constraint ensuring the presence of
-            # valuation accounts on both src and dest locations
-            outgoing_acc = move.location_id.valuation_internal_account_id
-            receiving_acc = move.location_dest_id.valuation_internal_account_id
-            # return journal_id, acc_src, acc_dest, acc_valuation
-            journal_id, acc_src_id, acc_dest_id, acc_valuation \
+            # get valuation accounts for product
+            # done in _get_accounting_data_for_valuation?
+            product_valuation_accounts \
+                = move.product_id.product_tmpl_id.get_product_accounts()
+            stock_journal_id, __, __, __ \
                 = move._get_accounting_data_for_valuation()
-            move._create_account_move_line(
-                outgoing_acc, receiving_acc, journal_id)
+
+            # calculate move cost
+            # TODO: recheck if this part respects product valuation method
+            move.value = float_round(
+                value=move.product_id.standard_price * move.quantity_done,
+                precision_rounding=self.company_id.currency_id.rounding,
+            )
+
+            if location_from.force_accounting_entries:
+                move._create_account_move_line(
+                    location_from.valuation_out_internal_account_id.id,
+                    product_valuation_accounts.get('stock_valuation').id,
+                    stock_journal_id)
+
+            if location_to.force_accounting_entries:
+                move._create_account_move_line(
+                    location_to.valuation_in_internal_account_id.id,
+                    product_valuation_accounts.get('stock_valuation').id,
+                    stock_journal_id)
+
         return res
