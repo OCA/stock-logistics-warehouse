603be96d0359705d2da49552e977c28aa0292f8f
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2018-07-23 17:34:57 -0500

    [FIX] Duplicate field and related constraint [IMP] Doc

--- stock_account_internal_move/models/stock_location.py
+++ stock_account_internal_move/models/stock_location.py
@@ -13,8 +13,8 @@ class StockLocation(models.Model):
          """check(
             NOT force_accounting_entries
             OR force_accounting_entries
-            AND valuation_out_internal_account_id IS NOT NULL
-            AND valuation_in_internal_account_id IS NOT NULL
+            AND valuation_out_account_id IS NOT NULL
+            AND valuation_in_account_id IS NOT NULL
          )""",
          _('You must provide a valuation in/out accounts'
            ' in order to force accounting entries.')),
@@ -29,15 +29,6 @@ class StockLocation(models.Model):
 
     force_accounting_entries = fields.Boolean(
         string='Force accounting entries?',
-    )
-    # both used only for usage == internal
-    valuation_in_internal_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Stock Valuation Account (incoming)',
-    )
-    valuation_out_internal_account_id = fields.Many2one(
-        comodel_name='account.account',
-        string='Stock Valuation Account (outgoing)',
     )
 
     @api.onchange('usage')
