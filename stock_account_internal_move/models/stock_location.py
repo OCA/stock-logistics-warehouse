b58dba753abf1efd1210f76c2025293fe5646e73
Artem Kostyuk <a.kostyuk@mobilunity.com>
2018-07-24 10:46:40 +0300

    [REF] stock_account_internal_move: Reuse fields from stock_account
    * Rewrite SQL constraints to Python
    * Adapt location view to display valuation accounts on internal locations

--- stock_account_internal_move/models/stock_location.py
+++ stock_account_internal_move/models/stock_location.py
@@ -1,35 +1,42 @@
 # Copyright (C) 2018 by Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 from odoo import _, api, fields, models
+from odoo.exceptions import ValidationError
 
 
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
-    _sql_constraints = [
-        ('valuation_account_present',
-         # ensures that there's always a pair of valuation accounts present
-         # when account entries on it are forced
-         """check(
-            NOT force_accounting_entries
-            OR force_accounting_entries
-            AND valuation_out_account_id IS NOT NULL
-            AND valuation_in_account_id IS NOT NULL
-         )""",
-         _('You must provide a valuation in/out accounts'
-           ' in order to force accounting entries.')),
-        ('force_accounting_entries_internal_only',
-         """check(
-            NOT force_accounting_entries
-            OR force_accounting_entries
-            AND usage = 'internal')""",
-         _('You cannot force accounting entries'
-           ' on a non-internal locations.')),
-    ]
-
     force_accounting_entries = fields.Boolean(
         string='Force accounting entries?',
     )
+
+    @api.constrains(
+        'usage',
+        'force_accounting_entries',
+    )
+    def _check_force_accouting_entries_internal_only(self):
+        for loc in self:
+            if loc.usage != 'internal' and loc.force_accounting_entries:
+                raise ValidationError(_(
+                    'You cannot force accounting entries'
+                    ' on a non-internal locations.'))
+
+    @api.constrains(
+        'force_accounting_entries',
+        'valuation_in_account_id',
+        'valuation_out_account_id',
+    )
+    def _check_internal_valuation_accounts_present(self):
+        """Ensure that every location requiring entries has valuation accs."""
+        for loc in self:
+            if loc.usage != 'internal' or not loc.force_accounting_entries:
+                continue  # this one doesn't require accounts, it's fine
+            if not loc.valuation_in_account_id \
+                    or not loc.valuation_out_account_id:
+                raise ValidationError(_(
+                    'You must provide a valuation in/out accounts'
+                    ' in order to force accounting entries.'))
 
     @api.onchange('usage')
     def _onchange_usage(self):
