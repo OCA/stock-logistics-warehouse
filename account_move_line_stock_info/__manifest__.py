48cf3145b151dd1eb1689fa3700e992594aece3c
mreficent <miquel.raich@eficent.com>
2018-08-30 19:10:04 +0200

    [MIG] account_move_line_stock_info: Migration to 11.0

--- account_move_line_stock_info/__manifest__.py
+++ account_move_line_stock_info/__manifest__.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # © 2016 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     "name": "Account Move Line Stock Move",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "depends": [
         "stock_account"
     ],
