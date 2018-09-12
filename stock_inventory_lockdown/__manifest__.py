67d2fc9d59a5b6ff35f5f588ae4f1112b79b6e51
Lois Rilo <lois.rilo@eficent.com>
2018-09-12 16:52:42 +0200

    [11.0][MIG] stock_inventory_lockdown

--- stock_inventory_lockdown/__manifest__.py
+++ stock_inventory_lockdown/__manifest__.py
@@ -1,17 +1,18 @@
-# -*- coding: utf-8 -*-
 # © 2013-2016 Numérigraphe SARL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Inventory Lock Down",
     "summary": "Lock down stock locations during inventories.",
-    "version": "10.0.1.0.1",
+    "version": "11.0.1.0.0",
+    "development_status": "Mature",
     "depends": ["stock"],
     "author": "Numérigraphe, Eficent, Odoo Community Association (OCA)",
     "category": "Warehouse Management",
     "images": [
         "images/move_error.png",
-        "images/location_locked.png"],
+        "images/location_locked.png",
+    ],
     "license": "AGPL-3",
     "installable": True,
 }
---
+++ stock_inventory_lockdown/readme/CONTRIBUTORS.rst
@@ -0,0 +1,6 @@
+* Loïc Bellier (Numérigraphe) <lb@numerigraphe.com>
+* Lionel Sausin (Numérigraphe) <ls@numerigraphe.com>
+* Laetitia Gangloff (Acsone) <laetitia.gangloff@acsone.eu>
+* Laurent Mignon (Acsone) <laurent.mignon@acsone.eu>
+* Lois Rilo (Eficent) <lois.rilo@eficent.com>
+* Jordi Ballester (Eficent) <jordi.ballester@eficent.com>
