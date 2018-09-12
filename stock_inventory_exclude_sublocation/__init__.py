2529908ec388a7bfdaa8aae95648caf0ddcf8d8f
Lois Rilo <lois.rilo@eficent.com>
2018-09-12 14:31:06 +0200

    [11.0][MIG] stock_inventory_exclude_sublocation

--- stock_inventory_exclude_sublocation/__init__.py
+++ stock_inventory_exclude_sublocation/__init__.py
@@ -1,7 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import models
-from . import tests
