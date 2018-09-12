515b43baab7c996ddc6c33e3d178ec3033290b15
Lois Rilo <lois.rilo@eficent.com>
2018-09-12 14:31:06 +0200

    [11.0][MIG] stock_inventory_exclude_sublocation

--- stock_inventory_exclude_sublocation/__manifest__.py
+++ stock_inventory_exclude_sublocation/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -6,14 +5,15 @@
     "name": "Stock Inventory Exclude Sublocation",
     "summary": "Allow to perform inventories of a location without including "
                "its child locations.",
-    "version": "10.0.1.0.0",
-    "author": "Eficent,"
+    "version": "11.0.1.0.0",
+    "development_status": "Mature",
+    "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
     "depends": ["stock", "stock_inventory_chatter"],
     "data": [
-        'views/stock_inventory_view.xml'
+        'views/stock_inventory_view.xml',
     ],
     "license": "AGPL-3",
     'installable': True,
---
+++ stock_inventory_exclude_sublocation/readme/USAGE.rst
@@ -0,0 +1,5 @@
+To use this module, you simply need to:
+
+#. Create a new inventory adjustment.
+#. Select the option inventory of all products.
+#. Check the box "Exclude Sublocations".
