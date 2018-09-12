44a001d5c22d66442436a55f9f4d2a22da37df8d
Lois Rilo <lois.rilo@eficent.com>
2018-09-12 14:31:06 +0200

    [11.0][MIG] stock_inventory_exclude_sublocation

--- stock_inventory_exclude_sublocation/models/stock_inventory.py
+++ stock_inventory_exclude_sublocation/models/stock_inventory.py
@@ -1,9 +1,8 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class Inventory(models.Model):
@@ -14,68 +13,78 @@ class Inventory(models.Model):
         track_visibility='onchange', readonly=True,
         states={'draft': [('readonly', False)]})
 
-    @api.multi
     def _get_inventory_lines_values(self):
-        if self.exclude_sublocation:
-            domain = ' location_id = %s'
-            args = (tuple(self.location_id.ids),)
+        """This method is a copy of standard one but considering only the
+        current location.
+        WARNING: In case of excluding sublocations standard method
+        is overridden by this one."""
+        if not self.exclude_sublocation:
+            # Early return if exclude_sublocation is not set
+            return super()._get_inventory_lines_values()
 
-            vals = []
-            product_obj = self.env['product.product']
-            # Empty recordset of products available in stock_quants
-            quant_products = self.env['product.product']
-            # Empty recordset of products to filter
-            products_to_filter = self.env['product.product']
+        # STAR OF MODIFIED CODE:
+        domain = ' location_id in %s'
+        args = (tuple(self.location_id.ids),)
+        # END OF MODIFIED CODE.
 
-            if self.company_id.id:
-                domain += ' AND company_id = %s'
-                args += (self.company_id.id,)
-            if self.partner_id:
-                domain += ' AND owner_id = %s'
-                args += (self.partner_id.id,)
-            if self.lot_id:
-                domain += ' AND lot_id = %s'
-                args += (self.lot_id.id,)
-            if self.product_id:
-                domain += ' AND product_id = %s'
-                args += (self.product_id.id,)
-                products_to_filter |= self.product_id
-            if self.package_id:
-                domain += ' AND package_id = %s'
-                args += (self.package_id.id,)
-            if self.category_id:
-                categ_products = product_obj.search(
-                    [('categ_id', '=', self.category_id.id)])
-                domain += ' AND product_id = ANY (%s)'
-                args += (categ_products.ids,)
-                products_to_filter |= categ_products
+        vals = []
+        Product = self.env['product.product']
+        # Empty recordset of products available in stock_quants
+        quant_products = self.env['product.product']
+        # Empty recordset of products to filter
+        products_to_filter = self.env['product.product']
 
-            # disable error about SQL injection as the code here is generating
-            # a vulnerability
-            # pylint: disable = E8103
-            self.env.cr.execute(
-                "SELECT product_id, SUM(qty) AS product_qty, location_id, "
-                " lot_id AS prod_lot_id, package_id, owner_id AS partner_id "
-                "FROM stock_quant "
-                "WHERE " + domain + " " +
-                "GROUP BY product_id, location_id, lot_id, package_id, "
-                "partner_id """, args)
+        # case 0: Filter on company
+        if self.company_id:
+            domain += ' AND company_id = %s'
+            args += (self.company_id.id,)
 
-            for product_data in self.env.cr.dictfetchall():
-                for void_field in [item[0] for item in product_data.items() if
-                                   item[1] is None]:
-                    product_data[void_field] = False
-                product_data['theoretical_qty'] = product_data['product_qty']
-                if product_data['product_id']:
-                    product_data['product_uom_id'] = product_obj.browse(
-                        product_data['product_id']).uom_id.id
-                    quant_products |= product_obj.browse(
-                        product_data['product_id'])
-                vals.append(product_data)
-            if self.exhausted:
-                exhausted_vals = self._get_exhausted_inventory_line(
-                    products_to_filter, quant_products)
-                vals.extend(exhausted_vals)
-            return vals
-        else:
-            return super(Inventory, self)._get_inventory_lines_values()
+        # case 1: Filter on One owner only or One product for a specific owner
+        if self.partner_id:
+            domain += ' AND owner_id = %s'
+            args += (self.partner_id.id,)
+        # case 2: Filter on One Lot/Serial Number
+        if self.lot_id:
+            domain += ' AND lot_id = %s'
+            args += (self.lot_id.id,)
+        # case 3: Filter on One product
+        if self.product_id:
+            domain += ' AND product_id = %s'
+            args += (self.product_id.id,)
+            products_to_filter |= self.product_id
+        # case 4: Filter on A Pack
+        if self.package_id:
+            domain += ' AND package_id = %s'
+            args += (self.package_id.id,)
+        # case 5: Filter on One product category + Exahausted Products
+        if self.category_id:
+            categ_products = Product.search(
+                [('categ_id', '=', self.category_id.id)])
+            domain += ' AND product_id = ANY (%s)'
+            args += (categ_products.ids,)
+            products_to_filter |= categ_products
+
+        self.env.cr.execute("""
+            SELECT product_id, sum(quantity) as product_qty,
+                location_id, lot_id as prod_lot_id, package_id,
+                owner_id as partner_id
+            FROM stock_quant
+            WHERE %s
+            GROUP BY product_id, location_id, lot_id,
+                package_id, partner_id """ % domain, args)
+
+        for product_data in self.env.cr.dictfetchall():
+            for void_field in [item[0] for item in product_data.items() if
+                               item[1] is None]:
+                product_data[void_field] = False
+            product_data['theoretical_qty'] = product_data['product_qty']
+            if product_data['product_id']:
+                product_data['product_uom_id'] = Product.browse(
+                    product_data['product_id']).uom_id.id
+                quant_products |= Product.browse(product_data['product_id'])
+            vals.append(product_data)
+        if self.exhausted:
+            exhausted_vals = self._get_exhausted_inventory_line(
+                products_to_filter, quant_products)
+            vals.extend(exhausted_vals)
+        return vals
