6d286a893d1ad5ac93f5e24d6909a9319f2995c9
Lois Rilo <lois.rilo@eficent.com>
2018-09-12 16:52:42 +0200

    [11.0][MIG] stock_inventory_lockdown

--- stock_inventory_lockdown/models/__init__.py
+++ stock_inventory_lockdown/models/__init__.py
@@ -1,7 +1,3 @@
-# -*- coding: utf-8 -*-
-# © 2013-2016 Numérigraphe SARL
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
-
 from . import stock_move
 from . import stock_inventory
 from . import stock_location
---
+++ stock_inventory_lockdown/readme/DESCRIPTION.rst
@@ -0,0 +1,1 @@
+This module lets you lock down the locations during an inventory.
