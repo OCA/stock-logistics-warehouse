54f2aab5f78ec01b38a82763878a763c70e6a481
Lois Rilo <lois.rilo@eficent.com>
2018-09-18 17:02:10 +0200

    [11.0][MIG] stock_removal_location_by_priority

--- stock_removal_location_by_priority/models/stock_quant.py
+++ stock_removal_location_by_priority/models/stock_quant.py
@@ -1,29 +1,29 @@
-# -*- coding: utf-8 -*-
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
-from odoo.tools.translate import _
+from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 
 
-class Quant(models.Model):
+class StockQuant(models.Model):
     _inherit = 'stock.quant'
 
     removal_priority = fields.Integer(
-        related='location_id.removal_priority', readonly=True, store=True)
+        related='location_id.removal_priority',
+        readonly=True, store=True,
+    )
 
     @api.model
-    def _quants_removal_get_order(self, removal_strategy=None):
+    def _get_removal_strategy_order(self, removal_strategy=None):
         if self.user_has_groups(
                 'stock_removal_location_by_priority.group_removal_priority'):
             if removal_strategy == 'fifo':
-                return 'in_date, removal_priority, id'
+                return 'in_date ASC NULLS FIRST, removal_priority ASC, id'
             elif removal_strategy == 'lifo':
-                return 'in_date desc, removal_priority asc, id desc'
+                return 'in_date DESC NULLS LAST, removal_priority ASC, id desc'
             raise UserError(_('Removal strategy %s not implemented.') % (
                 removal_strategy,))
         else:
-            return super(Quant, self)._quants_removal_get_order(
+            return super()._get_removal_strategy_order(
                 removal_strategy=removal_strategy)
---
+++ stock_removal_location_by_priority/readme/USAGE.rst
@@ -0,0 +1,3 @@
+After configure your locations properly, the system will select the quant
+at the location with more priority in case of equal date, no matter if you
+use FIFO or LIFO removal strategy.
