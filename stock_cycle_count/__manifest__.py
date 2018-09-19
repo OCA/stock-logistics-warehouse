e748a95c7899c68e811ae9b1e23fda010382481d
Lois Rilo <lois.rilo@eficent.com>
2018-09-19 18:02:32 +0200

    [11.0][MIG] stock_cycle_count

--- stock_cycle_count/__manifest__.py
+++ stock_cycle_count/__manifest__.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017-18 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -6,20 +5,18 @@
     "name": "Stock Cycle Count",
     "summary": "Adds the capability to schedule cycle counts in a "
                "warehouse through different rules defined by the user.",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
+    "development_status": "Mature",
+    "maintainers": ["lreficent"],
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
     "depends": [
-        "stock",
-        "mail",
+        "stock_account",
         "stock_inventory_discrepancy",
         "stock_inventory_exclude_sublocation",
     ],
-    "external_dependencies": {
-        "python": ["numpy"],
-    },
     "data": [
         "views/stock_cycle_count_view.xml",
         "views/stock_cycle_count_rule_view.xml",
--- stock_cycle_count/models/stock_move.py
+++ stock_cycle_count/models/stock_move.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
@@ -10,7 +9,7 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     @api.multi
-    def action_done(self):
-        super(StockMove, self).action_done()
+    def _action_done(self):
+        super()._action_done()
         self.mapped("location_id").check_zero_confirmation()
         return True
