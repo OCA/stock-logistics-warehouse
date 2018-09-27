1f8b838b06e8e057c46125885f79c4e701edbfd3
cubells <vicent@vcubells.net>
2018-09-27 14:01:36 +0200

    [MIG] stock_orderpoint_generator: Migration to 11.0

--- stock_orderpoint_generator/__manifest__.py
+++ stock_orderpoint_generator/__manifest__.py
@@ -1,19 +1,17 @@
-# -*- coding: utf-8 -*-
-# © 2012-2016 Camptocamp SA
+# Copyright 2012-2016 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     'name': 'Order point generator',
     'summary': 'Mass configuration of stock order points',
-    'version': '9.0.1.1.0',
+    'version': '11.0.1.0.0',
     'author': "Camptocamp, Odoo Community Association (OCA)",
     'category': 'Warehouse',
     'license': 'AGPL-3',
-    'website': "http://www.camptocamp.com",
+    'website': "https://github.com/OCA/stock-logistics-warehouse",
     'depends': ['stock'],
     'data': [
         'views/orderpoint_template_views.xml',
-        'views/product_views.xml',
         "wizard/orderpoint_generator_view.xml",
         "data/ir_cron.xml",
         "security/ir.model.access.csv",
--- stock_orderpoint_generator/tests/__init__.py
+++ stock_orderpoint_generator/tests/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2016 Cyril Gaudin (Camptocamp)
+# Copyright 2016 Cyril Gaudin (Camptocamp)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import test_orderpoint_generator
