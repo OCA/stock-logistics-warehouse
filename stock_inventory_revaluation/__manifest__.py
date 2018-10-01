048f6e68f3e9020ea729a2dabc2138068ef19c1d
Cédric Pigeon <cedric.pigeon@acsone.eu>
2018-10-01 10:03:34 +0200

    [10.0][FIX] stock_inventory_revaluation: unable to revaluate multiple quants with real method

--- stock_inventory_revaluation/__manifest__.py
+++ stock_inventory_revaluation/__manifest__.py
@@ -9,7 +9,7 @@
     "name": "Stock Inventory Revaluation",
     "summary": "Introduces inventory revaluation as single point to change "
                "the valuation of products.",
-    "version": "10.0.1.0.0",
+    "version": "10.0.1.1.0",
     "author": "Eficent Business and IT Consulting Services S.L., "
               "Serpent Consulting Services Pvt. Ltd.,"
               "Odoo Community Association (OCA)",
