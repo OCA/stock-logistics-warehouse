c2e19ec9d92b5a6781d9baab193a665664a701f5
cubells <vicent@vcubells.net>
2018-11-12 07:24:02 +0100

    [MIG] stock_quant_manual_assign: Migration to 11.0

--- stock_quant_manual_assign/__init__.py
+++ stock_quant_manual_assign/__init__.py
@@ -1,5 +1,1 @@
-# -*- coding: utf-8 -*-
-##############################################################################
-# For copyright and license notices, see __openerp__.py file in root directory
-##############################################################################
 from . import wizard
