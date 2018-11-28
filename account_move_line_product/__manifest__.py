1985dba1c928a2ac5bff6ad11c703322180e80a2
Beñat Jimenez <benat.jimenez@eficent.com>
2018-11-28 09:25:04 +0100

    [MIG] Migration of module to version 12

--- account_move_line_product/__manifest__.py
+++ account_move_line_product/__manifest__.py
@@ -4,10 +4,10 @@
 
 {
     "name": "Account Move Line Product",
-    "version": "11.0.1.0.0",
+    "version": "12.0.1.0.0",
     "summary": "Displays the product in the journal entries and items",
     "author": "Eficent, Odoo Community Association (OCA)",
-    "website": "http://www.eficent.com",
+    "website": "http://www.github.com/OCA/stock-logistics-warehouse",
     "category": "Generic",
     "depends": ["account"],
     "license": "AGPL-3",
