aca51c82d4ba1febc7915464d51692e8bd01d308
@PlanetaTIC <@PlanetaTIC>
2018-12-04 16:27:23 +0100

    [ADD] TH181130/0011 stock_picking_supplier_ref

---
+++ stock_picking_supplier_ref/models/__init__.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Trey, Kilobytes de Soluciones
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import stock_picking
---
+++ stock_picking_supplier_ref/models/stock_picking.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Trey, Kilobytes de Soluciones
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class StockPicking(models.Model):
+    _inherit = 'stock.picking'
+
+    reference = fields.Char(string='Supplier Reference')
