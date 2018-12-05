cb8f348fd5484909f8a5cd298f01127f54060fac
Alexis de Lattre <alexis.delattre@akretion.com>
2018-12-05 21:09:31 +0100

    [MIG] stock_location_lockdown from v9 to v10
    Improve README

--- stock_location_lockdown/models/stock_location.py
+++ stock_location_lockdown/models/stock_location.py
@@ -2,12 +2,13 @@
 # Copyright 2018 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from openerp import fields, models
+from odoo import fields, models
 
 
 class StockLocation(models.Model):
     _inherit = 'stock.location'
 
     block_stock_entrance = fields.Boolean(
-        help="if this box is checked, put stock in this location won't be "
-             "allowed. Usually used for virtual location, which has children")
+        help="if this box is checked, putting stock on this location won't be "
+             "allowed. Usually used for a virtual location that has "
+             "childrens.")
