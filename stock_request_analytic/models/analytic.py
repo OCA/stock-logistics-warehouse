5f260e213026400f3747e9ffc8d93b8da1d1b04d
aheficent <ahenriquez@eficent.com>
2018-12-24 14:02:18 +0100

    [ADD]stock_request_analytic

---
+++ stock_request_analytic/models/analytic.py
@@ -0,0 +1,12 @@
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class AccountAnalyticAccount(models.Model):
+    _inherit = 'account.analytic.account'
+
+    stock_request_ids = fields.One2many(
+        comodel_name='stock.request', inverse_name='analytic_account_id',
+        string='Stock Requests', copy=False)
