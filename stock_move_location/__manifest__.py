5ddc5ad5b9669557c34be35bb369b4be32a720b9
mpanarin <m.panarin@mobilunity.com>
2018-12-27 11:47:16 +0200

    [MIG] 11.0 stock_move_location

---
+++ stock_move_location/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright (C) 2011 Julius Network Solutions SARL <contact@julius.fr>
+# Copyright 2018 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+{
+    "name": "Move Stock Location",
+    "version": "11.0.1.0.0",
+    "author": "Julius Network Solutions, "
+              "Odoo Community Association (OCA)",
+    "summary": "This module allows to move all stock "
+               "in a stock location to an other one.",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    'license': 'AGPL-3',
+    "depends": [
+        "stock",
+    ],
+    "category": "Stock",
+    "data": [
+        'data/stock_move_sequence.xml',
+        'views/stock_view.xml',
+        'wizard/stock_move_location.xml',
+    ],
+}
---
+++ stock_move_location/readme/USAGE.rst
@@ -0,0 +1,9 @@
+* A new menuitem Stock > Move from location... opens a wizard
+  where 2 location ca be specified.
+* Select origin and destination locations and press "MOVE LOCATION"
+* Press `ADD ALL` button to add all products available
+* Those lines can be edited. Move quantity can't be more than a max available quantity
+* Move doesn't care about the reservations and will move stuff anyway
+* If during you operation with the wizard the real quantity will change
+  it will move only the available quantity at the button press
+* Products will be moved and a form view of inventory that did that will show up
