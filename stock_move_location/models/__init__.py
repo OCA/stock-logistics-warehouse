19c60699c2ff2dd9cec4aa374abfd8c9f205c310
mpanarin <m.panarin@mobilunity.com>
2018-12-27 11:47:16 +0200

    [MIG] 11.0 stock_move_location

--- stock_move_location/models/__init__.py
+++ stock_move_location/models/__init__.py
@@ -2,5 +2,5 @@
 # Copyright 2018 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from . import models
-from . import wizard
+from . import stock_inventory
+from . import inventory_line
