34855558c303732ce8a394f695a3965b8da0e59b
hveficent <hector.villarreal@eficent.com>
2019-01-02 17:16:43 +0100

    [11.0][IMP] Add optional encoded reason management

--- stock_change_qty_reason/tests/test_stock_change_qty_reason.py
+++ stock_change_qty_reason/tests/test_stock_change_qty_reason.py
@@ -1,5 +1,6 @@
 # pylint: disable=import-error,protected-access,too-few-public-methods
 # Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2019 Eficent Business and IT Consulting Services S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from odoo.tests.common import SavepointCase
@@ -12,9 +13,11 @@ class TestStockQuantityChangeReason(SavepointCase):
         super(TestStockQuantityChangeReason, cls).setUpClass()
 
         # MODELS
+        cls.stock_move = cls.env['stock.move']
         cls.product_product_model = cls.env['product.product']
         cls.product_category_model = cls.env['product.category']
         cls.wizard_model = cls.env['stock.change.product.qty']
+        cls.preset_reason_id = cls.env['stock.inventory.line.reason']
 
         # INSTANCES
         cls.category = cls.product_category_model.create({
@@ -26,11 +29,22 @@ class TestStockQuantityChangeReason(SavepointCase):
             'categ_id': self.category.id,
             'type': 'product'})
 
-    def _product_change_qty(self, product, new_qty, reason):
-        wizard = self.wizard_model.create({'product_id': product.id,
-                                           'new_quantity': new_qty,
-                                           'reason': reason})
+    def _product_change_qty(self, product, new_qty, reason,
+                            preset_reason_id=None):
+        values = {
+            'product_id': product.id,
+            'new_quantity': new_qty,
+            'reason': reason,
+        }
+        if preset_reason_id:
+            values.update({'preset_reason_id': preset_reason_id.id})
+        wizard = self.wizard_model.create(values)
         wizard.change_product_qty()
+
+    def _create_reason(self, name, description=None):
+        return self.preset_reason_id.create({
+            'name': name,
+            'description': description})
 
     def test_product_change_qty(self):
         """ Check product quantity update move reason is well set
@@ -51,19 +65,42 @@ class TestStockQuantityChangeReason(SavepointCase):
         self._product_change_qty(product6, 0, 'product_6_reason')
 
         # check stock moves created
-        move2 = self.env['stock.move'].search([('product_id', '=',
-                                                product2.id)])
-        move3 = self.env['stock.move'].search([('product_id', '=',
-                                                product3.id)])
-        move4 = self.env['stock.move'].search([('product_id', '=',
-                                                product4.id)])
-        move5 = self.env['stock.move'].search([('product_id', '=',
-                                                product5.id)])
-        move6 = self.env['stock.move'].search([('product_id', '=',
-                                                product6.id)])
+        move2 = self.stock_move.search([('product_id', '=',
+                                         product2.id)])
+        move3 = self.stock_move.search([('product_id', '=',
+                                         product3.id)])
+        move4 = self.stock_move.search([('product_id', '=',
+                                         product4.id)])
+        move5 = self.stock_move.search([('product_id', '=',
+                                         product5.id)])
+        move6 = self.stock_move.search([('product_id', '=',
+                                         product6.id)])
 
         self.assertEqual(move2.origin, 'product_2_reason')
         self.assertFalse(move3)
         self.assertFalse(move4)
         self.assertEqual(move5.origin, 'product_5_reason')
         self.assertFalse(move6)
+
+    def test_product_change_qty_with_preset_reason(self):
+        """ Check product quantity update move reason is well set
+        """
+        # create reason
+        reason = self._create_reason('Test', 'Description Test')
+        # create products
+        product2 = self._create_product('product_product_2')
+        product3 = self._create_product('product_product_3')
+
+        # update qty on hand and add reason
+        self._product_change_qty(product2, 10, reason.name, reason)
+        self._product_change_qty(product3, 0, reason.name, reason)
+
+        # check stock moves created
+        move2 = self.stock_move.search([('product_id', '=',
+                                         product2.id)])
+        move3 = self.stock_move.search([('product_id', '=',
+                                         product3.id)])
+        # asserts
+        self.assertEqual(move2.origin, reason.name)
+        self.assertEqual(move2.preset_reason_id, reason)
+        self.assertFalse(move3)
