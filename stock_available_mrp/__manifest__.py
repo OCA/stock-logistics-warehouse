696a6e745e6f909e7a0241845648df8129f89999
Timon Tschanz <timon.tschanz@camptocamp.com>
2019-01-04 13:03:03 +0100

    [11.0][MIG] port stock_available_mrp to 11.0

--- stock_available_mrp/__manifest__.py
+++ stock_available_mrp/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
-# © 2014 Numérigraphe SARL, Camptocamp
+# Copyright 2014 Numérigraphe SARL, Camptocamp
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 {
     'name': 'Consider the production potential is available to promise',
-    'version': '10.0.1.0.0',
-    "author": u"Numérigraphe,"
-              u"Odoo Community Association (OCA)",
+    'version': '11.0.1.0.0',
+    "author": "Numérigraphe,"
+              "Odoo Community Association (OCA)",
+    'website': 'https://github.com/OCA/stock-logistics-warehouse',
     'category': 'Hidden',
     'depends': [
         'stock_available',
--- stock_available_mrp/models/product_template.py
+++ stock_available_mrp/models/product_template.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# © 2014 Numérigraphe SARL
+# Copyright 2014 Numérigraphe SARL
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from odoo import models, api
