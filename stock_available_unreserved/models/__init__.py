1697f7b7193740e3a884f78e4f35d10fefe5be0c
ougc27 <oscar.garza@jarsa.com.mx>
2019-01-07 09:35:55 -0600

    [MIG] stock_available_unreserved: Migration to V12

--- stock_available_unreserved/models/__init__.py
+++ stock_available_unreserved/models/__init__.py
@@ -1,6 +1,7 @@
 # Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
 # Copyright 2016 Eficent Business and IT Consulting Services S.L.
 #   (http://www.eficent.com)
+# Copyright 2019 JARSA Sistemas S.A. de C.V.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import product
