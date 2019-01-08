01fa10a324696c0bc33bf1ef17761c3846a5109a
mpanarin <m.panarin@mobilunity.com>
2019-01-09 01:14:46 +0200

    [IMP] refactor to use pickings instead of inventories

--- stock_move_location/models/__init__.py
+++
@@ -1,6 +0,0 @@
-# Copyright (C) 2011 Julius Network Solutions SARL <contact@julius.fr>
-# Copyright 2018 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
-from . import stock_inventory
-from . import inventory_line
