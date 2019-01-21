70520afade020c5b1a508260203fd8419ef949e6
Raúl Martín <raul.martin@braintec-group.com>
2019-01-21 11:36:56 +0100

    Updating version numbering as it was a minor fix

--- stock_request/__manifest__.py
+++ stock_request/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Request",
     "summary": "Internal request for stock",
-    "version": "11.0.3.2.0",
+    "version": "11.0.3.1.1",
     "license": "LGPL-3",
     "website": "https://github.com/stock-logistics-warehouse",
     "author": "Eficent, "
