e2c0091d838cd15b65521c76e45559d1c79bbd72
Kitti U <kittiu@gmail.com>
2019-02-22 15:11:03 +0700

    [ADD] 12.0-mig-stock_orderpoint_move_link

--- stock_orderpoint_move_link/models/stock.py
+++ stock_orderpoint_move_link/models/stock.py
@@ -1,15 +1,14 @@
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017 Eficent Business, IT Consulting Services, S.L., Ecosoft
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
 from odoo import models
 
 
-class ProcurementRule(models.Model):
-    _inherit = 'procurement.rule'
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
 
     def _get_stock_move_values(self, product_id, product_qty, product_uom,
                                location_id, name, origin, values, group_id):
-        vals = super(ProcurementRule, self)._get_stock_move_values(
+        vals = super()._get_stock_move_values(
             product_id, product_qty, product_uom,
             location_id, name, origin, values, group_id)
         if 'orderpoint_id' in values:
