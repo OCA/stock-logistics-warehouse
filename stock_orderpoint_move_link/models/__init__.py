d3dff21866eac4af92acd81dc4ed8e2b34a7af82
Kitti U <kittiu@gmail.com>
2019-02-22 15:11:03 +0700

    [ADD] 12.0-mig-stock_orderpoint_move_link

--- stock_orderpoint_move_link/models/__init__.py
+++ stock_orderpoint_move_link/models/__init__.py
@@ -1,2 +1,4 @@
-from . import procurement_rule
+# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from . import stock
 from . import stock_move
---
+++ stock_orderpoint_move_link/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module adds to stock moves a direct link to the reordering rules that created them.
+In chained moves, the reordering rule is propagated.
