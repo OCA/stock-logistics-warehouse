98429d644e11482f793d2c3e1de70e4cdeec4446
lmgr0312 <lmiguel.gruiz@gmail.com>
2019-03-13 10:20:52 -0600

    [MRP Product Produce]Fix bug of duplicate in lines when it is used mts+mto+mtp

--- stock_mts_mto_mrp/models/__init__.py
+++ stock_mts_mto_mrp/models/__init__.py
@@ -1,3 +1,4 @@
 # Copyright 2019, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import mrp_production
+from . import mrp_product_produce
--- stock_mts_mto_mrp/models/mrp_production.py
+++ stock_mts_mto_mrp/models/mrp_production.py
@@ -2,7 +2,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 from odoo import api, models
-from odoo.exceptions import UserError
 
 
 class MrpProduction(models.Model):
@@ -10,13 +9,13 @@ class MrpProduction(models.Model):
 
     @api.multi
     def _adjust_procure_method(self):
-        try:
-            mto_route = self.env['stock.warehouse']._find_global_route(
-                'stock.route_warehouse0_mto', ('Make To Order'))
-        except UserError:
-            mto_route = False
         for move in self.move_raw_ids:
             qty_available = move.product_id.qty_available
-            # move_qty = move.product_uom_qty
+            move_qty = move.product_uom_qty
             if qty_available == 0.0:
                 move.procure_method = 'make_to_order'
+            elif move_qty > qty_available and qty_available != 0.0:
+                move.copy({
+                    'product_uom_qty': move_qty - qty_available,
+                    'procure_method': 'make_to_order'})
+                move.product_uom_qty = qty_available
