e644bdc9747b36d25757607e9c675a687e409477
mreficent <miquel.raich@eficent.com>
2019-03-19 19:24:59 +0100

    [MIG] stock_demand_estimate: Migration to 12.0

--- stock_demand_estimate/__manifest__.py
+++ stock_demand_estimate/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Demand Estimate",
     "summary": "Allows to create demand estimates.",
-    "version": "11.0.1.2.0",
+    "version": "12.0.1.0.0",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
@@ -22,4 +22,5 @@
         "wizards/stock_demand_estimate_wizard_view.xml",
     ],
     "license": "AGPL-3",
+    "installable": True,
 }
--- stock_demand_estimate/models/__init__.py
+++ stock_demand_estimate/models/__init__.py
@@ -1,5 +1,3 @@
-# Copyright 2016 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from . import stock_demand_estimate
