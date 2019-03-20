9069b424c62d9f7ba1084b964bc437ac21cc9dc5
Murtuza Saleh <m.saleh.serpentcs@gmail.com>
2019-03-20 16:52:21 +0530

    [MIG] stock_request v12
    [UPD] Update stock_request.pot

--- stock_request/models/stock_rule.py
+++ stock_request/models/stock_rule.py
@@ -4,17 +4,17 @@
 from odoo import models
 
 
-class ProcurementRule(models.Model):
-    _inherit = 'procurement.rule'
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
 
     def _get_stock_move_values(self, product_id, product_qty, product_uom,
                                location_id, name, origin, values, group_id):
-        result = super(ProcurementRule, self)._get_stock_move_values(
-            product_id, product_qty, product_uom, location_id, name, origin,
-            values, group_id)
+        result = super(StockRule, self)._get_stock_move_values(
+            product_id, product_qty, product_uom,
+            location_id, name, origin, values, group_id)
         if values.get('stock_request_id', False):
             result['allocation_ids'] = [(0, 0, {
-                'stock_request_id': values['stock_request_id'],
+                'stock_request_id': values.get('stock_request_id'),
                 'requested_product_uom_qty': product_qty,
             })]
         return result
