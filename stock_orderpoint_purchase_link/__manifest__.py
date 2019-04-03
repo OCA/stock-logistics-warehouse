895e82ea68e87e145d016523e0e0c58f64dcb763
Kitti U <kittiu@gmail.com>
2019-04-03 08:26:02 +0700

    [12.0][MIG] stock_orderpoint_purchase_link
    [UPD] Update stock_orderpoint_purchase_link.pot

--- stock_orderpoint_purchase_link/__manifest__.py
+++ stock_orderpoint_purchase_link/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Orderpoint Purchase Link",
     "summary": "Link Reordering rules to purchase orders",
-    "version": "11.0.1.0.2",
+    "version": "12.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Eficent, "
@@ -12,11 +12,11 @@
     "category": "Warehouse Management",
     "depends": [
         "stock_orderpoint_move_link",
-        "purchase",
+        "purchase_stock",
     ],
     "data": [
         "views/purchase_order_views.xml",
     ],
     "installable": True,
-    'auto_install': True,
+    "auto_install": True,
 }
