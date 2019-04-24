ee5a2965be4a8585121177e636f3b8ad3eeefebd
hveficent <hector.villarreal@eficent.com>
2019-04-24 13:48:16 +0200

    [12.0][IMP] stock_orderpoint_move_link: View transfers on orderpoint

--- stock_orderpoint_move_link/__init__.py
+++ stock_orderpoint_move_link/__init__.py
@@ -1,3 +1,3 @@
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2019 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from . import models
--- stock_orderpoint_move_link/models/stock_move.py
+++ stock_orderpoint_move_link/models/stock_move.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business, IT Consulting Services, S.L.
+# Copyright 2019 Eficent Business, IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import fields, models
 
