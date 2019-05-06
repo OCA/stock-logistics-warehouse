06ffee38a88d02ecd17984bb3082115fc58b7c8c
Jared Kipe <jared@hibou.io>
2019-05-06 07:57:07 -0700

    [MIG] Migrate stock_mts_mto_rule to 12.0
    Major changes to the way Warehouses update their routes/rules and self heal.
    Improved rule code to use `float_compare` and `float_is_zero`.

--- stock_mts_mto_rule/models/stock_rule.py
+++ stock_mts_mto_rule/models/stock_rule.py
@@ -1,18 +1,19 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import models, api, fields, _
-from odoo.exceptions import UserError
+from odoo import api, fields, models, _
+from odoo.exceptions import ValidationError
+from odoo.tools import float_compare, float_is_zero
 
 
-class ProcurementRule(models.Model):
-    _inherit = 'procurement.rule'
+class StockRule(models.Model):
+    _inherit = 'stock.rule'
 
     action = fields.Selection(
         selection_add=[('split_procurement', 'Choose between MTS and MTO')])
     mts_rule_id = fields.Many2one(
-        'procurement.rule', string="MTS Rule")
+        'stock.rule', string="MTS Rule")
     mto_rule_id = fields.Many2one(
-        'procurement.rule', string="MTO Rule")
+        'stock.rule', string="MTO Rule")
 
     @api.constrains('action', 'mts_rule_id', 'mto_rule_id')
     def _check_mts_mto_rule(self):
@@ -21,25 +22,27 @@ class ProcurementRule(models.Model):
                 if not rule.mts_rule_id or not rule.mto_rule_id:
                     msg = _('No MTS or MTO rule configured on procurement '
                             'rule: %s!') % (rule.name, )
-                    raise UserError(msg)
+                    raise ValidationError(msg)
                 if (rule.mts_rule_id.location_src_id.id !=
                         rule.mto_rule_id.location_src_id.id):
                     msg = _('Inconsistency between the source locations of '
                             'the mts and mto rules linked to the procurement '
                             'rule: %s! It should be the same.') % (rule.name,)
-                    raise UserError(msg)
+                    raise ValidationError(msg)
 
     @api.multi
     def get_mto_qty_to_order(self, product, product_qty, product_uom, values):
         self.ensure_one()
+        precision = self.env['decimal.precision']\
+            .precision_get('Product Unit of Measure')
         src_location_id = self.mts_rule_id.location_src_id.id
         product_location = product.with_context(location=src_location_id)
         virtual_available = product_location.virtual_available
         qty_available = product.uom_id._compute_quantity(
             virtual_available, product_uom)
-
-        if qty_available > 0:
-            if qty_available >= product_qty:
+        if float_compare(qty_available, 0.0, precision_digits=precision) > 0:
+            if float_compare(qty_available, product_qty,
+                             precision_digits=precision) >= 0:
                 return 0.0
             else:
                 return product_qty - qty_available
@@ -47,15 +50,17 @@ class ProcurementRule(models.Model):
 
     def _run_split_procurement(self, product_id, product_qty, product_uom,
                                location_id, name, origin, values):
-
+        precision = self.env['decimal.precision']\
+            .precision_get('Product Unit of Measure')
         needed_qty = self.get_mto_qty_to_order(product_id, product_qty,
                                                product_uom, values)
 
-        if needed_qty == 0.0:
+        if float_is_zero(needed_qty, precision_digits=precision):
             getattr(self.mts_rule_id, '_run_%s' % self.mts_rule_id.action)(
                 product_id, product_qty, product_uom, location_id, name,
                 origin, values)
-        elif needed_qty == product_qty:
+        elif float_compare(needed_qty, product_qty,
+                           precision_digits=precision) == 0.0:
             getattr(self.mto_rule_id, '_run_%s' % self.mto_rule_id.action)(
                 product_id, product_qty, product_uom, location_id, name,
                 origin, values)
