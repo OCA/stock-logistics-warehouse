c45ff63c4399fc34e8de509ef21710eaec877a91
Enric Tobella <etobella@creublanca.es>
2019-05-14 15:22:59 +0200

    [IMP] Views

--- stock_request/__manifest__.py
+++ stock_request/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Request",
     "summary": "Internal request for stock",
-    "version": "11.0.3.1.1",
+    "version": "11.0.3.1.2",
     "license": "LGPL-3",
     "website": "https://github.com/stock-logistics-warehouse",
     "author": "Eficent, "
