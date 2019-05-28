c4b8d8199304de7f5c9a42d57004dece47167ad3
hveficent <hector.villarreal@eficent.com>
2019-05-28 16:22:59 +0200

    [MIG] stock_account_change_qty_reason: Migration to 12.0

--- stock_account_change_qty_reason/__manifest__.py
+++ stock_account_change_qty_reason/__manifest__.py
@@ -7,7 +7,7 @@
     'author': 'Eficent, Odoo Community Association (OCA)',
     'website': "https://github.com/OCA/stock-logistics-warehouse",
     'category': 'Warehouse Management',
-    'version': '11.0.1.0.0',
+    'version': '12.0.1.0.0',
     'license': 'AGPL-3',
     'depends': [
         'stock_account',
