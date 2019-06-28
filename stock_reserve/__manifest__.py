305ea7174c1406b8e5fae851546a00cab7783f17
hzh0292 <hzh0292@qq.com>
2019-06-28 17:40:10 +0800

    Fixed according to code reviewer

--- stock_reserve/__manifest__.py
+++ stock_reserve/__manifest__.py
@@ -1,23 +1,3 @@
-##############################################################################
-#
-#    Author: Guewen Baconnier
-#    Copyright 2013 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
 {'name': 'Stock Reservation',
  'summary': 'Stock reservations on products',
  'version': '12.0.1.0.0',
@@ -25,13 +5,11 @@
  'category': 'Warehouse',
  'license': 'AGPL-3',
  'complexity': 'normal',
- 'images': [],
- 'website': "http://www.camptocamp.com",
+ 'website': "https://github.com/OCA/stock-logistics-warehouse",
  'depends': ['stock',
              ],
- 'demo': [],
- 'data': ['view/stock_reserve.xml',
-          'view/product.xml',
+ 'data': ['views/stock_reserve.xml',
+          'views/product.xml',
           'data/stock_data.xml',
           'security/ir.model.access.csv',
           ],
