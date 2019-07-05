2cff3598902637162563f95605e50abacdb7e8f4
Jesús Alan Ramos Rodríguez <alan.ramos@jarsa.com.mx>
2019-07-05 12:36:54 -0500

    [FIX] stock_mts_mto_mrp: allow to work with odoo native and mts+mto in production

--- stock_mts_mto_mrp/models/__init__.py
+++ stock_mts_mto_mrp/models/__init__.py
@@ -1,4 +1,3 @@
 # Copyright 2019, Jarsa Sistemas, S.A. de C.V.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 from . import mrp_production
-from . import mrp_product_produce
--- stock_mts_mto_mrp/models/mrp_production.py
+++ stock_mts_mto_mrp/models/mrp_production.py
@@ -1,7 +1,8 @@
 # Copyright 2019 Jarsa Sistemas, www.vauxoo.com
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, models
+from odoo import _, api, models
+from odoo.exceptions import UserError
 
 
 class MrpProduction(models.Model):
@@ -9,13 +10,45 @@ class MrpProduction(models.Model):
 
     @api.multi
     def _adjust_procure_method(self):
+        try:
+            mto_route = self.env['stock.warehouse']._find_global_route(
+                'stock.route_warehouse0_mto', _('Make To Order'))
+        except UserError:
+            mto_route = False
         for move in self.move_raw_ids:
-            qty_available = move.product_id.qty_available
-            move_qty = move.product_uom_qty
-            if qty_available == 0.0:
-                move.procure_method = 'make_to_order'
-            elif move_qty > qty_available and qty_available != 0.0:
-                move.copy({
-                    'product_uom_qty': move_qty - qty_available,
-                    'procure_method': 'make_to_order'})
-                move.product_uom_qty = qty_available
+            product = move.product_id
+            routes = (
+                product.route_ids +
+                product.route_from_categ_ids +
+                move.warehouse_id.route_ids)
+            # TODO: optimize with read_group?
+            pull = self.env['stock.rule'].search([
+                ('route_id', 'in', [x.id for x in routes]),
+                ('location_src_id', '=', move.location_id.id),
+                ('location_id', '=', move.location_dest_id.id),
+                ('action', '!=', 'push')], limit=1)
+            if pull and pull.action == 'split_procurement':
+                qty_available = (
+                    self.env['stock.quant']._get_available_quantity(
+                        product_id=product, location_id=move.location_id))
+                move_qty = move.product_qty
+                if qty_available == 0.0:
+                    move.procure_method = 'make_to_order'
+                elif move_qty > qty_available and qty_available != 0.0:
+                    rounding_method = self._context.get(
+                        'rounding_method', 'UP')
+                    qty_mto = move.product_uom._compute_quantity(
+                        move_qty - qty_available, product.uom_id,
+                        rounding_method=rounding_method)
+                    qty_mts = move.product_uom._compute_quantity(
+                        qty_available, product.uom_id,
+                        rounding_method=rounding_method)
+                    move.copy({
+                        'product_uom_qty': qty_mto,
+                        'procure_method': 'make_to_order'})
+                    move.product_uom_qty = qty_mts
+            elif pull and (pull.procure_method == 'make_to_order'):
+                move.procure_method = pull.procure_method
+            elif not pull:  # If there is no make_to_stock rule either
+                if mto_route and mto_route.id in [x.id for x in routes]:
+                    move.procure_method = 'make_to_order'
