501f9aa219dcb9391677d1fcb5388d8e7bb3cb1b
Lois Rilo <lois.rilo@eficent.com>
2019-07-11 15:57:21 +0200

    [12.0][MIG] stock_quant_manual_assign

--- stock_quant_manual_assign/__manifest__.py
+++ stock_quant_manual_assign/__manifest__.py
@@ -7,15 +7,15 @@
 
 {
     "name": "Stock - Manual Quant Assignment",
-    "version": "11.0.1.0.0",
-    "category": "Warehouse Management",
+    "version": "12.0.1.0.0",
+    "category": "Warehouse",
     "license": "AGPL-3",
     "author": "AvanzOSC, "
               "Tecnativa, "
               "Eficent, "
               "Fanha Giang, "
               "Odoo Community Association (OCA)",
-    "website": "https://odoo-community.org",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "depends": [
         "stock",
     ],
