c047e6bef93748df775b585b65bf95a901c30b33
Lois Rilo <lois.rilo@eficent.com>
2019-07-31 17:38:31 +0200

    [12.0][REW] stock_warehouse_calendar: add plan days helper method
    the method `wh_plan_days` can be used in any place where planning
    or scheduling needs to be done in a given warehouse context,
    considering the working days if set.

--- stock_warehouse_calendar/models/stock_rule.py
+++ stock_warehouse_calendar/models/stock_rule.py
@@ -1,7 +1,7 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018-19 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models
+from odoo import models
 from datetime import datetime
 
 
@@ -13,11 +13,10 @@ class StockRule(models.Model):
         res = super(StockRule, self)._get_stock_move_values(
             product_id, product_qty, product_uom,
             location_id, name, origin, values, group_id)
-        dt_planned = fields.Datetime.to_datetime(values['date_planned'])
         warehouse = self.propagate_warehouse_id or self.warehouse_id
         if warehouse.calendar_id and self.delay:
-            date_expected = warehouse.calendar_id.plan_days(
-                -1 * self.delay - 1, dt_planned)
+            date_expected = warehouse.wh_plan_days(
+                values['date_planned'], -1 * self.delay)
             if date_expected > datetime.now():
                 res['date'] = date_expected
                 res['date_expected'] = date_expected
