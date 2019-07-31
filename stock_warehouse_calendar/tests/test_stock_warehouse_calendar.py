293973fa5a392fc014b925e37f5105539a16a6a3
Lois Rilo <lois.rilo@eficent.com>
2019-07-31 17:38:31 +0200

    [12.0][REW] stock_warehouse_calendar: add plan days helper method
    the method `wh_plan_days` can be used in any place where planning
    or scheduling needs to be done in a given warehouse context,
    considering the working days if set.

--- stock_warehouse_calendar/tests/test_stock_warehouse_calendar.py
+++ stock_warehouse_calendar/tests/test_stock_warehouse_calendar.py
@@ -1,4 +1,4 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018-19 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 from odoo import fields
@@ -9,6 +9,7 @@ class TestStockWarehouseCalendar(TransactionCase):
 
     def setUp(self):
         super(TestStockWarehouseCalendar, self).setUp()
+        self.wh_obj = self.env['stock.warehouse']
         self.move_obj = self.env['stock.move']
         self.company = self.env.ref('base.main_company')
         self.warehouse = self.env.ref('stock.warehouse0')
@@ -16,10 +17,14 @@ class TestStockWarehouseCalendar(TransactionCase):
         self.company_partner = self.env.ref('base.main_partner')
         self.calendar = self.env.ref('resource.resource_calendar_std')
         self.warehouse.calendar_id = self.calendar
-        self.warehouse_2 = self.env['stock.warehouse'].create({
+        self.warehouse_2 = self.wh_obj.create({
             'code': 'WH-T',
             'name': 'Warehouse Test',
             'calendar_id': self.calendar.id,
+        })
+        self.warehouse_3 = self.wh_obj.create({
+            'code': 'WH-no-calendar',
+            'name': 'Warehouse Test 2',
         })
 
         self.product = self.env['product.product'].create({
@@ -47,7 +52,7 @@ class TestStockWarehouseCalendar(TransactionCase):
         self.transfer_rule = self.env['stock.rule'].create(rule_vals)
         self.product.route_ids = [(6, 0, self.transfer_route.ids)]
 
-    def test_procurement_with_calendar(self):
+    def test_01_procurement_with_calendar(self):
         values = {
             'date_planned': '2097-01-07 09:00:00',  # Monday
             'warehouse_id': self.warehouse,
@@ -66,3 +71,37 @@ class TestStockWarehouseCalendar(TransactionCase):
         friday = fields.Date.to_date('2097-01-04 09:00:00')
 
         self.assertEquals(date_expected, friday)
+
+    def test_02_procurement_with_calendar_early(self):
+        """Test procuring at the beginning of the day, with no work intervals
+        before."""
+        values = {
+            'date_planned': '2097-01-07 01:00:00',  # Monday
+            'warehouse_id': self.warehouse,
+            'company_id': self.company,
+            'rule_id': self.transfer_rule,
+        }
+        self.env['procurement.group'].run(
+            self.product, 100,
+            self.product.uom_id,
+            self.warehouse.lot_stock_id, 'Test',
+            'Test', values)
+        move = self.env['stock.move'].search(
+            [('product_id', '=', self.product.id)], limit=1)
+        date_expected = fields.Date.to_date(move.date_expected)
+        # Friday 4th Jan 2017
+        friday = fields.Date.to_date('2097-01-04 09:00:00')
+
+        self.assertEquals(date_expected, friday)
+
+    def test_03_wh_plan_days_future(self):
+        """Test plan days helper in warehouse."""
+        reference = "2097-01-09 12:00:00"  # Wednesday
+        # With calendar
+        result = self.warehouse_2.wh_plan_days(reference, 3).date()
+        next_monday = fields.Date.to_date("2097-01-14")
+        self.assertEquals(result, next_monday)
+        # Without calendar
+        result = self.warehouse_3.wh_plan_days(reference, 3).date()
+        saturday = fields.Date.to_date("2097-01-12")
+        self.assertEquals(result, saturday)
