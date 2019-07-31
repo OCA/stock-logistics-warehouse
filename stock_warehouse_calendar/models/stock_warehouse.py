d01663adbc5e037e2fb817847e305f13aa61f2a6
Lois Rilo <lois.rilo@eficent.com>
2019-07-31 17:38:31 +0200

    [12.0][REW] stock_warehouse_calendar: add plan days helper method
    the method `wh_plan_days` can be used in any place where planning
    or scheduling needs to be done in a given warehouse context,
    considering the working days if set.

--- stock_warehouse_calendar/models/stock_warehouse.py
+++ stock_warehouse_calendar/models/stock_warehouse.py
@@ -1,5 +1,7 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018-19 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from datetime import datetime, timedelta
 
 from odoo import fields, models
 
@@ -11,3 +13,30 @@ class StockWarehouse(models.Model):
         comodel_name='resource.calendar',
         string='Working Hours',
     )
+
+    def wh_plan_days(self, date_from, delta):
+        """Helper method to schedule warehouse operations based on its
+        working days (if set).
+
+        :param datetime date_from: reference date.
+        :param integer delta: offset to apply.
+        :return: datetime: resulting date.
+        """
+        self.ensure_one()
+        if not isinstance(date_from, datetime):
+            date_from = fields.Datetime.to_datetime(date_from)
+        if delta == 0:
+            return date_from
+
+        if self.calendar_id:
+            if delta < 0:
+                # We force the date planned to be at the beginning of the day.
+                # So no work intervals are found in the reference date.
+                dt_planned = date_from.replace(hour=0)
+            else:
+                # We force the date planned at the end of the day.
+                dt_planned = date_from.replace(hour=23)
+            date_result = self.calendar_id.plan_days(delta, dt_planned)
+        else:
+            date_result = date_from + timedelta(days=delta)
+        return date_result
