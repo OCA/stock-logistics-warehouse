957559d0f28524ac9024b06a9f41bfde97f1b6db
hveficent <hector.villarreal@eficent.com>
2019-08-01 10:25:47 +0200

    [12.0][IMP] stock_request: * Add readme autogenerated * Add some hooks * Improve test inheritance (reduce testing time) * Don't allow qty <= 0 * SR expected_date = order expected_date if SRO

--- stock_request/models/stock_request_order.py
+++ stock_request/models/stock_request_order.py
@@ -301,7 +301,7 @@ class StockRequestOrder(models.Model):
                 stock_request_ids=[(0, 0, dict(
                     product_id=product.id,
                     product_uom_id=product.uom_id.id,
-                    product_uom_qty=0.0,
+                    product_uom_qty=1.0,
                     expected_date=expected,
                 )) for product in products]
             ))
