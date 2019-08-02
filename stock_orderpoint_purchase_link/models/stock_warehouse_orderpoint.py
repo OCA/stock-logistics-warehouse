cc0f7b4a5bcc4e0afaa945058df7782560b3b6a0
hveficent <hector.villarreal@eficent.com>
2019-08-02 13:53:52 +0200

    action view purchase shows non-direct linked purchases

--- stock_orderpoint_purchase_link/models/stock_warehouse_orderpoint.py
+++ stock_orderpoint_purchase_link/models/stock_warehouse_orderpoint.py
@@ -1,7 +1,9 @@
 # Copyright 2018 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+import ast
 
 from odoo import fields, models
+from odoo.osv.expression import OR
 
 
 class StockWarehouseOrderpoint(models.Model):
@@ -12,3 +14,16 @@ class StockWarehouseOrderpoint(models.Model):
         string='Purchase Order Lines', copy=False,
         readonly=True,
     )
+
+    def action_view_purchase(self):
+        """ This function returns an action that display existing
+        purchase orders of given orderpoint.
+        """
+        result = super(StockWarehouseOrderpoint, self).action_view_purchase()
+        if self.purchase_line_ids:
+            order_ids = self.purchase_line_ids.mapped('order_id')
+            result['domain'] = OR([ast.literal_eval(result['domain']),
+                                   ast.literal_eval(
+                                       "[('id','in',%s)]" % order_ids.ids)])
+
+        return result
