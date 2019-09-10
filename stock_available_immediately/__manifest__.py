8b9aab15ca06ce0ebf7f2302d74dd53abe49a871
Konrad Szymański <ko.szymanski@gmail.com>
2019-09-10 09:55:24 +0200

    module stock_available_immediately missing but need by stock_available module

---
+++ stock_available_immediately/__manifest__.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Camptocamp, Akretion, Numérigraphe
+# Copyright 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Ignore planned receptions in quantity available to promise",
+    "version": "10.0.1.0.0",
+    "depends": ["stock_available"],
+    "author": "Camptocamp,Sodexis,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Hidden",
+    'installable': True
+}
---
+++ stock_available_immediately/models/product_product.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2014 Camptocamp, Akretion, Numérigraphe
+# Copyright 2016 Sodexis
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ProductProduct(models.Model):
+    _inherit = 'product.product'
+
+    @api.multi
+    def _compute_available_quantities_dict(self):
+        res, stock_dict = super(ProductProduct,
+                                self)._compute_available_quantities_dict()
+        for product in self:
+            res[product.id]['immediately_usable_qty'] -= \
+                stock_dict[product.id]['incoming_qty']
+        return res, stock_dict
+
+    @api.depends('virtual_available', 'incoming_qty')
+    def _compute_available_quantities(self):
+        return super(ProductProduct, self)._compute_available_quantities()
