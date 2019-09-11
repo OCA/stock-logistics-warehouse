c143fe5d0b6c9ed407b819cb15f6966ae89d0e08
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-09-11 12:55:40 +0200

    fixup! 12.0 Add stock_location_attribute (split from stock_location_zone)

--- stock_location_attribute/models/stock_location.py
+++ stock_location_attribute/models/stock_location.py
@@ -3,7 +3,7 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import _, api, fields, models, SUPERUSER_ID
+from odoo import fields, models
 
 
 class StockLocation(models.Model):
@@ -17,40 +17,3 @@ class StockLocation(models.Model):
     posx = fields.Integer('Box (X)')
     posy = fields.Integer('Box (Y)')
     posz = fields.Integer('Box (Z)')
-
-    location_name_format = fields.Char(
-        'Location Name Format',
-        help="Format string that will compute the name of the location. "
-             "Use location fields. Example: "
-             "'{area}-{corridor:0>2}.{rack:0>3}"
-             ".{level:0>2}'")
-
-    @api.multi
-    @api.onchange('corridor', 'row', 'rack', 'level',
-                  'posx', 'posy', 'posz')
-    def _compute_name(self):
-        for location in self:
-            if not location.kind == 'bin':
-                continue
-            area = location
-            while area and not area.location_name_format:
-                area = area.location_id
-            if not area:
-                continue
-            template = area.location_name_format
-            # We don't want to use the full browse record as it would
-            # give too much access to internals for the users.
-            # We cannot use location.read() as we may have a NewId.
-            # We should have the record's values in the cache at this
-            # point. We must be cautious not to leak an environment through
-            # relational fields.
-            location.name = template.format(**location._cache)
-
-    @api.multi
-    @api.returns('self', lambda value: value.id)
-    def copy(self, default=None):
-        self.ensure_one()
-        default = dict(default or {})
-        if 'name' not in default:
-            default['name'] = _("%s (copy)") % (self.name)
-        return super().copy(default=default)
