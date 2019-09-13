d541809d0a610fa25cc531e99b51332ca86d46f4
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-09-13 17:21:28 +0200

    Fix recursion

--- stock_putaway_recursive/models/stock_location.py
+++ stock_putaway_recursive/models/stock_location.py
@@ -8,17 +8,9 @@ class StockLocation(models.Model):
     _inherit = 'stock.location'
 
     def get_putaway_strategy(self, product):
-        loc = super().get_putaway_strategy(product)
-        if loc != self:
-            recursive_locations = self.env.context.get(
-                '_recursive_locations', []
-            )
-            # avoid infinite recursion if putaway was already applied on loc
-            if loc.id in recursive_locations:
-                return self
-            recursive_locations.append(loc.id)
-            # apply putaway recursively
-            return loc.with_context(
-                _recursive_locations=recursive_locations
-            ).get_putaway_strategy(product)
-        return loc
+        if not self:
+            # stop recursion
+            return self
+        current_location = super().get_putaway_strategy(product)
+        next_location = current_location.get_putaway_strategy(product)
+        return next_location or current_location
