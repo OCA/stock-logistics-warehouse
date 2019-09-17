45c697e286be23d5d54792a21d4c5cfa90a8afcf
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-09-17 11:47:57 +0200

    Fix calling check_move_dest_constraint on empty recordset

--- stock_putaway_abc_move_location_dest_constraint/models/__init__.py
+++ stock_putaway_abc_move_location_dest_constraint/models/__init__.py
@@ -1,1 +1,2 @@
 from . import product_strategy
+from . import stock_location
---
+++ stock_putaway_abc_move_location_dest_constraint/models/stock_location.py
@@ -0,0 +1,15 @@
+# Copyright 2019 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+from odoo import models
+
+
+class StockLocation(models.Model):
+
+    def check_move_dest_constraint(self, line=None, product=None):
+        # As stock.putaway.rule.location_out_id is not required when
+        # stock_putaway_abc is installed, we check here that this method
+        # is called on an existing stock.location to avoid error on ensure_one
+        # in stock_move_location_dest_constraint_base
+        if not self:
+            return False
+        return super().check_move_dest_constraint(line=line, product=product)
