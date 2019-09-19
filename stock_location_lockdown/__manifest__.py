d3e918af01dc5033db0d0d3256fb46d7809447e9
clementmbr <clement.mombereau@gmail.com>
2019-09-19 19:00:30 +0200

    [MIG] stock_location_lockdown : Migration to 12.0

--- stock_location_lockdown/__manifest__.py
+++ stock_location_lockdown/__manifest__.py
@@ -1,14 +1,13 @@
-# -*- coding: utf-8 -*-
 # Copyright 2018 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
     "name": "Stock Location Lockdown",
-    "summary": "Prevent to add stock on flagged locations",
+    "summary": "Prevent to add stock on locked locations",
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
-    "version": "10.0.1.1.0",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
--- stock_location_lockdown/tests/__init__.py
+++ stock_location_lockdown/tests/__init__.py
@@ -1,5 +1,4 @@
-# -*- coding: utf-8 -*-
-# Copyright 2018 Akretion
+# Copyright 2019 Akretion
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import test_block_stock_location_entrance
