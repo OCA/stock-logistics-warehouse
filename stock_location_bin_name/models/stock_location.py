fde5b04f30d22fc940eedb6e894972c39700ca75
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-09-20 15:35:46 +0200

    Sort imports alphabetically as asked on review

--- stock_location_bin_name/models/stock_location.py
+++ stock_location_bin_name/models/stock_location.py
@@ -2,7 +2,7 @@
 # Copyright 2016-2019 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class StockLocation(models.Model):
