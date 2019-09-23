1bb3e5b0e391302e960081a387dbff3cdcb9b888
Damien Crier <damien.crier@camptocamp.com>
2019-09-23 17:06:13 +0200

    code optimization

--- stock_minimum_shelf_life/models/stock_move.py
+++ stock_minimum_shelf_life/models/stock_move.py
@@ -1,6 +1,8 @@
 # Copyright 2019 Camptocamp
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from datetime import datetime
 
+from odoo.tools.misc import DEFAULT_SERVER_DATETIME_FORMAT as dt_fmt
 from odoo import models
 
 
@@ -16,13 +18,14 @@ class StockQuant(models.Model):
 
         # filter the quants that respect the minimum shelf life date
         if "minimum_expiry_shelf_life" in self.env.context:
-            quants = self.browse()
             minimum_expiry_shelf_life = (
                 self.env.context.get('minimum_expiry_shelf_life')
             )
-            for rec in res:
-                if rec.lot_id.removal_date > minimum_expiry_shelf_life:
-                    quants |= rec
+            quants = self.search(
+                [('id', 'in', res.ids),
+                 ('lot_id.removal_date', '>', minimum_expiry_shelf_life)
+                 ]
+            )
             return quants
 
         return res
@@ -32,14 +35,24 @@ class StockMove(models.Model):
     _inherit = 'stock.move'
 
     def _action_assign(self):
-        for rec in self:
-            if rec.picking_id.group_id.sale_id:
-                min_shelf_life_date = (
-                    rec.picking_id.group_id._get_lot_min_expiration_date()
-                )
-                return super(StockMove,
-                             rec.with_context(
-                                 minimum_expiry_shelf_life=min_shelf_life_date
-                             ))._action_assign()
-            else:
-                return super()._action_assign()
+        proc_sales_moves = self.filtered(
+            lambda m: m.picking_id.group_id.sale_id
+        )
+        groups = proc_sales_moves.mapped('picking_id.group_id')
+        dates = {}
+        for k, v in [
+                (g._get_lot_min_expiration_date().strftime(dt_fmt),
+                 g.id) for g in groups]:
+            dates.setdefault(k, []).append(v)
+
+        for dt, groups_ids in dates.items():
+            min_shelf_life_date = datetime.strptime(dt, dt_fmt)
+            moves_filtered = proc_sales_moves.filtered(
+                lambda m: m.picking_id.group_id.id in groups_ids
+            )
+            super(StockMove, moves_filtered.with_context(
+                minimum_expiry_shelf_life=min_shelf_life_date
+            ))._action_assign()
+
+        # run action_assign on other moves
+        super(StockMove, self - proc_sales_moves)._action_assign()
