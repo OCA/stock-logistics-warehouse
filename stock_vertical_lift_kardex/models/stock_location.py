a35742b6b9749fc57586b8c4f43bc11331ecbf69
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-10-10 08:11:31 +0200

    fixup! fixup! fixup! Add method on location to fetch a tray

--- stock_vertical_lift_kardex/models/stock_location.py
+++ stock_vertical_lift_kardex/models/stock_location.py
@@ -1,7 +1,10 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
+import logging
 from odoo import models
+
+_logger = logging.getLogger(__name__)
 
 
 class StockLocation(models.Model):
@@ -28,8 +31,26 @@ class StockLocation(models.Model):
         the tray. If a ``cell_location`` is passed and if the hardware supports
         a way to show a cell (such as a laser pointer), it should send this
         command as well.
+
+        Useful information that could be needed for the drivers:
+
+        * Any field of `self` (name, barcode, ...) which is the current tray.
+        * Any field of `cell_location` (name, barcode, ...) which is the cell
+          in the tray.
+        * ``self.vertical_lift_shuttle_id`` is the current Shuttle, where we
+          find details about the hardware, the current mode (pick, put, ...).
+        * ``self.tray_type_id`` is the kind of tray.
+        * ``self.tray_type_id.width_per_cell`` and
+          ``self.tray_type_id.depth_per_cell`` return the size of a cell in mm.
+        * ``cell_location.posx`` and ``posy`` are the coordinate from the
+          bottom-left of the tray.
+        * ``cell_location.tray_cell_center_position()`` returns the central
+          position of the cell in mm from the bottom-left of a tray. (distance
+          from left, distance from bottom). Can be used for instance for
+          highlighting the cell using a laser pointer.
         """
         if self.vertical_lift_shuttle_id.hardware == "kardex":
             payload = self._hardware_kardex_prepare_payload()
+            _logger.debug("Sending to kardex: {}", payload)
             # TODO implement the communication with kardex
         super()._hardware_vertical_lift_tray(cell_location=cell_location)
