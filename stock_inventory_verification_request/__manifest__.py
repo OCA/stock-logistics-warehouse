bdd2cfdcea07bffadfb860a8b420d178d9d8bb3f
mreficent <miquel.raich@eficent.com>
2019-11-06 17:21:12 +0100

    [IMP] category: 'Warehouse Management' -> 'Warehouse'
    To appear in OCA shop in the good category.

--- stock_inventory_verification_request/__manifest__.py
+++ stock_inventory_verification_request/__manifest__.py
@@ -11,7 +11,7 @@
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
-    "category": "Warehouse Management",
+    "category": "Warehouse",
     "depends": [
         "stock_inventory_discrepancy",
         "mail",
