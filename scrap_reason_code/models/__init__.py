7a5c224c8f4b888be57d30799b51fd2301679c61
Bhavesh Odedra <bodedra@ursainfosystems.com>
2019-11-26 22:07:15 +0530

    Scrap reason code module

---
+++ scrap_reason_code/models/__init__.py
@@ -0,0 +1,7 @@
+# Copyright (C) 2019 IBM Corp.
+# Copyright (C) 2019 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import reason_code
+from . import stock_move
+from . import stock_scrap
