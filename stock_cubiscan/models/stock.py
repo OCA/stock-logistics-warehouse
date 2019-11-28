557eaac5da8bb3b64b241a03cac0a6c6d129a870
Akim Juillerat <akim.juillerat@camptocamp.com>
2019-11-28 17:45:15 +0100

    Integrate stock_cubiscan with product packaging types

--- stock_cubiscan/models/stock.py
+++ stock_cubiscan/models/stock.py
@@ -1,7 +1,7 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockWarehouse(models.Model):
@@ -14,6 +14,7 @@ class StockWarehouse(models.Model):
 
 class ProductPackaging(models.Model):
     _inherit = "product.packaging"
+    # FIXME: Not sure this is still the best place for this constraint
     _sql_constraints = [
         (
             'product_packaging_type_unique',
@@ -22,28 +23,3 @@ class ProductPackaging(models.Model):
             'with the same type for a given product.',
         )
     ]
-
-    # TODO move these in an addon. Warning:
-    # * 'delivery' defines the same fields and add them in the 'Delivery
-    #   Packages' view
-    # * our put-away modules (wms/stock_putaway_storage_type_strategy) will
-    #   need these fields as well
-    max_weight = fields.Float()
-    length = fields.Integer('Length (mm)', help='length in millimeters')
-    width = fields.Integer('Width (mm)', help='width in millimeters')
-    height = fields.Integer('Height (mm)', help='height in millimeters')
-    volume = fields.Float(
-        'Volume (m³)',
-        digits=(8, 4),
-        compute='_compute_volume',
-        readonly=True,
-        store=False,
-        help='volume in cubic meters',
-    )
-
-    @api.depends('length', 'width', 'height')
-    def _compute_volume(self):
-        for pack in self:
-            pack.volume = (
-                pack.length * pack.width * pack.height
-            ) / 1000.0 ** 3
