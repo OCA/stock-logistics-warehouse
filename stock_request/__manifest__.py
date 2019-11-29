2681c952cb0a709ca5fc194e1611c85f3ff91893
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2019-11-29 11:11:35 -0600

    [FIX] stock_request

--- stock_request/__manifest__.py
+++ stock_request/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Request",
     "summary": "Internal request for stock",
-    "version": "12.0.1.1.5",
+    "version": "12.0.1.1.6",
     "license": "LGPL-3",
     "website": "https://github.com/stock-logistics-warehouse",
     "author": "Eficent, "
