7393de301038e37ea01c035c0922b95d17f2b898
Jeanphy <hzh0292@qq.com>
2019-12-09 17:07:45 +0800

    Fix type error

--- stock_reserve_sale/models/sale.py
+++ stock_reserve_sale/models/sale.py
@@ -60,10 +60,9 @@ class SaleOrderLine(models.Model):
     def _get_line_rule(self):
         self.ensure_one()
         if self.route_id and self.route_id.rule_ids:
-            return self.route_id.rule_ids.sorted(
-                from odoo.fields import first
-                return first(self.route_id.rule_ids.sorted(
-                key=lambda r: (r.route_sequence, r.sequence)))
+            from odoo.fields import first
+            return first(self.route_id.rule_ids.sorted(
+            key=lambda r: (r.route_sequence, r.sequence)))
         """ Get applicable rule for this product
 
         Reproduce get suitable rule from procurement
