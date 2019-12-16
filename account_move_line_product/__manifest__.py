24e315419514657560079a60746d9356644da209
ps-tubtim <pimolnat.s@outlook.com>
2019-12-16 11:50:21 +0700

    [IMP] account_move_line_product: black, isort

--- account_move_line_product/__manifest__.py
+++ account_move_line_product/__manifest__.py
@@ -4,15 +4,13 @@
 
 {
     "name": "Account Move Line Product",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "summary": "Displays the product in the journal entries and items",
     "author": "Eficent, Odoo Community Association (OCA)",
     "website": "http://www.github.com/OCA/stock-logistics-warehouse",
     "category": "Generic",
     "depends": ["account"],
     "license": "AGPL-3",
-    "data": [
-        "views/account_move_line_view.xml",
-    ],
-    'installable': True,
+    "data": ["views/account_move_line_view.xml"],
+    "installable": True,
 }
