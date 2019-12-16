65b91ba8fa29f6c28633c2a5cf9586961e64c2fc
ps-tubtim <pimolnat.s@outlook.com>
2019-12-16 14:49:31 +0700

    [IMP] account_move_line_stock_info: black, isort

--- account_move_line_stock_info/models/stock_move.py
+++ account_move_line_stock_info/models/stock_move.py
@@ -5,17 +5,19 @@ from odoo import api, fields, models
 
 
 class StockMove(models.Model):
-    _inherit = 'stock.move'
+    _inherit = "stock.move"
 
     account_move_line_ids = fields.One2many(
-        comodel_name='account.move.line', inverse_name='stock_move_id',
-        copy=False)
+        comodel_name="account.move.line", inverse_name="stock_move_id", copy=False
+    )
 
     @api.model
-    def _prepare_account_move_line(self, qty, cost,
-                                   credit_account_id, debit_account_id):
+    def _prepare_account_move_line(
+        self, qty, cost, credit_account_id, debit_account_id
+    ):
         res = super(StockMove, self)._prepare_account_move_line(
-            qty, cost, credit_account_id, debit_account_id)
+            qty, cost, credit_account_id, debit_account_id
+        )
         for line in res:
-            line[2]['stock_move_id'] = self.id
+            line[2]["stock_move_id"] = self.id
         return res
