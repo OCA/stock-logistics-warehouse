8676d226fa3181bc11bc5194dc83d2b7d421e723
david <david.vidal@tecnativa.com>
2019-12-16 10:31:20 +0100

    [MIG] stock_orderpoint_generator: Migration to 12.0

--- stock_orderpoint_generator/__manifest__.py
+++ stock_orderpoint_generator/__manifest__.py
@@ -5,7 +5,7 @@
 {
     'name': 'Order point generator',
     'summary': 'Mass configuration of stock order points',
-    'version': '11.0.2.0.0',
+    'version': '12.0.1.0.0',
     'author': "Camptocamp, "
               "Tecnativa, "
               "Odoo Community Association (OCA)",
