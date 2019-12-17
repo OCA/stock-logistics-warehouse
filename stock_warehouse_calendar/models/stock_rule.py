d5f7bb54511a1fa8081c601c95bf105c90948387
Lois Rilo <lois.rilo@forgeflow.com>
2019-12-17 09:32:43 +0100

    [IMP] stock_warehouse_calendar: black, isort

--- stock_warehouse_calendar/models/stock_rule.py
+++ stock_warehouse_calendar/models/stock_rule.py
@@ -1,23 +1,41 @@
 # Copyright 2018-19 Eficent Business and IT Consulting Services, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import models
 from datetime import datetime
+
+from odoo import models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
-    def _get_stock_move_values(self, product_id, product_qty, product_uom,
-                               location_id, name, origin, values, group_id):
+    def _get_stock_move_values(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        values,
+        group_id,
+    ):
         res = super(StockRule, self)._get_stock_move_values(
-            product_id, product_qty, product_uom,
-            location_id, name, origin, values, group_id)
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            values,
+            group_id,
+        )
         warehouse = self.propagate_warehouse_id or self.warehouse_id
         if warehouse.calendar_id and self.delay:
             date_expected = warehouse.wh_plan_days(
-                values['date_planned'], -1 * self.delay)
+                values["date_planned"], -1 * self.delay
+            )
             if date_expected > datetime.now():
-                res['date'] = date_expected
-                res['date_expected'] = date_expected
+                res["date"] = date_expected
+                res["date_expected"] = date_expected
         return res
