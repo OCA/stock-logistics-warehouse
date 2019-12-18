d00ca8dfd14d913ad7f743980165d389d523c358
ps-tubtim <pimolnat.s@outlook.com>
2019-12-18 17:37:20 +0700

    [IMP] stock_removal_location_by_priority: black, isort
    [MIG] stock_removal_location_by_priority: Migration to 13.0

--- stock_removal_location_by_priority/__manifest__.py
+++ stock_removal_location_by_priority/__manifest__.py
@@ -4,19 +4,18 @@
 {
     "name": "Stock Removal Location by Priority",
     "summary": "Establish a removal priority on stock locations.",
-    "version": "12.0.1.0.0",
-    "author": "Eficent, "
-              "Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "Eficent, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
     "depends": ["stock"],
     "data": [
-        'security/stock_security.xml',
-        'views/res_config_settings_views.xml',
-        'views/stock_location_view.xml',
+        "security/stock_security.xml",
+        "views/res_config_settings_views.xml",
+        "views/stock_location_view.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
-    'application': False,
-    'pre_init_hook': 'pre_init_hook',
+    "installable": True,
+    "application": False,
+    "pre_init_hook": "pre_init_hook",
 }
