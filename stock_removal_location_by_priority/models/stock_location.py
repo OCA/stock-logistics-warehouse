eb211cdba22d1a8cb84673d535a2cc19ae7e5246
ps-tubtim <pimolnat.s@outlook.com>
2019-12-18 17:37:20 +0700

    [IMP] stock_removal_location_by_priority: black, isort
    [MIG] stock_removal_location_by_priority: Migration to 13.0

--- stock_removal_location_by_priority/models/stock_location.py
+++ stock_removal_location_by_priority/models/stock_location.py
@@ -6,10 +6,11 @@ from odoo import fields, models
 
 
 class StockLocation(models.Model):
-    _inherit = 'stock.location'
+    _inherit = "stock.location"
 
     removal_priority = fields.Integer(
-        string="Removal Priority", default=10,
+        string="Removal Priority",
+        default=10,
         help="This priority applies when removing stock and incoming dates "
-             "are equal.",
+        "are equal.",
     )
