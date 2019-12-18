30968aa5f9dfb9e6bd08e2e1386e8752f7d1554f
ps-tubtim <pimolnat.s@outlook.com>
2019-12-18 17:37:20 +0700

    [IMP] stock_removal_location_by_priority: black, isort
    [MIG] stock_removal_location_by_priority: Migration to 13.0

--- stock_removal_location_by_priority/init_hook.py
+++ stock_removal_location_by_priority/init_hook.py
@@ -19,30 +19,36 @@ def pre_init_hook(cr):
 
 
 def set_stock_location_removal_priority_default(cr):
-    cr.execute("""SELECT column_name
+    cr.execute(
+        """SELECT column_name
     FROM information_schema.columns
     WHERE table_name='stock_location' AND
-    column_name='removal_priority'""")
+    column_name='removal_priority'"""
+    )
     if not cr.fetchone():
-        logger.info('Creating field removal_priority on stock_location')
+        logger.info("Creating field removal_priority on stock_location")
         cr.execute(
             """
             ALTER TABLE stock_location
             ADD COLUMN removal_priority integer
             DEFAULT 10;
-            """)
+            """
+        )
 
 
 def set_stock_quant_removal_priority_default(cr):
-    cr.execute("""SELECT column_name
+    cr.execute(
+        """SELECT column_name
     FROM information_schema.columns
     WHERE table_name='stock_quant' AND
-    column_name='removal_priority'""")
+    column_name='removal_priority'"""
+    )
     if not cr.fetchone():
-        logger.info('Creating field removal_priority on stock_quant')
+        logger.info("Creating field removal_priority on stock_quant")
         cr.execute(
             """
             ALTER TABLE stock_quant
             ADD COLUMN removal_priority integer
             DEFAULT 10;
-            """)
+            """
+        )
