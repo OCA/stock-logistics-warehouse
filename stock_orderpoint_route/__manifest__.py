97ec9ea28678c1e8de216d9eb293c1c89695659a
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-19 14:19:47 +0100

    [MIG] stock_orderpoint_route: Migration to 13.0

--- stock_orderpoint_route/__manifest__.py
+++ stock_orderpoint_route/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Orderpoint Route",
     "summary": "Allows to force a route to be used when procuring from orderpoints",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/stock-logistics-warehouse",
     "author": "Eficent, Camptocamp, Odoo Community Association (OCA)",
