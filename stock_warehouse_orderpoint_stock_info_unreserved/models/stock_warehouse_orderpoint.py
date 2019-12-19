9dad4691315dc75275321d8fc26594ad9731ce06
ps-tubtim <pimolnat.s@outlook.com>
2019-12-19 10:41:03 +0700

    [IMP] stock_warehouse_orderpoint_stock_info_unreserved: black, isort

--- stock_warehouse_orderpoint_stock_info_unreserved/models/stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info_unreserved/models/stock_warehouse_orderpoint.py
@@ -3,15 +3,16 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from collections import defaultdict
+
 from odoo import api, fields, models
 
 
 class StockWarehouseOrderpoint(models.Model):
-    _inherit = 'stock.warehouse.orderpoint'
+    _inherit = "stock.warehouse.orderpoint"
 
     product_location_qty_available_not_res = fields.Float(
-        string='Quantity On Location (Unreserved)',
-        compute='_compute_product_available_qty',
+        string="Quantity On Location (Unreserved)",
+        compute="_compute_product_available_qty",
     )
 
     @api.multi
@@ -22,10 +23,13 @@ class StockWarehouseOrderpoint(models.Model):
             op_by_loc[order.location_id].add(order.id)
         for location_id, order_in_loc in op_by_loc.items():
             order_in_loc = self.browse(order_in_loc)
-            products = order_in_loc.mapped('product_id').with_context(
-                location=location_id.id
-            )._compute_qty_available_not_reserved()
+            products = (
+                order_in_loc.mapped("product_id")
+                .with_context(location=location_id.id)
+                ._compute_qty_available_not_reserved()
+            )
             for order in order_in_loc:
                 product = products[order.product_id.id]
-                order.product_location_qty_available_not_res = \
-                    product['qty_available_not_res']
+                order.product_location_qty_available_not_res = product[
+                    "qty_available_not_res"
+                ]
--- stock_warehouse_orderpoint_stock_info_unreserved/tests/test_stock_warehouse_orderpoint.py
+++ stock_warehouse_orderpoint_stock_info_unreserved/tests/test_stock_warehouse_orderpoint.py
@@ -10,17 +10,17 @@ class TestStockWarehouseOrderpoint(SavepointCase):
     def setUpClass(cls):
         super().setUpClass()
         # Get required Model
-        cls.reordering_rule_model = cls.env['stock.warehouse.orderpoint']
-        cls.stock_move_model = cls.env['stock.move']
-        cls.product_model = cls.env['product.product']
-        cls.product_ctg_model = cls.env['product.category']
+        cls.reordering_rule_model = cls.env["stock.warehouse.orderpoint"]
+        cls.stock_move_model = cls.env["stock.move"]
+        cls.product_model = cls.env["product.product"]
+        cls.product_ctg_model = cls.env["product.category"]
 
         # Get required Model data
-        cls.product_uom = cls.env.ref('uom.product_uom_unit')
-        cls.location_stock = cls.env.ref('stock.stock_location_stock')
-        cls.location_shelf1 = cls.env.ref('stock.stock_location_components')
-        cls.location_customer = cls.env.ref('stock.stock_location_customers')
-        cls.location_supplier = cls.env.ref('stock.stock_location_suppliers')
+        cls.product_uom = cls.env.ref("uom.product_uom_unit")
+        cls.location_stock = cls.env.ref("stock.stock_location_stock")
+        cls.location_shelf1 = cls.env.ref("stock.stock_location_components")
+        cls.location_customer = cls.env.ref("stock.stock_location_customers")
+        cls.location_supplier = cls.env.ref("stock.stock_location_suppliers")
 
         # Create product category and product
         cls.product_ctg = cls._create_product_category()
@@ -32,45 +32,49 @@ class TestStockWarehouseOrderpoint(SavepointCase):
     @classmethod
     def _create_product_category(cls):
         """Create a Product Category."""
-        product_ctg = cls.product_ctg_model.create({
-            'name': 'test_product_ctg',
-        })
+        product_ctg = cls.product_ctg_model.create({"name": "test_product_ctg"})
         return product_ctg
 
     @classmethod
     def _create_product(cls):
         """Create a Stockable Product."""
-        product = cls.product_model.create({
-            'name': 'Test Product',
-            'categ_id': cls.product_ctg.id,
-            'type': 'product',
-            'uom_id': cls.product_uom.id,
-        })
+        product = cls.product_model.create(
+            {
+                "name": "Test Product",
+                "categ_id": cls.product_ctg.id,
+                "type": "product",
+                "uom_id": cls.product_uom.id,
+            }
+        )
         return product
 
     @classmethod
     def create_orderpoint(cls):
         """Create a Reordering rule for the product."""
-        record = cls.reordering_rule_model.create({
-            'name': 'Reordering Rule',
-            'product_id': cls.product.id,
-            'product_min_qty': '1',
-            'product_max_qty': '5',
-            'qty_multiple': '1',
-            'location_id': cls.location_stock.id,
-        })
+        record = cls.reordering_rule_model.create(
+            {
+                "name": "Reordering Rule",
+                "product_id": cls.product.id,
+                "product_min_qty": "1",
+                "product_max_qty": "5",
+                "qty_multiple": "1",
+                "location_id": cls.location_stock.id,
+            }
+        )
         return record
 
     def create_move(self, source_location, destination_location):
-        move = self.env['stock.move'].create({
-            'name': 'Test move',
-            'product_id': self.product.id,
-            'product_uom': self.product_uom.id,
-            'product_uom_qty': 10,
-            'quantity_done': 10,
-            'location_id': source_location.id,
-            'location_dest_id': destination_location.id
-        })
+        move = self.env["stock.move"].create(
+            {
+                "name": "Test move",
+                "product_id": self.product.id,
+                "product_uom": self.product_uom.id,
+                "product_uom_qty": 10,
+                "quantity_done": 10,
+                "location_id": source_location.id,
+                "location_dest_id": destination_location.id,
+            }
+        )
 
         move._action_confirm()
         return move
@@ -78,49 +82,43 @@ class TestStockWarehouseOrderpoint(SavepointCase):
     def test_product_qty(self):
         """Tests the product quantity in the Reordering rules"""
         # Create & process moves to test the product quantity
-        move_in = self.create_move(
-            self.location_supplier,
-            self.location_stock,
-        )
-        move_out = self.create_move(
-            self.location_stock,
-            self.location_customer,
-        )
+        move_in = self.create_move(self.location_supplier, self.location_stock)
+        move_out = self.create_move(self.location_stock, self.location_customer)
         self.reordering_record.refresh()
         self.assertEqual(
             self.reordering_record.product_location_qty_available_not_res,
             0.0,
-            'Quantity On Hand (Unreserved) does not match',
+            "Quantity On Hand (Unreserved) does not match",
         )
         self.assertEqual(
             self.reordering_record.product_location_qty_available_not_res,
             self.product.qty_available_not_res,
-            'Quantity On Hand (Unreserved) in the orderpoint '
-            'does not match with the product.'
+            "Quantity On Hand (Unreserved) in the orderpoint "
+            "does not match with the product.",
         )
         move_in._action_done()
         self.reordering_record.refresh()
         self.assertEqual(
             self.reordering_record.product_location_qty_available_not_res,
             10.0,
-            'Quantity On Hand (Unreserved) does not match',
+            "Quantity On Hand (Unreserved) does not match",
         )
         self.assertEqual(
             self.reordering_record.product_location_qty_available_not_res,
             self.product.qty_available_not_res,
-            'Quantity On Hand (Unreserved) in the orderpoint '
-            'does not match with the product.',
+            "Quantity On Hand (Unreserved) in the orderpoint "
+            "does not match with the product.",
         )
         move_out._action_done()
         self.reordering_record.refresh()
         self.assertEqual(
             self.reordering_record.product_location_qty_available_not_res,
             0.0,
-            'Quantity On Hand (Unreserved) does not match',
+            "Quantity On Hand (Unreserved) does not match",
         )
         self.assertEqual(
             self.reordering_record.product_location_qty_available_not_res,
             self.product.qty_available_not_res,
-            'Quantity On Hand (Unreserved) in the orderpoint '
-            'does not match with the product.',
+            "Quantity On Hand (Unreserved) in the orderpoint "
+            "does not match with the product.",
         )
