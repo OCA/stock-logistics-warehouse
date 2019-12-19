ba113edfc9aaafa6d0f274a9dd826fa8f35ccfad
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-19 14:04:20 +0100

    [IMP] stock_orderpoint_route: black, isort

--- stock_orderpoint_route/__manifest__.py
+++ stock_orderpoint_route/__manifest__.py
@@ -2,20 +2,13 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 {
     "name": "Stock Orderpoint Route",
-    "summary": "Allows to force a route to be used when procuring from "
-               "orderpoints",
+    "summary": "Allows to force a route to be used when procuring from orderpoints",
     "version": "12.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/stock-logistics-warehouse",
-    "author": "Eficent, "
-              "Camptocamp, "
-              "Odoo Community Association (OCA)",
+    "author": "Eficent, Camptocamp, Odoo Community Association (OCA)",
     "category": "Warehouse",
-    "depends": [
-        "stock",
-    ],
-    "data": [
-        "views/stock_warehouse_orderpoint_views.xml",
-    ],
+    "depends": ["stock"],
+    "data": ["views/stock_warehouse_orderpoint_views.xml"],
     "installable": True,
 }
--- stock_orderpoint_route/models/stock_warehouse_orderpoint.py
+++ stock_orderpoint_route/models/stock_warehouse_orderpoint.py
@@ -5,30 +5,29 @@ from odoo import api, fields, models
 
 
 class StockWarehouseOrderpoint(models.Model):
-    _inherit = 'stock.warehouse.orderpoint'
+    _inherit = "stock.warehouse.orderpoint"
 
     route_ids = fields.Many2many(
-        'stock.location.route', string='Allowed routes',
-        compute='_compute_route_ids',
+        "stock.location.route", string="Allowed routes", compute="_compute_route_ids"
     )
-    route_id = fields.Many2one('stock.location.route', string='Route',
-                               domain="[('id', 'in', route_ids)]",
-                               ondelete='restrict')
+    route_id = fields.Many2one(
+        "stock.location.route",
+        string="Route",
+        domain="[('id', 'in', route_ids)]",
+        ondelete="restrict",
+    )
 
-    @api.depends('product_id', 'warehouse_id',
-                 'warehouse_id.route_ids', 'location_id')
+    @api.depends("product_id", "warehouse_id", "warehouse_id.route_ids", "location_id")
     def _compute_route_ids(self):
-        route_obj = self.env['stock.location.route']
-        for wh in self.mapped('warehouse_id'):
+        route_obj = self.env["stock.location.route"]
+        for wh in self.mapped("warehouse_id"):
             wh_routes = wh.route_ids
             for record in self.filtered(lambda r: r.warehouse_id == wh):
                 routes = route_obj.browse()
                 if record.product_id:
                     routes += record.product_id.mapped(
-                        'route_ids'
-                    ) | record.product_id.mapped(
-                        'categ_id'
-                    ).mapped('total_route_ids')
+                        "route_ids"
+                    ) | record.product_id.mapped("categ_id").mapped("total_route_ids")
                 if record.warehouse_id:
                     routes |= wh_routes
                 parents = record.get_parents()
@@ -36,8 +35,8 @@ class StockWarehouseOrderpoint(models.Model):
                     lambda route: any(
                         p.location_id in parents
                         for p in route.rule_ids.filtered(
-                            lambda rule: rule.action in ('pull', 'pull_push')
-                        ).mapped('location_src_id')
+                            lambda rule: rule.action in ("pull", "pull_push")
+                        ).mapped("location_src_id")
                     )
                 )
 
@@ -49,10 +48,7 @@ class StockWarehouseOrderpoint(models.Model):
             result |= location
         return result
 
-    def _prepare_procurement_values(self, product_qty, date=False,
-                                    group=False):
-        res = super()._prepare_procurement_values(
-            product_qty, date=date, group=group
-        )
-        res['route_ids'] = self.route_id
+    def _prepare_procurement_values(self, product_qty, date=False, group=False):
+        res = super()._prepare_procurement_values(product_qty, date=date, group=group)
+        res["route_ids"] = self.route_id
         return res
