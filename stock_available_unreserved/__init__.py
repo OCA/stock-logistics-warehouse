2650946eacc069263e421659bb228b7d80cc5b48
Lois Rilo <lois.rilo@forgeflow.com>
2019-12-19 16:19:14 +0100

    [12.0][UPD] stock_available_unreserved: * update author's name after recent rebranding of Eficent to ForgeFlow. * change license to lgpl as agreed by the authors. * update readme to new format.

--- stock_available_unreserved/__init__.py
+++ stock_available_unreserved/__init__.py
@@ -1,6 +1,1 @@
-# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
-# Copyright 2016 Eficent Business and IT Consulting Services S.L.
-#   (http://www.eficent.com)
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-
 from . import models
