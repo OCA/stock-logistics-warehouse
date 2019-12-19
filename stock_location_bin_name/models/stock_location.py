28e8b83563d0f8affd113d3a9875063aebc072a8
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2019-12-19 13:13:20 +0100

    [MIG] stock_location_bin_name: Migration to 13.0

--- stock_location_bin_name/models/stock_location.py
+++ stock_location_bin_name/models/stock_location.py
@@ -2,11 +2,38 @@
 # Copyright 2016-2019 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+import string
+
 from odoo import api, fields, models
 
 
-class StockLocation(models.Model):
+class PartialFormatter(string.Formatter):
+    def __init__(self, missing="~", bad_fmt="!"):
+        self.missing = missing
+        self.bad_fmt = bad_fmt
 
+    def get_field(self, field_name, args, kwargs):
+        # Handle a key not found
+        try:
+            val = super().get_field(field_name, args, kwargs)
+        except (KeyError, AttributeError):
+            val = None, field_name
+        return val
+
+    def format_field(self, value, spec):
+        # handle an invalid format
+        if value is None:
+            return self.missing
+        try:
+            return super().format_field(value, spec)
+        except ValueError:
+            if self.bad_fmt is not None:
+                return self.bad_fmt
+            else:
+                raise
+
+
+class StockLocation(models.Model):
     _inherit = "stock.location"
 
     location_name_format = fields.Char(
@@ -14,19 +41,10 @@ class StockLocation(models.Model):
         help="Format string that will compute the name of the location. "
         "Use location fields. Example: "
         "'{area}-{corridor:0>2}.{rack:0>3}"
-        ".{level:0>2}'",
+        ".{level:0>2}'\n"
+        "Missing fields are replaced by '~' and formatting errors by '!'.",
     )
 
-    area = fields.Char(
-        string="Area",
-        # Field used for _onchange_attribute_compute_name, so we
-        # have the name in the record's cache. Does not need to be
-        # stored as we already have 'area_location_id'
-        related="area_location_id.name",
-        readonly=True,
-    )
-
-    @api.multi
     @api.onchange("corridor", "row", "rack", "level", "posx", "posy", "posz")
     def _onchange_attribute_compute_name(self):
         for location in self:
@@ -44,4 +62,6 @@ class StockLocation(models.Model):
             # We should have the record's values in the cache at this
             # point. We must be cautious not to leak an environment through
             # relational fields.
-            location.name = template.format(**location._cache)
+            values = dict(location._cache)
+            values["area"] = area.name
+            location.name = PartialFormatter().format(template, **values)
