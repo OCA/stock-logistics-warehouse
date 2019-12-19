cdeb546919691721992d690f75333c7070dda452
ps-tubtim <pimolnat.s@outlook.com>
2019-12-19 13:26:19 +0700

    [IMP] stock_available: black, isort

--- stock_available/__manifest__.py
+++ stock_available/__manifest__.py
@@ -3,18 +3,18 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'Stock available to promise',
-    'version': '12.0.1.0.1',
-    'author': 'Numérigraphe, Sodexis, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/stock-logistics-warehouse',
-    'development_status': 'Production/Stable',
-    'category': 'Warehouse',
-    'depends': ['stock'],
-    'license': 'AGPL-3',
-    'data': [
-        'views/product_template_view.xml',
-        'views/product_product_view.xml',
-        'views/res_config_settings_views.xml',
+    "name": "Stock available to promise",
+    "version": "13.0.1.0.0",
+    "author": "Numérigraphe, Sodexis, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "development_status": "Production/Stable",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "license": "AGPL-3",
+    "data": [
+        "views/product_template_view.xml",
+        "views/product_product_view.xml",
+        "views/res_config_settings_views.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
