13f714c49c6345f69ea907c8a8b4277334e1d3d0
ps-tubtim <pimolnat.s@outlook.com>
2019-12-19 13:26:19 +0700

    [IMP] stock_available: black, isort

--- stock_available/tests/test_stock_available.py
+++ stock_available/tests/test_stock_available.py
@@ -8,71 +8,74 @@ from odoo.tests.common import TransactionCase
 class TestStockLogisticsWarehouse(TransactionCase):
     def test_res_config(self):
         """Test the config file"""
-        stock_setting = self.env['res.config.settings'].create({})
+        stock_setting = self.env["res.config.settings"].create({})
 
-        self.assertEquals(
-            stock_setting.stock_available_mrp_based_on,
-            'qty_available')
-        stock_setting.stock_available_mrp_based_on = 'immediately_usable_qty'
+        self.assertEquals(stock_setting.stock_available_mrp_based_on, "qty_available")
+        stock_setting.stock_available_mrp_based_on = "immediately_usable_qty"
         stock_setting.set_values()
         self.assertEquals(
-            stock_setting.stock_available_mrp_based_on,
-            'immediately_usable_qty')
+            stock_setting.stock_available_mrp_based_on, "immediately_usable_qty"
+        )
 
     def test01_stock_levels(self):
         """checking that immediately_usable_qty actually reflects \
            the variations in stock, both on product and template"""
-        moveObj = self.env['stock.move']
-        productObj = self.env['product.product']
-        templateObj = self.env['product.template']
-        supplier_location = self.env.ref('stock.stock_location_suppliers')
-        stock_location = self.env.ref('stock.stock_location_stock')
-        customer_location = self.env.ref('stock.stock_location_customers')
-        uom_unit = self.env.ref('uom.product_uom_unit')
+        moveObj = self.env["stock.move"]
+        productObj = self.env["product.product"]
+        templateObj = self.env["product.template"]
+        supplier_location = self.env.ref("stock.stock_location_suppliers")
+        stock_location = self.env.ref("stock.stock_location_stock")
+        customer_location = self.env.ref("stock.stock_location_customers")
+        uom_unit = self.env.ref("uom.product_uom_unit")
 
         # Create product template
-        templateAB = templateObj.create(
-            {'name': 'templAB',
-             'uom_id': uom_unit.id,
-             })
+        templateAB = templateObj.create({"name": "templAB", "uom_id": uom_unit.id})
 
         # Create product A and B
         productA = productObj.create(
-            {'name': 'product A',
-             'standard_price': 1,
-             'type': 'product',
-             'uom_id': uom_unit.id,
-             'default_code': 'A',
-             'product_tmpl_id': templateAB.id,
-             })
+            {
+                "name": "product A",
+                "standard_price": 1,
+                "type": "product",
+                "uom_id": uom_unit.id,
+                "default_code": "A",
+                "product_tmpl_id": templateAB.id,
+            }
+        )
 
         productB = productObj.create(
-            {'name': 'product B',
-             'standard_price': 1,
-             'type': 'product',
-             'uom_id': uom_unit.id,
-             'default_code': 'B',
-             'product_tmpl_id': templateAB.id,
-             })
+            {
+                "name": "product B",
+                "standard_price": 1,
+                "type": "product",
+                "uom_id": uom_unit.id,
+                "default_code": "B",
+                "product_tmpl_id": templateAB.id,
+            }
+        )
 
         # Create a stock move from INCOMING to STOCK
         stockMoveInA = moveObj.create(
-            {'location_id': supplier_location.id,
-             'location_dest_id': stock_location.id,
-             'name': 'MOVE INCOMING -> STOCK ',
-             'product_id': productA.id,
-             'product_uom': productA.uom_id.id,
-             'product_uom_qty': 2,
-             })
+            {
+                "location_id": supplier_location.id,
+                "location_dest_id": stock_location.id,
+                "name": "MOVE INCOMING -> STOCK ",
+                "product_id": productA.id,
+                "product_uom": productA.uom_id.id,
+                "product_uom_qty": 2,
+            }
+        )
 
         stockMoveInB = moveObj.create(
-            {'location_id': supplier_location.id,
-             'location_dest_id': stock_location.id,
-             'name': 'MOVE INCOMING -> STOCK ',
-             'product_id': productB.id,
-             'product_uom': productB.uom_id.id,
-             'product_uom_qty': 3,
-             })
+            {
+                "location_id": supplier_location.id,
+                "location_dest_id": stock_location.id,
+                "name": "MOVE INCOMING -> STOCK ",
+                "product_id": productB.id,
+                "product_uom": productB.uom_id.id,
+                "product_uom_qty": 3,
+            }
+        )
 
         def compare_product_usable_qty(product, value):
             """
@@ -87,16 +90,16 @@ class TestStockLogisticsWarehouse(TransactionCase):
             product.refresh()
             self.assertEqual(product.immediately_usable_qty, value)
             # Now check search function
-            domain = [('immediately_usable_qty', '=', value)]
+            domain = [("immediately_usable_qty", "=", value)]
             results = self.env[product._name].search(domain)
             self.assertIn(product.id, results.ids)
-            domain = [('immediately_usable_qty', '!=', value)]
+            domain = [("immediately_usable_qty", "!=", value)]
             results = self.env[product._name].search(domain)
             self.assertNotIn(product.id, results.ids)
-            domain = [('immediately_usable_qty', '>', value-1)]
+            domain = [("immediately_usable_qty", ">", value - 1)]
             results = self.env[product._name].search(domain)
             self.assertIn(product.id, results.ids)
-            domain = [('immediately_usable_qty', '<', value+1)]
+            domain = [("immediately_usable_qty", "<", value + 1)]
             results = self.env[product._name].search(domain)
             self.assertIn(product.id, results.ids)
 
@@ -123,14 +126,16 @@ class TestStockLogisticsWarehouse(TransactionCase):
 
         # Create a stock move from STOCK to CUSTOMER
         stockMoveOutA = moveObj.create(
-            {'location_id': stock_location.id,
-             'location_dest_id': customer_location.id,
-             'name': ' STOCK --> CUSTOMER ',
-             'product_id': productA.id,
-             'product_uom': productA.uom_id.id,
-             'product_uom_qty': 1,
-             'state': 'confirmed',
-             })
+            {
+                "location_id": stock_location.id,
+                "location_dest_id": customer_location.id,
+                "name": " STOCK --> CUSTOMER ",
+                "product_id": productA.id,
+                "product_uom": productA.uom_id.id,
+                "product_uom_qty": 1,
+                "state": "confirmed",
+            }
+        )
 
         stockMoveOutA._action_done()
         compare_product_usable_qty(productA, 1)
