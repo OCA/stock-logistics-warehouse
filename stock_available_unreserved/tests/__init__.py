452e830c27f6b8e292dea4dd4aec94d6f33777f0
Brindsoft Technologies <brindsofts@gmail.com>
2019-12-22 23:58:01 +0000

    Migrate to V13

---
+++ stock_available_unreserved/tests/__init__.py
@@ -0,0 +1,7 @@
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# Copyright 2016 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 JARSA Sistemas S.A. de C.V.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from . import test_stock_available_unreserved
