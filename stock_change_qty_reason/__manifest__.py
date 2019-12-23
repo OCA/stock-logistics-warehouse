e6ad197d3a09b9ca2884c85b8e3aa59aad60e4d8
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-23 09:38:57 +0100

    [MIG] stock_change_qty_reason: Migration to 13.0

--- stock_change_qty_reason/__manifest__.py
+++ stock_change_qty_reason/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
-# Copyright 2019 Eficent Business and IT Consulting Services S.L.
+# Copyright 2019 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Change Quantity Reason",
@@ -8,7 +8,7 @@
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["stock"],
     "data": [
@@ -18,7 +18,6 @@
         "views/stock_inventory_line_reason_view.xml",
         "views/stock_inventory_line_view.xml",
         "views/stock_inventory_view.xml",
-        "wizard/stock_product_change_qty.xml",
     ],
     "installable": True,
 }
