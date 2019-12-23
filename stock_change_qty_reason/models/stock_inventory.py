52d70da7d893624ec09b3da96f6531f347afdb2e
Adrià Gil Sorribes <adria.gil@forgeflow.com>
2019-12-23 08:53:43 +0100

    [IMP] stock_change_qty_reason: black, isort

--- stock_change_qty_reason/models/stock_inventory.py
+++ stock_change_qty_reason/models/stock_inventory.py
@@ -4,27 +4,26 @@ from odoo import api, fields, models
 
 
 class StockInventory(models.Model):
-    _inherit = 'stock.inventory'
+    _inherit = "stock.inventory"
 
-    reason = fields.Char(help='Type in a reason for the '
-                              'product quantity change')
-    preset_reason_id = fields.Many2one('stock.inventory.line.reason')
+    reason = fields.Char(help="Type in a reason for the " "product quantity change")
+    preset_reason_id = fields.Many2one("stock.inventory.line.reason")
 
     def _get_inventory_lines_values(self):
         vals = super(StockInventory, self)._get_inventory_lines_values()
         for val in vals:
             if self.preset_reason_id:
-                val['preset_reason_id'] = self.preset_reason_id.id
+                val["preset_reason_id"] = self.preset_reason_id.id
             elif self.reason:
-                val['reason'] = self.reason
+                val["reason"] = self.reason
         return vals
 
-    @api.onchange('reason')
+    @api.onchange("reason")
     def onchange_reason(self):
         for line in self.line_ids:
             line.reason = self.reason
 
-    @api.onchange('preset_reason_id')
+    @api.onchange("preset_reason_id")
     def onchange_preset_reason(self):
         for line in self.line_ids:
             line.preset_reason_id = self.preset_reason_id
