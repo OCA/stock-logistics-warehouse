5d9f7e143c6a33ec45e5d05ebad8362bb3576dee
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-01-06 16:28:59 +0100

    [IMP] stock_orderpoint_move_link: black, isort

--- stock_orderpoint_move_link/models/stock.py
+++ stock_orderpoint_move_link/models/stock.py
@@ -4,16 +4,31 @@ from odoo import models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
-    def _get_stock_move_values(self, product_id, product_qty, product_uom,
-                               location_id, name, origin, values, group_id):
+    def _get_stock_move_values(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        values,
+        group_id,
+    ):
         vals = super()._get_stock_move_values(
-            product_id, product_qty, product_uom,
-            location_id, name, origin, values, group_id)
-        if 'orderpoint_id' in values:
-            vals['orderpoint_ids'] = [(4, values['orderpoint_id'].id)]
-        elif 'orderpoint_ids' in values:
-            vals['orderpoint_ids'] = [(4, o.id)
-                                      for o in values['orderpoint_ids']]
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            values,
+            group_id,
+        )
+        if "orderpoint_id" in values:
+            vals["orderpoint_ids"] = [(4, values["orderpoint_id"].id)]
+        elif "orderpoint_ids" in values:
+            vals["orderpoint_ids"] = [(4, o.id) for o in values["orderpoint_ids"]]
         return vals
--- stock_orderpoint_move_link/models/stock_warehouse_orderpoint.py
+++ stock_orderpoint_move_link/models/stock_warehouse_orderpoint.py
@@ -6,14 +6,17 @@ from odoo import api, models
 
 
 class StockWarehouseOrderpoint(models.Model):
-    _inherit = 'stock.warehouse.orderpoint'
+    _inherit = "stock.warehouse.orderpoint"
 
     @api.multi
     def action_view_stock_picking(self):
-        action = self.env.ref('stock.action_picking_tree_all')
+        action = self.env.ref("stock.action_picking_tree_all")
         result = action.read()[0]
-        result['context'] = {}
-        picking_ids = self.env['stock.move'].search(
-            [('orderpoint_ids', 'in', self.id)]).mapped('picking_id')
-        result['domain'] = "[('id','in',%s)]" % picking_ids.ids
+        result["context"] = {}
+        picking_ids = (
+            self.env["stock.move"]
+            .search([("orderpoint_ids", "in", self.id)])
+            .mapped("picking_id")
+        )
+        result["domain"] = "[('id','in',%s)]" % picking_ids.ids
         return result
