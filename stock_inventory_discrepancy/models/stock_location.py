f20c178be51f618f374860147c978a498e96969d
hveficent <hector.villarreal@forgeflow.com>
2020-01-07 09:34:06 +0100

    [IMP] stock_inventory_discrepancy: black, isort

--- stock_inventory_discrepancy/models/stock_location.py
+++ stock_inventory_discrepancy/models/stock_location.py
@@ -1,4 +1,4 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-2020 ForgeFlow S.L.
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
@@ -6,11 +6,12 @@ from odoo import fields, models
 
 
 class StockLocation(models.Model):
-    _inherit = 'stock.location'
+    _inherit = "stock.location"
 
     discrepancy_threshold = fields.Float(
-        string='Maximum Discrepancy Rate Threshold',
+        string="Maximum Discrepancy Rate Threshold",
         digits=(3, 2),
         help="Maximum Discrepancy Rate allowed for any product when doing "
-             "an Inventory Adjustment. Thresholds defined in Locations have "
-             "preference over Warehouse's ones.")
+        "an Inventory Adjustment. Thresholds defined in Locations have "
+        "preference over Warehouse's ones.",
+    )
