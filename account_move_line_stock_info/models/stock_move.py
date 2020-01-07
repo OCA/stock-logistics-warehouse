d9e89f9692d96db0b5bbc95f45e28694ef5038a9
ps-tubtim <pimolnats@ecosoft.co.th>
2020-01-07 10:15:49 +0700

    [MIG] account_move_line_stock_info: Migration to 13.0

--- account_move_line_stock_info/models/stock_move.py
+++ account_move_line_stock_info/models/stock_move.py
@@ -1,5 +1,5 @@
-# © 2016 Eficent Business and IT Consulting Services S.L.
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+# Copyright 2019 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, fields, models
 
@@ -13,10 +13,10 @@ class StockMove(models.Model):
 
     @api.model
     def _prepare_account_move_line(
-        self, qty, cost, credit_account_id, debit_account_id
+        self, qty, cost, credit_account_id, debit_account_id, description
     ):
         res = super(StockMove, self)._prepare_account_move_line(
-            qty, cost, credit_account_id, debit_account_id
+            qty, cost, credit_account_id, debit_account_id, description
         )
         for line in res:
             line[2]["stock_move_id"] = self.id
