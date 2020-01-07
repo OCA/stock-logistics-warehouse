3e52938a3aa2efb955bc4828cc282d44d257d12a
hveficent <hector.villarreal@forgeflow.com>
2020-01-07 14:03:21 +0100

    [MIG] stock_inventory_discrepancy: Migration to 13.0

--- stock_inventory_discrepancy/models/stock_location.py
+++ stock_inventory_discrepancy/models/stock_location.py
@@ -1,5 +1,5 @@
 # Copyright 2017-2020 ForgeFlow S.L.
-#   (http://www.eficent.com)
+#   (http://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import fields, models
@@ -15,3 +15,22 @@ class StockLocation(models.Model):
         "an Inventory Adjustment. Thresholds defined in Locations have "
         "preference over Warehouse's ones.",
     )
+    propagate_discrepancy_threshold = fields.Boolean(
+        string="Propagate discrepancy threshold",
+        help="Propagate Maximum Discrepancy Rate Threshold to child locations",
+    )
+
+    def write(self, values):
+        res = super().write(values)
+        # Set the discrepancy threshold for all child locations
+        if values.get("discrepancy_threshold", False):
+            for location in self.filtered(
+                lambda loc: loc.propagate_discrepancy_threshold and loc.child_ids
+            ):
+                location.child_ids.write(
+                    {
+                        "discrepancy_threshold": values["discrepancy_threshold"],
+                        "propagate_discrepancy_threshold": True,
+                    }
+                )
+        return res
