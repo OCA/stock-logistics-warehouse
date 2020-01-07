62730dc5e5dc188274a4551fe3de08472d4e8d2a
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-07 09:39:47 +0100

    [IMP] stock_inventory_chatter: black, isort

--- stock_inventory_chatter/models/stock.py
+++ stock_inventory_chatter/models/stock.py
@@ -6,21 +6,21 @@ from odoo import api, fields, models
 
 
 class StockInventory(models.Model):
-    _name = 'stock.inventory'
-    _inherit = ['stock.inventory', 'mail.thread']
+    _name = "stock.inventory"
+    _inherit = ["stock.inventory", "mail.thread"]
 
-    partner_id = fields.Many2one(track_visibility='always')
-    state = fields.Selection(track_visibility='onchange')
-    location_id = fields.Many2one(track_visibility='always')
-    filter = fields.Selection(track_visibility='onchange')
+    partner_id = fields.Many2one(track_visibility="always")
+    state = fields.Selection(track_visibility="onchange")
+    location_id = fields.Many2one(track_visibility="always")
+    filter = fields.Selection(track_visibility="onchange")
 
     @api.multi
     def _track_subtype(self, init_values):
         self.ensure_one()
-        if 'state' in init_values and self.state == 'cancel':
-            return 'stock_inventory_chatter.mt_inventory_canceled'
-        elif 'state' in init_values and self.state == 'confirm':
-            return 'stock_inventory_chatter.mt_inventory_confirmed'
-        elif 'state' in init_values and self.state == 'done':
-            return 'stock_inventory_chatter.mt_inventory_done'
+        if "state" in init_values and self.state == "cancel":
+            return "stock_inventory_chatter.mt_inventory_canceled"
+        elif "state" in init_values and self.state == "confirm":
+            return "stock_inventory_chatter.mt_inventory_confirmed"
+        elif "state" in init_values and self.state == "done":
+            return "stock_inventory_chatter.mt_inventory_done"
         return super(StockInventory, self)._track_subtype(init_values)
