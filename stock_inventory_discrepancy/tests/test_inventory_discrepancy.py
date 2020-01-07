495df6114c5db309e1afde1932df8b00429d2bf9
hveficent <hector.villarreal@forgeflow.com>
2020-01-07 14:03:21 +0100

    [MIG] stock_inventory_discrepancy: Migration to 13.0

--- stock_inventory_discrepancy/tests/test_inventory_discrepancy.py
+++ stock_inventory_discrepancy/tests/test_inventory_discrepancy.py
@@ -1,5 +1,5 @@
 # Copyright 2017-2020 ForgeFlow S.L.
-#   (http://www.eficent.com)
+#   (http://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.exceptions import UserError
@@ -14,7 +14,6 @@ class TestInventoryDiscrepancy(TransactionCase):
         self.obj_inventory = self.env["stock.inventory"]
         self.obj_product = self.env["product.product"]
         self.obj_warehouse = self.env["stock.warehouse"]
-        self.obj_upd_qty_wizard = self.env["stock.change.product.qty"]
 
         self.product1 = self.obj_product.create(
             {"name": "Test Product 1", "type": "product", "default_code": "PROD1"}
@@ -33,8 +32,10 @@ class TestInventoryDiscrepancy(TransactionCase):
         # Create Stock manager able to force validation on inventories.
         group_stock_man = self.env.ref("stock.group_stock_manager")
         group_inventory_all = self.env.ref(
-            "stock_inventory_discrepancy." "group_stock_inventory_validation_always"
+            "stock_inventory_discrepancy.group_stock_inventory_validation_always"
         )
+        group_employee = self.env.ref("base.group_user")
+
         self.manager = self.env["res.users"].create(
             {
                 "name": "Test Manager",
@@ -53,10 +54,27 @@ class TestInventoryDiscrepancy(TransactionCase):
             }
         )
 
+        self.user_2 = self.env["res.users"].create(
+            {
+                "name": "Test User 2",
+                "login": "user_2",
+                "email": "test2.user@example.com",
+                "groups_id": [(6, 0, [group_stock_user.id, group_inventory_all.id])],
+            }
+        )
+
+        self.no_user = self.env["res.users"].create(
+            {
+                "name": "No User",
+                "login": "no_user",
+                "email": "test.no_user@example.com",
+                "groups_id": [(6, 0, [group_employee.id])],
+            }
+        )
+
         starting_inv = self.obj_inventory.create(
             {
                 "name": "Starting inventory",
-                "filter": "product",
                 "line_ids": [
                     (
                         0,
@@ -89,8 +107,7 @@ class TestInventoryDiscrepancy(TransactionCase):
         inventory = self.obj_inventory.create(
             {
                 "name": "Test Discrepancy Computation",
-                "location_id": self.test_loc.id,
-                "filter": "none",
+                "location_ids": [(4, self.test_loc.id)],
                 "line_ids": [
                     (
                         0,
@@ -131,8 +148,7 @@ class TestInventoryDiscrepancy(TransactionCase):
         inventory = self.obj_inventory.create(
             {
                 "name": "Test Forcing Validation Method",
-                "location_id": self.test_loc.id,
-                "filter": "none",
+                "location_ids": [(4, self.test_loc.id)],
                 "line_ids": [
                     (
                         0,
@@ -155,7 +171,8 @@ class TestInventoryDiscrepancy(TransactionCase):
             0.1,
             "Threshold wrongly computed in Inventory Line.",
         )
-        inventory.with_context({"normal_view": True}).action_validate()
+        inventory.with_user(self.user).action_start()
+        inventory.with_user(self.user).action_validate()
         self.assertEqual(
             inventory.over_discrepancy_line_count,
             1,
@@ -166,7 +183,7 @@ class TestInventoryDiscrepancy(TransactionCase):
             "pending",
             "Inventory Adjustment not changing to Pending to " "Approve.",
         )
-        inventory.sudo(self.manager).action_force_done()
+        inventory.with_user(self.manager).action_force_done()
         self.assertEqual(
             inventory.state,
             "done",
@@ -174,13 +191,56 @@ class TestInventoryDiscrepancy(TransactionCase):
             "not working properly.",
         )
 
+    def test_discrepancy_validation_always(self):
+        """Tests the new workflow"""
+        inventory = self.obj_inventory.create(
+            {
+                "name": "Test Forcing Validation Method",
+                "location_ids": [(4, self.test_loc.id)],
+                "line_ids": [
+                    (
+                        0,
+                        0,
+                        {
+                            "product_id": self.product1.id,
+                            "product_uom_id": self.env.ref("uom.product_uom_unit").id,
+                            "product_qty": 3.0,
+                            "location_id": self.test_loc.id,
+                        },
+                    )
+                ],
+            }
+        )
+        self.assertEqual(
+            inventory.state, "draft", "Testing Inventory wrongly configurated"
+        )
+        self.assertEqual(
+            inventory.line_ids.discrepancy_threshold,
+            0.1,
+            "Threshold wrongly computed in Inventory Line.",
+        )
+        inventory.with_user(self.user_2).action_start()
+        # User with no privileges can't validate a Inventory Adjustment.
+        with self.assertRaises(UserError):
+            inventory.with_user(self.no_user).action_validate()
+        inventory.with_user(self.user_2).action_validate()
+        self.assertEqual(
+            inventory.over_discrepancy_line_count,
+            1,
+            "Computation of over-discrepancies failed.",
+        )
+        self.assertEqual(
+            inventory.state,
+            "done",
+            "Stock Managers belongs to group Validate All inventory Adjustments",
+        )
+
     def test_warehouse_threshold(self):
         """Tests the behaviour if the threshold is set on the WH."""
         inventory = self.obj_inventory.create(
             {
                 "name": "Test Threshold Defined in WH",
-                "location_id": self.test_wh.view_location_id.id,
-                "filter": "none",
+                "location_ids": [(4, self.test_wh.view_location_id.id)],
                 "line_ids": [
                     (
                         0,
@@ -201,17 +261,23 @@ class TestInventoryDiscrepancy(TransactionCase):
             "Threshold wrongly computed in Inventory Line.",
         )
 
-    def test_update_qty_user_error(self):
-        """Test if a user error raises when a stock user tries to update the
-        qty for a product and the correction is a discrepancy over the
-        threshold."""
-        upd_qty = self.obj_upd_qty_wizard.sudo(self.user).create(
+    def test_propagate_discrepancy_threshold(self):
+        view_test_loc = self.obj_location.create(
+            {"name": "Test Location", "usage": "view", "discrepancy_threshold": 0.1}
+        )
+        child_test_loc = self.obj_location.create(
             {
-                "product_id": self.product1.id,
-                "product_tmpl_id": self.product1.product_tmpl_id.id,
-                "new_quantity": 10.0,
-                "location_id": self.test_loc.id,
+                "name": "Child Test Location",
+                "usage": "internal",
+                "discrepancy_threshold": 0.2,
+                "location_id": view_test_loc.id,
             }
         )
-        with self.assertRaises(UserError):
-            upd_qty.change_product_qty()
+        view_test_loc.write(
+            {"discrepancy_threshold": 0.3, "propagate_discrepancy_threshold": True}
+        )
+        self.assertEqual(
+            child_test_loc.discrepancy_threshold,
+            0.3,
+            "Threshold Discrepancy wrongly propagated",
+        )
