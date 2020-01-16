f1599be70bd7deeb5de2c940131fd937f59e2b6e
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-16 15:18:29 +0100

    [MIG] stock_orderpoint_uom: Migration to 13.0

--- stock_orderpoint_uom/models/procurement_group.py
+++ stock_orderpoint_uom/models/procurement_group.py
@@ -1,5 +1,6 @@
 # Copyright (C) 2018 - TODAY, Open Source Integrators
-#   (http://www.opensourceintegrators.com)
+# (http://www.opensourceintegrators.com)
+# Copyright 2019 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo import api, models
@@ -9,16 +10,40 @@ class ProcurementGroup(models.Model):
     _inherit = "procurement.group"
 
     @api.model
-    def run(
-        self, product_id, product_qty, product_uom, location_id, name, origin, values
-    ):
-        if "orderpoint_id" in values:
-            orderpoint = values.get("orderpoint_id")
-            if orderpoint.procure_uom_id and product_uom != orderpoint.procure_uom_id:
-                product_qty = product_uom._compute_quantity(
-                    product_qty, orderpoint.procure_uom_id
-                )
-                product_uom = orderpoint.procure_uom_id
-        return super(ProcurementGroup, self).run(
-            product_id, product_qty, product_uom, location_id, name, origin, values
-        )
+    def run(self, procurements):
+        # 'Procurement' is a 'namedtuple', which is not editable.
+        # The 'procurement' which needs to be edited is created new
+        # and the previous one is deleted.
+        Proc = self.env["procurement.group"].Procurement
+        indexes_to_pop = []
+        new_procs = []
+        for i, procurement in enumerate(procurements):
+            if "orderpoint_id" in procurement.values:
+                orderpoint = procurement.values.get("orderpoint_id")
+                if (
+                    orderpoint.procure_uom_id
+                    and procurement.product_uom != orderpoint.procure_uom_id
+                ):
+                    new_product_qty = procurement.product_uom._compute_quantity(
+                        procurement.product_qty, orderpoint.procure_uom_id
+                    )
+                    new_product_uom = orderpoint.procure_uom_id
+                    new_procs.append(
+                        Proc(
+                            procurement.product_id,
+                            new_product_qty,
+                            new_product_uom,
+                            procurement.location_id,
+                            procurement.name,
+                            procurement.origin,
+                            procurement.company_id,
+                            procurement.values,
+                        )
+                    )
+                    indexes_to_pop.append(i)
+        if new_procs:
+            indexes_to_pop.reverse()
+            for index in indexes_to_pop:
+                procurements.pop(index)
+        procurements.extend(new_procs)
+        return super(ProcurementGroup, self).run(procurements)
