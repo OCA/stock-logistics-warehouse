a047e2c067bbcbfb10b163fbc185f8ffdbad6766
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-01-16 09:20:46 +0100

    [IMP] stock_orderpoint_uom: black, isort

--- stock_orderpoint_uom/models/stock_warehouse_orderpoint.py
+++ stock_orderpoint_uom/models/stock_warehouse_orderpoint.py
@@ -2,25 +2,29 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError
 
 
 class Orderpoint(models.Model):
     _inherit = "stock.warehouse.orderpoint"
 
-    procure_uom_id = fields.Many2one(comodel_name='uom.uom',
-                                     string="Procurement UoM")
+    procure_uom_id = fields.Many2one(comodel_name="uom.uom", string="Procurement UoM")
 
-    @api.constrains('product_id', 'procure_uom_id')
+    @api.constrains("product_id", "procure_uom_id")
     def _check_procure_uom(self):
-        if any(orderpoint.product_uom and
-                orderpoint.procure_uom_id and
-                orderpoint.product_uom.category_id !=
-                orderpoint.procure_uom_id.category_id
-                for orderpoint in self):
-                    raise UserError(
-                        _('Error: The product default Unit of Measure and '
-                          'the procurement Unit of Measure must be in the '
-                          'same category.'))
+        if any(
+            orderpoint.product_uom
+            and orderpoint.procure_uom_id
+            and orderpoint.product_uom.category_id
+            != orderpoint.procure_uom_id.category_id
+            for orderpoint in self
+        ):
+            raise UserError(
+                _(
+                    "Error: The product default Unit of Measure and "
+                    "the procurement Unit of Measure must be in the "
+                    "same category."
+                )
+            )
         return True
