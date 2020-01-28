31d0b4ca9a5f0305ad6ffa97fc71c716c5222d16
hveficent <hector.villarreal@forgeflow.com>
2020-01-28 16:53:34 +0100

    [IMP] stock_request: black, isort

--- stock_request/models/stock_rule.py
+++ stock_request/models/stock_rule.py
@@ -5,16 +5,38 @@ from odoo import models
 
 
 class StockRule(models.Model):
-    _inherit = 'stock.rule'
+    _inherit = "stock.rule"
 
-    def _get_stock_move_values(self, product_id, product_qty, product_uom,
-                               location_id, name, origin, values, group_id):
+    def _get_stock_move_values(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        values,
+        group_id,
+    ):
         result = super(StockRule, self)._get_stock_move_values(
-            product_id, product_qty, product_uom,
-            location_id, name, origin, values, group_id)
-        if values.get('stock_request_id', False):
-            result['allocation_ids'] = [(0, 0, {
-                'stock_request_id': values.get('stock_request_id'),
-                'requested_product_uom_qty': product_qty,
-            })]
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            values,
+            group_id,
+        )
+        if values.get("stock_request_id", False):
+            result["allocation_ids"] = [
+                (
+                    0,
+                    0,
+                    {
+                        "stock_request_id": values.get("stock_request_id"),
+                        "requested_product_uom_qty": product_qty,
+                    },
+                )
+            ]
         return result
