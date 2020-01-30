6c8a1894b11ec1414a9e6a7b36607f6fea21c3b3
hveficent <hector.villarreal@forgeflow.com>
2020-01-30 13:39:10 +0100

    [MIG] stock_request_kanban: Migration to 13.0

--- stock_request_kanban/wizard/wizard_stock_request_order_kanban.py
+++ stock_request_kanban/wizard/wizard_stock_request_order_kanban.py
@@ -1,19 +1,17 @@
 # Copyright 2017 Creu Blanca
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017-2020 ForgeFlow, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
-from odoo import fields, models, _
+from odoo import _, fields, models
 from odoo.exceptions import ValidationError
 
 
 class WizardStockRequestOrderKanban(models.TransientModel):
     _name = "wizard.stock.request.order.kanban"
+    _description = "Stock Request Order Kanban Wizard"
     _inherit = "wizard.stock.request.kanban"
 
-    order_id = fields.Many2one(
-        'stock.request.order',
-        required=True
-    )
+    order_id = fields.Many2one("stock.request.order", required=True)
 
     def validate_kanban(self, barcode):
         res = super().validate_kanban(barcode)
@@ -23,30 +21,30 @@ class WizardStockRequestOrderKanban(models.TransientModel):
             self.status = _("Barcode %s is on the order") % barcode
             self.status_state = 1
             return False
-        if self.order_id.state != 'draft':
-            raise ValidationError(_(
-                'Lines only can be added on orders with draft state'))
+        if self.order_id.state != "draft":
+            raise ValidationError(
+                _("Lines only can be added on orders with draft state")
+            )
         if not self.order_id.company_id:
             self.order_id.company_id = self.kanban_id.company_id
         elif self.order_id.company_id != self.kanban_id.company_id:
-            raise ValidationError(_('Company must be the same'))
+            raise ValidationError(_("Company must be the same"))
         if (
-            self.kanban_id.procurement_group_id and
-            self.order_id.procurement_group_id !=
-                self.kanban_id.procurement_group_id
+            self.kanban_id.procurement_group_id
+            and self.order_id.procurement_group_id
+            != self.kanban_id.procurement_group_id
         ):
-            raise ValidationError(_('Procurement group must be the same'))
+            raise ValidationError(_("Procurement group must be the same"))
         if self.order_id.location_id != self.kanban_id.location_id:
-            raise ValidationError(_('Location must be the same'))
+            raise ValidationError(_("Location must be the same"))
         if self.order_id.warehouse_id != self.kanban_id.warehouse_id:
-            raise ValidationError(_('Warehouse must be the same'))
+            raise ValidationError(_("Warehouse must be the same"))
         return res
 
     def stock_request_kanban_values(self):
         res = super().stock_request_kanban_values()
-        res['order_id'] = self.order_id.id,
-        res['expected_date'] = \
-            fields.Datetime.to_string(self.order_id.expected_date),
+        res["order_id"] = (self.order_id.id,)
+        res["expected_date"] = fields.Datetime.to_string(self.order_id.expected_date)
         return res
 
     def stock_request_ending(self):
