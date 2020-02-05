6431f035452266886809d9de741581f325ed74a2
jim.hoefnagels <jim.hoefnagels@dynapps.be>
2020-02-05 11:03:56 +0100

    [IMP] stock_cycle_count: black, isort

--- stock_cycle_count/models/stock_cycle_count_rule.py
+++ stock_cycle_count/models/stock_cycle_count_rule.py
@@ -2,13 +2,14 @@
 #   (http://www.eficent.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
-from odoo import api, fields, models, _
+from datetime import datetime, timedelta
+
+from odoo import _, api, fields, models
 from odoo.exceptions import UserError, ValidationError
-from datetime import timedelta, datetime
 
 
 class StockCycleCountRule(models.Model):
-    _name = 'stock.cycle.count.rule'
+    _name = "stock.cycle.count.rule"
     _description = "Stock Cycle Counts Rules"
 
     @api.multi
@@ -19,131 +20,149 @@ class StockCycleCountRule(models.Model):
     @api.model
     def _selection_rule_types(self):
         return [
-            ('periodic', _('Periodic')),
-            ('turnover', _('Value Turnover')),
-            ('accuracy', _('Minimum Accuracy')),
-            ('zero', _('Zero Confirmation'))]
+            ("periodic", _("Periodic")),
+            ("turnover", _("Value Turnover")),
+            ("accuracy", _("Minimum Accuracy")),
+            ("zero", _("Zero Confirmation")),
+        ]
 
     @api.multi
-    @api.constrains('rule_type', 'warehouse_ids')
+    @api.constrains("rule_type", "warehouse_ids")
     def _check_zero_rule(self):
         for rec in self:
-            if rec.rule_type == 'zero' and len(rec.warehouse_ids) > 1:
+            if rec.rule_type == "zero" and len(rec.warehouse_ids) > 1:
                 raise ValidationError(
-                    _('Zero confirmation rules can only have one warehouse '
-                      'assigned.')
+                    _(
+                        "Zero confirmation rules can only have one warehouse "
+                        "assigned."
+                    )
                 )
-            if rec.rule_type == 'zero':
-                zero_rule = self.search([
-                    ('rule_type', '=', 'zero'),
-                    ('warehouse_ids', '=', rec.warehouse_ids.id)])
+            if rec.rule_type == "zero":
+                zero_rule = self.search(
+                    [
+                        ("rule_type", "=", "zero"),
+                        ("warehouse_ids", "=", rec.warehouse_ids.id),
+                    ]
+                )
                 if len(zero_rule) > 1:
                     raise ValidationError(
-                        _('You can only have one zero confirmation rule per '
-                          'warehouse.')
+                        _(
+                            "You can only have one zero confirmation rule per "
+                            "warehouse."
+                        )
                     )
 
-    @api.depends('rule_type')
+    @api.depends("rule_type")
     def _compute_rule_description(self):
-        if self.rule_type == 'periodic':
-            self.rule_description = _('Ensures that at least a defined number '
-                                      'of counts in a given period will '
-                                      'be run.')
-        elif self.rule_type == 'turnover':
-            self.rule_description = _('Schedules a count every time the total '
-                                      'turnover of a location exceeds the '
-                                      'threshold. This considers every '
-                                      'product going into/out of the location')
-        elif self.rule_type == 'accuracy':
-            self.rule_description = _('Schedules a count every time the '
-                                      'accuracy of a location goes under a '
-                                      'given threshold.')
-        elif self.rule_type == 'zero':
-            self.rule_description = _('Perform an Inventory Adjustment every '
-                                      'time a location in the warehouse runs '
-                                      'out of stock in order to confirm it is '
-                                      'truly empty.')
+        if self.rule_type == "periodic":
+            self.rule_description = _(
+                "Ensures that at least a defined number "
+                "of counts in a given period will "
+                "be run."
+            )
+        elif self.rule_type == "turnover":
+            self.rule_description = _(
+                "Schedules a count every time the total "
+                "turnover of a location exceeds the "
+                "threshold. This considers every "
+                "product going into/out of the location"
+            )
+        elif self.rule_type == "accuracy":
+            self.rule_description = _(
+                "Schedules a count every time the "
+                "accuracy of a location goes under a "
+                "given threshold."
+            )
+        elif self.rule_type == "zero":
+            self.rule_description = _(
+                "Perform an Inventory Adjustment every "
+                "time a location in the warehouse runs "
+                "out of stock in order to confirm it is "
+                "truly empty."
+            )
         else:
-            self.rule_description = _('(No description provided.)')
+            self.rule_description = _("(No description provided.)")
 
     @api.multi
-    @api.constrains('periodic_qty_per_period', 'periodic_count_period')
+    @api.constrains("periodic_qty_per_period", "periodic_count_period")
     def _check_negative_periodic(self):
         for rec in self:
             if rec.periodic_qty_per_period < 1:
                 raise ValidationError(
-                    _('You cannot define a negative or null number of counts '
-                      'per period.')
+                    _(
+                        "You cannot define a negative or null number of counts "
+                        "per period."
+                    )
                 )
             if rec.periodic_count_period < 0:
-                raise ValidationError(
-                    _('You cannot define a negative period.')
-                )
+                raise ValidationError(_("You cannot define a negative period."))
 
-    @api.onchange('location_ids')
+    @api.onchange("location_ids")
     def _onchange_locaton_ids(self):
         """Get the warehouses for the selected locations."""
         wh_ids = []
         for loc in self.location_ids:
             wh_ids.append(loc.get_warehouse().id)
         wh_ids = list(set(wh_ids))
-        self.warehouse_ids = self.env['stock.warehouse'].browse(wh_ids)
+        self.warehouse_ids = self.env["stock.warehouse"].browse(wh_ids)
 
     name = fields.Char(required=True)
     rule_type = fields.Selection(
-        selection="_selection_rule_types",
-        string='Type of rule', required=True,
+        selection="_selection_rule_types", string="Type of rule", required=True
     )
     rule_description = fields.Char(
-        string='Rule Description', compute='_compute_rule_description',
+        string="Rule Description", compute="_compute_rule_description"
     )
     active = fields.Boolean(default=True)
-    periodic_qty_per_period = fields.Integer(
-        string='Counts per period', default=1,
-    )
-    periodic_count_period = fields.Integer(string='Period in days')
+    periodic_qty_per_period = fields.Integer(string="Counts per period", default=1)
+    periodic_count_period = fields.Integer(string="Period in days")
     turnover_inventory_value_threshold = fields.Float(
-        string='Turnover Inventory Value Threshold',
+        string="Turnover Inventory Value Threshold"
     )
     currency_id = fields.Many2one(
-        comodel_name='res.currency', string='Currency',
-        compute='_compute_currency_id',
+        comodel_name="res.currency", string="Currency", compute="_compute_currency_id"
     )
     accuracy_threshold = fields.Float(
-        string='Minimum Accuracy Threshold', digits=(3, 2),
+        string="Minimum Accuracy Threshold", digits=(3, 2)
     )
     apply_in = fields.Selection(
-        string='Apply this rule in:',
-        selection=[('warehouse', 'Selected warehouses'),
-                   ('location', 'Selected Location Zones.')],
-        default='warehouse',
+        string="Apply this rule in:",
+        selection=[
+            ("warehouse", "Selected warehouses"),
+            ("location", "Selected Location Zones."),
+        ],
+        default="warehouse",
     )
     warehouse_ids = fields.Many2many(
-        comodel_name='stock.warehouse',
-        relation='warehouse_cycle_count_rule_rel', column1='rule_id',
-        column2='warehouse_id', string='Warehouses where applied',
+        comodel_name="stock.warehouse",
+        relation="warehouse_cycle_count_rule_rel",
+        column1="rule_id",
+        column2="warehouse_id",
+        string="Warehouses where applied",
     )
     location_ids = fields.Many2many(
-        comodel_name='stock.location',
-        relation='location_cycle_count_rule_rel', column1='rule_id',
-        column2='location_id', string='Zones where applied',
+        comodel_name="stock.location",
+        relation="location_cycle_count_rule_rel",
+        column1="rule_id",
+        column2="location_id",
+        string="Zones where applied",
     )
 
     def compute_rule(self, locs):
-        if self.rule_type == 'periodic':
+        if self.rule_type == "periodic":
             proposed_cycle_counts = self._compute_rule_periodic(locs)
-        elif self.rule_type == 'turnover':
+        elif self.rule_type == "turnover":
             proposed_cycle_counts = self._compute_rule_turnover(locs)
-        elif self.rule_type == 'accuracy':
+        elif self.rule_type == "accuracy":
             proposed_cycle_counts = self._compute_rule_accuracy(locs)
         return proposed_cycle_counts
 
     @api.model
     def _propose_cycle_count(self, date, location):
         cycle_count = {
-            'date': fields.Datetime.from_string(date),
-            'location': location,
-            'rule_type': self
+            "date": fields.Datetime.from_string(date),
+            "location": location,
+            "rule_type": self,
         }
         return cycle_count
 
@@ -151,22 +170,34 @@ class StockCycleCountRule(models.Model):
     def _compute_rule_periodic(self, locs):
         cycle_counts = []
         for loc in locs:
-            latest_inventory_date = self.env['stock.inventory'].search([
-                ('location_id', '=', loc.id),
-                ('state', 'in', ['confirm', 'done', 'draft'])],
-                order="date desc", limit=1).date
+            latest_inventory_date = (
+                self.env["stock.inventory"]
+                .search(
+                    [
+                        ("location_id", "=", loc.id),
+                        ("state", "in", ["confirm", "done", "draft"]),
+                    ],
+                    order="date desc",
+                    limit=1,
+                )
+                .date
+            )
             if latest_inventory_date:
                 try:
-                    period = self.periodic_count_period / \
-                        self.periodic_qty_per_period
+                    period = self.periodic_count_period / self.periodic_qty_per_period
                     next_date = fields.Datetime.from_string(
-                        latest_inventory_date) + timedelta(days=period)
+                        latest_inventory_date
+                    ) + timedelta(days=period)
                     if next_date < datetime.today():
                         next_date = datetime.today()
                 except Exception as e:
                     raise UserError(
-                        _('Error found determining the frequency of periodic '
-                          'cycle count rule. %s') % str(e))
+                        _(
+                            "Error found determining the frequency of periodic "
+                            "cycle count rule. %s"
+                        )
+                        % str(e)
+                    )
             else:
                 next_date = datetime.today()
             cycle_count = self._propose_cycle_count(next_date, loc)
@@ -175,11 +206,15 @@ class StockCycleCountRule(models.Model):
 
     @api.model
     def _get_turnover_moves(self, location, date):
-        moves = self.env['stock.move'].search([
-            '|', ('location_id', '=', location.id),
-            ('location_dest_id', '=', location.id),
-            ('date', '>', date),
-            ('state', '=', 'done')])
+        moves = self.env["stock.move"].search(
+            [
+                "|",
+                ("location_id", "=", location.id),
+                ("location_dest_id", "=", location.id),
+                ("date", ">", date),
+                ("state", "=", "done"),
+            ]
+        )
         return moves
 
     @api.model
@@ -192,9 +227,16 @@ class StockCycleCountRule(models.Model):
     def _compute_rule_turnover(self, locs):
         cycle_counts = []
         for loc in locs:
-            last_inventories = self.env['stock.inventory'].search([
-                ('location_id', '=', loc.id),
-                ('state', 'in', ['confirm', 'done', 'draft'])]).mapped('date')
+            last_inventories = (
+                self.env["stock.inventory"]
+                .search(
+                    [
+                        ("location_id", "=", loc.id),
+                        ("state", "in", ["confirm", "done", "draft"]),
+                    ]
+                )
+                .mapped("date")
+            )
             if last_inventories:
                 latest_inventory = sorted(last_inventories, reverse=True)[0]
                 moves = self._get_turnover_moves(loc, latest_inventory)
@@ -204,16 +246,18 @@ class StockCycleCountRule(models.Model):
                         turnover = self._compute_turnover(m)
                         total_turnover += turnover
                     try:
-                        if total_turnover > \
-                                self.turnover_inventory_value_threshold:
+                        if total_turnover > self.turnover_inventory_value_threshold:
                             next_date = datetime.today()
-                            cycle_count = self._propose_cycle_count(next_date,
-                                                                    loc)
+                            cycle_count = self._propose_cycle_count(next_date, loc)
                             cycle_counts.append(cycle_count)
                     except Exception as e:
-                        raise UserError(_(
-                            'Error found when comparing turnover with the '
-                            'rule threshold. %s') % str(e))
+                        raise UserError(
+                            _(
+                                "Error found when comparing turnover with the "
+                                "rule threshold. %s"
+                            )
+                            % str(e)
+                        )
             else:
                 next_date = datetime.today()
                 cycle_count = self._propose_cycle_count(next_date, loc)
--- stock_cycle_count/models/stock_location.py
+++ stock_cycle_count/models/stock_location.py
@@ -3,64 +3,66 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import logging
+from datetime import datetime
 
 from odoo import api, fields, models, tools
 from odoo.tools import DEFAULT_SERVER_DATETIME_FORMAT
-from datetime import datetime
+
 _logger = logging.getLogger(__name__)
 
 try:
     from statistics import mean
-    STATS_PATH = tools.find_in_path('statistics')
+
+    STATS_PATH = tools.find_in_path("statistics")
 except (ImportError, IOError) as err:
     _logger.debug(err)
 
 
 class StockLocation(models.Model):
-    _inherit = 'stock.location'
+    _inherit = "stock.location"
 
     @api.multi
     def _compute_loc_accuracy(self):
         for rec in self:
-            history = self.env['stock.inventory'].search([
-                ('location_id', '=', rec.id), ('state', '=', 'done')])
+            history = self.env["stock.inventory"].search(
+                [("location_id", "=", rec.id), ("state", "=", "done")]
+            )
             history = history.sorted(key=lambda r: r.write_date, reverse=True)
             if history:
                 wh = rec.get_warehouse()
-                if wh.counts_for_accuracy_qty and \
-                        len(history) > wh.counts_for_accuracy_qty:
+                if (
+                    wh.counts_for_accuracy_qty
+                    and len(history) > wh.counts_for_accuracy_qty
+                ):
                     rec.loc_accuracy = mean(
-                        history[:wh.counts_for_accuracy_qty].mapped(
-                            'inventory_accuracy'))
+                        history[: wh.counts_for_accuracy_qty].mapped(
+                            "inventory_accuracy"
+                        )
+                    )
                 else:
-                    rec.loc_accuracy = mean(
-                        history.mapped('inventory_accuracy'))
+                    rec.loc_accuracy = mean(history.mapped("inventory_accuracy"))
 
     zero_confirmation_disabled = fields.Boolean(
-        string='Disable Zero Confirmations',
-        help='Define whether this location will trigger a zero-confirmation '
-             'validation when a rule for its warehouse is defined to perform '
-             'zero-confirmations.',
+        string="Disable Zero Confirmations",
+        help="Define whether this location will trigger a zero-confirmation "
+        "validation when a rule for its warehouse is defined to perform "
+        "zero-confirmations.",
     )
     cycle_count_disabled = fields.Boolean(
-        string='Exclude from Cycle Count',
-        help='Define whether the location is going to be cycle counted.',
+        string="Exclude from Cycle Count",
+        help="Define whether the location is going to be cycle counted.",
     )
     qty_variance_inventory_threshold = fields.Float(
-        string='Acceptable Inventory Quantity Variance Threshold',
+        string="Acceptable Inventory Quantity Variance Threshold"
     )
     loc_accuracy = fields.Float(
-        string='Inventory Accuracy', compute='_compute_loc_accuracy',
-        digits=(3, 2),
+        string="Inventory Accuracy", compute="_compute_loc_accuracy", digits=(3, 2)
     )
 
     @api.multi
     def _get_zero_confirmation_domain(self):
         self.ensure_one()
-        domain = [
-            ('location_id', '=', self.id),
-            ('quantity', '>', 0.0),
-        ]
+        domain = [("location_id", "=", self.id), ("quantity", ">", 0.0)]
         return domain
 
     @api.multi
@@ -68,13 +70,14 @@ class StockLocation(models.Model):
         for rec in self:
             if not rec.zero_confirmation_disabled:
                 wh = rec.get_warehouse()
-                rule_model = self.env['stock.cycle.count.rule']
-                zero_rule = rule_model.search([
-                    ('rule_type', '=', 'zero'),
-                    ('warehouse_ids', '=', wh.id)])
+                rule_model = self.env["stock.cycle.count.rule"]
+                zero_rule = rule_model.search(
+                    [("rule_type", "=", "zero"), ("warehouse_ids", "=", wh.id)]
+                )
                 if zero_rule:
-                    quants = self.env['stock.quant'].search(
-                        rec._get_zero_confirmation_domain())
+                    quants = self.env["stock.quant"].search(
+                        rec._get_zero_confirmation_domain()
+                    )
                     if not quants:
                         rec.create_zero_confirmation_cycle_count()
 
@@ -83,30 +86,41 @@ class StockLocation(models.Model):
         self.ensure_one()
         date = datetime.today().strftime(DEFAULT_SERVER_DATETIME_FORMAT)
         wh_id = self.get_warehouse().id
-        date_horizon = self.get_warehouse().get_horizon_date().strftime(
-            DEFAULT_SERVER_DATETIME_FORMAT)
-        counts_planned = self.env['stock.cycle.count'].search([
-            ('date_deadline', '<', date_horizon), ('state', '=', 'draft'),
-            ('location_id', '=', self.id)])
+        date_horizon = (
+            self.get_warehouse()
+            .get_horizon_date()
+            .strftime(DEFAULT_SERVER_DATETIME_FORMAT)
+        )
+        counts_planned = self.env["stock.cycle.count"].search(
+            [
+                ("date_deadline", "<", date_horizon),
+                ("state", "=", "draft"),
+                ("location_id", "=", self.id),
+            ]
+        )
         if counts_planned:
-            counts_planned.write({'state': 'cancelled'})
-        rule = self.env['stock.cycle.count.rule'].search([
-            ('rule_type', '=', 'zero'), ('warehouse_ids', '=', wh_id)])
-        self.env['stock.cycle.count'].create({
-            'date_deadline': date,
-            'location_id': self.id,
-            'cycle_count_rule_id': rule.id,
-            'state': 'draft'
-        })
+            counts_planned.write({"state": "cancelled"})
+        rule = self.env["stock.cycle.count.rule"].search(
+            [("rule_type", "=", "zero"), ("warehouse_ids", "=", wh_id)]
+        )
+        self.env["stock.cycle.count"].create(
+            {
+                "date_deadline": date,
+                "location_id": self.id,
+                "cycle_count_rule_id": rule.id,
+                "state": "draft",
+            }
+        )
         return True
 
     @api.multi
     def action_accuracy_stats(self):
         self.ensure_one()
-        action = self.env.ref('stock_cycle_count.act_accuracy_stats')
+        action = self.env.ref("stock_cycle_count.act_accuracy_stats")
         result = action.read()[0]
-        result['context'] = {"search_default_location_id": self.id}
-        new_domain = result['domain'][:-1] + \
-            ", ('location_id', 'child_of', active_ids)]"
-        result['domain'] = new_domain
+        result["context"] = {"search_default_location_id": self.id}
+        new_domain = (
+            result["domain"][:-1] + ", ('location_id', 'child_of', active_ids)]"
+        )
+        result["domain"] = new_domain
         return result
