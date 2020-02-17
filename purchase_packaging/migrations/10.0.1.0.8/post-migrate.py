38bf48cc6ae05be5d20af955341b9f61bcf79ae3
Denis Roussel <denis.roussel@acsone.eu>
2020-02-17 09:41:33 +0100

    [10.0][FIX] purchase_packaging: Better product_qty migration

--- purchase_packaging/migrations/10.0.1.0.8/post-migrate.py
+++
@@ -1,9 +0,0 @@
-import odoo
-
-
-def migrate(cr, version):
-    # We recompute possible void values
-    env = odoo.api.Environment(cr, odoo.SUPERUSER_ID, {})
-    void_purchase_lines = env['purchase.order.line'].search([
-        ('product_qty', '=', False)])
-    void_purchase_lines._compute_product_qty()
