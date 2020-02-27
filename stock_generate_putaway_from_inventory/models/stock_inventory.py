ccea3655c0b0dd59c3213b7065a8bc34b4b1647f
Kevin Khao <kevinkhao@gmail.com>
2020-02-27 11:13:25 +0100

    [IMP] stock_generate_putaway_from_inventory: black

--- stock_generate_putaway_from_inventory/models/stock_inventory.py
+++ stock_generate_putaway_from_inventory/models/stock_inventory.py
@@ -7,7 +7,7 @@ from odoo.exceptions import UserError
 
 
 class StockLocation(models.Model):
-    _inherit = 'stock.location'
+    _inherit = "stock.location"
 
     def _get_putaway_strategy(self):
         if self.putaway_strategy_id:
@@ -17,24 +17,28 @@ class StockLocation(models.Model):
 
 
 class StockInventory(models.Model):
-    _inherit = 'stock.inventory'
+    _inherit = "stock.inventory"
 
     @api.multi
     def generate_putaway_strategy(self):
         putaway_locations = {}
         for inventory in self:
-            if self.state != 'done':
-                raise UserError(_(
-                    'Please, validate the stock adjustment before'))
+            if self.state != "done":
+                raise UserError(
+                    _("Please, validate the stock adjustment before")
+                )
             strategy = self.location_id._get_putaway_strategy()
             if not strategy:
-                raise UserError(_(
-                    'Please, specify a Putaway Strategy '
-                    'on the inventory\'s location (or a parent one)'))
+                raise UserError(
+                    _(
+                        "Please, specify a Putaway Strategy "
+                        "on the inventory's location (or a parent one)"
+                    )
+                )
             putaway_locations.update(self._prepare_putaway_locations(strategy))
         for putaway_location in putaway_locations.values():
-            putaway_location.pop('qty')
-            self.env['stock.product.putaway.strategy'].create(putaway_location)
+            putaway_location.pop("qty")
+            self.env["stock.product.putaway.strategy"].create(putaway_location)
 
     def _prepare_putaway_locations(self, strategy):
         self.ensure_one()
@@ -42,26 +46,29 @@ class StockInventory(models.Model):
         for line in self.line_ids:
             if line.product_id.product_putaway_ids:
                 continue
-            if (line.product_id.id not in putaway_locations
-                    or line.product_qty >
-                    putaway_locations[line.product_id.id]['qty']):
+            if (
+                line.product_id.id not in putaway_locations
+                or line.product_qty
+                > putaway_locations[line.product_id.id]["qty"]
+            ):
                 # If there is several lines for a product, we will use the
                 # one having more products
-                putaway_locations[line.product_id.id] = line.\
-                    _prepare_putaway_location(strategy)
+                putaway_locations[
+                    line.product_id.id
+                ] = line._prepare_putaway_location(strategy)
         return putaway_locations
 
 
 class StockInventoryLine(models.Model):
-    _inherit = 'stock.inventory.line'
+    _inherit = "stock.inventory.line"
 
     def _prepare_putaway_location(self, strategy):
         self.ensure_one()
         res = {
-            'qty': self.product_qty,
-            'product_product_id': self.product_id.id,
-            'product_tmpl_id': self.product_id.product_tmpl_id.id,
-            'fixed_location_id': self.location_id.id,
-            'putaway_id': strategy.id,
+            "qty": self.product_qty,
+            "product_product_id": self.product_id.id,
+            "product_tmpl_id": self.product_id.product_tmpl_id.id,
+            "fixed_location_id": self.location_id.id,
+            "putaway_id": strategy.id,
         }
         return res
