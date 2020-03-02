d1aa62920e5837402bb974a9498a98ab4678434e
Akim Juillerat <akim.juillerat@camptocamp.com>
2020-03-02 13:33:24 +0100

    Create module stock_location_children

---
+++ stock_location_children/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "Stock location children",
+    "summary": "Add relation between stock location and all its children",
+    "version": "13.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Warehouse Management",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["stock"],
+}
