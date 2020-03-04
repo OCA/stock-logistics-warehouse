6b0aea43a697801c09d3d1f048624ad7d3aa5e47
Iván Todorovich <ivan.todorovich@druidoo.io>
2020-03-04 13:16:06 +0100

    Apply suggestions from code review

--- product_history_for_cpo/__init__.py
+++ product_history_for_cpo/__init__.py
@@ -1,7 +1,1 @@
-#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
-#    Copyright (C) 2020-Today: Druidoo (<https://www.druidoo.io>)
-#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-#    @author Julien WESTE
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-
 from . import models
--- product_history_for_cpo/models/__init__.py
+++ product_history_for_cpo/models/__init__.py
@@ -1,8 +1,2 @@
-#    Copyright (C) 2013-Today GRAP (http://www.grap.coop)
-#    Copyright (C) 2020-Today: Druidoo (<https://www.druidoo.io>)
-#    License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
-#    @author Julien WESTE
-#    @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-
 from . import compute_purchase_order_line
 from . import product_history
