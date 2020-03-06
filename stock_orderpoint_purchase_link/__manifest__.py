13c620341a7d845813abf7b1707c53c89729618e
Joan Sisquella <joan.sisquella@forgeflow.com>
2020-03-06 12:45:52 +0100

    [MIG] stock_orderpoint_purchase_link: Migration to 13.0
    [UPD] Update stock_orderpoint_purchase_link.pot

--- stock_orderpoint_purchase_link/__manifest__.py
+++ stock_orderpoint_purchase_link/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2018 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2018-20 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
     "name": "Stock Orderpoint Purchase Link",
     "summary": "Link Reordering rules to purchase orders",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
-    "author": "Eficent, " "Odoo Community Association (OCA)",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Warehouse Management",
     "depends": ["stock_orderpoint_move_link", "purchase_stock"],
     "data": ["views/purchase_order_views.xml"],
