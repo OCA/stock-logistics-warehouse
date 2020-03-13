3a10b6225d7db014363e41f0bd8ca9d226acf184
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-13 14:19:14 +0700

    [IMP] stock_request_direction: black, isort

--- stock_request_direction/models/stock_request_order.py
+++ stock_request_direction/models/stock_request_order.py
@@ -5,27 +5,26 @@ from odoo import api, fields, models
 
 
 class StockRequestOrder(models.Model):
-    _inherit = 'stock.request.order'
+    _inherit = "stock.request.order"
 
-    direction = fields.Selection([('outbound', 'Outbound'),
-                                  ('inbound', 'Inbound')],
-                                 string='Direction',
-                                 states={'draft': [('readonly', False)]},
-                                 readonly=True)
+    direction = fields.Selection(
+        [("outbound", "Outbound"), ("inbound", "Inbound")],
+        string="Direction",
+        states={"draft": [("readonly", False)]},
+        readonly=True,
+    )
 
-    @api.onchange('direction')
+    @api.onchange("direction")
     def _onchange_location_id(self):
-        if self.direction == 'outbound':
+        if self.direction == "outbound":
             # Stock Location set to Partner Locations/Customers
-            self.location_id = \
-                self.company_id.partner_id.property_stock_customer.id
+            self.location_id = self.company_id.partner_id.property_stock_customer.id
         else:
             # Otherwise the Stock Location of the Warehouse
-            self.location_id = \
-                self.warehouse_id.lot_stock_id.id
+            self.location_id = self.warehouse_id.lot_stock_id.id
 
     def change_childs(self):
         super().change_childs()
-        if not self._context.get('no_change_childs', False):
+        if not self._context.get("no_change_childs", False):
             for line in self.stock_request_ids:
                 line.direction = self.direction
