5730074638749794ac554ff03111af8d6f687b6e
ps-tubtim <pimolnats@ecosoft.co.th>
2020-03-13 14:57:34 +0700

    [IMP] stock_request_picking_type: black, isort

--- stock_request_picking_type/models/stock_request_order.py
+++ stock_request_picking_type/models/stock_request_order.py
@@ -5,17 +5,34 @@ from odoo import api, fields, models
 
 
 class StockRequestOrder(models.Model):
-    _inherit = 'stock.request.order'
+    _inherit = "stock.request.order"
 
     @api.model
     def _get_default_picking_type(self):
-        return self.env['stock.picking.type'].search([
-            ('code', '=', 'stock_request_order'),
-            ('warehouse_id.company_id', 'in',
-             [self.env.context.get('company_id', self.env.user.company_id.id),
-              False])],
-            limit=1).id
+        return (
+            self.env["stock.picking.type"]
+            .search(
+                [
+                    ("code", "=", "stock_request_order"),
+                    (
+                        "warehouse_id.company_id",
+                        "in",
+                        [
+                            self.env.context.get(
+                                "company_id", self.env.user.company_id.id
+                            ),
+                            False,
+                        ],
+                    ),
+                ],
+                limit=1,
+            )
+            .id
+        )
 
     picking_type_id = fields.Many2one(
-        'stock.picking.type', 'Operation Type',
-        default=_get_default_picking_type, required=True)
+        "stock.picking.type",
+        "Operation Type",
+        default=_get_default_picking_type,
+        required=True,
+    )
