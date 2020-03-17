98988078d571c7639da53672775e2da9243d89a0
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-03-17 11:02:38 +0100

    run pre-commit with new prettiers

--- stock_vertical_lift/static/src/js/vertical_lift.js
+++ stock_vertical_lift/static/src/js/vertical_lift.js
@@ -1,56 +1,59 @@
-odoo.define('stock_vertical_lift.vertical_lift', function (require) {
+odoo.define("stock_vertical_lift.vertical_lift", function(require) {
     "use strict";
 
-    var KanbanRecord = require('web.KanbanRecord');
-    var FormController = require('web.FormController');
+    var KanbanRecord = require("web.KanbanRecord");
+    var FormController = require("web.FormController");
 
     KanbanRecord.include({
-
-        _openRecord: function () {
-            if (this.modelName === 'vertical.lift.shuttle' &&
-                this.$el.hasClass("open_shuttle_screen")) {
+        _openRecord: function() {
+            if (
+                this.modelName === "vertical.lift.shuttle" &&
+                this.$el.hasClass("open_shuttle_screen")
+            ) {
                 var self = this;
                 this._rpc({
-                    method: 'action_open_screen',
+                    method: "action_open_screen",
                     model: self.modelName,
                     args: [self.id],
-                }).then(function (action) {
-                    self.trigger_up('do_action', {action: action});
+                }).then(function(action) {
+                    self.trigger_up("do_action", {action: action});
                 });
             } else {
                 this._super.apply(this, arguments);
             }
         },
-
     });
 
     FormController.include({
-        init: function () {
+        init: function() {
             this._super.apply(this, arguments);
-            if (this.modelName.startsWith('vertical.lift.operation.')) {
-                this.call('bus_service', 'addChannel', 'notify_vertical_lift_screen');
+            if (this.modelName.startsWith("vertical.lift.operation.")) {
+                this.call("bus_service", "addChannel", "notify_vertical_lift_screen");
                 this.call(
-                    'bus_service', 'on', 'notification',
-                    this, this.vlift_bus_notification,
+                    "bus_service",
+                    "on",
+                    "notification",
+                    this,
+                    this.vlift_bus_notification
                 );
-                this.call('bus_service', 'startPolling');
+                this.call("bus_service", "startPolling");
             }
         },
-        vlift_bus_notification: function (notifications) {
+        vlift_bus_notification: function(notifications) {
             var self = this;
-            _.each(notifications, function (notification) {
+            _.each(notifications, function(notification) {
                 var channel = notification[0];
                 var message = notification[1];
-                if (channel === 'notify_vertical_lift_screen') {
+                if (channel === "notify_vertical_lift_screen") {
                     switch (message.action) {
-                    case 'refresh':
-                        self.vlift_bus_action_refresh(message.params);
-                        break;
+                        case "refresh":
+                            self.vlift_bus_action_refresh(message.params);
+                            break;
                     }
                 }
             });
         },
-        vlift_bus_action_refresh: function (params) {
+        vlift_bus_action_refresh: function(params) {
             var selectedIds = this.getSelectedIds();
             if (!selectedIds.length) {
                 return;
@@ -60,17 +63,17 @@ odoo.define('stock_vertical_lift.vertical_lift', function (require) {
                 this.reload();
             }
         },
-        destroy: function () {
-            if (this.modelName.startsWith('vertical.lift.operation.')) {
-                this.call('bus_service', 'deleteChannel',
-                    'notify_vertical_lift_screen');
+        destroy: function() {
+            if (this.modelName.startsWith("vertical.lift.operation.")) {
+                this.call(
+                    "bus_service",
+                    "deleteChannel",
+                    "notify_vertical_lift_screen"
+                );
             }
             this._super.apply(this, arguments);
         },
-
     });
 
-
     return {};
-
 });
