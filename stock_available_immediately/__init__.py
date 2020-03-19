7a6358f51e4f5a7597cc3919d5f50bf7401849ce
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2020-03-19 17:47:52 +0100

    [MIG] stock_available_immediately: Migration to 12.0
    * New README by fragments
    * Bump manifest version
    * Adapted tests
    
    [UPD] Update stock_available_immediately.pot

--- stock_available_immediately/__init__.py
+++ stock_available_immediately/__init__.py
@@ -1,6 +1,3 @@
-# Copyright 2014 Camptocamp, Akretion, Numérigraphe
-# Copyright 2016 Sodexis
-# Copyright 2019 Sergio Díaz <sergiodm.1989@gmail.com>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
