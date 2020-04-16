420241c3ea067cb399a447cd4dfb8afc9985c0d2
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-04-16 22:00:04 +0300

    [ADD] stock_picking_fillwithstock

--- stock_move_location/__init__.py
+++ stock_move_location/__init__.py
@@ -1,7 +1,3 @@
-# Copyright (C) 2011 Julius Network Solutions SARL <contact@julius.fr>
-# Copyright 2018 Camptocamp SA
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
-
 from . import wizard
 from . import models
 from .init_hook import enable_multi_locations
--- stock_move_location/__manifest__.py
+++ stock_move_location/__manifest__.py
@@ -6,7 +6,10 @@
 {
     "name": "Move Stock Location",
     "version": "15.0.1.0.0",
-    "author": "Julius Network Solutions, Odoo Community Association (OCA)",
+    "author": "Julius Network Solutions, "
+              "BCIM,"
+              "Camptocamp,"
+              "Odoo Community Association (OCA)",
     "summary": "This module allows to move all stock "
     "in a stock location to an other one.",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
@@ -17,7 +20,8 @@
         "data/stock_quant_view.xml",
         "security/ir.model.access.csv",
         "views/stock_picking_type_views.xml",
-        "wizard/stock_move_location.xml",
+        'views/stock_picking.xml',
+        'wizard/stock_move_location.xml',
     ],
     "post_init_hook": "enable_multi_locations",
 }
