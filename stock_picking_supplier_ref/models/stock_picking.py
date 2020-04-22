613f7c0764ecb2cb0d6cade091ff0042b6e5a600
@PlanetaTIC <@PlanetaTIC>
2020-04-22 18:51:48 +0200

    [FIX] Flake8

--- stock_picking_supplier_ref/models/stock_picking.py
+++ stock_picking_supplier_ref/models/stock_picking.py
@@ -2,7 +2,7 @@
 # Copyright 2017 Trey, Kilobytes de Soluciones
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockPicking(models.Model):
