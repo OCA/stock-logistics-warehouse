ea243c04d10e3f8638f26ea114cdf3d1017d9588
Iryna Vushnevska <i.vyshnevska@mobilunity.com>
2020-04-23 16:10:14 +0300

    [IMP] add posibility to move only not reserved quantity

--- stock_move_location/__manifest__.py
+++ stock_move_location/__manifest__.py
@@ -7,9 +7,9 @@
     "name": "Move Stock Location",
     "version": "15.0.1.0.0",
     "author": "Julius Network Solutions, "
-              "BCIM,"
-              "Camptocamp,"
-              "Odoo Community Association (OCA)",
+    "BCIM,"
+    "Camptocamp,"
+    "Odoo Community Association (OCA)",
     "summary": "This module allows to move all stock "
     "in a stock location to an other one.",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
@@ -20,8 +20,8 @@
         "data/stock_quant_view.xml",
         "security/ir.model.access.csv",
         "views/stock_picking_type_views.xml",
-        'views/stock_picking.xml',
-        'wizard/stock_move_location.xml',
+        "views/stock_picking.xml",
+        "wizard/stock_move_location.xml",
     ],
     "post_init_hook": "enable_multi_locations",
 }
