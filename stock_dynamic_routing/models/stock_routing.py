5e4714c26950b13fda952355cae60928a1c7677c
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-05-25 12:05:35 +0200

    Improve usability, show routing description

--- stock_dynamic_routing/models/stock_routing.py
+++ stock_dynamic_routing/models/stock_routing.py
@@ -4,7 +4,7 @@
 from collections import defaultdict
 from functools import lru_cache
 
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 def _default_sequence(model):
@@ -36,6 +36,7 @@ class StockRouting(models.Model):
     rule_ids = fields.One2many(
         comodel_name="stock.routing.rule", inverse_name="routing_id"
     )
+    routing_message = fields.Html(compute="_compute_routing_message")
 
     _sql_constraints = [
         (
@@ -44,6 +45,100 @@ class StockRouting(models.Model):
             "A routing configuration already exists for this location and picking type",
         )
     ]
+
+    def _routing_message_template(self):
+        pull_rules = self.rule_ids.filtered(lambda r: r.method == "pull")
+        push_rules = self.rule_ids.filtered(lambda r: r.method == "push")
+        pull_message = ""
+        push_message = ""
+
+        if pull_rules:
+            rule_messages = []
+            for rule in pull_rules:
+                msg = _(
+                    "If the destination of the move is already"
+                    " <strong>{rule.location_dest_id.display_name}</strong>,"
+                    " the operation type of the move is changed to"
+                    " <strong>{rule.picking_type_id.display_name}</strong>."
+                    "<br/>"
+                    "If the destination of the move is a parent location of "
+                    " <strong>{rule.location_dest_id.display_name}</strong>,"
+                    " the destination is set to "
+                    "<strong>{rule.location_dest_id.display_name}</strong> "
+                    " and the operation type of the move is changed to"
+                    " <strong>{rule.picking_type_id.display_name}</strong>."
+                    "<br/>"
+                    "If the destination of the move is unrelated to "
+                    " <strong>{rule.location_dest_id.display_name}</strong>, "
+                    "a new move is added before, from"
+                    " <strong>{rule.location_src_id.display_name}</strong> to "
+                    " <strong>{rule.location_dest_id.display_name}</strong>, "
+                    "using the operation type "
+                    " <strong>{rule.picking_type_id.display_name}</strong>."
+                ).format(rule=rule)
+                rule_messages.append("<li>" + msg + "</li>")
+            pull_message = _(
+                "<h2>Pull rules:</h2>"
+                "When a move with operation type "
+                "<strong>{routing.picking_type_id.display_name}</strong>"
+                " has a source location"
+                " <strong>{routing.location_id.display_name}</strong>"
+                " (or a sub-location), one of these rules can apply (depending"
+                " of their domain):"
+                "<ul>"
+                "{rule_messages}"
+                "</ul>"
+            ).format(routing=self, rule_messages="\n".join(rule_messages))
+
+        if push_rules:
+            rule_messages = []
+            for rule in push_rules:
+                msg = _(
+                    "If the source of the move is already"
+                    " <strong>{rule.location_src_id.display_name}</strong>"
+                    " or a sub-location, the operation type of the move"
+                    " is changed to"
+                    " <strong>{rule.picking_type_id.display_name}</strong>."
+                    "<br/>"
+                    "If the source of the move is outside or a parent location of "
+                    " <strong>{rule.location_dest_id.display_name}</strong>,"
+                    " the destination of the move is set to "
+                    " <strong>{rule.location_src_id.display_name}</strong>, "
+                    " a new move is added after it, from"
+                    " <strong>{rule.location_src_id.display_name}</strong> to "
+                    " <strong>{rule.location_dest_id.display_name}</strong> "
+                    "using the operation type "
+                    " <strong>{rule.picking_type_id.display_name}</strong>."
+                ).format(rule=rule)
+                rule_messages.append("<li>" + msg + "</li>")
+            push_message = _(
+                "<h2>Push rules:</h2>"
+                "When a move with operation type "
+                "<strong>{routing.picking_type_id.display_name}</strong>"
+                " has a destination location"
+                " <strong>{routing.location_id.display_name}</strong>"
+                " (or a sub-location), one of these rules can apply (depending"
+                " of their domain):"
+                "<ul>"
+                "{rule_messages}"
+                "</ul>"
+            ).format(routing=self, rule_messages="\n".join(rule_messages))
+        return pull_message + "<br/>" + push_message
+
+    @api.depends(
+        "location_id", "picking_type_id", "rule_ids.method", "rule_ids.picking_type_id"
+    )
+    def _compute_routing_message(self):
+        """Generate a description of the routing for humans"""
+        for routing in self:
+            if not (
+                routing.picking_type_id and routing.location_id and routing.rule_ids
+            ):
+                routing.routing_message = ""
+                continue
+            routing.routing_message = routing._routing_message_template().format(
+                routing=routing
+            )
 
     def _default_sequence(self):
         return _default_sequence(self)
