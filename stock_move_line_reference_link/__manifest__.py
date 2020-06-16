0138cc999cb3ec468d61879722fdf4c3c62ad94f
mreficent <miquel.raich@eficent.com>
2020-06-16 12:29:51 +0200

    stock_move_line_reference_link

---
+++ stock_move_line_reference_link/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Stock Move Line Reference Link",
+    "summary": "Add link in stock move line references.",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "category": "Warehouse Management",
+    "depends": ["stock"],
+    "data": ["views/move_line_views.xml"],
+    "installable": True,
+}
