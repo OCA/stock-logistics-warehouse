8fa0d48b4c47ef84d01ca7bd5dc7c6c668996c3e
eLBati <lb@takobi.online>
2020-07-02 17:13:14 +0200

    ADD stock_picking_type_user_restriction

---
+++ stock_picking_type_user_restriction/models/stock_picking_type.py
@@ -0,0 +1,10 @@
+from odoo import models, fields
+
+
+class PickingType(models.Model):
+    _inherit = 'stock.picking.type'
+    assigned_user_ids = fields.Many2many(
+        "res.users", string="Assigned users",
+        help="Restrict some users to only access their assigned operation types. "
+             "In order to apply the restriction, the user needs the "
+             "'User: Assigned Operation Types Only' group")
