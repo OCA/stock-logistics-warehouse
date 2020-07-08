ab62cbc7dc28dd2c6c975800f94301c1707674b0
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2020-07-08 15:01:51 +0200

    Raise priority only if the direct next move has consolidation
    In the following graph:
    
                      PICK/001 ━►  PACK/001  ┓
                                             ┃
                      PICK/002 ┓             ┣━► OUT/001
                               ┣━► PACK/002  ┛
           INT/001 ━► PICK/003 ┛
    
    If we have the consolidate flag on "PACK", we want to raise the priority
    only when we start to move goods in PACK/001 or PACK/002 (not INT/001),
    because this is in the packing zone that we are limited in space.
    
    Also, when for instance a move of PICK/002 or PICK/003 is set to done,
    *any* move (all products) that go to PACK/002 are concerned by the
    priority raise: we want to finish the transfer.

--- stock_picking_consolidation_priority/models/stock_move.py
+++ stock_picking_consolidation_priority/models/stock_move.py
@@ -21,80 +21,81 @@ class StockMove(models.Model):
 
         Consider this chain of moves::
 
-            PICK/001 ━►  PACK/001  ┓
-                                   ┃
-            PICK/002 ┓             ┣━► OUT/001
-                     ┣━► PACK/002  ┛
-            PICK/003 ┛
+                      PICK/001 ━►  PACK/001  ┓
+                                             ┃
+                      PICK/002 ┓             ┣━► OUT/001
+                               ┣━► PACK/002  ┛
+           INT/001 ━► PICK/003 ┛
 
         If the flag "consolidate_priority" is set on the picking type of OUT,
-        when we set any of the PICK to done, the 3 PICK and the 2 PACK must
-        be returned to have their priority raised.
+        as soon as one of the move of PACK/001 or PACK/002 is done, all the
+        moves of the INT, 3 PICK, and the 2 PACK must be returned to have their
+        priority raised, as we want to consolidate everything asap in OUT/001.
 
-        If the flag is set on PACK and PICK/002 or PICK/003 is set to done, the
-        other PICK that leads to PACK/002 must be returned to have its priority
-        raised. But if only PICK/001 is set to done, nothing is returned.
+        If the flag is set on PACK and one move in PICK/001 is set to done,
+        other moves of PICK/001 are returned to finish PACK/001.
+
+        If the flag is set on PACK and one move in PICK/002 is set to done, all
+        the moves of INT/001 and PICK/003 and the other moves of PICK/002 have
+        to be returned as they will help to consolidate PACK/002.
+
+
+        If the flag is set on PACK, when a move in INT/001 is set to done,
+        nothing happens, but when a move in PICK/003 is set to done, all the
+        moves of INT/001 and PICK/002 and the other moves of PICK/003 have to
+        be returned as they will help to consolidate PACK/002.
 
         If the flag is set on both PACK and OUT, the result is the same as the
         first case: all PICK and PACK are returned.
         """
         query = """
         WITH RECURSIVE
-
-        -- Walk through all relations in stock_move_move_rel to find the
-        -- destination moves. For each move, join on stock_picking_type
-        -- to check if the flag "consolidate_priority" is set
-        destinations (id, consolidate_origins) AS (
-            -- starting move
-            SELECT stock_move.id,
-                   -- the moves here are done: their origin moves are
-                   -- supposed to be done too, we don't need to raise their
-                   -- priority
-                   false as consolidate_origins
-            FROM stock_move
-            WHERE stock_move.id IN %s
-            UNION
-            -- recurse to find all the destinations
-            SELECT move_dest.id,
-                   stock_picking_type.consolidate_priority as consolidate_origins
-            FROM stock_move move_dest
-            INNER JOIN stock_move_move_rel
+        -- For every destination move for which we have the flag set, walk back
+        -- through stock_move_move_rel to find all the origin moves
+        origins (id, picking_id, is_consolidation_dest) AS (
+            -- Find the destination move of the current moves which have the flag
+            -- consolidate_priority on their picking type. From there, find all
+            -- the other moves of the consolidation transfer.
+            -- They are the starting point to search the origin moves.
+            -- In the final query, the rows with "consolidate_priority" equal
+            -- to true are excluded, because we want to raise the priority of
+            -- the moves *before* the picking type with the flag
+            SELECT consolidation_dest_moves.id,
+                   stock_picking.id,
+                   stock_picking_type.consolidate_priority as is_consolidation_dest
+            FROM stock_move_move_rel
+            INNER JOIN stock_move move_dest
             ON move_dest.id = stock_move_move_rel.move_dest_id
             INNER JOIN stock_picking
             ON stock_picking.id = move_dest.picking_id
+            -- select *all* the moves of the transfer with the consolidation flag,
+            -- origin moves will be searched for all of them in the recursive part
+            INNER JOIN stock_move consolidation_dest_moves
+            ON consolidation_dest_moves.picking_id = stock_picking.id
             INNER JOIN stock_picking_type
             ON stock_picking_type.id = stock_picking.picking_type_id
-            INNER JOIN destinations
-            ON destinations.id = stock_move_move_rel.move_orig_id
-        ),
+            WHERE stock_move_move_rel.move_orig_id IN %s
+            AND stock_picking_type.consolidate_priority = true
 
-        -- For every destination move for which we have the flag set, walk back
-        -- through stock_move_move_rel to find all the origin moves
-        origins (id, consolidate_origins) AS (
-            -- for all the destinations which have to consolidate their origins,
-            -- it finds all the origin moves, in the final query, the rows with
-            -- "consolidate_origins" equal to true are excluded, because we want
-            -- to raise the priority of the moves *before* the picking type with
-            -- the flag
-            SELECT destinations.id, true
-            FROM destinations
-            WHERE consolidate_origins = true
-            -- We use union here to keep duplicate in case a move both has the flag
-            -- "consolidate_priority" on its picking type AND is the origin move
-            -- for another move with the flag. Anyway, the final query filters
+            -- We use union here to keep duplicate in case a move both has the
+            -- flag "consolidate_priority" on its picking type AND is the
+            -- origin move for another move with the flag (e.g, option
+            -- activated both on PACK and OUT). Anyway, the final query filters
             -- on the second part of the union.
             UNION ALL
+
             -- recurse to find all the origin moves which have a destination that
             -- needs priority consolidation
-            SELECT move_orig.id, false
+            SELECT move_orig.id,
+                   move_orig.picking_id,
+                   false as is_consolidation_dest
             FROM stock_move move_orig
             INNER JOIN stock_move_move_rel
             ON move_orig.id = stock_move_move_rel.move_orig_id
             INNER JOIN origins
             ON origins.id = stock_move_move_rel.move_dest_id
         )
-        SELECT DISTINCT id FROM origins
-        WHERE consolidate_origins = false
+        SELECT id FROM origins WHERE is_consolidation_dest = false
         """
         return (query, (tuple(self.ids),))
 
@@ -105,6 +106,9 @@ class StockMove(models.Model):
         return {"priority": self._consolidate_priority_value}
 
     def _consolidate_priority(self):
+        self.flush(["move_dest_ids", "move_orig_ids", "picking_id"])
+        self.env["stock.picking"].flush(["picking_type_id"])
+        self.env["stock.picking.type"].flush(["consolidate_priority"])
         query, params = self._query_get_consolidate_moves()
         self.env.cr.execute(query, params)
         move_ids = [row[0] for row in self.env.cr.fetchall()]
