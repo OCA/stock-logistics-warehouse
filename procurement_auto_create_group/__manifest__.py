a6b14a4ff32645a3447c9654ac7d665c6cd899f7
HviorForgeFlow <hector.villarreal@forgeflow.com>
2020-09-08 11:08:15 +0200

    [MIG] procurement_auto_create_group: Migration to 13.0

--- procurement_auto_create_group/__manifest__.py
+++ procurement_auto_create_group/__manifest__.py
@@ -1,13 +1,13 @@
-# Copyright 2017 Eficent Business and IT Consulting Services, S.L.
+# Copyright 2017-2020 ForgeFlow, S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Procurement Auto Create Group",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "development_status": "Production/Stable",
     "license": "AGPL-3",
     "summary": "Allows to configure the system to propose automatically new "
     "procurement groups during the procurement run.",
-    "author": "Eficent," "Odoo Community Association (OCA)",
+    "author": "ForgeFlow," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
     "depends": ["stock"],
