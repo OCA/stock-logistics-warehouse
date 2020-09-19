1213385e6c2c33a7fc288cdc8297fa8fe3f34cc0
Deivis Laya <deivis@vauxoo.com>
2020-09-19 18:34:23 +0000

    [MIG] stock_mts_mto_rule: Migration to 13.0

--- stock_mts_mto_rule/__manifest__.py
+++ stock_mts_mto_rule/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock MTS+MTO Rule",
     "summary": "Add a MTS+MTO route",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.1",
     "development_status": "Mature",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
@@ -11,6 +11,6 @@
     "license": "AGPL-3",
     "application": False,
     "installable": True,
-    "depends": ["stock",],
-    "data": ["data/stock_data.xml", "view/pull_rule.xml", "view/warehouse.xml",],
+    "depends": ["stock"],
+    "data": ["data/stock_data.xml", "view/pull_rule.xml", "view/warehouse.xml"],
 }
