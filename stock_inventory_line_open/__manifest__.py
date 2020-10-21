25012d8d162a7ed0a07a22d1cf7f898f9aea11e7
Lois Rilo <lois.rilo@forgeflow.com>
2020-10-21 16:50:26 +0200

    [13.0][ADD] stock_inventory_line_open

---
+++ stock_inventory_line_open/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Inventory Line Open",
+    "version": "13.0.1.0.0",
+    "author": "ForgeFlow," "Odoo Community Association (OCA)",
+    "development_status": "Beta",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse",
+    "summary": "Open inventory lines on validated inventory adjustments",
+    "depends": ["stock"],
+    "data": ["views/stock_inventory_view.xml"],
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+}
