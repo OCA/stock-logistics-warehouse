cd4680d27eef70b57f1b4a7416330096c45c5ee3
Víctor Martínez <victor.martinez@tecnativa.com>
2020-10-27 17:18:12 +0100

    [IMP] stock_putaway_product_form: black, isort, prettier

--- stock_putaway_product_template/models/putaway_strategy.py
+++ stock_putaway_product_template/models/putaway_strategy.py
@@ -5,14 +5,15 @@ from odoo import api, fields, models
 
 
 class PutAwayStrategy(models.Model):
-    _inherit = 'product.putaway'
+    _inherit = "product.putaway"
 
     # Remove product domain to allow to select product templates
     product_location_ids = fields.One2many(domain=[])
 
     def _get_putaway_rule(self, product):
-        return super(PutAwayStrategy, self.with_context(
-            filter_putaway_rule=True))._get_putaway_rule(product)
+        return super(
+            PutAwayStrategy, self.with_context(filter_putaway_rule=True)
+        )._get_putaway_rule(product)
 
 
 class FixedPutAwayStrategy(models.Model):
@@ -26,23 +27,25 @@ class FixedPutAwayStrategy(models.Model):
         ondelete="cascade",
     )
 
-    @api.depends('product_id')
+    @api.depends("product_id")
     def _compute_product_tmpl_id(self):
         for rec in self:
             if rec.product_id:
                 rec.product_tmpl_id = rec.product_id.product_tmpl_id
             else:
-                params = self.env.context.get('params', {})
-                if params.get('model', '') == 'product.template':
-                    rec.product_tmpl_id = params.get('id', False)
+                params = self.env.context.get("params", {})
+                if params.get("model", "") == "product.template":
+                    rec.product_tmpl_id = params.get("id", False)
 
     def filtered(self, func):
         res = super(FixedPutAwayStrategy, self).filtered(func)
-        if res or not self.env.context.get('filter_putaway_rule'):
+        if res or not self.env.context.get("filter_putaway_rule"):
             return res
         product = func.__closure__[0].cell_contents
-        if product._name != 'product.product':
+        if product._name != "product.product":
             return res
         return self.with_context(filter_putaway_rule=False).filtered(
-            lambda x: (x.product_tmpl_id == product.product_tmpl_id and
-                       not x.product_id))
+            lambda x: (
+                x.product_tmpl_id == product.product_tmpl_id and not x.product_id
+            )
+        )
