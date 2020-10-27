66be5fa431f9b5ad55a12cddefaa6c8a272db866
Víctor Martínez <victor.martinez@tecnativa.com>
2020-10-27 17:18:12 +0100

    [IMP] stock_putaway_product_form: black, isort, prettier

--- stock_putaway_product_template/hooks.py
+++ stock_putaway_product_template/hooks.py
@@ -2,6 +2,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 import logging
+
 from openupgradelib import openupgrade
 
 _logger = logging.getLogger(__name__)
@@ -9,11 +10,12 @@ _logger = logging.getLogger(__name__)
 
 def post_init_hook(cr, registry):
     openupgrade.logged_query(
-        cr, """
+        cr,
+        """
         UPDATE stock_fixed_putaway_strat sfps
         SET product_tmpl_id=pp.product_tmpl_id
         FROM product_product pp
             WHERE pp.id=sfps.product_id AND
                 sfps.product_tmpl_id <> pp.product_tmpl_id
-        """
+        """,
     )
