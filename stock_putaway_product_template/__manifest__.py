bb7d24c178f588546c469a95d16e188508059f19
Víctor Martínez <victor.martinez@tecnativa.com>
2020-10-28 10:07:42 +0100

    [MIG] stock_putaway_product_template: Migration to 13.0

--- stock_putaway_product_template/__manifest__.py
+++ stock_putaway_product_template/__manifest__.py
@@ -1,16 +1,13 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Putaway strategies on product form view",
-    "summary": "Edit putaway strategies directly from the product form view",
-    "version": "12.0.1.0.0",
+    "name": "Product template in putaway strategies",
+    "summary": "Add product template in putaway strategies from the product view",
+    "version": "13.0.1.0.0",
     "category": "Inventory",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Akretion, Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["stock"],
-    "external_dependencies": {"python": ["openupgradelib"]},
-    "data": ["views/product.xml"],
-    "demo": ["demo/putaway_strategies.xml"],
+    "data": ["views/stock_putaway_rule_views.xml"],
     "maintainers": ["kevinkhao", "sebastienbeau"],
-    "post_init_hook": "post_init_hook",
 }
