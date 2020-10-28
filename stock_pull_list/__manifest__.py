f9eaaa00dd4ccd9630633097de931c2e6cda1c80
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2020-10-28 06:46:09 +0100

    [MIG] : Migration to 13.0

--- stock_pull_list/__manifest__.py
+++ stock_pull_list/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Pull List",
     "summary": "The pull list checks the stock situation and calculates "
     "needed quantities.",
-    "version": "12.0.1.0.1",
+    "version": "13.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
