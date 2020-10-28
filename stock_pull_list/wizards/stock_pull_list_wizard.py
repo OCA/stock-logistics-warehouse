be26d39895f0c0ce20defc5863ddb66aedb187b1
Jordi Ballester Alomar <jordi.ballester@eficent.com>
2020-10-28 06:17:14 +0100

    [IMP] : black, isort, prettier

--- stock_pull_list/wizards/stock_pull_list_wizard.py
+++ stock_pull_list/wizards/stock_pull_list_wizard.py
@@ -1,10 +1,10 @@
 # Copyright 2020 ForgeFlow, S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
+import itertools
+
 from odoo import _, api, fields, models
 from odoo.exceptions import UserError
-
-import itertools
 
 
 class PullListWizard(models.TransientModel):
@@ -16,20 +16,15 @@ class PullListWizard(models.TransientModel):
         res = super().default_get(fields)
         company = self.env.user.company_id
         wh = self.env["stock.warehouse"].search(
-            [("company_id", "=", company.id)], limit=1)
-        res.update({
-            "warehouse_id": wh.id,
-            "location_id": wh.lot_stock_id.id,
-        })
+            [("company_id", "=", company.id)], limit=1
+        )
+        res.update(
+            {"warehouse_id": wh.id, "location_id": wh.lot_stock_id.id,}
+        )
         return res
 
-    location_id = fields.Many2one(
-        comodel_name="stock.location",
-        required=True,
-    )
-    warehouse_id = fields.Many2one(
-        comodel_name="stock.warehouse",
-    )
+    location_id = fields.Many2one(comodel_name="stock.location", required=True,)
+    warehouse_id = fields.Many2one(comodel_name="stock.warehouse",)
     line_ids = fields.One2many(
         comodel_name="stock.pull.list.wizard.line",
         inverse_name="wizard_id",
@@ -38,17 +33,14 @@ class PullListWizard(models.TransientModel):
     # Step 1 - filtering options.
     exclude_reserved = fields.Boolean()
     location_dest_id = fields.Many2one(
-        string="Destination Location",
-        comodel_name="stock.location",
+        string="Destination Location", comodel_name="stock.location",
     )
     date_to = fields.Date()
     consolidate_by_product = fields.Boolean(
         help="All needs for each product will be grouped in one line, "
-             "disregarding date.",
+        "disregarding date.",
     )
-    procurement_group_ids = fields.Many2many(
-        comodel_name="procurement.group"
-    )
+    procurement_group_ids = fields.Many2many(comodel_name="procurement.group")
     # Step 2 - filtering options.
     select_all = fields.Boolean(default=True)
     rule_action = fields.Selection(
@@ -56,7 +48,7 @@ class PullListWizard(models.TransientModel):
     )
     available_in_source_location = fields.Boolean(
         help="Select only rules with enough available stock in source "
-             "location. Applies for rules with a source location.",
+        "location. Applies for rules with a source location.",
     )
     # Step 2 - grouping options.
     max_lines = fields.Integer()
@@ -112,8 +104,9 @@ class PullListWizard(models.TransientModel):
 
     def _get_available_qty(self, product, location):
         product_obj = self.env["product.product"]
-        product_l = product_obj.with_context(
-            {"location": location.id}).browse(product.id)
+        product_l = product_obj.with_context({"location": location.id}).browse(
+            product.id
+        )
         if self.exclude_reserved:
             return product_l.qty_available_not_res
         return product_l.qty_available
@@ -125,23 +118,24 @@ class PullListWizard(models.TransientModel):
             "company_id": self.env.user.company_id,
         }
         stock_rule_id = self.env["procurement.group"]._get_rule(
-            product_id, location_id, values)
+            product_id, location_id, values
+        )
         return stock_rule_id
 
     def action_prepare(self):
         domain = self._get_moves_demand_domain()
         # `read_group` is not possible here because of the date format the
         # method returns.
-        demand_moves = self.env["stock.move"].search(
-            domain, order="date_expected asc")
+        demand_moves = self.env["stock.move"].search(domain, order="date_expected asc")
         demand_dict = {}
-        force_date = fields.Date.today() if self.consolidate_by_product \
-            else False
+        force_date = fields.Date.today() if self.consolidate_by_product else False
         for demand in demand_moves:
             key = (
-                demand.product_id, demand.location_id,
+                demand.product_id,
+                demand.location_id,
                 fields.Date.to_date(demand.date_expected)
-                if not force_date else force_date,
+                if not force_date
+                else force_date,
             )
             prev = demand_dict.setdefault(key, 0.0)
             # TODO: when exclude_reserved is selected, handle partially avail.
@@ -149,20 +143,24 @@ class PullListWizard(models.TransientModel):
 
         domain = self._get_moves_incoming_domain()
         incoming_moves = self.env["stock.move"].search(
-            domain, order="date_expected asc")
+            domain, order="date_expected asc"
+        )
         incoming_dict = {}
         for supply in incoming_moves:
             move_for_date = demand_moves.filtered(
-                lambda m: m.product_id == supply.product_id and
-                m.date_expected >= supply.date_expected)
+                lambda m: m.product_id == supply.product_id
+                and m.date_expected >= supply.date_expected
+            )
             if move_for_date:
-                date_selected = move_for_date[0].date_expected \
-                    if not force_date else force_date
+                date_selected = (
+                    move_for_date[0].date_expected if not force_date else force_date
+                )
             else:
                 # Supply is later than last demand -> ignore it.
                 continue
             key = (
-                supply.product_id, supply.location_dest_id,
+                supply.product_id,
+                supply.location_dest_id,
                 fields.Date.to_date(date_selected),
             )
             prev = incoming_dict.setdefault(key, 0.0)
@@ -173,17 +171,17 @@ class PullListWizard(models.TransientModel):
         qty_assigned = {}
         for key, demand_qty in demand_dict.items():
             supply_qty = incoming_dict.get(key, 0.0)
-            lines.append((0, 0, self._prepare_line_values(
-                key, demand_qty, supply_qty)))
-        self.update({
-            "line_ids": lines,
-        })
+            lines.append((0, 0, self._prepare_line_values(key, demand_qty, supply_qty)))
+        self.update(
+            {"line_ids": lines,}
+        )
         res = self._act_window_pull_list_step_2()
         return res
 
     def _act_window_pull_list_step_2(self):
         view_id = self.env.ref(
-            "stock_pull_list.view_run_stock_pull_list_wizard_wizard_step_2").id
+            "stock_pull_list.view_run_stock_pull_list_wizard_wizard_step_2"
+        ).id
         res = {
             "name": _("Pull List"),
             "src_model": "stock.pull.list.wizard",
@@ -202,8 +200,9 @@ class PullListWizard(models.TransientModel):
             if self.select_all:
                 line.selected = True
                 continue
-            rule_invalid = self.rule_action and \
-                self.rule_action != line.stock_rule_id.action
+            rule_invalid = (
+                self.rule_action and self.rule_action != line.stock_rule_id.action
+            )
             if self.available_in_source_location:
                 available = line._is_available_in_source_location()
             else:
@@ -254,8 +253,7 @@ class PullListWizard(models.TransientModel):
         # User requesting the procurement is passed by context to be able to
         # update final MO, PO or trasfer with that information.
         # TODO: migration to v13: requested_uid is not needed.
-        pg_obj = self.env["procurement.group"].with_context(
-            requested_uid=self.env.user)
+        pg_obj = self.env["procurement.group"].with_context(requested_uid=self.env.user)
         grouping_keys = self._get_procurement_group_keys()
         fields = self._get_fields_for_keys()
         for gk in grouping_keys:
@@ -275,8 +273,7 @@ class PullListWizard(models.TransientModel):
                     group = pg_obj.create(self._prepare_proc_group_values())
                     proc_groups.append(group.id)
 
-                values = self._prepare_procurement_values(
-                    line.date_expected, group)
+                values = self._prepare_procurement_values(line.date_expected, group)
                 try:
                     pg_obj.run(
                         line.product_id,
@@ -285,7 +282,7 @@ class PullListWizard(models.TransientModel):
                         line.location_id,
                         "Pull List %s" % self.id,
                         "Pull List %s" % self.id,
-                        values
+                        values,
                     )
                 except UserError as error:
                     errors.append(error.name)
@@ -307,28 +304,21 @@ class PullListWizardLine(models.TransientModel):
     _name = "stock.pull.list.wizard.line"
     _description = "Stock Pull List Wizard Line"
 
-    wizard_id = fields.Many2one(
-        comodel_name="stock.pull.list.wizard",
-    )
-    product_id = fields.Many2one(
-        comodel_name="product.product",
-    )
-    location_id = fields.Many2one(
-        comodel_name="stock.location",
-    )
+    wizard_id = fields.Many2one(comodel_name="stock.pull.list.wizard",)
+    product_id = fields.Many2one(comodel_name="product.product",)
+    location_id = fields.Many2one(comodel_name="stock.location",)
     date_expected = fields.Date()
     available_qty = fields.Float()
     incoming_qty = fields.Float()
     raw_demand_qty = fields.Float()
     needed_qty = fields.Float()
-    stock_rule_id = fields.Many2one(
-        comodel_name="stock.rule",
-    )
+    stock_rule_id = fields.Many2one(comodel_name="stock.rule",)
     selected = fields.Boolean(default=True)
 
     def _is_available_in_source_location(self):
         if not self.stock_rule_id.location_src_id:
             return False
         qty_avail = self.wizard_id._get_available_qty(
-            self.product_id, self.stock_rule_id.location_src_id)
+            self.product_id, self.stock_rule_id.location_src_id
+        )
         return qty_avail > self.needed_qty
