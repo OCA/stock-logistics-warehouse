6fbc79b2461f1182a2e8053926797b14c5ba6041
ps-tubtim <pimolnats@ecosoft.co.th>
2020-11-09 11:04:28 +0700

    [IMP] stock_request_picking_type: black, isort, prettier

--- stock_request_picking_type/__manifest__.py
+++ stock_request_picking_type/__manifest__.py
@@ -2,22 +2,19 @@
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
-    'name': 'Stock Request Picking Type',
-    'summary': 'Add Stock Requests to the Inventory App',
-    'version': '12.0.2.1.1',
-    'license': 'LGPL-3',
-    'website': 'https://github.com/stock-logistics-warehouse',
-    'author': 'Open Source Integrators, '
-              'Odoo Community Association (OCA)',
-    'category': 'Warehouse',
-    'depends': [
-        'stock_request_submit',
+    "name": "Stock Request Picking Type",
+    "summary": "Add Stock Requests to the Inventory App",
+    "version": "13.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://github.com/stock-logistics-warehouse",
+    "author": "Open Source Integrators, " "Odoo Community Association (OCA)",
+    "category": "Warehouse",
+    "depends": ["stock_request_submit"],
+    "data": [
+        "data/stock_picking_type.xml",
+        "views/stock_request_order_views.xml",
+        "views/stock_picking_views.xml",
     ],
-    'data': [
-        'data/stock_picking_type.xml',
-        'views/stock_request_order_views.xml',
-        'views/stock_picking_views.xml',
-    ],
-    'development_status': 'Beta',
-    'maintainers': ['max3903']
+    "development_status": "Beta",
+    "maintainers": ["max3903"],
 }
