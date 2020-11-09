48f1db0a5540547f6f4dbf11f18e598ac95d1468
ps-tubtim <pimolnats@ecosoft.co.th>
2020-11-09 11:04:28 +0700

    [IMP] stock_request_picking_type: black, isort, prettier

--- stock_request_picking_type/migrations/12.0.2.0.0/post-migration.py
+++ stock_request_picking_type/migrations/12.0.2.0.0/post-migration.py
@@ -6,9 +6,11 @@ def migrate(env, version):
     if not version:
         return
 
-    env.execute("UPDATE stock_request_order "
-                "SET picking_type_id = ("
-                "SELECT id "
-                "FROM stock_picking_type "
-                "WHERE code = 'stock_request_order') "
-                "WHERE picking_type_id IS NULL;")
+    env.execute(
+        "UPDATE stock_request_order "
+        "SET picking_type_id = ("
+        "SELECT id "
+        "FROM stock_picking_type "
+        "WHERE code = 'stock_request_order') "
+        "WHERE picking_type_id IS NULL;"
+    )
