cc1ec3a3b3151165c880efdfbc4b5feec418b5f5
Víctor Martínez <victor.martinez@tecnativa.com>
2020-11-27 09:32:49 +0100

    [ADD] stock_archive_constraint: Allows to block archiving products and locations with associated stock.quant or stock.move

---
+++ stock_archive_constraint/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2020 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock archive constraint",
+    "version": "12.0.1.0.0",
+    "license": "AGPL-3",
+    "website": "https://github.com/stock-logistics-warehouse",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "development_status": "Production/Stable",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "installable": True,
+    "maintainers": ["victoralmau"],
+}
