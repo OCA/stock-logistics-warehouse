d21f5d93d51abf71fab00118936d2ca3965f458d
Cédric Pigeon <cedric.pigeon@acsone.eu>
2020-12-01 13:19:04 +0100

    [13.0] add stock_quant_view_reservations

---
+++ stock_quant_view_reservation/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2020 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Quant View Reservation",
+    "summary": """
+        Allows to see details of reservations on a quant""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock"],
+    "data": ["views/stock_quant.xml", "views/stock_move_line_views.xml"],
+}
