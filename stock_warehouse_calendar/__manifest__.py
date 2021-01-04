767fe7367d03dfc244cdd7ed32ae42426583e216
Mateu Griful <mateu.griful@forgeflow.com>
2021-01-04 13:41:26 +0100

    [IMP] stock_warehouse_calendar: black, isort, prettier

--- stock_warehouse_calendar/__manifest__.py
+++ stock_warehouse_calendar/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Adds a calendar to the Warehouse",
     "version": "13.0.1.0.1",
     "license": "LGPL-3",
-    "website": "https://github.com/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "category": "Warehouse Management",
     "depends": ["stock", "resource"],
