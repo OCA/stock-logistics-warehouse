c3fa8295f52cf3eb409c25667dae882a814a32e6
Sébastien BEAU <sebastien.beau@akretion.com>
2021-01-06 15:03:10 +0100

    [MIG] stock_packaging_calculator: Migration to 14.0

--- stock_packaging_calculator/__manifest__.py
+++ stock_packaging_calculator/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock packaging calculator",
     "summary": "Compute product quantity to pick by packaging",
-    "version": "13.0.1.6.0",
+    "version": "14.0.1.0.0",
     "development_status": "Alpha",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
