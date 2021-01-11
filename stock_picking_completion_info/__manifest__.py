7f1192e1bd4616d155652083eab043dce64b7911
hparfr <raph@hpar.fr>
2021-01-11 16:27:05 +0100

    [MIG] stock_picking_completion_info: Migration to 14.0

--- stock_picking_completion_info/__manifest__.py
+++ stock_picking_completion_info/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Stock Picking Completion Info",
     "summary": "Display on current document completion information according "
     "to next operations",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "development_status": "Alpha",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
