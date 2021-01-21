2037b72064aa3f8fe85d524647d063ae746b59f9
Víctor Martínez <victor.martinez@tecnativa.com>
2021-01-21 08:05:05 +0100

    [IMP] stock_archive_constraint: black, isort, prettier

--- stock_archive_constraint/models/product_product.py
+++ stock_archive_constraint/models/product_product.py
@@ -10,43 +10,52 @@ class ProductProduct(models.Model):
 
     @api.constrains("active")
     def _check_active_stock_archive_constraint_stock_quant(self):
-        res = self.env['stock.quant'].search(
+        res = self.env["stock.quant"].search(
             [
-                ('location_id.usage', 'in', ('internal', 'transit')),
-                ('product_id', 'in', self.filtered(lambda x: not x.active).ids),
-                ('quantity', '!=', 0.0)
-            ], limit=1
+                ("location_id.usage", "in", ("internal", "transit")),
+                ("product_id", "in", self.filtered(lambda x: not x.active).ids),
+                ("quantity", "!=", 0.0),
+            ],
+            limit=1,
         )
         if res:
             raise ValidationError(
-                _("It is not possible to archive product '%s' which has "
-                  "associated stock quantities." % res[0].product_id.display_name)
+                _(
+                    "It is not possible to archive product '%s' which has "
+                    "associated stock quantities." % res[0].product_id.display_name
+                )
             )
 
     @api.constrains("active")
     def _check_active_stock_archive_constraint_stock_move(self):
-        res = self.env['stock.move'].search(
+        res = self.env["stock.move"].search(
             [
-                ('product_id', 'in', self.filtered(lambda x: not x.active).ids),
-                ('state', 'not in', ('done', 'cancel'))
-            ], limit=1
+                ("product_id", "in", self.filtered(lambda x: not x.active).ids),
+                ("state", "not in", ("done", "cancel")),
+            ],
+            limit=1,
         )
         if res:
             raise ValidationError(
-                _("It is not possible to archive product '%s' which has "
-                  "associated picking lines." % res[0].product_id.display_name)
+                _(
+                    "It is not possible to archive product '%s' which has "
+                    "associated picking lines." % res[0].product_id.display_name
+                )
             )
 
     @api.constrains("active")
     def _check_active_stock_archive_constraint_stock_move_line(self):
-        res = self.env['stock.move.line'].search(
+        res = self.env["stock.move.line"].search(
             [
-                ('product_id', 'in', self.filtered(lambda x: not x.active).ids),
-                ('state', 'not in', ('done', 'cancel'))
-            ], limit=1
+                ("product_id", "in", self.filtered(lambda x: not x.active).ids),
+                ("state", "not in", ("done", "cancel")),
+            ],
+            limit=1,
         )
         if res:
             raise ValidationError(
-                _("It is not possible to archive product '%s' which has "
-                  "associated stock reservations." % res[0].product_id.display_name)
+                _(
+                    "It is not possible to archive product '%s' which has "
+                    "associated stock reservations." % res[0].product_id.display_name
+                )
             )
