8ad88cdf93ffbc77978029e3904dba45b8e8b884
Víctor Martínez <victor.martinez@tecnativa.com>
2021-01-21 08:06:14 +0100

    [MIG] stock_archive_constraint: Migration to 13.0

--- stock_archive_constraint/__manifest__.py
+++ stock_archive_constraint/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock archive constraint",
-    "version": "12.0.1.0.0",
+    "version": "13.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/stock-logistics-warehouse",
     "author": "Tecnativa, Odoo Community Association (OCA)",
