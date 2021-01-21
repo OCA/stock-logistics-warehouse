55b491e3aff26c847c72f30840dde4496404c551
Víctor Martínez <victor.martinez@tecnativa.com>
2021-01-21 08:05:05 +0100

    [IMP] stock_archive_constraint: black, isort, prettier

--- stock_archive_constraint/models/stock_location.py
+++ stock_archive_constraint/models/stock_location.py
@@ -10,51 +10,60 @@ class StockLocation(models.Model):
 
     @api.constrains("active")
     def _check_active_stock_archive_constraint_stock_quant(self):
-        res = self.env['stock.quant'].search(
+        res = self.env["stock.quant"].search(
             [
-                '&',
-                ('location_id.usage', 'in', ('internal', 'transit')),
-                '|',
-                ('location_id', 'in', self.filtered(lambda x: not x.active).ids),
-                ('location_id', 'child_of', self.filtered(lambda x: not x.active).ids),
-            ], limit=1
+                "&",
+                ("location_id.usage", "in", ("internal", "transit")),
+                "|",
+                ("location_id", "in", self.filtered(lambda x: not x.active).ids),
+                ("location_id", "child_of", self.filtered(lambda x: not x.active).ids),
+            ],
+            limit=1,
         )
         if res:
             raise ValidationError(
-                _("It is not possible to archive location '%s' which has "
-                  "associated stock quantities." % res[0].display_name)
+                _(
+                    "It is not possible to archive location '%s' which has "
+                    "associated stock quantities." % res[0].display_name
+                )
             )
 
     @api.constrains("active")
     def _check_active_stock_archive_constraint_stock_move(self):
-        res = self.env['stock.move'].search(
+        res = self.env["stock.move"].search(
             [
-                '&',
-                ('state', 'not in', ('done', 'cancel')),
-                '|',
-                ('location_id', 'in', self.filtered(lambda x: not x.active).ids),
-                ('location_id', 'child_of', self.filtered(lambda x: not x.active).ids)
-            ], limit=1
+                "&",
+                ("state", "not in", ("done", "cancel")),
+                "|",
+                ("location_id", "in", self.filtered(lambda x: not x.active).ids),
+                ("location_id", "child_of", self.filtered(lambda x: not x.active).ids),
+            ],
+            limit=1,
         )
         if res:
             raise ValidationError(
-                _("It is not possible to archive location '%s' which has "
-                  "associated picking lines." % res[0].display_name)
+                _(
+                    "It is not possible to archive location '%s' which has "
+                    "associated picking lines." % res[0].display_name
+                )
             )
 
     @api.constrains("active")
     def _check_active_stock_archive_constraint_stock_move_line(self):
-        res = self.env['stock.move.line'].search(
+        res = self.env["stock.move.line"].search(
             [
-                '&',
-                ('state', 'not in', ('done', 'cancel')),
-                '|',
-                ('location_id', 'in', self.filtered(lambda x: not x.active).ids),
-                ('location_id', 'child_of', self.filtered(lambda x: not x.active).ids)
-            ], limit=1
+                "&",
+                ("state", "not in", ("done", "cancel")),
+                "|",
+                ("location_id", "in", self.filtered(lambda x: not x.active).ids),
+                ("location_id", "child_of", self.filtered(lambda x: not x.active).ids),
+            ],
+            limit=1,
         )
         if res:
             raise ValidationError(
-                _("It is not possible to archive location '%s' which has "
-                  "associated stock reservations." % res[0].display_name)
+                _(
+                    "It is not possible to archive location '%s' which has "
+                    "associated stock reservations." % res[0].display_name
+                )
             )
