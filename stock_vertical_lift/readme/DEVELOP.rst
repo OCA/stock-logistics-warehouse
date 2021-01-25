cd65caff20aab67bcf57db8df56ef8da9a98aae8
Carlos Serra-Toro <carlos.serra@camptocamp.com>
2021-01-25 10:22:00 +0100

    [IMP] stock_vertical_lift: Add skip operation to vertical lift shuttles
    In the screen for the vertical lift shuttles, accessible through
    Inventory > Operations > Vertical Lift Shuttles, a new button has been
    added to allow to skip an operation. This button can also be triggered
    by scanning the barcode O-BTN.skip.svg that is inside the folder
    'images'.
    
    When a skip is done, the next stock.move.line to pick is chosen and
    shown to the operator. A skipped move line is added to the end of the
    list of pending move lines to be picked, so they will be shown again
    in the future as soon as the other move lines have been successfully
    processed.
    
    This option was added because, sometimes, the operator can not process
    a move line for whatever reason. Right now, the only way of proceeding
    is to wait until he/she can effectively process it, which involves a
    delay in the operations. With this new skip operation, the operator
    can continue processing the rest of move lines.

---
+++ stock_vertical_lift/readme/DEVELOP.rst
@@ -0,0 +1,1 @@
+The barcodes used are of the type Code 128 (with the code set B).
--- stock_vertical_lift/tests/test_pick.py
+++ stock_vertical_lift/tests/test_pick.py
@@ -37,6 +37,15 @@ class TestPick(VerticalLiftCase):
         self.assertEqual(operation.current_move_line_id, self.out_move_line)
         self.assertEqual(operation.state, "scan_destination")
 
+    def test_pick_select_next_move_line_was_skipped(self):
+        """Previously skipped moves can be reprocessed"""
+        self.picking_out.move_line_ids.write({"vertical_lift_skipped": True})
+        operation = self._open_screen("pick")
+        operation.select_next_move_line()
+        self.assertEqual(operation.current_move_line_id, self.out_move_line)
+        self.assertEqual(operation.state, "scan_destination")
+        self.assertFalse(operation.current_move_line_id.vertical_lift_skipped)
+
     def test_pick_save(self):
         operation = self._open_screen("pick")
         # assume we already scanned the destination, current state is save
@@ -45,6 +54,31 @@ class TestPick(VerticalLiftCase):
         operation.button_save()
         self.assertEqual(operation.current_move_line_id.state, "done")
         self.assertEqual(operation.state, "release")
+
+    def test_pick_skip_from_scan_destination(self):
+        """Being in state Scan Destination, skip it"""
+        self._test_pick_skip_from_state("scan_destination")
+
+    def test_pick_skip_from_save(self):
+        """Being in state Save, skip it"""
+        self._test_pick_skip_from_state("save")
+
+    def test_pick_skip_from_release(self):
+        """Being in state Release, skip it"""
+        self._test_pick_skip_from_state("release")
+
+    def _test_pick_skip_from_state(self, state):
+        operation = self._open_screen("pick")
+        operation.state = state
+        operation.current_move_line_id = self.out_move_line
+        first_move_line = operation.current_move_line_id
+        self.assertFalse(operation.current_move_line_id.vertical_lift_skipped)
+        operation.button_skip()
+        second_move_line = operation.current_move_line_id
+        self.assertNotEqual(first_move_line, second_move_line)
+        self.assertFalse(operation.current_move_line_id.vertical_lift_skipped)
+        self.assertEqual(operation.current_move_line_id.state, "assigned")
+        self.assertEqual(operation.state, "scan_destination")
 
     def test_pick_related_fields(self):
         operation = self._open_screen("pick")
