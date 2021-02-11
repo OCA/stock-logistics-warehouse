24f4faf80c6d4c38072bcc886ecf8c9dc55b1b3f
Guewen Baconnier <guewen.baconnier@camptocamp.com>
2021-02-11 09:42:29 +0100

    Apply suggestions from code review
    Co-authored-by: Simone Orsi <simahawk@users.noreply.github.com>

--- stock_helper/__manifest__.py
+++ stock_helper/__manifest__.py
@@ -2,9 +2,9 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
 {
-    "name": "Stock Move Helpers",
+    "name": "Stock Helpers",
     "summary": "Add methods shared between various stock modules",
-    "version": "13.0.1.1.0",
+    "version": "13.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Hidden",
