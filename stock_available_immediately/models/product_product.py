cff9044692bfbfc683dd306f1b36140354da86cd
Víctor Martínez <victor.martinez@tecnativa.com>
2021-02-18 12:19:26 +0100

    [IMP] stock_available_immediately: black, isort, prettier

--- stock_available_immediately/models/product_product.py
+++ stock_available_immediately/models/product_product.py
@@ -7,17 +7,19 @@ from odoo import api, models
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
     @api.multi
     def _compute_available_quantities_dict(self):
-        res, stock_dict = \
-            super(ProductProduct, self)._compute_available_quantities_dict()
+        res, stock_dict = super(
+            ProductProduct, self
+        )._compute_available_quantities_dict()
         for product in self:
-            res[product.id]['immediately_usable_qty'] -= \
-                stock_dict[product.id]['incoming_qty']
+            res[product.id]["immediately_usable_qty"] -= stock_dict[product.id][
+                "incoming_qty"
+            ]
         return res, stock_dict
 
-    @api.depends('virtual_available', 'incoming_qty')
+    @api.depends("virtual_available", "incoming_qty")
     def _compute_available_quantities(self):
         return super(ProductProduct, self)._compute_available_quantities()
