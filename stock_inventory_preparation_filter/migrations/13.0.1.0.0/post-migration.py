437a251948d278798c16f32e02556cae25f3a3e0
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-03-10 00:33:42 +0100

    [OU-FIX] stock_inventory_preparation_filter: Proper column_exists args

--- stock_inventory_preparation_filter/migrations/13.0.1.0.0/post-migration.py
+++ stock_inventory_preparation_filter/migrations/13.0.1.0.0/post-migration.py
@@ -6,7 +6,7 @@ from openupgradelib import openupgrade
 
 def convert_m2o_to_x2m_fields(env):
     categ_column = openupgrade.get_legacy_name("category_id")
-    if openupgrade.column_exists(env.cr, categ_column):
+    if openupgrade.column_exists(env.cr, "stock_inventory", categ_column):
         openupgrade.m2o_to_x2m(
             env.cr,
             env["stock.inventory"],
@@ -15,7 +15,7 @@ def convert_m2o_to_x2m_fields(env):
             categ_column,
         )
     lot_column = openupgrade.get_legacy_name("lot_column")
-    if openupgrade.column_exists(env.cr, lot_column):
+    if openupgrade.column_exists(env.cr, "stock_inventory", lot_column):
         openupgrade.m2o_to_x2m(
             env.cr,
             env["stock.inventory"],
