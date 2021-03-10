5f3dae9c73379c4b971a9a9b02a4e9c75e4d14de
João Marques <joao.marques@tecnativa.com>
2021-03-10 11:18:25 +0000

    [IMP] stock_request_analytic: add analytic tags to Stock Requests
    TT28488

--- stock_request_analytic/models/stock_rule.py
+++ stock_request_analytic/models/stock_rule.py
@@ -13,7 +13,13 @@ class ProcurementRule(models.Model):
             product_id, product_qty, product_uom, location_id, name, origin,
             values, group_id)
         if values.get('stock_request_id'):
-            analytic_account_id = self.env['stock.request'].browse(
-                values['stock_request_id']).analytic_account_id.id
-            res.update(analytic_account_id=analytic_account_id)
+            stock_request = self.env['stock.request'].browse(
+                values['stock_request_id']
+            )
+            analytic_account = stock_request.analytic_account_id
+            analytic_tags = stock_request.analytic_tag_ids
+            res.update(
+                analytic_account_id=analytic_account.id,
+                analytic_tag_ids=[(4, tag.id) for tag in analytic_tags],
+            )
         return res
