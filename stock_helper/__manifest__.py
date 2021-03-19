f446891ac5e3014dbeeacb21156370a49635281b
davidborromeo <david.borromeo@forgeflow.com>
2021-03-19 13:02:53 +0100

    [MIG] stock_helper: Migration to 14.0

--- stock_helper/__manifest__.py
+++ stock_helper/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Helpers",
     "summary": "Add methods shared between various stock modules",
-    "version": "13.0.1.1.0",
+    "version": "14.0.1.0.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Hidden",
