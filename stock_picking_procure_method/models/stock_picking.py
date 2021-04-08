0e99afb720d31d401445bcce1744fa8abb7f1ab1
Ernesto Tejeda <ernesto.tejeda@tecnativa.com>
2021-04-08 17:00:00 -0400

    [IMP] stock_picking_procure_method: black, isort, prettier

--- stock_picking_procure_method/models/stock_picking.py
+++ stock_picking_procure_method/models/stock_picking.py
@@ -6,26 +6,27 @@ from odoo import api, fields, models
 
 
 class StockPicking(models.Model):
-    _inherit = 'stock.picking'
+    _inherit = "stock.picking"
 
     procure_method = fields.Selection(
-        selection='_selection_procure_method',
-        compute='_compute_procure_method',
-        inverse='_inverse_procure_method',
-        string='Supply Method',
-        help='By default, the system will take from the stock in the source '
-             'location and passively wait for availability. The other '
-             'possibility allows you to directly create a procurement on the '
-             'source location (and thus ignore its current stock) to gather '
-             'products. If we want to chain moves and have this one to wait '
-             'for the previous, this second option should be chosen.',
+        selection="_selection_procure_method",
+        compute="_compute_procure_method",
+        inverse="_inverse_procure_method",
+        string="Supply Method",
+        help="By default, the system will take from the stock in the source "
+        "location and passively wait for availability. The other "
+        "possibility allows you to directly create a procurement on the "
+        "source location (and thus ignore its current stock) to gather "
+        "products. If we want to chain moves and have this one to wait "
+        "for the previous, this second option should be chosen.",
     )
 
     def _selection_procure_method(self):
-        return self.env['stock.move'].fields_get(
-            allfields=['procure_method'])['procure_method']['selection']
+        return self.env["stock.move"].fields_get(allfields=["procure_method"])[
+            "procure_method"
+        ]["selection"]
 
-    @api.depends('move_lines.procure_method')
+    @api.depends("move_lines.procure_method")
     def _compute_procure_method(self):
         for picking in self:
             procure_method = False
@@ -38,6 +39,6 @@ class StockPicking(models.Model):
             picking.procure_method = procure_method
 
     def _inverse_procure_method(self):
-        self.filtered('procure_method').mapped('move_lines').update({
-            'procure_method': self.procure_method,
-        })
+        self.filtered("procure_method").mapped("move_lines").update(
+            {"procure_method": self.procure_method,}
+        )
