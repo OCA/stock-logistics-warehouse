23d243ff42f0ff2ed4896b90a20a66c5801b2284
João Marques <joao.marques@tecnativa.com>
2021-04-09 14:31:36 +0100

    [IMP] stock_request_analytic: black, isort, prettier

--- stock_request_analytic/models/stock_rule.py
+++ stock_request_analytic/models/stock_rule.py
@@ -7,15 +7,29 @@ from odoo import models
 class ProcurementRule(models.Model):
     _inherit = "stock.rule"
 
-    def _get_stock_move_values(self, product_id, product_qty, product_uom,
-                               location_id, name, origin, values, group_id):
+    def _get_stock_move_values(
+        self,
+        product_id,
+        product_qty,
+        product_uom,
+        location_id,
+        name,
+        origin,
+        values,
+        group_id,
+    ):
         res = super(ProcurementRule, self)._get_stock_move_values(
-            product_id, product_qty, product_uom, location_id, name, origin,
-            values, group_id)
-        if values.get('stock_request_id'):
-            stock_request = self.env['stock.request'].browse(
-                values['stock_request_id']
-            )
+            product_id,
+            product_qty,
+            product_uom,
+            location_id,
+            name,
+            origin,
+            values,
+            group_id,
+        )
+        if values.get("stock_request_id"):
+            stock_request = self.env["stock.request"].browse(values["stock_request_id"])
             analytic_account = stock_request.analytic_account_id
             analytic_tags = stock_request.analytic_tag_ids
             res.update(
