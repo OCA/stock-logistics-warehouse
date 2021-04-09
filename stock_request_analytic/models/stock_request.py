28f86015c4e0d7599d3da72d3026f146989a6353
João Marques <joao.marques@tecnativa.com>
2021-04-09 14:31:36 +0100

    [IMP] stock_request_analytic: black, isort, prettier

--- stock_request_analytic/models/stock_request.py
+++ stock_request_analytic/models/stock_request.py
@@ -9,15 +9,22 @@ class StockRequest(models.Model):
     _inherit = "stock.request"
 
     analytic_account_id = fields.Many2one(
-        'account.analytic.account', string='Analytic Account')
-    analytic_tag_ids = fields.Many2many(
-        'account.analytic.tag', string='Analytic Tags')
+        "account.analytic.account", string="Analytic Account"
+    )
+    analytic_tag_ids = fields.Many2many("account.analytic.tag", string="Analytic Tags")
 
-    @api.constrains('analytic_account_id')
+    @api.constrains("analytic_account_id")
     def _check_analytic_company_constrains(self):
-        if any(r.company_id and r.analytic_account_id and
-               r.analytic_account_id.company_id != r.company_id for r in self):
+        if any(
+            r.company_id
+            and r.analytic_account_id
+            and r.analytic_account_id.company_id != r.company_id
+            for r in self
+        ):
             raise ValidationError(
-                _('You cannot link a analytic account '
-                  'to a stock request that belongs to '
-                  'another company.'))
+                _(
+                    "You cannot link a analytic account "
+                    "to a stock request that belongs to "
+                    "another company."
+                )
+            )
