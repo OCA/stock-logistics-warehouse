f74d37594ad1023a1af1e0730474b17e89b28116
João Marques <joao.marques@tecnativa.com>
2021-04-09 14:55:44 +0100

    [MIG] stock_request_analytic: Migration to 13.0
    Co-authored-by: Pimolnat Suntian <pimolnats@ecosoft.co.th>
    
    TT28896

--- stock_request_analytic/tests/test_stock_request_analytic.py
+++ stock_request_analytic/tests/test_stock_request_analytic.py
@@ -1,8 +1,8 @@
-# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+# Copyright 2017-2020 ForgeFlow, S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
 
 from odoo import fields
-from odoo.exceptions import ValidationError
+from odoo.exceptions import UserError
 
 from odoo.addons.stock_request.tests import test_stock_request
 
@@ -13,8 +13,9 @@ class TestStockRequestAnalytic(test_stock_request.TestStockRequest):
         self.analytic_model = self.env["account.analytic.account"]
         self.analytic = self.analytic_model.create({"name": "Pizza"})
         self.analytic2 = self.analytic_model.create(
-            {"name": "Pizza", "company_id": self.company_2.id,}
+            {"name": "Pizza", "company_id": self.company_2.id}
         )
+        self.analytic3 = self.analytic_model.create({"name": "Hamburger"})
         self.demand_loc = self.env["stock.location"].create(
             {
                 "name": "demand_loc",
@@ -43,12 +44,11 @@ class TestStockRequestAnalytic(test_stock_request.TestStockRequest):
                 "procure_method": "make_to_stock",
                 "warehouse_id": self.warehouse.id,
                 "company_id": self.main_company.id,
-                "propagate": "False",
             }
         )
         self.pizza.route_ids = [(6, 0, self.demand_route.ids)]
 
-    def prepare_order_request_analytic(self, aa, company, analytic_tags=None):
+    def prepare_order_request_analytic(self, analytic, company, analytic_tags=None):
         expected_date = fields.Datetime.now()
         analytic_tags = analytic_tags or self.env["account.analytic.tag"]
         vals = {
@@ -64,7 +64,7 @@ class TestStockRequestAnalytic(test_stock_request.TestStockRequest):
                         "product_id": self.pizza.id,
                         "product_uom_id": self.pizza.uom_id.id,
                         "product_uom_qty": 5.0,
-                        "analytic_account_id": aa.id,
+                        "analytic_account_id": analytic.id,
                         "analytic_tag_ids": [(4, tag.id) for tag in analytic_tags],
                         "company_id": company.id,
                         "warehouse_id": self.warehouse.id,
@@ -72,6 +72,46 @@ class TestStockRequestAnalytic(test_stock_request.TestStockRequest):
                         "expected_date": expected_date,
                     },
                 )
+            ],
+        }
+        return vals
+
+    def prepare_order_request_multi_analytic(self, analytic1, analytic2, company):
+        expected_date = fields.Datetime.now()
+        vals = {
+            "company_id": company.id,
+            "warehouse_id": self.warehouse.id,
+            "location_id": self.demand_loc.id,
+            "expected_date": expected_date,
+            "stock_request_ids": [
+                (
+                    0,
+                    0,
+                    {
+                        "product_id": self.pizza.id,
+                        "product_uom_id": self.pizza.uom_id.id,
+                        "product_uom_qty": 5.0,
+                        "analytic_account_id": analytic1.id,
+                        "company_id": company.id,
+                        "warehouse_id": self.warehouse.id,
+                        "location_id": self.demand_loc.id,
+                        "expected_date": expected_date,
+                    },
+                ),
+                (
+                    0,
+                    0,
+                    {
+                        "product_id": self.pizza.id,
+                        "product_uom_id": self.pizza.uom_id.id,
+                        "product_uom_qty": 5.0,
+                        "analytic_account_id": analytic2.id,
+                        "company_id": company.id,
+                        "warehouse_id": self.warehouse.id,
+                        "location_id": self.demand_loc.id,
+                        "expected_date": expected_date,
+                    },
+                ),
             ],
         }
         return vals
@@ -87,11 +127,23 @@ class TestStockRequestAnalytic(test_stock_request.TestStockRequest):
         self.assertEqual(req.move_ids.mapped("analytic_account_id"), self.analytic)
         self.assertEqual(req.move_ids.mapped("analytic_tag_ids"), analytic_tag)
         self.assertEqual(order.analytic_count, 1)
-        action = order.action_view_analytic()
+        action = order.with_context(
+            analytic_type="analytic_account"
+        ).action_view_analytic()
         self.assertTrue(action["res_id"], self.analytic.id)
+        action2 = self.analytic.action_view_stock_request()
+        self.assertTrue(action2["res_id"], order.id)
+
+    def test_stock_multi_analytic(self):
+        vals = self.prepare_order_request_multi_analytic(
+            self.analytic, self.analytic3, self.main_company
+        )
+        order = self.env["stock.request.order"].create(vals)
+        order.action_confirm()
+        self.assertEqual(order.analytic_count, 2)
 
     def test_company(self):
-        with self.assertRaises(ValidationError):
+        with self.assertRaises(UserError):
             vals = self.prepare_order_request_analytic(
                 self.analytic2, self.main_company
             )
