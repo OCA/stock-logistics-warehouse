ef7b4250d0511cedbe8be36cebe639e365e26216
João Marques <joao.marques@tecnativa.com>
2021-04-09 14:31:36 +0100

    [IMP] stock_request_analytic: black, isort, prettier

--- stock_request_analytic/models/stock_request_order.py
+++ stock_request_analytic/models/stock_request_order.py
@@ -5,45 +5,43 @@ from odoo import api, fields, models
 
 
 class StockRequestOrder(models.Model):
-    _inherit = 'stock.request.order'
+    _inherit = "stock.request.order"
 
-    analytic_count = fields.Integer(
-        compute='_compute_analytic_ids',
-        readonly=True,
-    )
+    analytic_count = fields.Integer(compute="_compute_analytic_ids", readonly=True,)
     analytic_account_ids = fields.One2many(
-        comodel_name='account.analytic.account',
-        compute='_compute_analytic_ids',
-        string='Analytic Accounts',
+        comodel_name="account.analytic.account",
+        compute="_compute_analytic_ids",
+        string="Analytic Accounts",
         readonly=True,
     )
     analytic_tag_ids = fields.One2many(
-        comodel_name='account.analytic.tag',
-        compute='_compute_analytic_ids',
-        string='Analytic Tags',
+        comodel_name="account.analytic.tag",
+        compute="_compute_analytic_ids",
+        string="Analytic Tags",
         readonly=True,
     )
 
-    @api.depends('stock_request_ids')
+    @api.depends("stock_request_ids")
     def _compute_analytic_ids(self):
         for req in self.sudo():
             req.analytic_account_ids = req.stock_request_ids.mapped(
-                'analytic_account_id')
-            req.analytic_tag_ids = req.stock_request_ids.mapped(
-                'analytic_tag_ids')
+                "analytic_account_id"
+            )
+            req.analytic_tag_ids = req.stock_request_ids.mapped("analytic_tag_ids")
             req.analytic_count = len(req.analytic_account_ids)
 
     @api.multi
     def action_view_analytic(self):
-        action = self.env.ref(
-            'analytic.action_account_analytic_account_form').read()[0]
-        analytics = self.mapped('analytic_account_ids')
+        action = self.env.ref("analytic.action_account_analytic_account_form").read()[0]
+        analytics = self.mapped("analytic_account_ids")
         if len(analytics) > 1:
-            action['domain'] = [('id', 'in', analytics.ids)]
+            action["domain"] = [("id", "in", analytics.ids)]
         elif analytics:
-            action['views'] = [
-                (self.env.ref(
-                    'analytic.action_account_analytic_account_form').id,
-                    'form')]
-            action['res_id'] = analytics.id
+            action["views"] = [
+                (
+                    self.env.ref("analytic.action_account_analytic_account_form").id,
+                    "form",
+                )
+            ]
+            action["res_id"] = analytics.id
         return action
