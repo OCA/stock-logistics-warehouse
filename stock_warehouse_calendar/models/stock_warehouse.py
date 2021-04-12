7e72ea408c18ae43529f509f50b25accf537657d
davidborromeo <david.borromeo@forgeflow.com>
2021-04-12 12:12:34 +0200

    [MIG] stock_warehouse_calendar: Time zone selection addition

--- stock_warehouse_calendar/models/stock_warehouse.py
+++ stock_warehouse_calendar/models/stock_warehouse.py
@@ -3,9 +3,10 @@
 
 from odoo import fields, models
 
-
 class StockWarehouse(models.Model):
     _inherit = 'stock.warehouse'
 
     calendar_id = fields.Many2one('resource.calendar',
                                   'Working Hours')
+
+    partner_tz = fields.Selection(related='partner_id.tz')
