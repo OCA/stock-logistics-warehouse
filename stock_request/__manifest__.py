a3acfce28f31461bd2cb8ac71fcd43f6a1df22ac
Kitti U <kittiu@gmail.com>
2021-04-19 15:52:18 +0700

    [IMP] : black, isort, prettier

--- stock_request/__manifest__.py
+++ stock_request/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Internal request for stock",
     "version": "13.0.1.3.1",
     "license": "LGPL-3",
-    "website": "https://github.com/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Warehouse Management",
     "depends": ["stock"],
