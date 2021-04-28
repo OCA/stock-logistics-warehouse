829d3cc75a392af1a5eac8b55204203691a77d8f
Matthieu Méquignon <matthieu.mequignon@camptocamp.com>
2021-04-28 13:14:27 +0200

    stock_measuring_device: Auto-refresh zipcube wiz when measurement is done

--- stock_measuring_device_zippcube/__manifest__.py
+++ stock_measuring_device_zippcube/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Stock Zippcube",
     "summary": "Implement interface with Bosche Zippcube devices"
     "for packaging measurement",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "category": "Warehouse",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "license": "AGPL-3",
