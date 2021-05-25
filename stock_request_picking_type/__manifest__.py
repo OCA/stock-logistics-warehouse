32225b13c2fc67eecd947b9993b27f83b5f96b2a
Reed Hayashikawa <rhayashikawa@opensourceintegrators.com>
2021-05-25 11:49:19 -0700

    [IMP] stock_request_picking_type: black, isort, prettier

--- stock_request_picking_type/__manifest__.py
+++ stock_request_picking_type/__manifest__.py
@@ -4,9 +4,9 @@
 {
     "name": "Stock Request Picking Type",
     "summary": "Add Stock Requests to the Inventory App",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "LGPL-3",
-    "website": "https://github.com/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
     "category": "Warehouse",
     "depends": ["stock_request"],
