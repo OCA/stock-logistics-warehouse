425a2601d1dbc09e0307e68dae0563ca7ce65e4e
Manuel Calero <manuelcalerosolis@gmail.com>
2021-05-26 20:40:56 +0200

    [MIG] stock_production_lot_quantity_tree: Migration to 13.0

--- stock_production_lot_quantity_tree/__manifest__.py
+++ stock_production_lot_quantity_tree/__manifest__.py
@@ -2,18 +2,14 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Stock Production Lot Quantity Tree',
-    'summary': """
+    "name": "Stock Production Lot Quantity Tree",
+    "summary": """
         Allows to display product quantity field on production lot tree view""",
-    'version': '12.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ACSONE SA/NV,Odoo Community Association (OCA)',
-    'maintainers': ['rousseldenis'],
-    'website': 'https://github.com/OCA/stock-logistics-warehouse',
-    'depends': [
-        'stock',
-    ],
-    'data': [
-        'views/stock_production_lot.xml',
-    ],
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "maintainers": ["rousseldenis"],
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock"],
+    "data": ["views/stock_production_lot.xml"],
 }
