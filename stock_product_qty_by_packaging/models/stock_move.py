edbb2c4ce5f419b145f0c021dcf10a3bfda70f8b
Simone Orsi <simahawk@gmail.com>
2021-06-11 11:49:06 +0200

    s_product_qty_by_packaging: minor fixes

--- stock_product_qty_by_packaging/models/stock_move.py
+++ stock_product_qty_by_packaging/models/stock_move.py
@@ -1,7 +1,7 @@
 # Copyright 2020 Camptocamp SA
 # @author: Simone Orsi <simone.orsi@camptocamp.com>
 # @author: Sébastien Alix <sebastien.alix@camptocamp.com>
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl)
 
 from odoo import models
 
