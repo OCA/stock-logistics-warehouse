35d82ca02db3284fb475f39256d08dfa42712ada
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2021-06-26 11:33:23 +0200

    [OU-ADD] stock_request: Migration scripts

---
+++ stock_request/migrations/13.0.1.0.0/post-migration.py
@@ -0,0 +1,10 @@
+# Copyright 2021 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "stock_request", "migrations/13.0.1.0.0/noupdate_changes.xml"
+    )
