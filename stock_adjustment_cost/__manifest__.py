db0990c87611552cd408bbba3822cce9f2d4c363
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-07-01 18:25:04 +0530

    [14.0][ADD]stock_adjustment_cost: added module

---
+++ stock_adjustment_cost/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright (C) 2021 Open Source Integrators
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Adjustment Cost",
+    "version": "14.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "Open Source Integrators,Odoo Community Association (OCA)",
+    "maintainer": "Open Source Integrators",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Stock",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_inventory_line_view.xml",
+    ],
+    "installable": True,
+}
