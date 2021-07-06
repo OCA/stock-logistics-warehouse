77f87827f1ea2d181ac01b1a1545a40cdbfe124e
Reed Hayashikawa <rhayashikawa@opensourceintegrators.com>
2021-07-06 16:40:25 -0700

    [IMP] stock_putaway_method: black, isort, prettier

--- stock_putaway_method/models/product_strategy.py
+++ stock_putaway_method/models/product_strategy.py
@@ -3,15 +3,15 @@ from odoo import api, fields, models
 
 
 class PutAwayStrategy(models.Model):
-    _inherit = 'product.putaway'
+    _inherit = "product.putaway"
 
     method = fields.Selection(
-        selection='_get_putaway_options',
-        string='Method',
-        default='fixed',
+        selection="_get_putaway_options",
+        string="Method",
+        default="fixed",
         required=True,
     )
 
     @api.model
     def _get_putaway_options(self):
-        return [('fixed', 'Fixed Location')]
+        return [("fixed", "Fixed Location")]
