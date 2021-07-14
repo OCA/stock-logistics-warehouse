f967343f383156e43788aaff9a5e37e4c2f403a7
Mateu Griful <mateu.griful@forgeflow.com>
2021-07-14 13:58:02 +0200

    [ADD] stock_packaging_qty

---
+++ stock_move_packaging_qty/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Stock Packaging Qty",
+    "summary": "Add packaging fields in the stock moves",
+    "version": "13.0.1.1.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse",
+    "depends": ["stock"],
+    "data": ["views/stock_picking_form_view.xml", "views/stock_move_tree_view.xml"],
+    "license": "LGPL-3",
+    "installable": True,
+}
