c0dcd33eda940efbbf2a5a74c9bf44d14705e79c
Carlos Roca <carlos.roca@tecnativa.com>
2021-07-23 12:24:28 +0200

    [MIG] stock_reserve: Migration to v13.0

--- stock_reserve/__manifest__.py
+++ stock_reserve/__manifest__.py
@@ -1,42 +1,22 @@
-##############################################################################
-#
-#    Author: Guewen Baconnier
-#    Copyright 2013 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# Copyright 2013 Camptocamp SA - Guewen Baconnier
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Reservation",
     "summary": "Stock reservations on products",
-    "version": "10.0.1.0.0",
-    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
     "category": "Warehouse",
     "license": "AGPL-3",
     "complexity": "normal",
-    "images": [],
-    "website": "http://www.camptocamp.com",
-    "depends": ["stock",],
-    "demo": [],
+    "website": "https://github.com/stock-logistics-warehouse",
+    "depends": ["stock"],
     "data": [
         "view/stock_reserve.xml",
         "view/product.xml",
         "data/stock_data.xml",
         "security/ir.model.access.csv",
+        "data/cron.xml",
     ],
     "auto_install": False,
-    "test": ["test/stock_reserve.yml",],
     "installable": True,
 }
