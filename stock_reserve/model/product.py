4b46ab51184750c96c8bee24a7acf27f2fdf29c9
Carlos Roca <carlos.roca@tecnativa.com>
2021-07-23 12:23:01 +0200

    [IMP] stock_reserve: black, isort, prettier

--- stock_reserve/model/product.py
+++ stock_reserve/model/product.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Author: Guewen Baconnier
@@ -19,59 +18,62 @@
 #
 ##############################################################################
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 
 
 class ProductTemplate(models.Model):
-    _inherit = 'product.template'
+    _inherit = "product.template"
 
     reservation_count = fields.Float(
-        compute='_compute_reservation_count',
-        string='# Sales')
+        compute="_compute_reservation_count", string="# Sales"
+    )
 
     @api.multi
     def _compute_reservation_count(self):
         for product in self:
             product.reservation_count = sum(
-                product.product_variant_ids.mapped('reservation_count'))
+                product.product_variant_ids.mapped("reservation_count")
+            )
 
     @api.multi
     def action_view_reservations(self):
         self.ensure_one()
-        ref = 'stock_reserve.action_stock_reservation_tree'
-        product_ids = self.mapped('product_variant_ids.id')
+        ref = "stock_reserve.action_stock_reservation_tree"
+        product_ids = self.mapped("product_variant_ids.id")
         action_dict = self.env.ref(ref).read()[0]
-        action_dict['domain'] = [('product_id', 'in', product_ids)]
-        action_dict['context'] = {
-            'search_default_draft': 1,
-            'search_default_reserved': 1
-            }
+        action_dict["domain"] = [("product_id", "in", product_ids)]
+        action_dict["context"] = {
+            "search_default_draft": 1,
+            "search_default_reserved": 1,
+        }
         return action_dict
 
 
 class ProductProduct(models.Model):
-    _inherit = 'product.product'
+    _inherit = "product.product"
 
     reservation_count = fields.Float(
-        compute='_compute_reservation_count',
-        string='# Sales')
+        compute="_compute_reservation_count", string="# Sales"
+    )
 
     @api.multi
     def _compute_reservation_count(self):
         for product in self:
-            domain = [('product_id', '=', product.id),
-                      ('state', 'in', ['draft', 'assigned'])]
-            reservations = self.env['stock.reservation'].search(domain)
-            product.reservation_count = sum(reservations.mapped('product_qty'))
+            domain = [
+                ("product_id", "=", product.id),
+                ("state", "in", ["draft", "assigned"]),
+            ]
+            reservations = self.env["stock.reservation"].search(domain)
+            product.reservation_count = sum(reservations.mapped("product_qty"))
 
     @api.multi
     def action_view_reservations(self):
         self.ensure_one()
-        ref = 'stock_reserve.action_stock_reservation_tree'
+        ref = "stock_reserve.action_stock_reservation_tree"
         action_dict = self.env.ref(ref).read()[0]
-        action_dict['domain'] = [('product_id', '=', self.id)]
-        action_dict['context'] = {
-            'search_default_draft': 1,
-            'search_default_reserved': 1
-            }
+        action_dict["domain"] = [("product_id", "=", self.id)]
+        action_dict["context"] = {
+            "search_default_draft": 1,
+            "search_default_reserved": 1,
+        }
         return action_dict
--- stock_reserve/model/stock_reserve.py
+++ stock_reserve/model/stock_reserve.py
@@ -1,4 +1,3 @@
-# -*- coding: utf-8 -*-
 ##############################################################################
 #
 #    Author: Guewen Baconnier
@@ -19,7 +18,7 @@
 #
 ##############################################################################
 
-from odoo import models, fields, api
+from odoo import api, fields, models
 from odoo.exceptions import except_orm
 from odoo.tools.translate import _
 
@@ -46,18 +45,20 @@ class StockReservation(models.Model):
     * date_validity  (once passed, the reservation will be released)
     * note
     """
-    _name = 'stock.reservation'
-    _description = 'Stock Reservation'
-    _inherits = {'stock.move': 'move_id'}
+
+    _name = "stock.reservation"
+    _description = "Stock Reservation"
+    _inherits = {"stock.move": "move_id"}
 
     move_id = fields.Many2one(
-        'stock.move',
-        'Reservation Move',
+        "stock.move",
+        "Reservation Move",
         required=True,
         readonly=True,
-        ondelete='cascade',
-        index=True)
-    date_validity = fields.Date('Validity Date')
+        ondelete="cascade",
+        index=True,
+    )
+    date_validity = fields.Date("Validity Date")
 
     @api.model
     def default_get(self, fields_list):
@@ -74,14 +75,13 @@ class StockReservation(models.Model):
         # if there is 'location_id' field requested, ensure that
         # picking_type_id is also requested, because it is required
         # to compute location_id
-        if ('location_id' in fields_list and
-                'picking_type_id' not in fields_list):
-            fields_list = fields_list + ['picking_type_id']
+        if "location_id" in fields_list and "picking_type_id" not in fields_list:
+            fields_list = fields_list + ["picking_type_id"]
 
         res = super(StockReservation, self).default_get(fields_list)
 
-        if 'product_qty' in res:
-            del res['product_qty']
+        if "product_qty" in res:
+            del res["product_qty"]
 
         # At this point picking_type_id and location_id
         # should be computed in default way:
@@ -93,19 +93,20 @@ class StockReservation(models.Model):
         #
         # If picking_type_id is present and location_id is not, try to find
         # default value for location_id
-        if not res.get('picking_type_id', None):
-            res['picking_type_id'] = self._default_picking_type_id()
+        if not res.get("picking_type_id", None):
+            res["picking_type_id"] = self._default_picking_type_id()
 
-        picking_type_id = res.get('picking_type_id')
-        if picking_type_id and not res.get('location_id', False):
-            picking = self.env['stock.picking'].new(
-                {'picking_type_id': picking_type_id})
+        picking_type_id = res.get("picking_type_id")
+        if picking_type_id and not res.get("location_id", False):
+            picking = self.env["stock.picking"].new(
+                {"picking_type_id": picking_type_id}
+            )
             picking.onchange_picking_type()
-            res['location_id'] = picking.location_id.id
-        if 'location_dest_id' in fields_list:
-            res['location_dest_id'] = self._default_location_dest_id()
-        if 'product_uom_qty' in fields_list:
-            res['product_uom_qty'] = 1.0
+            res["location_id"] = picking.location_id.id
+        if "location_dest_id" in fields_list:
+            res["location_dest_id"] = self._default_location_dest_id()
+        if "product_uom_qty" in fields_list:
+            res["product_uom_qty"] = 1.0
         return res
 
     @api.model
@@ -115,7 +116,7 @@ class StockReservation(models.Model):
         """
         try:
             location = self.env.ref(ref, raise_if_not_found=True)
-            location.check_access_rule('read')
+            location.check_access_rule("read")
             location_id = location.id
         except (except_orm, ValueError):
             location_id = False
@@ -123,12 +124,12 @@ class StockReservation(models.Model):
 
     @api.model
     def _default_picking_type_id(self):
-        ref = 'stock.picking_type_out'
+        ref = "stock.picking_type_out"
         return self.env.ref(ref, raise_if_not_found=False).id
 
     @api.model
     def _default_location_dest_id(self):
-        ref = 'stock_reserve.stock_location_reservation'
+        ref = "stock_reserve.stock_location_reservation"
         return self.get_location_from_ref(ref)
 
     @api.multi
@@ -138,9 +139,9 @@ class StockReservation(models.Model):
         The reservation is done using the default UOM of the product.
         A date until which the product is reserved can be specified.
         """
-        self.write({'date_expected': fields.Datetime.now()})
-        self.mapped('move_id').action_confirm()
-        self.mapped('move_id.picking_id').action_assign()
+        self.write({"date_expected": fields.Datetime.now()})
+        self.mapped("move_id").action_confirm()
+        self.mapped("move_id.picking_id").action_assign()
         return True
 
     @api.multi
@@ -148,16 +149,18 @@ class StockReservation(models.Model):
         """
         Release moves from reservation
         """
-        self.mapped('move_id').action_cancel()
+        self.mapped("move_id").action_cancel()
         return True
 
     @api.model
     def release_validity_exceeded(self, ids=None):
         """ Release all the reservation having an exceeded validity date """
-        domain = [('date_validity', '<', fields.date.today()),
-                  ('state', '=', 'assigned')]
+        domain = [
+            ("date_validity", "<", fields.date.today()),
+            ("state", "=", "assigned"),
+        ]
         if ids:
-            domain.append(('id', 'in', ids))
+            domain.append(("id", "in", ids))
         self.search(domain).release()
         return True
 
@@ -167,18 +170,19 @@ class StockReservation(models.Model):
         self.release()
         return super(StockReservation, self).unlink()
 
-    @api.onchange('product_id')
+    @api.onchange("product_id")
     def _onchange_product_id(self):
         """ set product_uom and name from product onchange """
         # save value before reading of self.move_id as this last one erase
         # product_id value
-        move = self.move_id or self.env['stock.move'].new(
-            {'product_id': self.product_id})
+        move = self.move_id or self.env["stock.move"].new(
+            {"product_id": self.product_id}
+        )
         move.onchange_product_id()
         self.name = move.name
         self.product_uom = move.product_uom
 
-    @api.onchange('product_uom_qty')
+    @api.onchange("product_uom_qty")
     def _onchange_quantity(self):
         """ On change of product quantity avoid negative quantities """
         if not self.product_id or self.product_uom_qty <= 0.0:
@@ -187,13 +191,12 @@ class StockReservation(models.Model):
     @api.multi
     def open_move(self):
         self.ensure_one()
-        action = self.env.ref('stock.stock_move_action')
+        action = self.env.ref("stock.stock_move_action")
         action_dict = action.read()[0]
-        action_dict['name'] = _('Reservation Move')
+        action_dict["name"] = _("Reservation Move")
         # open directly in the form view
-        view_id = self.env.ref('stock.view_move_form').id
+        view_id = self.env.ref("stock.view_move_form").id
         action_dict.update(
-            views=[(view_id, 'form')],
-            res_id=self.move_id.id,
-            )
+            views=[(view_id, "form")], res_id=self.move_id.id,
+        )
         return action_dict
