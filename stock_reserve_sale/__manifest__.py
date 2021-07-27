88b20257b056fa75e558cd413a957f5d8894b6ac
Carlos Roca <carlos.roca@tecnativa.com>
2021-07-27 07:11:44 +0200

    [MIG] stock_reserve_sale: Migration to v13.0

--- stock_reserve_sale/__manifest__.py
+++ stock_reserve_sale/__manifest__.py
@@ -1,40 +1,19 @@
-##############################################################################
-#
-#    Author: Guewen Baconnier, Leonardo Pistone
-#    Copyright 2013-2015 Camptocamp SA
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-
+# Copyright 2013 Camptocamp SA - Guewen Baconnier
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Reserve Sales",
-    "version": "8.0.1.0.0",
-    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "version": "13.0.1.0.0",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
     "category": "Warehouse",
     "license": "AGPL-3",
     "complexity": "normal",
-    "images": [],
-    "website": "http://www.camptocamp.com",
-    "depends": ["sale_stock", "stock_reserve",],
-    "demo": [],
+    "website": "https://github.com/stock-logistics-warehouse",
+    "depends": ["sale_stock", "stock_reserve"],
     "data": [
         "wizard/sale_stock_reserve_view.xml",
         "view/sale.xml",
         "view/stock_reserve.xml",
     ],
-    "test": ["test/sale_reserve.yml", "test/sale_line_reserve.yml",],
-    "installable": False,
+    "installable": True,
     "auto_install": False,
 }
