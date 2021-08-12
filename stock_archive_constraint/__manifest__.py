e758ae942046a11d91a3a1bb7b608239c085cea3
Marçal Isern <marsal.isern@qubiq.es>
2021-08-12 15:45:59 +0200

    [MIG] stock_archive_constraint: Migration to 14.0

--- stock_archive_constraint/__manifest__.py
+++ stock_archive_constraint/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock archive constraint",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Tecnativa, Odoo Community Association (OCA)",
