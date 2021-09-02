d9aee1b216fe6a408b07e9f958c2fa795a99cc40
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-09-02 15:31:05 +0530

    [FIX]stock_request_direction: fixed an error while on click on one2many editabe tree on sro request model

--- stock_request_direction/models/stock_request.py
+++ stock_request_direction/models/stock_request.py
@@ -16,9 +16,10 @@ class StockRequest(models.Model):
 
     @api.onchange("direction")
     def _onchange_location_id(self):
-        if self.direction == "outbound":
-            # Stock Location set to Partner Locations/Customers
-            self.location_id = self.company_id.partner_id.property_stock_customer.id
-        else:
-            # Otherwise the Stock Location of the Warehouse
-            self.location_id = self.warehouse_id.lot_stock_id.id
+        if not self._context.get("default_location_id"):
+            if self.direction == "outbound":
+                # Stock Location set to Partner Locations/Customers
+                self.location_id = self.company_id.partner_id.property_stock_customer.id
+            else:
+                # Otherwise the Stock Location of the Warehouse
+                self.location_id = self.warehouse_id.lot_stock_id.id
