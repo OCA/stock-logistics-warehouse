aa31b5c09ac4fb452a115ef0d49ba60dff43503a
Vimal Patel <vimal.patel.serpentcs@gmail.com>
2021-09-02 15:31:05 +0530

    [FIX]stock_request_direction: fixed an error while on click on one2many editabe tree on sro request model

--- stock_request_direction/__manifest__.py
+++ stock_request_direction/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Requests Direction",
     "summary": "From or to your warehouse?",
-    "version": "14.0.1.0.0",
+    "version": "14.0.1.0.1",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
