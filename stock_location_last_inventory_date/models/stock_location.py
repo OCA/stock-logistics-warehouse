7fe31804976a5089432ea52a1b32e4b9ff322d94
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2021-09-08 06:59:36 +0200

    [13.0][FIX] stock_location_last_inventory_date
    The prevoius implementation was only taking into account the last inventory
    specific to the location.
    This change includes also inventories done on parents location.
    
    Also display the computed last inventory date in the form view.

--- stock_location_last_inventory_date/models/stock_location.py
+++ stock_location_last_inventory_date/models/stock_location.py
@@ -1,6 +1,6 @@
 # Copyright 2021 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class StockLocation(models.Model):
@@ -9,44 +9,19 @@ class StockLocation(models.Model):
     last_inventory_date = fields.Datetime(
         "Last Inventory Date",
         compute="_compute_last_inventory_date",
-        store=True,
-        help="Indicates the last inventory date for the location (only for "
-        "validated inventories). It is only computed for leaf locations.",
+        help="Indicates the last inventory date for the location, "
+        "including inventory done on parents location.",
     )
 
-    # This field reuses the Many2many already defined in the model
-    # stock.inventory, so this definition adds little overhead, and
-    # allows to create the list of depends needed by the field for the
-    # Last Inventory Date.
-    validated_inventory_ids = fields.Many2many(
-        "stock.inventory",
-        relation="stock_inventory_stock_location_rel",
-        column1="stock_location_id",
-        column2="stock_inventory_id",
-        string="Stock Inventories",
-        help="Stock inventories in state validated for this location.",
-        domain="[('location_ids', 'in', id), ('state', '=', 'done')]",
-    )
-
-    @api.depends(
-        "usage",
-        "child_ids",
-        "validated_inventory_ids",
-        "validated_inventory_ids.date",
-        "validated_inventory_ids.state",
-        "validated_inventory_ids.location_ids.usage",
-        "validated_inventory_ids.location_ids.child_ids",
-    )
     def _compute_last_inventory_date(self):
-        """Store date of the last inventory for each leaf location"""
-        for loc in self:
-            if (
-                loc.usage != "view"
-                and not loc.child_ids
-                and loc.validated_inventory_ids
-            ):
-                loc.last_inventory_date = loc.validated_inventory_ids.sorted(
-                    lambda inventory: inventory.date
-                )[-1].date
-            else:
-                loc.last_inventory_date = False
+        for location in self:
+            location_ids = [
+                int(location_id)
+                for location_id in location.parent_path.rstrip("/").split("/")
+            ]
+            last_inventory = self.env["stock.inventory"].search(
+                [("location_ids", "in", location_ids), ("state", "=", "done")],
+                order="date desc",
+                limit=1,
+            )
+            location.last_inventory_date = last_inventory.date
