eba2d1ec4ebcc80f219ec36cdc17c43363e02158
joan <joan.sisquella@forgeflow.com>
2021-09-10 11:38:35 +0200

    [MIG] stock_inventory_exclude_sublocation: Migration to 14.0

--- stock_inventory_exclude_sublocation/__manifest__.py
+++ stock_inventory_exclude_sublocation/__manifest__.py
@@ -1,11 +1,11 @@
-# Copyright 2017-2019 ForgeFlow S.L. (http://www.forgeflow.com)
+# Copyright 2017-2021 ForgeFlow S.L. (http://www.forgeflow.com)
 # Copyright 2019 Tecnativa - Pedro M. Baeza
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Inventory Exclude Sublocation",
     "summary": "Allow to perform inventories of a location without including "
     "its child locations.",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "development_status": "Mature",
     "author": "ForgeFlow, Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
