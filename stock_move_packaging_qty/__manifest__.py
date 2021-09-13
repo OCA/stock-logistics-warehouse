e779a18cbfdd4d21b0ea263b1d503773e9e132c7
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2021-09-13 15:59:24 +0200

    [FIX] stock_move_packaging_qty version
    version conflict prevents upload to pypi

--- stock_move_packaging_qty/__manifest__.py
+++ stock_move_packaging_qty/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Move Packaging Qty",
     "summary": "Add packaging fields in the stock moves",
-    "version": "12.0.1.0.0",
+    "version": "12.0.1.0.1",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
