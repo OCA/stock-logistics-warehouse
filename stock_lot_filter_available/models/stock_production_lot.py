dd00e7f949693331712b99890ef14d7d31aa8bd8
CarlosRoca13 <carlos.roca@tecnativa.com>
2021-09-15 10:39:06 +0200

    [IMP] stock_lot_filter_available: Add more conditions to avoid errors

--- stock_lot_filter_available/models/stock_production_lot.py
+++ stock_lot_filter_available/models/stock_production_lot.py
@@ -1,6 +1,8 @@
 # Copyright 2021 Tecnativa - Carlos Roca
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import _, api, fields, models
+import operator as operator_lib
+
+from odoo import _, fields, models
 
 
 class StockProductionLot(models.Model):
@@ -8,12 +10,16 @@ class StockProductionLot(models.Model):
 
     product_qty = fields.Float(search="_search_product_qty")
 
-    @api.model
-    def _is_unsupported_search_operator(self, operator):
-        return operator != ">"
-
     def _search_product_qty(self, operator, value):
-        if self._is_unsupported_search_operator(operator) or value:
+        operator_mapping = {
+            ">": operator_lib.gt,
+            ">=": operator_lib.ge,
+            "<": operator_lib.lt,
+            "<=": operator_lib.le,
+            "=": operator_lib.eq,
+            "!=": operator_lib.ne,
+        }
+        if operator not in operator_mapping:
             raise ValueError(_("Unsupported search"))
         ids = []
         domain = [
@@ -23,7 +29,18 @@ class StockProductionLot(models.Model):
         groups = self.env["stock.quant"].read_group(
             domain, ["lot_id", "quantity"], ["lot_id"]
         )
+        involved_lot_ids = [group["lot_id"][0] for group in groups]
+        if (
+            (operator == "=" and value == 0)
+            or (operator == "<" and value > 0)
+            or (operator == "<=" and value >= 0)
+        ):
+            ids.extend(
+                self.env["stock.production.lot"]
+                .search([("id", "not in", involved_lot_ids)])
+                .ids
+            )
         for group in groups:
-            if group["quantity"] > value:
+            if operator_mapping[operator](group["quantity"], value):
                 ids.append(group["lot_id"][0])
         return [("id", "in", ids)]
