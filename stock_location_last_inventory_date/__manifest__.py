5e92f5e7a809ffab5494ae846a21dfc002edcbf2
Hai Lang <hailn@trobz.com>
2021-09-24 15:13:47 +0700

    [MIG] stock_location_last_inventory_date: Migration to 14.0

--- stock_location_last_inventory_date/__manifest__.py
+++ stock_location_last_inventory_date/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Location Last Inventory Date",
     "summary": "Show the last inventory date for a leaf location",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "development_status": "Alpha",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
