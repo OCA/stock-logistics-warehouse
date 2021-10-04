26847743adb6fedd008f939f1f3f2f19dce153c0
david <david.vidal@tecnativa.com>
2021-10-04 11:55:20 +0200

    [FIX] sale_stock_available_info_popup: cache miss
    TT29615
    
    sale_stock_available_info_popup 13.0.1.0.1

--- sale_stock_available_info_popup/__manifest__.py
+++ sale_stock_available_info_popup/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.0.1",
     "license": "AGPL-3",
     "depends": ["sale_stock", "stock_available"],
     "data": ["views/sale_order_views.xml"],
