c371ab833f6ddf3ccdd044ed9f8ab4b8a2104367
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-10-06 17:50:12 +0200

    [stock_fifo_update_kit_cost][new] This module allows the cost of a product that is sold as a kit to be updated automatically when the components are delivered.

---
+++ stock_fifo_update_kit_cost/models/product.py
@@ -0,0 +1,30 @@
+# Copyright 20121 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import models
+
+
+class Product(models.Model):
+    _inherit = "product.product"
+
+    def _run_fifo(self, quantity, company):
+        vals = super(Product, self)._run_fifo(quantity, company)
+        phantom_boms = (
+            self.env["mrp.bom.line"]
+            .search(
+                [
+                    ("product_id", "in", self.ids),
+                    ("bom_id.type", "=", "phantom"),
+                    "|",
+                    ("company_id", "=", company.id),
+                    ("company_id", "=", False),
+                ]
+            )
+            .mapped("bom_id")
+        )
+        products = phantom_boms.mapped("product_id")
+        products |= phantom_boms.mapped("product_tmpl_id.product_variant_ids")
+        for product in products:
+            cost = product._get_price_from_bom(phantom_boms)
+            product.sudo().with_context(force_company=company.id).standard_price = cost
+        return vals
---
+++ stock_fifo_update_kit_cost/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows the cost of a product that is sold as a kit to be updated
+automatically when the components are delivered.
