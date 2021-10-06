bbee314c75d86f82ff91d297f1d5959725fc2b56
Jordi Ballester <jordi.ballester@forgeflow.com>
2021-10-06 17:50:12 +0200

    [stock_fifo_update_kit_cost][new] This module allows the cost of a product that is sold as a kit to be updated automatically when the components are delivered.

---
+++ stock_fifo_update_kit_cost/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2018 Camptocamp SA
+# Copyright 2016-19 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Stock Fifo Update Kit Cost",
+    "summary": "Automatically update the cost of a kit when the components "
+    "are shipped",
+    "version": "13.0.1.0.1",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "development_status": "Beta",
+    "maintainers": ["JordiBForgeFlow"],
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "depends": ["stock_account"],
+    "license": "LGPL-3",
+}
