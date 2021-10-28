b71d574921ef294f2d152a03ba319322e5f86a82
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-10-28 11:20:08 +0200

    [MIG] account_move_line_stock_info: migration to 15.0

--- account_move_line_stock_info/tests/test_account_move_line_stock_move.py
+++ account_move_line_stock_info/tests/test_account_move_line_stock_move.py
@@ -61,7 +61,7 @@ class TestAccountMoveLineStockInfo(TransactionCase):
     def _create_user(self, login, groups, company):
         """Create a user."""
         group_ids = [group.id for group in groups]
-        user = self.res_users_model.with_context({"no_reset_password": True}).create(
+        user = self.res_users_model.with_context(**{"no_reset_password": True}).create(
             {
                 "name": "Test User",
                 "login": login,
