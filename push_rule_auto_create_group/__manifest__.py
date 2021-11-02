41f78e9db371059aff498f5b31d4db306c5a6797
Bernat Puig Font <bernat.puig@forgeflow.com>
2021-11-02 14:57:50 +0100

    [IMP] push_rule_auto_create_group: black, isort, prettier

--- push_rule_auto_create_group/__manifest__.py
+++ push_rule_auto_create_group/__manifest__.py
@@ -6,16 +6,15 @@
     "development_status": "Alpha",
     "license": "AGPL-3",
     "summary": "Allows to configure the system to propose automatically new "
-               "procurement groups in applying push rules.",
-    "author": "ForgeFlow,"
-              "Odoo Community Association (OCA)",
+    "procurement groups in applying push rules.",
+    "author": "ForgeFlow," "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
     "depends": [
         "stock",
     ],
     "data": [
-        'views/stock_location_view.xml',
+        "views/stock_location_view.xml",
     ],
     "installable": True,
 }
