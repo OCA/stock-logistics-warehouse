a6df013a53a4cb640fa694066dae0dc1f7d9c4bf
Daniel Reis <dreis.pt@hotmail.com>
2021-11-03 17:59:59 +0000

    [ADD] stock_orderpoint_origin, stock_orderpoint_origin_mrp_link: add replenishment source documents to PO

---
+++ stock_orderpoint_origin/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2021 Open Source Integrators
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+{
+    "name": "Stock Orderpoint Replenishment demand origin details",
+    "summary": "Link Purchase Orders to the replenishment demand Sales Orders",
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "author": "Open Source Integrators, Odoo Community Association (OCA)",
+    "category": "Warehouse",
+    "depends": ["purchase_stock", "sale_stock"],
+    "installable": True,
+}
---
+++ stock_orderpoint_origin_mrp_link/readme/USAGE.rst
@@ -0,0 +1,5 @@
+- Create order from the Replenishment menu, for example, using the "Order Once" button.
+- Verify the Origin field of the created orders. It should include the references to the
+  orders generating this demand at the time of replenishment creation.
+- If demand was generated from Manufacturing Orders, the "Manufacturin" smart button
+  should be available.
