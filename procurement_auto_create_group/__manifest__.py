5da5f42a06923dba0a09d25d6d843584251b1484
Bernat Puig Font <bernat.puig@forgeflow.com>
2021-11-04 13:10:08 +0100

    [MIG] procurement_auto_create_rule: Migration to 15.0

--- procurement_auto_create_group/__manifest__.py
+++ procurement_auto_create_group/__manifest__.py
@@ -3,7 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Procurement Auto Create Group",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "development_status": "Production/Stable",
     "license": "AGPL-3",
     "summary": "Allows to configure the system to propose automatically new "
