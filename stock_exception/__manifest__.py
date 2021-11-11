3662eef4caded13cdbea057afe47b79c514e6053
newtratip <tharathip.chaweewongphan@gmail.com>
2021-11-11 09:15:08 +0700

    [14.0][ADD] stock_exception

---
+++ stock_exception/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2021 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+{
+    "name": "Stock Exception",
+    "summary": "Custom exceptions on stock picking",
+    "version": "14.0.1.0.0",
+    "category": "Generic Modules/Warehouse Management",
+    "author": "Ecosoft, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock", "base_exception"],
+    "license": "AGPL-3",
+    "data": [
+        "security/ir.model.access.csv",
+        "data/stock_exception_data.xml",
+        "wizard/stock_exception_confirm_view.xml",
+        "views/stock_view.xml",
+    ],
+    "installable": True,
+}
---
+++ stock_exception/models/exception_rule.py
@@ -0,0 +1,17 @@
+# Copyright 2021 Ecosoft Co., Ltd (https://ecosoft.co.th)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html)
+
+from odoo import fields, models
+
+
+class ExceptionRule(models.Model):
+    _inherit = "exception.rule"
+
+    picking_ids = fields.Many2many(comodel_name="stock.picking", string="Pickings")
+    model = fields.Selection(
+        selection_add=[
+            ("stock.picking", "Stock Picking"),
+            ("stock.move", "Stock Move"),
+        ],
+        ondelete={"stock.picking": "cascade", "stock.move": "cascade"},
+    )
