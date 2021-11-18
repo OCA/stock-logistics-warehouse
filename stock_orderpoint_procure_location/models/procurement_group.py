0e6d878816241b097abce9d322b21e5fa003f23c
Bernat Puig Font <bernat.puig@forgeflow.com>
2021-11-18 12:39:22 +0100

    [MIG] stock_orderpoint_procure_location: Migration to 14.0

--- stock_orderpoint_procure_location/models/procurement_group.py
+++ stock_orderpoint_procure_location/models/procurement_group.py
@@ -8,24 +8,7 @@ class ProcurementGroup(models.Model):
     _inherit = "procurement.group"
 
     @api.model
-    def run(
-        self,
-        product_id,
-        product_qty,
-        product_uom,
-        location_id,
-        name,
-        origin,
-        values,
-    ):
-        if values and "procure_location_id" in values:
-            location_id = values.get("procure_location_id")
-        return super(ProcurementGroup, self).run(
-            product_id,
-            product_qty,
-            product_uom,
-            location_id,
-            name,
-            origin,
-            values,
-        )
+    def run(self, procurements):
+        if procurements and "procure_location_id" in procurements:
+            procurements["location_id"] = procurements.get("procure_location_id")
+        return super(ProcurementGroup, self).run(procurements)
