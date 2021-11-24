830db49a86e4a8400901bf1782fe876df5985098
Denis Roussel <denis.roussel@acsone.eu>
2021-11-24 10:57:26 +0100

    [14.0][IMP] stock_request_purchase: Use _for_xml_id for actions

--- stock_request_purchase/models/stock_request.py
+++ stock_request_purchase/models/stock_request.py
@@ -39,7 +39,7 @@ class StockRequest(models.Model):
             )
 
     def action_view_purchase(self):
-        action = self.env.ref("purchase.purchase_rfq").read()[0]
+        action = self.env["ir.actions.act_window"]._for_xml_id("purchase.purchase_rfq")
 
         purchases = self.mapped("purchase_ids")
         if len(purchases) > 1:
