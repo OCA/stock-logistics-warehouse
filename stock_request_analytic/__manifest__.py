95939fb87f580fd84efb25c35597394f518ee8af
Denis Roussel <denis.roussel@acsone.eu>
2021-11-24 10:16:28 +0100

    [IMP] stock_request_analytic: black, isort, prettier

--- stock_request_analytic/__manifest__.py
+++ stock_request_analytic/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Internal request for stock",
     "version": "13.0.1.0.0",
     "license": "LGPL-3",
-    "website": "https://github.com/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Analytic",
     "depends": ["stock_request", "stock_analytic"],
