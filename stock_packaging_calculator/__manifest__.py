e067ec70e86fd00a682055084b275e5172a22be5
Christopher Ormaza <chris.ormaza@forgeflow.com>
2021-12-13 13:25:18 -0500

    [15.0][MIG] stock_packaging_calculator

--- stock_packaging_calculator/__manifest__.py
+++ stock_packaging_calculator/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock packaging calculator",
     "summary": "Compute product quantity to pick by packaging",
-    "version": "14.0.1.1.0",
+    "version": "15.0.1.0.0",
     "development_status": "Alpha",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
