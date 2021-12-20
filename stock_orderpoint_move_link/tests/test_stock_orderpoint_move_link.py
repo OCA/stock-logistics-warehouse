d0a7134eb6f915b847b2b818a22a0e2fad469c44
Bernat Puig Font <bernat.puig@forgeflow.com>
2021-12-20 12:21:10 +0100

    [MIG] stock_orderpoint_move_link: Migration to 15.0

--- stock_orderpoint_move_link/tests/test_stock_orderpoint_move_link.py
+++ stock_orderpoint_move_link/tests/test_stock_orderpoint_move_link.py
@@ -2,10 +2,10 @@
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 import ast
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestStockOrderpointMoveLink(SavepointCase):
+class TestStockOrderpointMoveLink(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
