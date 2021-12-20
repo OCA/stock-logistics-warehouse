cafb3bd630712d8bae5016cfb318fcc0d87137c4
Joan Mateu Jordi <joan.mateu@forgeflow.com>
2021-12-20 11:55:34 +0100

    [15.0][MIG] stock_orderpoint_uom: Migration to 15.0

--- stock_orderpoint_uom/__manifest__.py
+++ stock_orderpoint_uom/__manifest__.py
@@ -4,7 +4,7 @@
     "name": "Stock Orderpoint UoM",
     "summary": "Allows to create procurement orders in the UoM indicated in "
     "the orderpoint",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
