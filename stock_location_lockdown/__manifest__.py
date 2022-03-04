694104adaf2408360c5ce767f8732fd514b8fb9d
David Montull <david.montull@bt-group.com>
2022-03-04 13:01:52 +0100

    [15.0][MIG] stock_location_lockdown: Migration to 15.0

--- stock_location_lockdown/__manifest__.py
+++ stock_location_lockdown/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Akretion, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "license": "AGPL-3",
     "application": False,
     "installable": True,
