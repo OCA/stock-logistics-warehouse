cbcfbd3112de99e7ad696deed44453117685ad41
Héctor Villarreal Ortega <hector.villarreal@forgeflow.com>
2022-03-09 18:47:41 +0100

    [12.0][MIG] scrap_reason_code

--- scrap_reason_code/__manifest__.py
+++ scrap_reason_code/__manifest__.py
@@ -2,7 +2,7 @@
 
 {
     "name": "Scrap Reason Code",
-    "version": "13.0.1.1.1",
+    "version": "12.0.1.0.0",
     "license": "AGPL-3",
     "summary": "Reason code for scrapping",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
