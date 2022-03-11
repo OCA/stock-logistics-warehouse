a5d7a5263d72f170f4a06556b5eb668855b36fec
Maxime Chambreuil <mchambreuil@opensourceintegrators.com>
2022-03-11 17:17:11 -0600

    [FIX] stock_inventory_revaluation_mrp

--- stock_inventory_revaluation_mrp/__manifest__.py
+++ stock_inventory_revaluation_mrp/__manifest__.py
@@ -12,7 +12,7 @@
     "depends": [
         "stock_inventory_revaluation_adjustment",
         "mrp_account",
-        "mrp_analytic_cost",
+        "mrp_account_analytic_wip",
     ],
     "data": [
         "security/ir.model.access.csv",
