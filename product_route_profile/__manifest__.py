a2c69b8acf6c7b8a9c21e85823cb1da20bdfc140
Kevin.roche <kevin.roche@akretion.com>
2022-04-28 01:32:25 +0200

    [ADD] module: product_route_profile

---
+++ product_route_profile/__manifest__.py
@@ -0,0 +1,25 @@
+# Copyright 2022 Akretion (https://www.akretion.com).
+# @author Kévin Roche <kevin.roche@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Product Route Profile",
+    "summary": "Add Route profile concept on product",
+    "version": "14.0.1.0.0",
+    "category": "Warehouse",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainers": ["Kev-Roche"],
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        "views/route_profile.xml",
+        "views/product_template.xml",
+        "security/ir.model.access.csv",
+    ],
+    "post_init_hook": "post_init_hook",
+}
---
+++ product_route_profile/models/route_profile.py
@@ -0,0 +1,23 @@
+# Copyright 2022 Akretion (https://www.akretion.com).
+# @author Kévin Roche <kevin.roche@akretion.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class RouteProfile(models.Model):
+    _name = "route.profile"
+    _description = "Route Profile"
+
+    name = fields.Char("Name")
+    company_id = fields.Many2one(
+        comodel_name="res.company",
+        default=lambda self: self.env.company.id,
+        required=False,
+        string="Company",
+    )
+    route_ids = fields.Many2many(
+        "stock.location.route",
+        string="Routes",
+        domain=[("product_selectable", "=", True)],
+    )
