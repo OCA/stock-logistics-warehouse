4ddd585a00b6a0dd5a37c83d3a9340bd4ce1ff59
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2022-04-29 00:08:01 +0200

    Update dotfiles + switch to GH actions

--- stock_move_line_reference_link/__manifest__.py
+++ stock_move_line_reference_link/__manifest__.py
@@ -6,7 +6,7 @@
     "summary": "Add link in stock move line references.",
     "version": "13.0.1.0.0",
     "license": "LGPL-3",
-    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Warehouse Management",
     "depends": ["stock"],
