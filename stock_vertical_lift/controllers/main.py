0507f88030ffdef2c54febed8d91c0adde11fc11
Simone Orsi <simahawk@gmail.com>
2022-05-03 16:21:01 +0200

    stock_vertical_lift: add controller tests

--- stock_vertical_lift/controllers/main.py
+++ stock_vertical_lift/controllers/main.py
@@ -1,6 +1,8 @@
 import logging
 import os
 
+from werkzeug.exceptions import Unauthorized
+
 from odoo import http
 from odoo.http import request
 
@@ -10,9 +12,12 @@ _logger = logging.getLogger(__name__)
 class VerticalLiftController(http.Controller):
     @http.route(["/vertical-lift"], type="http", auth="public", csrf=False)
     def vertical_lift(self, answer, secret):
-        if secret == os.environ.get("VERTICAL_LIFT_SECRET", ""):
+        if secret == self._get_env_secret():
             rec = request.env["vertical.lift.command"].sudo().record_answer(answer)
             return str(rec.id)
         else:
             _logger.error("secret mismatch: %r", secret)
-            raise http.AuthenticationError()
+            return Unauthorized()
+
+    def _get_env_secret(self):
+        return os.environ.get("VERTICAL_LIFT_SECRET", "")
--- stock_vertical_lift/tests/__init__.py
+++ stock_vertical_lift/tests/__init__.py
@@ -3,3 +3,4 @@ from . import test_location
 from . import test_inventory
 from . import test_pick
 from . import test_put
+from . import test_controller
