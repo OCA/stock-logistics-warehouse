fb47b8781a215a26a57a18767cab0f8b0c687ea6
Patrick Wilson <36892066+patrickrwilson@users.noreply.github.com>
2022-05-12 10:08:33 -0400

    Remove create/write
    Since the location is changed via the on change method, there is no need for the create/write methods.

--- scrap_reason_code/models/stock_scrap.py
+++ scrap_reason_code/models/stock_scrap.py
@@ -22,24 +22,3 @@ class StockScrap(models.Model):
     def _onchange_reason_code_id(self):
         if self.reason_code_id.location_id:
             self.scrap_location_id = self.reason_code_id.location_id
-
-    def write(self, vals):
-        if "reason_code_id" in vals:
-            vals.update(
-                {
-                    "scrap_location_id": self.env["scrap.reason.code"]
-                    .browse(vals.get("reason_code_id"))
-                    .location_id
-                }
-            )
-        return super(StockScrap, self).write(vals)
-
-    @api.model
-    def create(self, vals):
-        if "reason_code_id" in vals:
-            vals["scrap_location_id"] = (
-                self.env["scrap.reason.code"]
-                .browse(vals.get("reason_code_id"))
-                .location_id.id
-            )
-        return super(StockScrap, self).create(vals)
