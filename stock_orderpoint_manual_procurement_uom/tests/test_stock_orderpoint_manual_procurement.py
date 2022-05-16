2e113e5cc3e40d49406f17030ccb0ffb3a17f8ae
Denis Roussel <denis.roussel@acsone.eu>
2022-05-16 20:41:32 +0200

    [IMP] Update templates with copier

--- stock_orderpoint_manual_procurement_uom/tests/test_stock_orderpoint_manual_procurement.py
+++ stock_orderpoint_manual_procurement_uom/tests/test_stock_orderpoint_manual_procurement.py
@@ -57,7 +57,7 @@ class TestStockWarehouseOrderpoint(common.TransactionCase):
         self.reorder = self.create_orderpoint()
 
     def _create_user(self, login, groups, company):
-        """ Create a user."""
+        """Create a user."""
         group_ids = [group.id for group in groups]
         user = self.user_model.with_context({"no_reset_password": True}).create(
             {
