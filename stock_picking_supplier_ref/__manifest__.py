5aabd392ab55ad13dbabdda7c2669646721f571e
Reyes4711 <carlos.rg.92.47@gmail.com>
2022-05-23 09:17:49 +0200

    [14.0][MIG] stock_picking_supplier_ref: Migration to 14.0

--- stock_picking_supplier_ref/__manifest__.py
+++ stock_picking_supplier_ref/__manifest__.py
@@ -3,19 +3,15 @@
 
 {
     "name": "Stock Picking Supplier Reference",
-    "version": "10.0.1.0.0",
+    "version": "14.0.1.0.0",
     "category": "Stock",
     "author": "Trey, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "license": "AGPL-3",
-    "description": """
+    "summary": """
         Adds a supplier reference field inside supplier's pickings and
         allows search for this reference.""",
-    "depends": [
-        "stock",
-    ],
-    "data": [
-        "views/stock_picking.xml",
-    ],
+    "depends": ["stock"],
+    "data": ["views/stock_picking.xml"],
     "installable": True,
 }
---
+++ stock_picking_supplier_ref/readme/CONTRIBUTORS.rst
@@ -0,0 +1,5 @@
+* `Trey <https://www.trey.es>`_:
+
+* `Studio73 <https://www.studio73.es>`_:
+
+    * Carlos Reyes <carlos@studio73.es>
