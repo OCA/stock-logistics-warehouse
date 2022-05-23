41db7b3765c144947a9c3a91a90d54b25686fcf1
Reyes4711 <carlos.rg.92.47@gmail.com>
2022-05-23 08:50:27 +0200

    [10.0][IMP] stock_picking_supplier_ref: black, isort, prettier

--- stock_picking_supplier_ref/__manifest__.py
+++ stock_picking_supplier_ref/__manifest__.py
@@ -1,22 +1,21 @@
-# -*- coding: utf-8 -*-
 # Copyright 2017 Trey, Kilobytes de Soluciones
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'Stock Picking Supplier Reference',
-    'version': '10.0.1.0.0',
-    'category': 'Stock',
-    'author': 'Trey, Odoo Community Association (OCA)',
-    'website': 'https://www.trey.es',
-    'license': 'AGPL-3',
-    'description': '''
+    "name": "Stock Picking Supplier Reference",
+    "version": "10.0.1.0.0",
+    "category": "Stock",
+    "author": "Trey, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "license": "AGPL-3",
+    "description": """
         Adds a supplier reference field inside supplier's pickings and
-        allows search for this reference.''',
-    'depends': [
-        'stock',
+        allows search for this reference.""",
+    "depends": [
+        "stock",
     ],
-    'data': [
-        'views/stock_picking.xml',
+    "data": [
+        "views/stock_picking.xml",
     ],
-    'installable': True,
+    "installable": True,
 }
