ede0e91008d152b05d2fdb2c142807ddbcb67122
CarlosRoca13 <carlos.roca@tecnativa.com>
2022-06-07 14:33:11 +0200

    [MIG] stock_secondary_unit: Migration to 15.0

--- stock_secondary_unit/tests/test_stock_secondary_unit.py
+++ stock_secondary_unit/tests/test_stock_secondary_unit.py
@@ -1,10 +1,10 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo.tests import Form, SavepointCase, tagged
+from odoo.tests import Form, TransactionCase, tagged
 
 
 @tagged("-at_install", "post_install")
-class TestProductSecondaryUnit(SavepointCase):
+class TestProductSecondaryUnit(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
