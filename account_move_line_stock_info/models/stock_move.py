788759b8c6bcb9d2040466c168be079d0fe948fd
AaronHForgeFlow <ahenriquez@eficent.com>
2022-08-26 11:32:47 +0200

    [IMP] account_move_line_stock_info: show stock move in list view

--- account_move_line_stock_info/models/stock_move.py
+++ account_move_line_stock_info/models/stock_move.py
@@ -8,7 +8,9 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     account_move_line_ids = fields.One2many(
-        comodel_name="account.move.line", inverse_name="stock_move_id", copy=False
+        comodel_name="account.move.line",
+        inverse_name="stock_move_id",
+        copy=False,
     )
 
     @api.model
