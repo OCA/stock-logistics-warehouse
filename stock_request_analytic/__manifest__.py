f690b5cd2fe4621f0f04d091336ac8c15f06d8e5
Denis Roussel <denis.roussel@acsone.eu>
2022-09-05 10:01:09 +0200

    [FIX] Licensing issues

--- stock_request_analytic/__manifest__.py
+++ stock_request_analytic/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2017-2020 ForgeFlow, S.L. (https://www.forgeflow.com)
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Stock Request Analytic",
     "summary": "Internal request for stock",
     "version": "14.0.1.0.2",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "category": "Analytic",
