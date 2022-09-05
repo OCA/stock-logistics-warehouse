c0b7e9195ab575e15f9c5ca6161381302f31f41b
Denis Roussel <denis.roussel@acsone.eu>
2022-09-05 10:01:09 +0200

    [FIX] Licensing issues

--- stock_pull_list/wizards/stock_pull_list_wizard.py
+++ stock_pull_list/wizards/stock_pull_list_wizard.py
@@ -1,5 +1,5 @@
 # Copyright 2020 ForgeFlow, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import itertools
 
