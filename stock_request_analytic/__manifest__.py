835b9d341d47337fc64773353bebaff942c27d73
Denis Roussel <denis.roussel@acsone.eu>
2022-09-06 14:25:47 +0200

    [FIX] stock_request_analytic: Change license to AGPL as depending on stock_analytic

--- stock_request_analytic/__manifest__.py
+++ stock_request_analytic/__manifest__.py
@@ -1,11 +1,11 @@
 # Copyright 2017 Eficent Business and IT Consulting Services, S.L.
-# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "Stock Request Analytic",
     "summary": "Internal request for stock",
     "version": "12.0.1.0.1",
-    "license": "LGPL-3",
+    "license": "AGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Eficent, "
               "Odoo Community Association (OCA)",
