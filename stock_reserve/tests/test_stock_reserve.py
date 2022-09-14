2261a8becbe54be38041d4e24e2f45230f645250
Fernando La Chica <fernandolachica@gmail.com>
2022-09-14 17:29:52 +0200

    [MIG] stock_reserve 15.0:
    - Additionally include note field in stock reservation. It [was](https://github.com/odoo/odoo/commit/71203e0aa28e358dfe432835557ea38bf1edae24) removed

--- stock_reserve/tests/test_stock_reserve.py
+++ stock_reserve/tests/test_stock_reserve.py
@@ -14,6 +14,7 @@ class TestStockReserve(common.TransactionCase):
         product_form = Form(self.env["product.product"])
         product_form.name = "Test Product"
         product_form.type = "product"
+        product_form.detailed_type = "product"
         self.product = product_form.save()
         self.env["stock.quant"].create(
             {
@@ -23,31 +24,26 @@ class TestStockReserve(common.TransactionCase):
             }
         )
 
+    def _create_stock_reservation(self, qty):
+        form_reservation = Form(self.env["stock.reservation"])
+        form_reservation.product_id = self.product
+        form_reservation.product_uom_qty = qty
+        form_reservation.location_id = self.warehouse.lot_stock_id
+        return form_reservation.save()
+
     def test_reservation_and_reservation_release(self):
-        form_reservation_1 = Form(self.env["stock.reservation"])
-        form_reservation_1.product_id = self.product
-        form_reservation_1.product_uom_qty = 6
-        form_reservation_1.location_id = self.warehouse.lot_stock_id
-        reservation_1 = form_reservation_1.save()
+        reservation_1 = self._create_stock_reservation(6)
         reservation_1.reserve()
         self.assertEqual(self.product.virtual_available, 4)
-        form_reservation_2 = Form(self.env["stock.reservation"])
-        form_reservation_2.product_id = self.product
-        form_reservation_2.product_uom_qty = 1
-        form_reservation_2.location_id = self.warehouse.lot_stock_id
-        reservation_2 = form_reservation_2.save()
+        reservation_2 = self._create_stock_reservation(1)
         reservation_2.reserve()
         self.assertEqual(self.product.virtual_available, 3)
         reservation_1.release_reserve()
         self.assertEqual(self.product.virtual_available, 9)
 
     def test_cron_release(self):
-        form_reservation_1 = Form(self.env["stock.reservation"])
-        form_reservation_1.product_id = self.product
-        form_reservation_1.product_uom_qty = 6
-        form_reservation_1.location_id = self.warehouse.lot_stock_id
-        form_reservation_1.date_validity = fields.Date.from_string("2021-01-01")
-        reservation_1 = form_reservation_1.save()
+        reservation_1 = self._create_stock_reservation(6)
+        reservation_1.date_validity = fields.Date.from_string("2021-01-01")
         reservation_1.reserve()
         self.assertEqual(self.product.virtual_available, 4)
         cron = self.env.ref("stock_reserve.ir_cron_release_stock_reservation")
@@ -55,11 +51,7 @@ class TestStockReserve(common.TransactionCase):
         self.assertEqual(self.product.virtual_available, 10)
 
     def test_cron_reserve(self):
-        form_reservation_1 = Form(self.env["stock.reservation"])
-        form_reservation_1.product_id = self.product
-        form_reservation_1.product_uom_qty = 11
-        form_reservation_1.location_id = self.warehouse.lot_stock_id
-        reservation_1 = form_reservation_1.save()
+        reservation_1 = self._create_stock_reservation(11)
         reservation_1.reserve()
         self.assertEqual(reservation_1.state, "partially_available")
         self.env["stock.quant"].create(
