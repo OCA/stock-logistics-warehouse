c406ae4b99e5771db3fff64b4660d0565ca5c5da
Víctor Martínez <victor.martinez@tecnativa.com>
2022-09-22 13:13:20 +0200

    [IMP] stock_reserve_sale: Show Reserve Stock button in sent orders.
    TT39113

--- stock_reserve_sale/__manifest__.py
+++ stock_reserve_sale/__manifest__.py
@@ -2,12 +2,12 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 {
     "name": "Stock Reserve Sales",
-    "version": "13.0.1.0.0",
+    "version": "13.0.1.1.0",
     "author": "Camptocamp, Odoo Community Association (OCA)",
     "category": "Warehouse",
     "license": "AGPL-3",
     "complexity": "normal",
-    "website": "https://github.com/stock-logistics-warehouse",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "depends": ["sale_stock", "stock_reserve"],
     "data": [
         "wizard/sale_stock_reserve_view.xml",
