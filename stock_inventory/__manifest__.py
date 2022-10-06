87854f2ab26cdebd007c2d6852f78edb49227986
DavidJForgeFlow <david.jimenez@forgeflow.com>
2022-10-06 15:28:51 +0200

    [15.0][ADD] stock_inventory
    [15.0][IMP] stock_inventory: add product selection and fixup

---
+++ stock_inventory/__manifest__.py
@@ -0,0 +1,20 @@
+{
+    "name": "Stock Inventory Adjustment",
+    "version": "15.0.1.0.0",
+    "license": "LGPL-3",
+    "maintainer": ["DavidJForgeFlow"],
+    "development_status": "Beta",
+    "category": "Inventory/Inventory",
+    "summary": "Allows to do an easier follow up of the Inventory Adjustments",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock"],
+    "data": [
+        "security/ir.model.access.csv",
+        "views/stock_inventory.xml",
+        "views/stock_quant.xml",
+        "views/stock_move_line.xml",
+    ],
+    "installable": True,
+    "application": False,
+}
