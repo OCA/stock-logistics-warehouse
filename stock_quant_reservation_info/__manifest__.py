644e4f5e9b697dff82f34e02588ceb243b7f7560
DavidJForgeFlow <david.jimenez@forgeflow.com>
2022-10-06 15:49:59 +0200

    [14.0][FIX] stock_quant_reservation_info: only reservations from internal locations

--- stock_quant_reservation_info/__manifest__.py
+++ stock_quant_reservation_info/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Move Reservation Info",
     "summary": "Allows to see the reserved info of Products",
-    "version": "14.0.1.0.4",
+    "version": "14.0.1.0.5",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Generic",
