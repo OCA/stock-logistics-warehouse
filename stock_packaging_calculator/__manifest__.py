0a6f4cc3b5baa83f9d43a2877e25738dd2a1fd0e
Thierry Ducrest <thierry@ducrest.net>
2022-10-10 14:08:14 +0200

    [MIG] stock_packaging_calculator: Migration to 16.0

--- stock_packaging_calculator/__manifest__.py
+++ stock_packaging_calculator/__manifest__.py
@@ -3,8 +3,8 @@
 {
     "name": "Stock packaging calculator",
     "summary": "Compute product quantity to pick by packaging",
-    "version": "15.0.1.0.0",
-    "development_status": "Alpha",
+    "version": "16.0.1.0.0",
+    "development_status": "Beta",
     "category": "Warehouse Management",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Camptocamp, Odoo Community Association (OCA)",
