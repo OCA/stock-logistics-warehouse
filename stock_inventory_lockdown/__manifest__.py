292852a5cfb78b039bc0dcbdc5b6dc129b77505e
Ben Stannard <bstannard@kencove.com>
2022-10-24 10:57:08 -0400

    [MIG] stock_inventory_lockdown: Migration to 14.0

--- stock_inventory_lockdown/__manifest__.py
+++ stock_inventory_lockdown/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Inventory Lock Down",
     "summary": "Lock down stock locations during inventories.",
-    "version": "13.0.1.0.2",
+    "version": "14.0.1.0.0",
     "development_status": "Mature",
     "depends": ["stock"],
     "author": "Numérigraphe, ForgeFlow, Odoo Community Association (OCA)",
