ba7946794f054d74a4d406d9542569698b567837
Denis Roussel <denis.roussel@acsone.eu>
2022-10-27 17:06:45 +0200

    [FIX] stock_request_purchase_analytic: Change license to AGPL as depending on stock_analytic

--- stock_request_purchase_analytic/tests/test_stock_request_purchase_analytic.py
+++ stock_request_purchase_analytic/tests/test_stock_request_purchase_analytic.py
@@ -1,5 +1,5 @@
 # Copyright 2017-2019 Eficent Business and IT Consulting Services S.L.
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl-3.0).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 from odoo.addons.stock_request_purchase.tests \
     import test_stock_request_purchase
