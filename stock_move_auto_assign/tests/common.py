e81c258576ebf673cf1a32706f174cbec47f91ad
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2022-11-03 15:16:30 +0100

    [MIG] stock_move_auto_assign: Migration to 16.0

--- stock_move_auto_assign/tests/common.py
+++ stock_move_auto_assign/tests/common.py
@@ -1,10 +1,10 @@
 # Copyright 2020-2021 Camptocamp SA
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
-from odoo.tests import SavepointCase
+from odoo.tests import TransactionCase
 
 
-class StockMoveAutoAssignCase(SavepointCase):
+class StockMoveAutoAssignCase(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
