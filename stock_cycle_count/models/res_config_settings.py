f7efc9c0f6a1b22c850c2f921423588583c95cd7
DavidJForgeFlow <david.jimenez@forgeflow.com>
2022-11-07 12:02:05 +0100

    [14.0][IMP] stock_cycle_count: add auto start adjustment option

---
+++ stock_cycle_count/models/res_config_settings.py
@@ -0,0 +1,27 @@
+# Copyright 2019 ForgeFlow S.L.
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    auto_start_inventory_from_cycle_count = fields.Boolean(
+        related="company_id.auto_start_inventory_from_cycle_count",
+        string="Auto Start Inventory Adjustment from Cycle Count",
+        help="If enabled, confirming a Cycle Count will start the "
+        "related Inventory Adjustment.",
+        readonly=False,
+    )
+
+    inventory_adjustment_counted_quantities = fields.Selection(
+        related="company_id.inventory_adjustment_counted_quantities",
+        selection=[
+            ("counted", "Default to stock on hand"),
+            ("zero", "Default to zero"),
+        ],
+        string="Inventory Adjustment Counted quantities from Cycle Count",
+        help="If enabled, confirming a Cycle Count will start the related "
+        "Inventory Adjustment.",
+        readonly=False,
+    )
