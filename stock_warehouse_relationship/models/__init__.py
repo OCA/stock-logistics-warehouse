5a8ca9037c03632b99ba6777b41f553dde5c62a1
Pierre Verkest <pierreverkest84@gmail.com>
2022-11-18 11:09:52 +0100

    [ADD] stock_warehouse_relationship: add warehouse id on various model
    useful for other modules such as stokc_multi_warehouse_security

---
+++ stock_warehouse_relationship/models/__init__.py
@@ -0,0 +1,5 @@
+from . import stock_move
+from . import stock_picking
+from . import stock_quant
+from . import stock_quant_package
+from . import stock_move_line
---
+++ stock_warehouse_relationship/models/stock_move.py
@@ -0,0 +1,32 @@
+# Copyright (C) 2019 Akretion
+# Copyright 2022 Foodles (http://www.foodles.co).
+# @author Pierre Verkest <pierreverkest84@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class StockMove(models.Model):
+    _inherit = "stock.move"
+
+    warehouse_id = fields.Many2one(index=True)
+
+    @api.model
+    def _get_move_default_warehouse(self, vals):
+        if vals.get("picking_type_id"):
+            pick_type = self.env["stock.picking.type"].browse(vals["picking_type_id"])
+            wh = pick_type.warehouse_id
+        else:
+            locations = self.env["stock.location"].browse(
+                [vals["location_id"], vals["location_dest_id"]]
+            )
+            whs = locations.mapped("warehouse_id")
+            wh = len(whs) <= 1 and whs or self.env["stock.warehouse"]
+        return wh
+
+    @api.model
+    def create(self, vals):
+        if not vals.get("warehouse_id"):
+            wh = self._get_move_default_warehouse(vals)
+            vals["warehouse_id"] = wh.id or False
+        return super().create(vals)
