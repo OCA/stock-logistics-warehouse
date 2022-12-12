f4fe657eb031c9b5d7d23b905e88ba6bd0d710bc
Denis Roussel <denis.roussel@acsone.eu>
2022-12-12 10:39:31 +0100

    [16.0][ADD] stock_route_mto

---
+++ stock_route_mto/models/stock_route.py
@@ -0,0 +1,14 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import fields, models
+
+
+class StockRoute(models.Model):
+
+    _inherit = "stock.route"
+
+    is_mto = fields.Boolean(
+        help="Is a MTO (Make to Order) route. Check this if you want to identify"
+        "this route as an MTO one."
+    )
