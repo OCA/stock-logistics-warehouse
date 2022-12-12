9d3ce3b2b9d8d616f3addb0c7896bcbcc526220d
Denis Roussel <denis.roussel@acsone.eu>
2022-12-12 10:39:31 +0100

    [16.0][ADD] stock_route_mto

---
+++ stock_route_mto/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2022 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Route Mto",
+    "summary": """
+        Allows to identify MTO routes through a checkbox and availability to filter them.""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock"],
+    "data": [
+        "data/stock_route.xml",
+        "views/stock_route.xml",
+    ],
+}
