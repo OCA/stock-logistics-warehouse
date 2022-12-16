e9e86ba121b37029fdfc3b064056c06a7e9a0d41
Juany Davila <juanyadavilap@gmail.com>
2022-12-16 14:03:55 -0500

    [MIG] stock_picking_show_linked: migration to 15.0

--- stock_picking_show_linked/__manifest__.py
+++ stock_picking_show_linked/__manifest__.py
@@ -1,5 +1,4 @@
 # Copyright 2022 ForgeFlow S.L. (https://www.forgeflow.com)
-# Part of ForgeFlow. See LICENSE file for full copyright and licensing details.
 {
     "name": "Stock Picking Show Linked",
     "summary": """
@@ -7,7 +6,7 @@
        (in the case of chained routes) through a button
        in the parent picking view.
     """,
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
