93642b0f7cfbeb4177bad312a3ccc0f97af6a39f
Juany Davila <juanyadavilap@gmail.com>
2022-12-16 14:03:55 -0500

    [MIG] stock_picking_show_linked: migration to 15.0

--- stock_picking_show_linked/models/stock_picking.py
+++ stock_picking_show_linked/models/stock_picking.py
@@ -1,5 +1,4 @@
 # Copyright 2022 ForgeFlow S.L. (https://www.forgeflow.com)
-# Part of ForgeFlow. See LICENSE file for full copyright and licensing details.
 
 from odoo import api, fields, models
 
