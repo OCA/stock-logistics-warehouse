ff83fb64f1b220056be96ca62ab0741e992fb698
Aungkokolin1997 <aungkokolin23397@gmail.com>
2022-12-27 10:13:20 +0630

    [FIX] stock_move_location: stock_move_location_line_ids

--- stock_move_location/wizard/stock_move_location_line.py
+++ stock_move_location/wizard/stock_move_location_line.py
@@ -11,12 +11,8 @@ class StockMoveLocationWizardLine(models.TransientModel):
     _name = "wiz.stock.move.location.line"
     _description = "Wizard move location line"
 
-    move_location_wizard_id = fields.Many2many(
-        string="Move location Wizard",
-        comodel_name="wiz.stock.move.location",
-        column1="move_location_line_wiz_id",
-        column2="move_location_wiz_id",
-        readonly=True,
+    move_location_wizard_id = fields.Many2one(
+        string="Move location Wizard", comodel_name="wiz.stock.move.location",
     )
     product_id = fields.Many2one(
         string="Product", comodel_name="product.product", required=True
