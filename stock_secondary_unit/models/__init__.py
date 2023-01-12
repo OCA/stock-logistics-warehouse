22b5910ed65a38aa815379866ee47d974f8b9c56
sergio-teruel <sergio.teruel@tecnativa.com>
2023-01-12 16:47:54 +0100

    [IMP] stock_secondary_unit: Compatibility with product variants TT40323

--- stock_secondary_unit/models/__init__.py
+++ stock_secondary_unit/models/__init__.py
@@ -1,3 +1,6 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from . import product
+# Keep order
+from . import stock_product_secondary_unit_mixin
+from . import product_product
+from . import product_template
 from . import stock_move
