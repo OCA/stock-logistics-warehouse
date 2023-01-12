ba13db84190bf7faced44f7a53bb4be12cb1e160
Miquel Raïch <miquel.raich@forgeflow.com>
2023-01-12 18:38:00 +0100

    [MIG] stock_change_qty_reason: Migration to 15.0

--- stock_change_qty_reason/models/__init__.py
+++ stock_change_qty_reason/models/__init__.py
@@ -1,7 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import res_config_settings
-from . import stock_inventory
-from . import stock_inventory_line
-from . import stock_inventory_line_reason
-from . import stock_move
+from . import stock_move_line
+from . import stock_quant
+from . import stock_quant_reason
