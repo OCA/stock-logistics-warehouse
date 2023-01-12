7752404cb2cb9a8ab42d6283b7e849012e42965f
Miquel Raïch <miquel.raich@forgeflow.com>
2023-01-12 18:38:00 +0100

    [MIG] stock_change_qty_reason: Migration to 15.0

---
+++ stock_change_qty_reason/models/stock_quant.py
@@ -0,0 +1,40 @@
+# Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, fields, models
+
+
+class StockQuant(models.Model):
+    """Class to inherit model stock.quant"""
+
+    _inherit = "stock.quant"
+
+    reason = fields.Char(help="Type in a reason for the product quantity change")
+    preset_reason_id = fields.Many2one("stock.quant.reason")
+
+    @api.model
+    def _get_inventory_fields_write(self):
+        res = super()._get_inventory_fields_write()
+        res.extend(["reason", "preset_reason_id"])
+        return res
+
+    def _get_inventory_move_values(self, qty, location_id, location_dest_id, out=False):
+        """Function to super _get_inventory_move_values"""
+        res = super()._get_inventory_move_values(
+            qty, location_id, location_dest_id, out
+        )
+        context = (
+            self.reason if not self.preset_reason_id else self.preset_reason_id.name
+        )
+        line = res["move_line_ids"][0][2]
+        line["reason"] = context
+        if res.get("origin"):
+            res["origin"] = " ,".join([res.get("origin"), context])
+        else:
+            res["origin"] = context
+        if self.preset_reason_id:
+            line["preset_reason_id"] = self.preset_reason_id.id
+            self.preset_reason_id = False
+        if self.reason:
+            self.reason = False
+        return res
