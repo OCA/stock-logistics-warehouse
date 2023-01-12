72f5e4dd0d4e54855c10f50e0ab4bbd78d8d9492
Miquel Raïch <miquel.raich@forgeflow.com>
2023-01-12 18:38:00 +0100

    [MIG] stock_change_qty_reason: Migration to 15.0

--- stock_change_qty_reason/models/res_config_settings.py
+++ stock_change_qty_reason/models/res_config_settings.py
@@ -1,4 +1,4 @@
-# Copyright 2019 ForgeFlow S.L.
+# Copyright 2019-2023 ForgeFlow S.L.
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 from odoo import fields, models
 
--- stock_change_qty_reason/tests/test_stock_change_qty_reason.py
+++ stock_change_qty_reason/tests/test_stock_change_qty_reason.py
@@ -1,22 +1,22 @@
 # pylint: disable=import-error,protected-access,too-few-public-methods
 # Copyright 2016-2017 ACSONE SA/NV (<http://acsone.eu>)
-# Copyright 2019 ForgeFlow S.L.
+# Copyright 2019-2023 ForgeFlow S.L.
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestStockQuantityChangeReason(SavepointCase):
+class TestStockQuantityChangeReason(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super(TestStockQuantityChangeReason, cls).setUpClass()
 
         # MODELS
-        cls.stock_move = cls.env["stock.move"]
+        cls.stock_move_line = cls.env["stock.move.line"]
         cls.product_product_model = cls.env["product.product"]
         cls.product_category_model = cls.env["product.category"]
-        cls.wizard_model = cls.env["stock.change.product.qty"]
-        cls.preset_reason_id = cls.env["stock.inventory.line.reason"]
+        cls.stock_quant = cls.env["stock.quant"]
+        cls.preset_reason_id = cls.env["stock.quant.reason"]
         cls.stock_location = cls.env.ref("stock.stock_location_stock")
 
         # INSTANCES
@@ -27,14 +27,13 @@ class TestStockQuantityChangeReason(SavepointCase):
             {"name": name, "categ_id": self.category.id, "type": "product"}
         )
 
-    def _product_change_qty(self, product, new_qty):
+    def _product_change_qty(self, product, location, new_qty):
         values = {
-            "product_tmpl_id": product.product_tmpl_id.id,
             "product_id": product.id,
-            "new_quantity": new_qty,
+            "location_id": location.id,
+            "inventory_quantity": new_qty,
         }
-        wizard = self.wizard_model.create(values)
-        wizard.change_product_qty()
+        self.stock_quant.with_context(inventory_mode=True).create(values)
 
     def _create_reason(self, name, description=None):
         return self.preset_reason_id.create({"name": name, "description": description})
@@ -42,30 +41,23 @@ class TestStockQuantityChangeReason(SavepointCase):
     def test_inventory_adjustment_onchange_reason_preset_reason(self):
         """Check that adding a reason or a preset reason explode to lines"""
         product2 = self._create_product("product_product_2")
-        self._product_change_qty(product2, 50)
-        inventory = self.env["stock.inventory"].create(
+        self._product_change_qty(product2, self.stock_location, 50)
+        inventory_quant = self.env["stock.quant"].create(
             {
-                "name": "remove product2",
-                "product_ids": [(4, product2.id)],
-                "location_ids": [(4, self.stock_location.id)],
+                "product_id": product2.id,
+                "location_id": self.stock_location.id,
+                "inventory_quantity": 10,
             }
         )
-        inventory.preset_reason_id = self._create_reason("Test 1", "Description Test 1")
-        inventory.action_start()
-        self.assertEqual(len(inventory.line_ids), 1)
-        inventory.reason = "Reason 2"
-        inventory.onchange_reason()
-        self.assertEqual(inventory.line_ids.reason, inventory.reason)
-        inventory.preset_reason_id = self._create_reason("Test 2", "Description Test 2")
-        inventory.onchange_preset_reason()
-        self.assertEqual(
-            inventory.line_ids.preset_reason_id, inventory.preset_reason_id
-        )
-        inventory.line_ids[0].write({"product_qty": 10})
-        inventory.action_validate()
-        move = self.stock_move.search(
+        inventory_quant.user_id = self.env.user.id
+        inventory_quant.inventory_quantity_set = True
+        preset_reason_id = self._create_reason("Test 1", "Description Test 1")
+        inventory_quant.preset_reason_id = preset_reason_id
+        inventory_quant.action_apply_inventory()
+        move_line = self.stock_move_line.search(
             [("product_id", "=", product2.id), ("preset_reason_id", "!=", False)]
         )
-        self.assertEqual(len(move), 1)
-        self.assertEqual(move.origin, inventory.preset_reason_id.name)
-        self.assertEqual(move.preset_reason_id, inventory.preset_reason_id)
+        self.assertEqual(len(move_line), 1)
+        self.assertEqual(inventory_quant.preset_reason_id.name, False)
+        self.assertEqual(move_line.move_id.origin, preset_reason_id.name)
+        self.assertEqual(move_line.preset_reason_id, preset_reason_id)
