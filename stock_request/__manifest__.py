5d2110ad859c575452441e749d38be659dfa1f07
Pedro M. Baeza <pedro.baeza@tecnativa.com>
2023-01-23 08:35:28 +0100

    [FIX] stock_request: Proper multi-company rules
    It includes migration scripts.

--- stock_request/__manifest__.py
+++ stock_request/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Request",
     "summary": "Internal request for stock",
-    "version": "15.0.1.3.0",
+    "version": "15.0.1.4.0",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
---
+++ stock_request/migrations/15.0.1.4.0/post-migration.py
@@ -0,0 +1,10 @@
+# Copyright 2023 Tecnativa - Pedro M. Baeza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+from openupgradelib import openupgrade
+
+
+@openupgrade.migrate()
+def migrate(env, version):
+    openupgrade.load_data(
+        env.cr, "stock_request", "migrations/14.0.1.3.0/noupdate_changes.xml"
+    )
