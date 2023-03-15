8e3c876f895699458a44b587ca07e90f1641446a
Denis Roussel <denis.roussel@acsone.eu>
2023-03-15 15:12:52 +0100

    [16.0][ADD] stock_storage_category_capacity_name

---
+++ stock_storage_category_capacity_name/__manifest__.py
@@ -0,0 +1,13 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Storage Category Capacity Name",
+    "summary": """
+        Allows to have a better display name for Stock Storage Category Capacity model""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock"],
+}
---
+++ stock_storage_category_capacity_name/readme/DESCRIPTION.rst
@@ -0,0 +1,2 @@
+This module allows to enhance the display of Stock Storage Category Capacity
+records as in Odoo core, there is no implementation of name display.
