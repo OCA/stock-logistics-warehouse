31da3691c2a585bc4ea56855e8adda4d0d33c7ae
Urvisha-OSI <udesai@opensourceintegrators.com>
2023-03-23 19:08:23 +0530

    [IMP] stock_change_qty_reason: pre-commit stuff

--- stock_change_qty_reason/__manifest__.py
+++ stock_change_qty_reason/__manifest__.py
@@ -8,7 +8,7 @@
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "depends": ["stock"],
     "data": [
