8aee7aa26a4d9ab01e1eac6df7a670500681501c
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2023-03-30 14:39:15 +0200

    [ADD] stock_picking_commercial_partner
    When you organize the work into your warehouse you could need to search
    for pickings related to a specific commercial entity. This use case could
    be motivated by the fact that pickings for the same commercial entity
    are usually delivered together.
    
    This addon add on the picking form the 'Commercial Entity' field the related
    partner is part of. This field is readonly and comes from the partner information.
    It also allows you to quickly search for pickings related to a specific
    commercial entity and to group by this field in the picking tree view.

---
+++ stock_picking_commercial_partner/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Picking Commercial Entity",
+    "summary": """
+        Add Commercial Partner on the Stock Picking""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_picking.xml",
+    ],
+    "demo": [],
+}
