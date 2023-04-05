2525ca5cc40c186d4a089614b5c36980b2271a09
jumeldi74 <jumeldi74@gmail.com>
2023-04-05 09:00:59 +0700

    [ADD] stock_adjustment_access_right: add new module

---
+++ stock_adjustment_access_right/__manifest__.py
@@ -0,0 +1,16 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Stock Adjustment Access Right",
+    "version": "16.0.1.0.2",
+    "license": "AGPL-3",
+    "summary": "Access Right of Inventory Adjustment",
+    "author": "Jumeldi, PT.SAI, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "depends": ["stock"],
+    "data": [
+        "views/stock_inventory_view.xml",
+    ],
+    "installable": True,
+}
