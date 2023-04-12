9c26fb179fc27a9276f1f50a5fbdcf0005297cd7
Jacques-Etienne Baudoux <je@bcim.be>
2023-04-12 21:48:42 +0200

    [ADD] stock_orderpoint_no_horizon

---
+++ stock_orderpoint_no_horizon/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Camptocamp SA
+# @author: Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Orderpoint No Horizon",
+    "summary": "Consider all future moves, do not limit horizon to the rule lead days.",
+    "version": "14.0.1.0.0",
+    "author": "Camptocamp, BCIM, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "depends": ["stock"],
+    "license": "AGPL-3",
+}
