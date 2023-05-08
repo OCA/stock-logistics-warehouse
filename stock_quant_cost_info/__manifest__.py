3d49fe327ffbfaa1218b16a1dcf75bc2c3a58084
Yadier A. De Quesada <yadier.dequesada@tecnativa.com>
2023-05-08 15:12:18 -0400

    [MIG] stock_quant_cost_info: Migration to 16.0

--- stock_quant_cost_info/__manifest__.py
+++ stock_quant_cost_info/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Quant Cost Info",
     "summary": "Shows the cost of the quants",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
