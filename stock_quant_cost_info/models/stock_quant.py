efe864a0d550666bcbceab27b572bae880f1adfa
sergio-teruel <sergio.teruel@tecnativa.com>
2023-05-22 11:58:20 +0200

    [IMP] stock_quant_cost_info: Hide field for non inventory_quantity_set TT43480

--- stock_quant_cost_info/models/stock_quant.py
+++ stock_quant_cost_info/models/stock_quant.py
@@ -17,8 +17,6 @@ class StockQuant(models.Model):
     @api.depends("inventory_diff_quantity", "product_id.standard_price")
     def _compute_adjustment_cost(self):
         for record in self:
-            record.adjustment_cost = False
-            if record.inventory_diff_quantity:
-                record.adjustment_cost = (
-                    record.inventory_diff_quantity * record.product_id.standard_price
-                )
+            record.adjustment_cost = (
+                record.inventory_diff_quantity * record.product_id.standard_price
+            )
