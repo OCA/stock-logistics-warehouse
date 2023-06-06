165991d0315517730e86034d1ffc894b5c644e8c
gschrott-osi <gschrott@opensourceintegrators.com>
2023-06-06 08:56:38 -0700

    [FIX]stock_inventory_revaluation_adjustment:proposed cost correct views

--- stock_inventory_revaluation_adjustment/__manifest__.py
+++ stock_inventory_revaluation_adjustment/__manifest__.py
@@ -18,7 +18,6 @@
         "views/cost_adjustment_type.xml",
         "views/cost_adjustment.xml",
         "views/product_template_view.xml",
-        "views/product_template_hide_proposed_cost.xml"
     ],
     "installable": True,
     "maintainers": ["patrickrwilson"],
