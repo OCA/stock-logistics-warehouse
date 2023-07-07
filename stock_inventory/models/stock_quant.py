c903dd8c63a4b63231d28a10c2c6122d915eca4e
AaronHForgeFlow <ahenriquez@eficent.com>
2023-07-07 11:59:12 +0200

    [FIX] stock_inventory: do not link stock move lines to the inventory if they were created for different purpose

--- stock_inventory/models/stock_quant.py
+++ stock_inventory/models/stock_quant.py
@@ -1,3 +1,7 @@
+from datetime import datetime
+
+from dateutil.relativedelta import relativedelta
+
 from odoo import api, fields, models
 
 
@@ -18,20 +22,28 @@ class StockQuant(models.Model):
                     or rec.location_id in x.location_ids.child_ids
                 )
             )
+            # For zero quants that are actually zero quantity this will return the
+            # last stock move line, because no one was created, that is the reason
+            # for the create_date filter, it is not accurate but better than link
+            # old stock moves
+            date_today = datetime.now()
+            date_to_create = date_today - relativedelta(minutes=10)
             moves = record_moves.search(
                 [
                     ("product_id", "=", rec.product_id.id),
                     ("lot_id", "=", rec.lot_id.id),
                     ("company_id", "=", rec.company_id.id),
+                    ("create_date", ">", date_to_create),
                     "|",
                     ("location_id", "=", rec.location_id.id),
                     ("location_dest_id", "=", rec.location_id.id),
                 ],
                 order="create_date asc",
             )
-            move = moves[len(moves) - 1]
-            adjustment.stock_move_ids |= move
-            move.inventory_adjustment_id = adjustment
+            if len(moves):
+                move = moves[len(moves) - 1]
+                adjustment.stock_move_ids |= move
+                move.inventory_adjustment_id = adjustment
             rec.to_do = False
         return res
 
