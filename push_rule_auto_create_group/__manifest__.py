791e46529e9edd7ffd3a0e65cf79e8fd200f2b17
Nedi11 <91092824+Nedi11@users.noreply.github.com>
2023-07-19 10:51:39 +0200

    push_rule_auto_create_group v16
    push_rule_auto_create_group module compatible with odoo v16

---
+++ push_rule_auto_create_group/__manifest__.py
@@ -0,0 +1,21 @@
+# Copyright 2020 ForgeFlow, S.L.
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Push Rule Auto Create Group",
+    "version": "11.0.1.0.1",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "summary": "Allows to configure the system to propose automatically new "
+               "procurement groups in applying push rules.",
+    "author": "ForgeFlow,"
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Warehouse Management",
+    "depends": [
+        "stock",
+    ],
+    "data": [
+        'views/stock_location_view.xml',
+    ],
+    "installable": True,
+}
