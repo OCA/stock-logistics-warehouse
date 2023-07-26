bd25f471b39c6fa7f63ad2ea51315ab9abe2ea76
Denis Roussel <denis.roussel@acsone.eu>
2023-07-26 11:57:13 +0200

    [FIX] stock_location_product_restriction: Don't fetch if ids are void

--- stock_location_product_restriction/tests/test_stock_location.py
+++ stock_location_product_restriction/tests/test_stock_location.py
@@ -1,7 +1,6 @@
 # Copyright 2020 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
-from odoo.tests.common import TransactionCase
+from odoo.tests.common import Form, TransactionCase
 
 
 class TestStockLocation(TransactionCase):
@@ -258,3 +257,8 @@ class TestStockLocation(TransactionCase):
         self.loc_lvl_1_1_1.flush_recordset()
         self.assertTrue(self.loc_lvl_1_1_1.has_restriction_violation)
         self.assertTrue(self.loc_lvl_1_1_1.restriction_violation_message)
+
+    def test_05(self):
+        # Check location creation
+        with Form(self.StockLocation) as location_form:
+            location_form.name = "Test"
