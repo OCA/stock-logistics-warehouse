c0e3cc58271f7c3f83e3b6d33b2eebbfce5009a0
ferran-73 <ferran@studio73.es>
2023-09-05 10:16:05 +0200

    [16.0][MIG] stock_picking_supplier_ref

--- stock_picking_supplier_ref/__manifest__.py
+++ stock_picking_supplier_ref/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     "name": "Stock Picking Supplier Reference",
-    "version": "14.0.1.1.1",
+    "version": "16.0.1.0.0",
     "category": "Stock",
     "author": "Trey, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-workflow",
@@ -14,5 +14,4 @@
     "depends": ["stock"],
     "data": ["views/stock_picking.xml"],
     "installable": True,
-    "external_dependencies": {"python": ["openupgradelib"]},
 }
