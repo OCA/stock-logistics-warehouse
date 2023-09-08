723612a871aa457b4672f70e04bbb22e1a8a3777
Víctor Martínez <victor.martinez@tecnativa.com>
2023-09-08 14:31:43 +0200

    [IMP] stock_request_mrp: Exceptions from production cancel
    TT44811

--- stock_request_mrp/__manifest__.py
+++ stock_request_mrp/__manifest__.py
@@ -1,5 +1,6 @@
 # Copyright 2017-20 ForgeFlow S.L. (https://www.forgeflow.com)
 # Copyright 2022 Tecnativa - Pedro M. Baeza
+# Copyright 2023 Tecnativa - Víctor Martínez
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
 
 {
@@ -16,6 +17,7 @@
         "views/stock_request_views.xml",
         "views/stock_request_order_views.xml",
         "views/mrp_production_views.xml",
+        "views/stock_request_templates.xml",
     ],
     "installable": True,
     "auto_install": True,
