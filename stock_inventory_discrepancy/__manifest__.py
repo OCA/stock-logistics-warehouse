abb1c09689080ca36598144951a7248a1afde066
Carolina Fernandez <carolina.fernandez@tecnativa.com>
2023-09-18 06:35:55 -0300

    [MIG] stock_inventory_discrepancy: Migration to 16.0

--- stock_inventory_discrepancy/__manifest__.py
+++ stock_inventory_discrepancy/__manifest__.py
@@ -1,11 +1,12 @@
 # Copyright 2017-2020 ForgeFlow S.L. (http://www.forgeflow.com)
+# Copyright 2023 Tecnativa - Carolina Fernandez
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Stock Inventory Discrepancy",
     "summary": "Adds the capability to show the discrepancy of every line in "
     "an inventory and to block the inventory validation when the "
     "discrepancy is over a user defined threshold.",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
