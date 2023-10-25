9852a8aa7d123821f915538732e658868ec30bf7
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-25 13:01:31 +0200

    [IMP] stock_archive_constraint: test performance improvement - Include context keys for avoiding mail operations overhead.

--- stock_archive_constraint/tests/test_location_archive_constraint.py
+++ stock_archive_constraint/tests/test_location_archive_constraint.py
@@ -9,6 +9,16 @@ class TestLocationArchiveConstraint(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.env = cls.env(
             context=dict(cls.env.context, test_stock_archive_constraint=True)
         )
