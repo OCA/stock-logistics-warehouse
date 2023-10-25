18dd7521ef4e3190582608889a592b655bc03f14
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-25 13:13:45 +0200

    [IMP] stock_quant_cost_info: test performance improvement

--- stock_quant_cost_info/tests/test_stock_quant_cost_info.py
+++ stock_quant_cost_info/tests/test_stock_quant_cost_info.py
@@ -7,13 +7,24 @@ from odoo.tests.common import TransactionCase, tagged
 
 @tagged("post_install", "-at_install")
 class TestStockQuantCostInfo(TransactionCase):
-    def setUp(self):
-        super().setUp()
-        product_obj = self.env["product.product"]
-        self.product_1 = product_obj.create(
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
+        product_obj = cls.env["product.product"]
+        cls.product_1 = product_obj.create(
             {"name": "product test 1", "type": "product", "standard_price": 1000}
         )
-        self.product_2 = product_obj.create(
+        cls.product_2 = product_obj.create(
             {"name": "product test 2", "type": "product", "standard_price": 2000}
         )
 
