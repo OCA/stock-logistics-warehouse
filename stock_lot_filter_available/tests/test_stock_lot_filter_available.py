6de034be2e772910259c609b4070340c26bd666c
josep-tecnativa <josep.guardiola@tecnativa.com>
2023-10-25 13:04:10 +0200

    [IMP] stock_lot_filter_available: test performance improvement - Include context keys for avoiding mail operations overhead.

--- stock_lot_filter_available/tests/test_stock_lot_filter_available.py
+++ stock_lot_filter_available/tests/test_stock_lot_filter_available.py
@@ -8,6 +8,16 @@ class TestStockLotFilterAvailable(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
+        # Remove this variable in v16 and put instead:
+        # from odoo.addons.base.tests.common import DISABLED_MAIL_CONTEXT
+        DISABLED_MAIL_CONTEXT = {
+            "tracking_disable": True,
+            "mail_create_nolog": True,
+            "mail_create_nosubscribe": True,
+            "mail_notrack": True,
+            "no_reset_password": True,
+        }
+        cls.env = cls.env(context=dict(cls.env.context, **DISABLED_MAIL_CONTEXT))
         cls.StockProductionLot = cls.env["stock.production.lot"]
         cls.company = cls.env.ref("base.main_company")
         cls.loc = cls.env.ref("stock.stock_location_stock")
