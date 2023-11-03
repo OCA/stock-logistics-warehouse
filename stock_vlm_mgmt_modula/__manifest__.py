4504b357c06c849cf72f666815c209c5b6dfb3c4
David <david.vidal@tecnativa.com>
2023-11-03 19:05:18 +0100

    [ADD] stock_vlm_mgmt_modula: New module for 14.0
    Integration with Modula VLMs
    
    TT46700

---
+++ stock_vlm_mgmt_modula/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2023 Tecnativa - David Vidal
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+{
+    "name": "Modula integration with stock_vlm_mgmt",
+    "summary": "Light alternative for Modula VLM integrations",
+    "version": "14.0.1.0.0",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "maintainers": ["chienandalu"],
+    "license": "AGPL-3",
+    "category": "Stock",
+    "depends": ["stock_vlm_mgmt"],
+    "data": [],
+}
