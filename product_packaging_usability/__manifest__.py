5d42b38b3e75447cb8358845326cfd35198ef539
David Beal <david.beal@akretion.com>
2023-11-15 12:15:23 +0100

    [ADD][16.0] product_packaging_usability module

---
+++ product_packaging_usability/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright Akretion
+# License ALGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Product Packaging Usability",
+    "summary": "Add sugar to Product Packaging",
+    "version": "16.0.1.0.0",
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "maintainer": ["bealdav"],
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Stock",
+    "depends": ["product"],
+    "data": ["views/product_packaging.xml"],
+    "installable": True,
+    "license": "AGPL-3",
+}
---
+++ product_packaging_usability/readme/DESCRIPTION.rst
@@ -0,0 +1,11 @@
+- add search view on Product Packaging model
+- add button in product view to switch to packaging
+
+
+.. figure:: ../static/description/im1.png
+
+
+.. figure:: ../static/description/im2.png
+
+
+.. figure:: ../static/description/im3.png
