9c604b067874cb63b9448123d160c26540a79ab1
Aritz Olea <aritz.olea@factorlibre.com>
2023-11-21 10:36:20 +0100

    [MIG] stock_removal_location_by_priority: Migration to 16.0

--- stock_removal_location_by_priority/models/stock_location.py
+++ stock_removal_location_by_priority/models/stock_location.py
@@ -9,7 +9,6 @@ class StockLocation(models.Model):
     _inherit = "stock.location"
 
     removal_priority = fields.Integer(
-        string="Removal Priority",
         default=10,
         help="This priority applies when removing stock and incoming dates are equal.",
     )
