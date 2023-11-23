6f4c2fb73436b64fa61b18656fbe00b073b821e9
sergio-teruel <sergio.teruel@tecnativa.com>
2023-11-23 17:26:27 +0100

    [FIX] stock_secondary_unit: The second qty is computed when a sml is created. Second qty compute the qty_done field. The second uom not locked for non draft move lines

--- stock_secondary_unit/models/stock_move.py
+++ stock_secondary_unit/models/stock_move.py
@@ -1,7 +1,6 @@
 # Copyright 2018 Tecnativa - Sergio Teruel
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 from odoo import api, fields, models
-from odoo.tools.float_utils import float_round
 
 
 class StockMove(models.Model):
@@ -49,16 +48,7 @@ class StockMoveLine(models.Model):
     def create(self, vals):
         move = self.env["stock.move"].browse(vals.get("move_id", False))
         if move.secondary_uom_id:
-            uom = self.env["uom.uom"].browse(vals["product_uom_id"])
-            factor = move.secondary_uom_id.factor * uom.factor
-            move_line_qty = vals.get("product_uom_qty", vals.get("qty_done", 0.0))
-            qty = float_round(
-                move_line_qty / (factor or 1.0),
-                precision_rounding=move.secondary_uom_id.uom_id.rounding,
-            )
-            vals.update(
-                {"secondary_uom_qty": qty, "secondary_uom_id": move.secondary_uom_id.id}
-            )
+            vals["secondary_uom_id"] = move.secondary_uom_id.id
         return super().create(vals)
 
     @api.depends("secondary_uom_id", "secondary_uom_qty")
