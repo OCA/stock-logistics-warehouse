cb3b8f32488b31c101df52ba92df75e74d3872e9
Eduard Brahas <eduardbrhas@outlook.it>
2024-01-12 16:53:39 +0100

    [14.0][FIX] sale_stock_available_info_popup: version

--- sale_stock_available_info_popup/__manifest__.py
+++ sale_stock_available_info_popup/__manifest__.py
@@ -7,7 +7,7 @@
     "author": "Tecnativa, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse Management",
-    "version": "15.0.1.0.0",
+    "version": "14.0.0.0.0",
     "license": "AGPL-3",
     "depends": ["sale_stock", "stock_available"],
     "data": ["views/sale_order_views.xml"],
