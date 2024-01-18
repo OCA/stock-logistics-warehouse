2f4450e194bd50e60a8038689eb0cccfcc802782
Michael Tietz <mtietz@mt-software.de>
2024-01-18 15:30:14 +0100

    [IMP] stock_helper: Getting closest warehouse for record set

--- stock_helper/models/stock_location.py
+++ stock_helper/models/stock_location.py
@@ -2,6 +2,8 @@
 # Copyright 2022-2023 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
+from collections import defaultdict
+
 from odoo import models
 
 
@@ -37,8 +39,8 @@ class StockLocation(models.Model):
                 return rule.location_src_id
             current_location = rule.location_src_id
 
-    def get_closest_warehouse(self):
-        """Returns closest warehouse for current location.
+    def _get_closest_warehouse(self):
+        """Returns dict of closest warehouse per location
 
         By default the get_warehouse (which is located in the odoo core module stock)
         returns the warehouse via searching the view_location_id with parent_of.
@@ -48,14 +50,39 @@ class StockLocation(models.Model):
 
         With this methods we will really get the closest warehouse of a location
         """
-        self.ensure_one()
-        location_ids = [int(x) for x in self.parent_path.split("/") if x]
         warehouses = (
             self.env["stock.warehouse"]
-            .search([("view_location_id", "in", location_ids)])
+            .search([])
             .sorted(lambda w: w.view_location_id.parent_path, reverse=True)
         )
-        for warehouse in warehouses:
-            if self.parent_path.startswith(warehouse.view_location_id.parent_path):
-                return warehouse
-        return warehouses.browse()
+        res = defaultdict(self.env["stock.warehouse"])
+        for location in self.filtered("parent_path"):
+            for warehouse in warehouses:
+                if location.parent_path.startswith(
+                    warehouse.view_location_id.parent_path
+                ):
+                    res[location.id] = warehouse
+                    break
+        return res
+
+    def get_closest_warehouse(self):
+        """Returns closest warehouse for current location."""
+        self.ensure_one()
+        return self._get_closest_warehouse()[self.id]
+
+    def _get_source_location_from_route(self, route, procure_method):
+        self.ensure_one()
+        route.ensure_one()
+        values = {
+            "route_ids": route,
+        }
+        current_location = self
+        while current_location:
+            rule = self.env["procurement.group"]._get_rule(
+                self.env["product.product"], current_location, values
+            )
+            if not rule:
+                return self.browse()
+            if rule.procure_method == procure_method:
+                return rule.location_src_id
+            current_location = rule.location_src_id
