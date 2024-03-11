3f99432583c3c7f406975b7f1e5643b90447d963
Denis Roussel <denis.roussel@acsone.eu>
2024-03-11 15:06:30 +0100

    [IMP] stock_reserve_rule: Don't evaluate strategies if needed quantity is already taken

--- stock_reserve_rule/models/stock_move.py
+++ stock_reserve_rule/models/stock_move.py
@@ -98,6 +98,15 @@ class StockMove(models.Model):
                             strict=strict,
                         )
                         still_need -= taken_in_loc
+                        # We should break between quants if original needs is fulfilled
+                        # TODO: Check if float_is_zero should be more appropriate
+                        need_zero = (
+                            float_compare(still_need, 0, precision_rounding=rounding)
+                            != 1
+                        )
+                        if need_zero:
+                            # useless to eval the other rules when still_need <= 0
+                            break
                     except StopIteration:
                         break
 
