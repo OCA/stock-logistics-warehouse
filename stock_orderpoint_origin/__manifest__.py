f850ec3d05bacf26c30cfd63865223567c04dc88
Gil Arasa Verge <gav@wvbs.eu>
2024-03-24 13:05:37 +0100

    [MIG] stock_orderpoint_origin: Migration to 16.0

--- stock_orderpoint_origin/__manifest__.py
+++ stock_orderpoint_origin/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock Orderpoint Replenishment demand origin details",
     "summary": "Link Purchase Orders to the replenishment demand Sales Orders",
-    "version": "14.0.1.0.2",
+    "version": "16.0.1.0.2",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "Open Source Integrators, Odoo Community Association (OCA)",
