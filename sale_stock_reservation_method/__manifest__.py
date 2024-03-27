50c603675c9178e49fe6fe7a4770dffd7572fddf
JordiMForgeFlow <jordi.masvidal@forgeflow.com>
2024-03-27 10:49:06 +0100

    [14.0][ADD] sale_stock_reservation_method

---
+++ sale_stock_reservation_method/__manifest__.py
@@ -0,0 +1,22 @@
+# Copyright 2024 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Reservation Method",
+    "summary": "Backport of reservation method of v15",
+    "version": "14.0.1.0.0",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Stock Management",
+    "maintainers": ["JordiMForgeFlow"],
+    "depends": [
+        "stock_reservation_method",
+        "sale",
+    ],
+    "demo": [],
+    "data": [],
+    "installable": True,
+    "auto_install": True,
+    "development_status": "Beta",
+    "license": "LGPL-3",
+}
---
+++ sale_stock_reservation_method/readme/DESCRIPTION.rst
@@ -0,0 +1,3 @@
+Backport for reservation method implemented on V15 on sale stock standard module.
+
+This module is not compatible with procurement_jit, so make sure to disable it.
