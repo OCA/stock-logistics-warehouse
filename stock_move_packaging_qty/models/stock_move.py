ebb9ab7450982fb69ded45f8c70ec8be45143f83
Jairo Llopis <jairo@moduon.team>
2024-04-09 13:44:37 +0100

    [IMP] stock_move_packaging_qty: report done packaging qtys
    The title sounds like a simple task, but it requires many changes:
    
    - Store done pkg qtys in `stock.move.line` records.
    - The qtys are not computed. Instead, the user must write them by hand. This happens because many times the packaging just indicates an approximate content, but is not always exact until reaching this point of the process, where both the done qtys (both UoM and packagings) are measured and get real values.
    - Added compute and inverse on `stock.move` for simple scenarios where one move = one move line.
    - Add all this on all reports and views.
    
    This change also opens the door to be able to get a real inventory measured in product packagings. That's not implemented in this MVP, but the idea is recorded.
    
    @moduon MT-5531

--- stock_move_packaging_qty/models/stock_move.py
+++ stock_move_packaging_qty/models/stock_move.py
@@ -1,17 +1,24 @@
 # Copyright 2020 Camptocamp SA
 # Copyright 2021 ForgeFlow, S.L.
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import api, fields, models
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
+from odoo import _, api, exceptions, fields, models
+from odoo.tools import float_compare
 
 
-class StockPicking(models.Model):
+class StockMove(models.Model):
     _inherit = "stock.move"
 
     product_packaging_qty = fields.Float(
-        string="Pkg. Qty.",
+        string="Pkgs. Demand",
         compute="_compute_product_packaging_qty",
         inverse="_inverse_product_packaging_qty",
-        help="Amount of packages demanded.",
+        help="Amount of product packagings demanded.",
+    )
+    product_packaging_qty_done = fields.Float(
+        string="Pkgs. Done",
+        compute="_compute_product_packaging_qty_done",
+        inverse="_inverse_product_packaging_qty_done",
+        help="Amount of product packagings done.",
     )
 
     @api.depends(
@@ -31,6 +38,18 @@ class StockPicking(models.Model):
                 move.product_uom_qty / move._get_single_package_uom_qty()
             )
 
+    @api.depends(
+        "move_line_ids.product_packaging_qty_done",
+        "move_line_nosuggest_ids.product_packaging_qty_done",
+    )
+    def _compute_product_packaging_qty_done(self):
+        """Get the sum of done packaging qtys from move lines."""
+        for move in self:
+            lines = move._get_move_lines()
+            move.product_packaging_qty_done = sum(
+                lines.mapped("product_packaging_qty_done")
+            )
+
     @api.onchange("product_packaging_qty")
     def _inverse_product_packaging_qty(self):
         """Store the quantity in the product's UoM.
@@ -42,6 +61,23 @@ class StockPicking(models.Model):
             if move.product_packaging_id and move.product_packaging_qty:
                 uom_factor = move._get_single_package_uom_qty()
                 move.product_uom_qty = move.product_packaging_qty * uom_factor
+
+    def _inverse_product_packaging_qty_done(self):
+        """Store the done packaging dqty in the move line if there's just one."""
+        for move in self:
+            lines = move._get_move_lines()
+            # Setting 0 done pkgs with no lines? Nothing to do
+            if not lines and not move.product_packaging_qty_done:
+                continue
+            if len(lines) != 1:
+                raise exceptions.UserError(
+                    _(
+                        "There are %d move lines involved. "
+                        "Please set their product packaging done qty directly.",
+                        len(lines),
+                    )
+                )
+            lines.product_packaging_qty_done = move.product_packaging_qty_done
 
     @api.onchange("product_packaging_id")
     def _onchange_product_packaging(self):
@@ -62,3 +98,32 @@ class StockPicking(models.Model):
         return self.product_packaging_id.product_uom_id._compute_quantity(
             self.product_packaging_id.qty, self.product_uom
         )
+
+    def _set_quantities_to_reservation(self):
+        """Add packaging qtys when clicking on "Set Quantities"."""
+        result = super()._set_quantities_to_reservation()
+        digits = self.env["stock.move.line"].fields_get(["qty_done"], ["digits"])[
+            "qty_done"
+        ]["digits"][1]
+        for line in self.move_line_ids:
+            if float_compare(line.qty_done, line.reserved_uom_qty, digits):
+                continue
+            if not line.product_packaging_id:
+                line.product_packaging_qty_done = 0
+                continue
+            line.product_packaging_qty_done = (
+                line.product_packaging_id._check_qty(
+                    line.qty_done, line.product_uom_id, "DOWN"
+                )
+                / line.product_packaging_id.qty
+            )
+        return result
+
+    def _clear_quantities_to_zero(self):
+        """Clear packaging qtys when clicking on "Clear Quantities"."""
+        result = super()._clear_quantities_to_zero()
+        for line in self.move_line_ids:
+            if line.qty_done:
+                continue
+            line.product_packaging_qty_done = 0
+        return result
