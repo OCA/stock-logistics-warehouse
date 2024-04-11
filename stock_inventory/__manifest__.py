4dc8e6d14acc9514acd50f00d26bcf5f594ac22a
DavidJForgeFlow <david.jimenez@forgeflow.com>
2024-04-11 12:27:17 +0200

    [IMP] stock_inventory: new small features and ux improvements
    Allow to create stock.quants making the quants smart button not invisible when quantity is equal to 0. (Removing one of the invisible conditions).
    
    [IMP] stock_inventory: adds exclude_sublocation flag
    
    [IMP] stock_inventory: Add flag to autocomplete adjustment when fully done.
    
    [IMP] stock_inventory
    
    Add company to adjustments to avoid multi-company errors. Adds information fields to tree view. Also adds 'Assign to' in stock inventory that propagates to quants. Also propagates the date field.

--- stock_inventory/__manifest__.py
+++ stock_inventory/__manifest__.py
@@ -11,9 +11,11 @@
     "depends": ["stock"],
     "data": [
         "security/ir.model.access.csv",
+        "security/security.xml",
         "views/stock_inventory.xml",
         "views/stock_quant.xml",
         "views/stock_move_line.xml",
+        "views/res_config_settings_view.xml",
     ],
     "installable": True,
     "application": False,
---
+++ stock_inventory/models/res_config_settings.py
@@ -0,0 +1,12 @@
+# Copyright 2024 ForgeFlow S.L. (http://www.forgeflow.com)
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+
+from odoo import fields, models
+
+
+class ResConfigSettings(models.TransientModel):
+    _inherit = "res.config.settings"
+
+    stock_inventory_auto_complete = fields.Boolean(
+        related="company_id.stock_inventory_auto_complete", readonly=False
+    )
