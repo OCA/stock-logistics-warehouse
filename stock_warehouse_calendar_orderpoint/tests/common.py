9f97d93ba686df23eb73f0cba7453f61051061de
SilvioC2C <silvio.gregorini@camptocamp.com>
2024-04-16 12:38:30 +0200

    fixup! fix tests config

--- stock_warehouse_calendar_orderpoint/tests/common.py
+++ stock_warehouse_calendar_orderpoint/tests/common.py
@@ -10,14 +10,17 @@ class CommonCalendarOrderpoint(SavepointCase):
         super().setUpClass()
         cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
         cls.product = cls.env.ref("product.product_delivery_02").copy()
-        cls.seller = cls.env["product.supplierinfo"].create(
+        cls.wh = cls.env.ref("stock.warehouse0")
+        cls.wh.write(
             {
-                "name": cls.env.ref("base.res_partner_3").id,
-                "product_tmpl_id": cls.product.product_tmpl_id.id,
-                "product_id": cls.product.id,
+                "calendar_id": cls.env.ref("resource.resource_calendar_std").id,
+                "orderpoint_calendar_id": cls.env.ref(
+                    "stock_warehouse_calendar_orderpoint.resource_calendar_orderpoint_demo"
+                ).id,
+                "orderpoint_on_workday": True,
+                "orderpoint_on_workday_policy": "skip_to_first_workday",
             }
         )
-        cls.wh = cls.env.ref("stock.warehouse0")
         cls.orderpoint = cls.env["stock.warehouse.orderpoint"].create(
             {
                 "warehouse_id": cls.wh.id,
@@ -28,14 +31,4 @@ class CommonCalendarOrderpoint(SavepointCase):
                 "product_uom": cls.env.ref("uom.product_uom_unit"),
             }
         )
-        # OP calendar to compute lead dates from Wednesday
-        cls.wh.orderpoint_calendar_id = cls.env.ref(
-            "stock_warehouse_calendar_orderpoint.resource_calendar_orderpoint_demo"
-        )
-        cls.wh.orderpoint_on_workday = True
-        cls.wh.orderpoint_on_workday_policy = "skip_to_first_workday"
-        cls.wh.calendar_id = cls.env.ref("resource.resource_calendar_std")
-        # 1 day delay for supplier
-        cls.seller.delay = 1
-        # Rule lead time
-        cls.orderpoint.rule_ids.delay = 2
+        cls.orderpoint.rule_ids.write({"action": "pull", "delay": 2})
