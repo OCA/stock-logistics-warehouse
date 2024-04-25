fe6326a210fc486a8399f4fd1e8d13b1e3152384
Emilio Pascual <emilio@moduon.team>
2024-04-25 11:21:53 +0200

    [FIX] stock_move_packaging_qty: add packaging qty reserved
    - Add packaging qty reserved in stock move line with compute to know packaging reserved when reserved.
    
    MT-5861 @moduon
    
    Co-authored-by: Emilio Pascual <53056345+EmilioPascual@users.noreply.github.com>

--- stock_move_packaging_qty/models/stock_move_line.py
+++ stock_move_packaging_qty/models/stock_move_line.py
@@ -1,6 +1,7 @@
 # Copyright 2024 Moduon Team S.L.
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl-3.0)
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.tools import float_round
 
 
 class StockMoveLine(models.Model):
@@ -9,10 +10,28 @@ class StockMoveLine(models.Model):
     product_packaging_id = fields.Many2one(
         related="move_id.product_packaging_id", readonly=True
     )
+    product_packaging_qty_reserved = fields.Float(
+        string="Reserved Pkg. Qty.",
+        help="Product packaging quantity reserved.",
+        compute="_compute_product_packaging_qty_reserved",
+        store=True,
+    )
     product_packaging_qty_done = fields.Float(
         string="Done Pkg. Qty.",
         help="Product packaging quantity done.",
     )
+
+    @api.depends("product_packaging_id", "reserved_qty")
+    def _compute_product_packaging_qty_reserved(self):
+        """Get the quantity done in product packaging."""
+        self.product_packaging_qty_reserved = False
+        for line in self:
+            if not line.product_packaging_id.qty:
+                continue
+            line.product_packaging_qty_reserved = float_round(
+                line.reserved_qty / line.product_packaging_id.qty,
+                precision_rounding=line.product_packaging_id.product_uom_id.rounding,
+            )
 
     def _get_aggregated_properties(self, move_line=False, move=False):
         """Aggregate by product packaging too."""
