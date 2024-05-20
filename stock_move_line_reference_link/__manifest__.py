136628fec8c45f368a5c12d524bdb2092d97e04c
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2024-05-20 17:23:09 +0530

    stock_move_line_reference_link: Migration to 17.0

--- stock_move_line_reference_link/__manifest__.py
+++ stock_move_line_reference_link/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Stock Move Line Reference Link",
     "summary": "Add link in stock move line references.",
-    "version": "13.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/stock-logistics-workflow",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
