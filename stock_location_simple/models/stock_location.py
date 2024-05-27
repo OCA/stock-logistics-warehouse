1291bca1166f70b323b4afbdc66114a6b2d63c8b
clementmbr <clement.mombereau@akretion.com.br>
2024-05-27 18:21:11 -0300

    [ADD] new module stock_location_simple

---
+++ stock_location_simple/models/stock_location.py
@@ -0,0 +1,10 @@
+# Copyright 2024 Akretion
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+from odoo import _, api, fields, models
+
+
+class StockLocation(models.Model):
+    _inherit = "stock.location"
+
+    # TODO
