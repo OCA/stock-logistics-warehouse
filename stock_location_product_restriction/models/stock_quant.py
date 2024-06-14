602879b14a27951d8c43f4a476971b5c07400e59
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2024-06-14 12:05:42 +0200

    [FIX] stock_location_product_restriction: Ensure restriction is always checked
    Transform check at move validation by a constraint on the stock.quant to ensure that the rule is checked in every cases

--- stock_location_product_restriction/models/stock_quant.py
+++ stock_location_product_restriction/models/stock_quant.py
@@ -1,55 +1,49 @@
-# Copyright 2020 ACSONE SA/NV
+# Copyright 2024 ACSONE SA/NV
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-
 from collections import defaultdict
 
-from odoo import _, models
+from odoo import _, api, models
 from odoo.exceptions import ValidationError
 
 
-class StockMove(models.Model):
+class StockQuant(models.Model):
 
-    _inherit = "stock.move"
+    _inherit = "stock.quant"
 
+    @api.constrains("location_id", "product_id")
     def _check_location_product_restriction(self):
         """
-        Check if the move can be executed according to potential restriction
+        Check if the quant can be put into the location according to restriction
         defined on the stock_location
         """
         StockLocation = self.env["stock.location"]
         ProductProduct = self.env["product.product"]
-        # We only check moves with a location_dest that can
+        # We only check quants with a location_id that can
         # only contain the same product
-        moves_to_ckeck = self.filtered(
-            lambda m: m.location_dest_id.product_restriction == "same"
+        quants_to_check = self.filtered(
+            lambda q: q.location_id.product_restriction == "same"
         )
-        if not moves_to_ckeck:
+        if not quants_to_check:
             return
-        product_ids_location_dest_id = defaultdict(set)
+        product_ids_location_id = defaultdict(set)
         error_msgs = []
-        # check dest locations into the stock moves
-        for move in moves_to_ckeck:
-            product_ids_location_dest_id[move.location_dest_id.id].add(
-                move.product_id.id
-            )
-        for location_id, product_ids in product_ids_location_dest_id.items():
+        for quant in quants_to_check:
+            product_ids_location_id[quant.location_id.id].add(quant.product_id.id)
+        for location_id, product_ids in product_ids_location_id.items():
             if len(product_ids) > 1:
                 location = StockLocation.browse(location_id)
                 products = ProductProduct.browse(list(product_ids))
                 error_msgs.append(
                     _(
                         "The location {location} can only contain items of the same "
-                        "product. You plan to move different products to "
+                        "product. You plan to put different products into "
                         "this location. ({products})"
                     ).format(
                         location=location.name,
                         products=", ".join(products.mapped("name")),
                     )
                 )
-
-        # check dest locations by taking into account product already into the
-        # locations
-        # here we use a plain SQL to avoid performance issue
+        # Get existing product already in the locations
         SQL = """
             SELECT
                 location_id,
@@ -61,22 +55,21 @@ class StockMove(models.Model):
             GROUP BY
                 location_id
         """
-        self.env.cr.execute(
-            SQL, (tuple(moves_to_ckeck.mapped("location_dest_id").ids),)
-        )
+        self.env.cr.execute(SQL, (tuple(quants_to_check.mapped("location_id").ids),))
         existing_product_ids_by_location_id = dict(self.env.cr.fetchall())
+
         for (
-            location_dest_id,
+            location_id,
             existing_product_ids,
         ) in existing_product_ids_by_location_id.items():
-            product_ids_to_move = product_ids_location_dest_id[location_dest_id]
-            if set(existing_product_ids).symmetric_difference(product_ids_to_move):
-                location = StockLocation.browse(location_dest_id)
+            product_ids_to_add = product_ids_location_id[location_id]
+            if set(existing_product_ids).symmetric_difference(product_ids_to_add):
+                location = StockLocation.browse(location_id)
                 existing_products = ProductProduct.browse(existing_product_ids)
-                to_move_products = ProductProduct.browse(list(product_ids_to_move))
+                to_move_products = ProductProduct.browse(list(product_ids_to_add))
                 error_msgs.append(
                     _(
-                        "You plan to move the product {product} to the location {location} "
+                        "You plan to add the product {product} into the location {location} "
                         "but the location must only contain items of same "
                         "product and already contains items of other "
                         "product(s) "
@@ -87,9 +80,6 @@ class StockMove(models.Model):
                         existing_products=" | ".join(existing_products.mapped("name")),
                     )
                 )
+
         if error_msgs:
             raise ValidationError("\n".join(error_msgs))
-
-    def _action_done(self, cancel_backorder=False):
-        self._check_location_product_restriction()
-        return super()._action_done(cancel_backorder=cancel_backorder)
