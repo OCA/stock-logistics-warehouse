207c8b3e1e025544aafa2957a65243afe38bd7e5
clementmbr <clement.mombereau@akretion.com.br>
2024-06-15 18:55:32 +0200

    [IMP] add bool to filter technical locations in simplified menu

--- stock_location_simple/models/stock_location.py
+++ stock_location_simple/models/stock_location.py
@@ -1,10 +1,25 @@
 # Copyright 2024 Akretion
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class StockLocation(models.Model):
     _inherit = "stock.location"
 
     is_linked_to_warehouse = fields.Boolean()
+
+    is_technical_location = fields.Boolean(
+        compute="_compute_is_technical_location",
+        help="Technical locations will not be displayed in the simplified "
+        "Locations menu, and can not be set as parent of a new location created through"
+        "the simplified menu.",
+        store=True,
+    )
+
+    @api.depends("location_id")
+    def _compute_is_technical_location(self):
+        physical_location_id = self.env.ref("stock.stock_location_locations")
+        for rec in self:
+            # Useful check to set 'Subcontracting Locations' as technical locations.
+            rec.is_technical_location = rec.location_id == physical_location_id
