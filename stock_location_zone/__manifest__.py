e4901deafc05a35b5aef6eb60f2a2853f59a10bd
Jacques-Etienne Baudoux <je@bcim.be>
2024-06-16 23:54:28 +0200

    stock_location_zone: fix kind
    allow view locations in location structure

--- stock_location_zone/__manifest__.py
+++ stock_location_zone/__manifest__.py
@@ -1,5 +1,5 @@
 # Copyright 2017 Syvain Van Hoof (Okia sprl) <sylvainvh@okia.be>
-# Copyright 2016-2019 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# Copyright 2016 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
