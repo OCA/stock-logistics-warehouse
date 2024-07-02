27496dcf7293f93cf1a0f9dbb95dda640c53dd0d
Ruchir Shukla <ruchir@bizzappdev.com>
2024-07-02 14:20:54 +0530

    [MIG] stock_inventory: Migration to 17.0

--- stock_inventory/__manifest__.py
+++ stock_inventory/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Stock Inventory Adjustment",
-    "version": "16.0.2.3.0",
+    "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "maintainer": ["DavidJForgeFlow"],
     "development_status": "Beta",
