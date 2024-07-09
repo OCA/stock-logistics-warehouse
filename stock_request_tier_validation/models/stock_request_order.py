ec0d888ddb991902ef3fe6d2796952305a8e9fba
Kaynnan Lemes <kaynnanx1@gmail.com>
2024-07-09 12:24:32 -0300

    [FIX] stock_request_tier_validation: action_confirm validation after compute state in SRO

--- stock_request_tier_validation/models/stock_request_order.py
+++ stock_request_tier_validation/models/stock_request_order.py
@@ -1,6 +1,7 @@
 # Copyright 2019-2020 ForgeFlow S.L. (https://www.forgeflow.com)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
-from odoo import api, models
+from odoo import _, api, models
+from odoo.exceptions import ValidationError
 
 
 class StockRequestOrder(models.Model):
@@ -14,3 +15,34 @@ class StockRequestOrder(models.Model):
         res = super()._get_under_validation_exceptions()
         res.append("route_id")
         return res
+
+    def action_confirm(self):
+        """
+        Confirms the stock request order.
+
+        Validates if needed, ensures tier validation,
+        and checks for open validation processes.
+        This method reinforces tier.validation due to
+        the state being a computed method, aligning
+        Stock Request Orders (SROs) with Stock Requests (SRs).
+        """
+        for rec in self:
+            # Validate if needed
+            if rec.need_validation:
+                reviews = rec.request_validation()
+                rec._validate_tier(reviews)
+                if not self._calc_reviews_validated(reviews):
+                    raise ValidationError(
+                        _(
+                            "This action needs to be validated for at least "
+                            "one record. \nPlease request a validation."
+                        )
+                    )
+
+            # Check for open validation processes
+            if rec.review_ids and not rec.validated:
+                raise ValidationError(
+                    _("A validation process is still open for at least one record.")
+                )
+
+        return super().action_confirm()
