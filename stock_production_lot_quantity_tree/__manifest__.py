f6d9104b00f54419b3f0146843cfc068843a22a7
Adriana Saiz <adriana.saiz@factorlibre.com>
2024-07-18 10:27:40 +0200

    [MIG] stock_production_lot_quantity_tree: Migration to 16.0

--- stock_production_lot_quantity_tree/__manifest__.py
+++ stock_production_lot_quantity_tree/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Production Lot Quantity Tree",
     "summary": """
         Allows to display product quantity field on production lot tree view""",
-    "version": "13.0.1.0.0",
+    "version": "16.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "maintainers": ["rousseldenis"],
