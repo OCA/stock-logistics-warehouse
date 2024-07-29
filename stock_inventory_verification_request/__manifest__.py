fc72ed2a460c4ef47d37d20354e8a1c130f34429
Joan Sisquella <joan.sisquella@forgeflow.com>
2024-07-29 15:49:55 +0200

    [IMP] stock_inventory_verification_request: black, isort, prettier

--- stock_inventory_verification_request/__manifest__.py
+++ stock_inventory_verification_request/__manifest__.py
@@ -4,11 +4,10 @@
 {
     "name": "Stock Inventory Verification Request",
     "summary": "Adds the capability to request a Slot Verification when "
-               "a inventory is Pending to Approve",
+    "a inventory is Pending to Approve",
     "version": "12.0.3.0.1",
-    "maintainers": ['LoisRForgeFlow'],
-    "author": "ForgeFlow, "
-              "Odoo Community Association (OCA)",
+    "maintainers": ["LoisRForgeFlow"],
+    "author": "ForgeFlow, " "Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Warehouse",
     "depends": [
@@ -16,14 +15,14 @@
         "mail",
     ],
     "data": [
-        'security/ir.model.access.csv',
-        'security/stock_security.xml',
-        'views/stock_slot_verification_request_view.xml',
-        'views/stock_inventory_view.xml',
-        'views/stock_location_view.xml',
-        'data/slot_verification_request_sequence.xml',
+        "security/ir.model.access.csv",
+        "security/stock_security.xml",
+        "views/stock_slot_verification_request_view.xml",
+        "views/stock_inventory_view.xml",
+        "views/stock_location_view.xml",
+        "data/slot_verification_request_sequence.xml",
     ],
     "license": "AGPL-3",
-    'installable': True,
-    'application': False,
+    "installable": True,
+    "application": False,
 }
--- stock_inventory_verification_request/models/stock_inventory.py
+++ stock_inventory_verification_request/models/stock_inventory.py
@@ -6,16 +6,19 @@ from odoo import api, fields, models
 
 
 class StockInventory(models.Model):
-    _inherit = 'stock.inventory'
+    _inherit = "stock.inventory"
 
     requested_verification = fields.Boolean(
-        string='Requested Verification?', copy=False)
+        string="Requested Verification?", copy=False
+    )
     slot_verification_ids = fields.One2many(
-        comodel_name='stock.slot.verification.request',
-        string='Slot Verification Requests', inverse_name='inventory_id')
+        comodel_name="stock.slot.verification.request",
+        string="Slot Verification Requests",
+        inverse_name="inventory_id",
+    )
     solving_slot_verification_request_id = fields.Many2one(
         comodel_name="stock.slot.verification.request",
-        help="This Inventory adjustment was created from the specified SVR."
+        help="This Inventory adjustment was created from the specified SVR.",
     )
 
     @api.multi
@@ -23,41 +26,48 @@ class StockInventory(models.Model):
         self.ensure_one()
         self.requested_verification = True
         for line in self.line_ids:
-            if line.discrepancy_threshold and (line.discrepancy_percent >
-                                               line.discrepancy_threshold):
-                self.env['stock.slot.verification.request'].create({
-                    'inventory_id': self.id,
-                    'inventory_line_id': line.id,
-                    'location_id': line.location_id.id,
-                    'state': 'wait',
-                    'product_id': line.product_id.id,
-                    'company_id': self.company_id.id,
-                })
+            if line.discrepancy_threshold and (
+                line.discrepancy_percent > line.discrepancy_threshold
+            ):
+                self.env["stock.slot.verification.request"].create(
+                    {
+                        "inventory_id": self.id,
+                        "inventory_line_id": line.id,
+                        "location_id": line.location_id.id,
+                        "state": "wait",
+                        "product_id": line.product_id.id,
+                        "company_id": self.company_id.id,
+                    }
+                )
 
 
 class StockInventoryLine(models.Model):
-    _inherit = 'stock.inventory.line'
-    _rec_name = 'product_id'
+    _inherit = "stock.inventory.line"
+    _rec_name = "product_id"
 
     slot_verification_ids = fields.One2many(
-        comodel_name='stock.slot.verification.request',
-        inverse_name='inventory_line_id',
-        string='Slot Verification Request')
+        comodel_name="stock.slot.verification.request",
+        inverse_name="inventory_line_id",
+        string="Slot Verification Request",
+    )
 
     @api.multi
     def action_open_svr(self):
         """Open the corresponding Slot Verification Request directly from the
         Inventory Lines."""
-        request_svr_ids = self.mapped('slot_verification_ids').ids
-        action = self.env.ref('stock_inventory_verification_request.'
-                              'action_slot_verification_request')
+        request_svr_ids = self.mapped("slot_verification_ids").ids
+        action = self.env.ref(
+            "stock_inventory_verification_request." "action_slot_verification_request"
+        )
         result = action.read()[0]
         if len(request_svr_ids) > 1:
-            result['domain'] = [('id', 'in', request_svr_ids)]
+            result["domain"] = [("id", "in", request_svr_ids)]
         elif len(request_svr_ids) == 1:
             view = self.env.ref(
-                'stock_inventory_verification_request.stock_'
-                'slot_verification_request_form_view', False)
-            result['views'] = [(view and view.id or False, 'form')]
-            result['res_id'] = request_svr_ids[0] or False
+                "stock_inventory_verification_request.stock_"
+                "slot_verification_request_form_view",
+                False,
+            )
+            result["views"] = [(view and view.id or False, "form")]
+            result["res_id"] = request_svr_ids[0] or False
         return result
