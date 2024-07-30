78b5d0a2fa5d0e8b25ce2f5a6a931efb836012fe
Michael Tietz <mtietz@mt-software.de>
2024-07-30 14:31:13 +0200

    [IMP] stock_move_auto_assign_auto_release: Release release_ready pickings instead of moves Instead of just releasing the release ready moves of a give product it now releases the whole transfer This ensures that a transfer with a release_policy=one gets not split

--- stock_move_auto_assign_auto_release/models/stock_picking.py
+++ stock_move_auto_assign_auto_release/models/stock_picking.py
@@ -1,8 +1,11 @@
 # Copyright 2022 ACSONE SA/NV
+# Copyright 2024 Michael Tietz (MT Software) <mtietz@mt-software.de>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import _, api, fields, models
 from odoo.osv.expression import NEGATIVE_TERM_OPERATORS
+
+from odoo.addons.queue_job.job import identity_exact
 
 
 class StockPicking(models.Model):
@@ -47,3 +50,17 @@ class StockPicking(models.Model):
         if not is_auto_release_allowed:
             domain = [("id", "not in", self.search(domain).ids)]
         return domain
+
+    def _delay_auto_release_available_to_promise(self):
+        for picking in self:
+            picking.with_delay(
+                identity_key=identity_exact,
+                description=_(
+                    "Auto release available to promise %(name)s", name=picking.name
+                ),
+            ).auto_release_available_to_promise()
+
+    def auto_release_available_to_promise(self):
+        to_release = self.filtered("is_auto_release_allowed")
+        to_release.release_available_to_promise()
+        return to_release
--- stock_move_auto_assign_auto_release/readme/CONTRIBUTORS.rst
+++ stock_move_auto_assign_auto_release/readme/CONTRIBUTORS.rst
@@ -1,1 +1,2 @@
 * Laurent Mignon <laurent.mignon@acsone.eu>
+* Michael Tietz (MT Software) <mtietz@mt-software.de>
