52485fcb07eff1ea02003650ab3cb1f427157359
Lois Rilo <lois.rilo@forgeflow.com>
2024-08-14 10:11:19 +0200

    [MIG] stock_quant_reservation_info_mrp: Migration to 16.0

--- stock_quant_reservation_info_mrp/__manifest__.py
+++ stock_quant_reservation_info_mrp/__manifest__.py
@@ -5,7 +5,7 @@
 {
     "name": "Stock Move Reservation Info MRP",
     "summary": "Allows to see the manufacturing order related to the reserved info of Products",
-    "version": "15.0.1.0.0",
+    "version": "16.0.1.0.0",
     "author": "ForgeFlow, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "category": "Generic",
