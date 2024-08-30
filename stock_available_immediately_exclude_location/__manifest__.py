c48e3bbb29b78246972977cbfefca5b72d7e306f
Sébastien Alix <sebastien.alix@camptocamp.com>
2024-08-30 11:39:05 +0200

    [IMP] stock_available_immediately_exclude_location: pre-commit stuff

--- stock_available_immediately_exclude_location/__manifest__.py
+++ stock_available_immediately_exclude_location/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Exclude locations from immediately usable quantity",
     "version": "16.0.1.0.0",
-    "website": "https://github.com/OCA/stock-logistics-availability",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "license": "AGPL-3",
     "depends": ["stock_available_immediately"],
