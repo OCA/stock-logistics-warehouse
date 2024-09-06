a59ca0c21a91389d04bd9adfd71ad28dfcdd7b9d
ivan <iperez@coninpe.es>
2024-09-06 13:48:16 +0200

    [FIX]stock_inventory_non_stocked: Forget changing a class name and a file

--- stock_inventory_non_stocked/tests/test_assign_inventory_responsible.py
+++ stock_inventory_non_stocked/tests/test_assign_inventory_responsible.py
@@ -4,7 +4,7 @@
 from odoo.tests.common import TransactionCase
 
 
-class TestCheckInventoryResponsible(TransactionCase):
+class TestAssignInventoryResponsible(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
