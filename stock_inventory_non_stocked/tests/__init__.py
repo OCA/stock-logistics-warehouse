fd44fe684ae9574a4cc232c2db28c45d529d9b62
ivan <iperez@coninpe.es>
2024-09-06 09:15:15 +0200

    [FIX]stock_inventory_non_stocked: fix pre-commit

--- stock_inventory_non_stocked/tests/__init__.py
+++ stock_inventory_non_stocked/tests/__init__.py
@@ -1,1 +1,5 @@
-from . import test_compute_has_quants, test_create_non_stocked_prod, test_check_inventory_responsible
+from . import (
+    test_compute_has_quants,
+    test_create_non_stocked_prod,
+    test_check_inventory_responsible,
+)
