c63f86ca9386306ead9c2ac8bfac3da0604a2dac
twalter-c2c <tomasz.walter@camptocamp.com>
2024-09-13 17:11:27 +0200

    [14.0][IMP] stock_route_mto: black, isort, prettier

--- stock_route_mto/__manifest__.py
+++ stock_route_mto/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Stock Route Mto",
     "summary": """
         Allows to identify MTO routes through a checkbox and availability to filter them.""",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
