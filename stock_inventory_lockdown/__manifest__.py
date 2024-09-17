c2c0fc3efab2e1af0dc02e477e8aee214d242ffa
Joan Sisquella <joan.sisquella@forgeflow.com>
2024-09-17 16:05:50 +0200

    [MIG] stock_inventory_lockdown: Migration to 15.0

--- stock_inventory_lockdown/__manifest__.py
+++ stock_inventory_lockdown/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Inventory Lock Down",
     "summary": "Lock down stock locations during inventories.",
-    "version": "16.0.1.0.0",
+    "version": "15.0.1.0.0",
     "development_status": "Beta",
     "depends": ["stock", "stock_inventory"],
     "author": "Numérigraphe, ForgeFlow, Odoo Community Association (OCA)",
