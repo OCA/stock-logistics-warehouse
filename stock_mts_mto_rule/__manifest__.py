7260683db079a80483085952440c2af2bd572f7a
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2024-10-28 17:43:38 +0530

    [IMP] stock_mts_mto_rule: Fix company_id for demo data

--- stock_mts_mto_rule/__manifest__.py
+++ stock_mts_mto_rule/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Stock MTS+MTO Rule",
     "summary": "Add a MTS+MTO route",
-    "version": "17.0.1.0.0",
+    "version": "17.0.1.0.1",
     "development_status": "Mature",
     "category": "Warehouse",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
