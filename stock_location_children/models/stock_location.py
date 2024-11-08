614d20694bba3a5352c713909a4f4c6b29aa4b31
Denis Roussel <denis.roussel@acsone.eu>
2024-11-08 12:23:39 +0100

    [IMP] stock_location_children: Improve readability
    Improve readability of code without impacting performances

--- stock_location_children/models/stock_location.py
+++ stock_location_children/models/stock_location.py
@@ -1,6 +1,9 @@
 # Copyright 2020 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+import operator
+
 from odoo import api, fields, models
+from odoo.fields import Command
 
 
 class StockLocation(models.Model):
@@ -8,17 +11,19 @@ class StockLocation(models.Model):
     _inherit = "stock.location"
 
     children_ids = fields.Many2many(
-        "stock.location",
-        "stock_location_children_ids",
-        "parent_id",
-        "children_id",
+        comodel_name="stock.location",
+        relation="stock_location_children_ids",
+        column1="parent_id",
+        column2="children_id",
         compute="_compute_children_ids",
         store=True,
+        # Don't put recursive here as the query in compute method is sufficent
         help="All the children (multi-level) stock location of this location",
     )
 
     @api.depends("child_ids", "children_ids.child_ids")
     def _compute_children_ids(self):
+
         query = """SELECT sub.id, ARRAY_AGG(sl2.id) AS children
             FROM stock_location sl2,
             (
@@ -33,13 +38,10 @@ class StockLocation(models.Model):
         self.flush_model(["location_id", "child_ids"])
         self.env.cr.execute(query, (tuple(self.ids),))
         rows = self.env.cr.dictfetchall()
+        result_by_location = dict(zip(map(operator.itemgetter("id"), rows), rows))
         for loc in self:
-            all_ids = []
-            for row in rows:
-                if row.get("id") == loc.id:
-                    all_ids = row.get("children")
-                    break
-            if all_ids:
-                loc.children_ids = [(6, 0, all_ids)]
+            children = result_by_location.get(loc.id, {}).get("children")
+            if children:
+                loc.children_ids = [Command.set(children)]
             else:
-                loc.children_ids = [(5, 0, 0)]
+                loc.children_ids = [Command.clear()]
