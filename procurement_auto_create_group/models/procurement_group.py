ea1cfd9aafca9b1f68f0bd43c04fef1af81b1ff9
Mounir Bouhyouh <mounir.bouhyouh@accomodata.be>
2025-01-21 16:37:18 +0100

    [MIG] procurement_auto_create_group - Migrate to 18.

---
+++ procurement_auto_create_group/models/procurement_group.py
@@ -0,0 +1,18 @@
+# Copyright 2017-2020 ForgeFlow, S.L.
+# Copyright 2021 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+from odoo import api, models
+
+
+class ProcurementGroup(models.Model):
+    _inherit = "procurement.group"
+
+    @api.model
+    def _get_rule(self, product_id, location_id, values):
+        rule = super()._get_rule(product_id, location_id, values)
+        # If there isn't a date planned in the values it means that this
+        # method has been called outside of a procurement process.
+        if rule and rule.auto_create_group and values.get("date_planned"):
+            values["group_id"] = rule._get_auto_procurement_group(product_id)
+        return rule
---
+++ procurement_auto_create_group/readme/USAGE.rst
@@ -0,0 +1,5 @@
+#. Run a new procurement and make sure that it determines a rule
+   with the option 'Auto-create Procurement Group' set.
+#. When the procurement rule is executed, a procurement group with
+   format 'PG/000001' will be created. You can find this procurement
+   group in 'Additional info' tab of a transfer.
