658d2da52c7cc9debda2b7084e8cb05fcbe58d39
JasminSForgeFlow <jasmin.solanki@forgeflow.com>
2025-03-04 14:43:25 +0530

    [ADD] stock_location_is_sublocation

---
+++ stock_location_is_sublocation/__manifest__.py
@@ -0,0 +1,17 @@
+# Copyright 2020-2021 Camptocamp SA
+# Copyright 2016-2025 ForgeFlow S.L. (https://www.forgeflow.com)
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+{
+    "name": "Stock Location Is Sublocation",
+    "summary": "Add method to check stock location is sublocation",
+    "version": "18.0.1.0.0",
+    "development_status": "Production/Stable",
+    "author": "Camptocamp, ForgeFlow, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "category": "Hidden",
+    "depends": ["stock"],
+    "data": [],
+    "installable": True,
+    "license": "LGPL-3",
+}
---
+++ stock_location_is_sublocation/tests/common.py
@@ -0,0 +1,19 @@
+# Copyright 2020-2021 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+from odoo.tests import TransactionCase
+
+
+class StockHelperCommonCase(TransactionCase):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.env = cls.env(context=dict(cls.env.context, tracking_disable=True))
+
+        cls.wh = cls.env.ref("stock.warehouse0")
+
+        cls.customer_loc = cls.env.ref("stock.stock_location_customers")
+        cls.supplier_loc = cls.env.ref("stock.stock_location_suppliers")
+        cls.stock_loc = cls.wh.lot_stock_id
+        cls.shelf1_loc = cls.env.ref("stock.stock_location_components")
+        cls.shelf2_loc = cls.env.ref("stock.stock_location_14")
