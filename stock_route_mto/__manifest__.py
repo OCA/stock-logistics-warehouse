c766295805630565cdb9a4371b0e841e81a2b75e
Jacques-Etienne Baudoux <je@bcim.be>
2025-05-07 15:31:28 +0200

    [FIX] stock_route_mto: init hook
    The warehouse defines what are mto routes. Mark them as is_mto

--- stock_route_mto/__manifest__.py
+++ stock_route_mto/__manifest__.py
@@ -1,4 +1,5 @@
 # Copyright 2022 ACSONE SA/NV
+# Copyright 2025 Jacques-Etienne Baudoux (BCIM) <je@bcim.be>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 {
@@ -11,8 +12,10 @@
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
     "depends": ["stock"],
+    "maintainers": ["rousseldenis", "jbaudoux"],
     "data": [
         "data/stock_route.xml",
         "views/stock_route.xml",
     ],
+    "post_init_hook": "post_init_hook",
 }
