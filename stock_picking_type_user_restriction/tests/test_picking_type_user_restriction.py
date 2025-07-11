b00b7da93bb2c2cfb8d632c5d0436de6133d16bc
Samuel Macias Oropeza (smo) <smacias@opensourceintegrators.com>
2025-07-11 10:21:45 -0600

    [FIX] Tests

--- stock_picking_type_user_restriction/tests/test_picking_type_user_restriction.py
+++ stock_picking_type_user_restriction/tests/test_picking_type_user_restriction.py
@@ -1,7 +1,7 @@
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 
-class TestUserRestriction(SavepointCase):
+class TestUserRestriction(TransactionCase):
     @classmethod
     def setUpClass(cls):
         super().setUpClass()
