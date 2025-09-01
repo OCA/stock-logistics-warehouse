1bac708967893625981601e1b0ebf19d11f367a9
Denis Roussel <denis.roussel@acsone.eu>
2025-09-01 10:15:42 +0200

    [IMP] stock_location_product_restriction: Add a function to check for location restriction
    Add an helper function in order to check if a product can enter a location

--- stock_location_product_restriction/models/stock_location.py
+++ stock_location_product_restriction/models/stock_location.py
@@ -48,43 +48,7 @@ class StockLocation(models.Model):
     )
 
     @api.model
-    def _selection_product_restriction(self):
-        return [
-            ("any", "Items of any products are allowed into the location"),
-            (
-                "same",
-                "Only items of the same product allowed into the location",
-            ),
-        ]
-
-    @api.depends("specific_product_restriction", "parent_product_restriction")
-    def _compute_product_restriction(self):
-        default_value = "any"
-        for rec in self:
-            rec.product_restriction = (
-                rec.specific_product_restriction
-                or rec.parent_product_restriction
-                or default_value
-            )
-
-    @api.depends("product_restriction")
-    def _compute_restriction_violation(self):
-        records = self
-        self.env["stock.quant"].flush_model(
-            [
-                "product_id",
-                "location_id",
-                "quantity",
-                "reserved_quantity",
-                "available_quantity",
-                "inventory_quantity",
-            ]
-        )
-        self.flush_model(["product_restriction", "fill_state"])
-        ProductProduct = self.env["product.product"]
-        precision_digits = max(
-            6, self.sudo().env.ref("product.decimal_product_uom").digits * 2
-        )
+    def _get_product_restriction_query(self):
         SQL = """
            SELECT
                stock_quant.location_id,
@@ -104,8 +68,98 @@ class StockLocation(models.Model):
                         OR inventory_quantity IS NULL))
            GROUP BY
                stock_quant.location_id
+        """
+        return SQL
+
+    @api.model
+    def _get_product_restriction_query_having(self):
+        return """
             HAVING count(distinct(product_id)) > 1
-       """
+        """
+
+    @api.model
+    def _selection_product_restriction(self):
+        return [
+            ("any", "Items of any products are allowed into the location"),
+            (
+                "same",
+                "Only items of the same product allowed into the location",
+            ),
+        ]
+
+    @api.depends("specific_product_restriction", "parent_product_restriction")
+    def _compute_product_restriction(self):
+        default_value = "any"
+        for rec in self:
+            rec.product_restriction = (
+                rec.specific_product_restriction
+                or rec.parent_product_restriction
+                or default_value
+            )
+
+    def _check_has_location_product_restriction(self, product):
+        """
+        Call this if you want to check if product can
+        enter a location.
+        """
+        product_ids_by_location_id = self._get_product_ids_by_locations()
+        for location in self:
+            product_ids = product_ids_by_location_id.get(location.id)
+            if product_ids and product.id not in product_ids:
+                return True
+        return False
+
+    def _get_product_ids_by_locations(self):
+        self.env["stock.quant"].flush_model(
+            [
+                "product_id",
+                "location_id",
+                "quantity",
+                "reserved_quantity",
+                "available_quantity",
+                "inventory_quantity",
+            ]
+        )
+        self.flush_model(["product_restriction", "fill_state"])
+
+        precision_digits = max(
+            6, self.sudo().env.ref("product.decimal_product_uom").digits * 2
+        )
+        SQL = SQL = self._get_product_restriction_query()
+        # Browse only real record ids
+        ids = tuple(
+            [record.id for record in self if not isinstance(record.id, fields.NewId)]
+        )
+        if not ids:
+            product_ids_by_location_id = dict()
+        else:
+            self.env.cr.execute(
+                SQL, (ids, precision_digits, precision_digits, precision_digits)
+            )
+            product_ids_by_location_id = dict(self.env.cr.fetchall())
+        return product_ids_by_location_id
+
+    @api.depends("product_restriction")
+    def _compute_restriction_violation(self):
+        records = self
+        self.env["stock.quant"].flush_model(
+            [
+                "product_id",
+                "location_id",
+                "quantity",
+                "reserved_quantity",
+                "available_quantity",
+                "inventory_quantity",
+            ]
+        )
+        self.flush_model(["product_restriction", "fill_state"])
+        ProductProduct = self.env["product.product"]
+        precision_digits = max(
+            6, self.sudo().env.ref("product.decimal_product_uom").digits * 2
+        )
+
+        SQL = self._get_product_restriction_query()
+        SQL += self._get_product_restriction_query_having()
         # Browse only real record ids
         ids = tuple(
             [record.id for record in records if not isinstance(record.id, fields.NewId)]
