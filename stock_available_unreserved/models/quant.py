7442addd7d2d20ba151b3e24cf4fd7c08457d3ca
Miquel Raïch <miquel.raich@forgeflow.com>
2025-11-06 17:03:24 +0100

    [FIX] stock_available_unreserved: handle merge of quants

--- stock_available_unreserved/models/quant.py
+++ stock_available_unreserved/models/quant.py
@@ -1,8 +1,11 @@
 # Copyright 2018 Camptocamp SA
 # Copyright 2016 ForgeFlow S.L. (https://www.forgeflow.com)
 # License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
-
+import logging
+from psycopg2 import Error
 from odoo import api, fields, models
+
+_logger = logging.getLogger(__name__)
 
 
 class StockQuant(models.Model):
@@ -29,3 +32,36 @@ class StockQuant(models.Model):
     def _compute_unreserved_quantity(self):
         for rec in self:
             rec.unreserved_quantity = rec.quantity - rec.reserved_quantity
+
+    @api.model
+    def _merge_quants(self):
+        """ replaces Odoo _merge_quants """
+        query = """WITH
+                       dupes AS (
+                           SELECT min(id) as to_update_quant_id,
+                               (array_agg(id ORDER BY id))[2:array_length(array_agg(id), 1)]
+                                   as to_delete_quant_ids,
+                               SUM(reserved_quantity) as reserved_quantity,
+                               SUM(quantity) as quantity,
+                               SUM(unreserved_quantity) as unreserved_quantity
+                           FROM stock_quant
+                           GROUP BY product_id, company_id, location_id, lot_id,
+                               package_id, owner_id, in_date
+                           HAVING count(id) > 1
+                       ),
+                       _up AS (
+                           UPDATE stock_quant q
+                               SET quantity = d.quantity,
+                                   reserved_quantity = d.reserved_quantity,
+                                   unreserved_quantity = d.unreserved_quantity
+                           FROM dupes d
+                           WHERE d.to_update_quant_id = q.id
+                       )
+                   DELETE FROM stock_quant WHERE id in (
+                        SELECT unnest(to_delete_quant_ids) from dupes)
+        """
+        try:
+            with self.env.cr.savepoint():
+                self.env.cr.execute(query)
+        except Error as e:
+            _logger.info('An error occurred while merging quants: %s', e.pgerror)
