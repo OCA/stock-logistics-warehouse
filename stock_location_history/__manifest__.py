b1b52865e9a540036cdafbe78b57a64ca8dc8a82
Johannan <jojaluga.osi@gmail.com>
2025-11-22 19:29:52 -0600

    Automatic lot creation on stage change

--- stock_location_history/__manifest__.py
+++ stock_location_history/__manifest__.py
@@ -7,11 +7,12 @@
     "depends": ["stock"],
     "category": "Stock",
     "data": [
-        "data/stock_location_stage_data.xml",
+        # "data/stock_location_stage_data.xml",
         "security/ir.model.access.csv",
         "views/stock_location_views.xml",
         "views/stock_location_stage_views.xml",
         "views/stock_location_history_views.xml",
+        "views/stock_lot_views.xml",
     ],
     "development_status": "Beta",
     "maintainers": ["jasiel-OSI"],
