6e904472140441092852297ec9d2e8d613f9cc6a
santostelmo <telmo.santos@camptocamp.com>
2025-11-24 10:02:49 +0100

    [IMP] stock_move_auto_assign_auto_release: improve performance

--- stock_move_auto_assign_auto_release/models/product_product.py
+++ stock_move_auto_assign_auto_release/models/product_product.py
@@ -12,7 +12,7 @@ class ProductProduct(models.Model):
     def _moves_auto_release_domain(self):
         return [
             ("product_id", "=", self.id),
-            ("is_auto_release_allowed", "=", True),
+            ("state", "not in", ("done", "cancel")),
         ]
 
     def pickings_auto_release(self):
@@ -23,6 +23,13 @@ class ProductProduct(models.Model):
         """
         self.ensure_one()
         moves = self.env["stock.move"].search(self._moves_auto_release_domain())
+        # NOTE 'is_auto_release_allowed' not in the domain to avoid a
+        # time consuming computation of 'release_ready' and
+        # 'ordered_available_to_promise_qty' under the hood.
+        # Filtering moves manually here limits the computation of
+        # is_auto_release_allowed' (with 'release_ready' and
+        # 'ordered_available_to_promise_qty' fields) only to relevant moves.
+        moves = moves.filtered("is_auto_release_allowed")
         pickings = moves.picking_id
         if not pickings:
             return
