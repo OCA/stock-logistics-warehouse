6733403fe749c6fbcc2eaed37cd19aaf2238a2c0
Yann Papouin <ypa@decgroupe.com>
2025-12-18 09:14:19 +0100

    [MIG] stock_mts_mto_rule: Migration to 18.0

--- stock_mts_mto_rule/models/stock_rule.py
+++ stock_mts_mto_rule/models/stock_rule.py
@@ -1,6 +1,6 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import _, api, fields, models
+from odoo import api, fields, models
 from odoo.exceptions import ValidationError
 from odoo.osv import expression
 from odoo.tools import float_compare, float_is_zero
@@ -21,15 +21,15 @@ class StockRule(models.Model):
         for rule in self:
             if rule.action == "split_procurement":
                 if not rule.mts_rule_id or not rule.mto_rule_id:
-                    msg = _(
-                        "No MTS or MTO rule configured on procurement " "rule: %s!"
+                    msg = self.env._(
+                        "No MTS or MTO rule configured on procurement rule: %s!"
                     ) % (rule.name,)
                     raise ValidationError(msg)
                 if (
                     rule.mts_rule_id.location_src_id.id
                     != rule.mto_rule_id.location_src_id.id
                 ):
-                    msg = _(
+                    msg = self.env._(
                         "Inconsistency between the source locations of "
                         "the mts and mto rules linked to the procurement "
                         "rule: %s! It should be the same."
@@ -70,7 +70,7 @@ class StockRule(models.Model):
                 procurement.values,
             )
             if float_is_zero(needed_qty, precision_digits=precision):
-                getattr(self.env["stock.rule"], "_run_%s" % rule.mts_rule_id.action)(
+                getattr(self.env["stock.rule"], f"_run_{rule.mts_rule_id.action}")(
                     [(procurement, rule.mts_rule_id)]
                 )
             elif (
@@ -79,29 +79,28 @@ class StockRule(models.Model):
                 )
                 == 0.0
             ):
-                getattr(self.env["stock.rule"], "_run_%s" % rule.mto_rule_id.action)(
+                getattr(self.env["stock.rule"], f"_run_{rule.mto_rule_id.action}")(
                     [(procurement, rule.mto_rule_id)]
                 )
             else:
                 mts_qty = procurement.product_qty - needed_qty
                 mts_procurement = procurement._replace(product_qty=mts_qty)
-                getattr(self.env["stock.rule"], "_run_%s" % rule.mts_rule_id.action)(
+                getattr(self.env["stock.rule"], f"_run_{rule.mts_rule_id.action}")(
                     [(mts_procurement, rule.mts_rule_id)]
                 )
 
-                # Search all confirmed stock_moves of mts_procuremet and assign them
+                # Search all confirmed stock_moves of mts_procurement and assign them
                 # to adjust the product's free qty
                 group_id = mts_procurement.values.get("group_id")
-                group_domain = expression.AND(
-                    [domain, [("group_id", "=", group_id.id)]]
-                )
+                if group_id:
+                    domain = expression.AND([domain, [("group_id", "=", group_id.id)]])
                 moves_to_assign = self.env["stock.move"].search(
                     group_domain, order="priority desc, date asc"
                 )
                 moves_to_assign._action_assign()
 
                 mto_procurement = procurement._replace(product_qty=needed_qty)
-                getattr(self.env["stock.rule"], "_run_%s" % rule.mto_rule_id.action)(
+                getattr(self.env["stock.rule"], f"_run_{rule.mto_rule_id.action}")(
                     [(mto_procurement, rule.mto_rule_id)]
                 )
         return True
