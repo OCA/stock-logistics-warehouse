96805eedeacaaf56fba9ee8c8d831ae8d3485035
Miquel Raïch <miquel.raich@forgeflow.com>
2026-02-26 11:13:18 +0100

    [ADD] stock_location_code

---
+++ stock_location_code/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 ForgeFlow S.L. (https://www.forgeflow.com)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Stock Location Code",
+    "summary": "Adds a code field to locations",
+    "version": "19.0.1.0.0",
+    "category": "Supply Chain/Inventory",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "author": "ForgeFlow, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "depends": ["stock"],
+    "data": ["views/stock_location_view.xml"],
+}
