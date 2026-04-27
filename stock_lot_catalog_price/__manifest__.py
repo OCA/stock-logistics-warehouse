823bbd784c6957c4ced2711a876805210c1c97cd
Víctor Martínez <victor.martinez@tecnativa.com>
2026-04-27 10:19:40 +0200

    [ADD] stock_lot_catalog_price: New module
    TT61752

---
+++ stock_lot_catalog_price/__manifest__.py
@@ -0,0 +1,15 @@
+# Copyright 2026 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl-3.0)
+
+{
+    "name": "StockLot Catalog Price",
+    "version": "18.0.1.0.0",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "category": "Warehouse",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock_lot_catalog", "stock_lot_list_price"],
+    "data": [],
+    "maintainers": ["victoralmau"],
+}
