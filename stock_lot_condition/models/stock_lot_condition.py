3de35a05401e711a737b678325b774a64cddc3b1
Víctor Martínez <victor.martinez@tecnativa.com>
2026-04-30 10:37:41 +0200

    [IMP] stock_lot_condition: Add color field

--- stock_lot_condition/models/stock_lot_condition.py
+++ stock_lot_condition/models/stock_lot_condition.py
@@ -1,5 +1,6 @@
-# Copyright 2025 Tecnativa - Víctor Martínez
+# Copyright 2025-2026 Tecnativa - Víctor Martínez
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from random import randint
 
 from odoo import fields, models
 
@@ -9,8 +10,12 @@ class StockLotCondition(models.Model):
     _description = "Stock Lot Condition"
     _order = "sequence, id"
 
+    def _get_default_color(self):
+        return randint(1, 11)
+
     sequence = fields.Integer()
     name = fields.Char(required=True, translate=True)
+    color = fields.Integer(default=_get_default_color)
     active = fields.Boolean(default=True)
     description = fields.Char(translate=True)
     required_note = fields.Boolean(
