0a42d8c9a29b6050669c988aab6197dfdcdc62c0
Víctor Martínez <victor.martinez@tecnativa.com>
2026-05-06 13:32:12 +0200

    [ADD] stock_lot_catalog_warehouse: New module
    TT61752

---
+++ stock_lot_catalog_warehouse/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2026 Tecnativa - Víctor Martínez
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+{
+    "name": "Stock Lot Catalog Warehouse",
+    "version": "18.0.1.0.0",
+    "category": "Stock",
+    "website": "https://github.com/OCA/stock-logistics-warehouse",
+    "author": "Tecnativa, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "depends": ["stock_lot_catalog", "stock_lot_warehouse"],
+    "data": [
+        "views/stock_lot_views.xml",
+    ],
+    "maintainers": ["victoralmau"],
+}
