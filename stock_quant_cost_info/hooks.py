c6967407a6881a3da96f823b85b3ede8f818709b
sara mohammed <sm284243@gmail.com>
2026-06-28 19:01:23 +0300

    [MIG] stock_quant_cost_info: fix pre_init_hook signature for v17

--- stock_quant_cost_info/hooks.py
+++ stock_quant_cost_info/hooks.py
@@ -1,14 +1,14 @@
 # Copyright 2019 Tecnativa - Ernesto Tejeda
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/lgpl.html).
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
 
-def pre_init_hook(cr):
+def pre_init_hook(env):
+    cr = env.cr
     cr.execute(
         """ALTER TABLE stock_quant
-    ADD COLUMN adjustment_cost numeric
+    ADD COLUMN IF NOT EXISTS adjustment_cost numeric
     DEFAULT 0"""
     )
-
     cr.execute(
         """ALTER TABLE stock_quant
     ALTER COLUMN adjustment_cost DROP DEFAULT;"""
