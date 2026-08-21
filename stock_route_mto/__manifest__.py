96959a20c126b341d546fc2cdc5e63d4c63e0ce4
kalpeshgajera-uncannycs <kgajera@uncannycs.com>
2026-08-21 12:28:49 +0530

    [19.0][MIG]stock_route_mto: Migration to 19.0

--- stock_route_mto/__manifest__.py
+++ stock_route_mto/__manifest__.py
@@ -7,7 +7,7 @@
     "summary": """
         Allows to identify MTO routes through a checkbox and availability to filter
         them.""",
-    "version": "18.0.1.0.0",
+    "version": "19.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/stock-logistics-warehouse",
