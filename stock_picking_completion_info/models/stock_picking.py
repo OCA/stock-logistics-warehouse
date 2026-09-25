OCA/wms

[FIX] stock_picking_completion_info: only drop the completion info a move can reach
Every move state change invalidated completion_info on the whole stock.picking
model.

The field cannot express its dependency through api.depends: it runs over
common_dest_move_ids, which is computed and not stored, so the ORM cannot walk
it backwards. It still has to be invalidated by hand, but only where it can
have gone stale: the transfer holding the written move and the transfers
holding the moves sharing its destination, kept to those whose operation type
shows the information. That reaches exactly as far as the field does at any
depth of a chain - a transfer depends on its siblings, and when those move on,
that write carries its own invalidation.

The move write also returns the result of super() again instead of True.

stock_picking_completion_info/models/stock_picking.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 
 
 class PickingType(models.Model):
@@ -11,6 +11,41 @@ class PickingType(models.Model):
         help="Inform operator of a completed operation at processing and at"
         " completion"
     )
+
+    @api.model
+    @tools.ormcache()
+    def _completion_info_type_ids(self):
+        """The operation types that show the completion information.
+
+        Archived operation types count as well: their transfers still compute
+        the field, and archiving does not go through the flag write below.
+        """
+        return tuple(
+            self.sudo()
+            .with_context(active_test=False)
+            .search([("display_completion_info", "=", True)])
+            .ids
+        )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        if any(vals.get("display_completion_info") for vals in vals_list):
+            self.env.registry.clear_caches()
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        if "display_completion_info" in vals:
+            self.env.registry.clear_caches()
+        return res
+
+    def unlink(self):
+        flagged = any(self.mapped("display_completion_info"))
+        res = super().unlink()
+        if flagged:
+            self.env.registry.clear_caches()
+        return res
 
 
 class StockPicking(models.Model):
@@ -78,9 +113,32 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def write(self, vals):
-        super().write(vals)
+        res = super().write(vals)
         if "state" in vals:
-            # invalidate cache, the api.depends do not allow to find all
-            # the conditions to invalidate the field
-            self.env["stock.picking"].invalidate_model(fnames=["completion_info"])
-        return True
+            self._invalidate_completion_info()
+        return res
+
+    def _invalidate_completion_info(self):
+        """Drop the completion information of the transfers this move reaches.
+
+        The api.depends cannot express the conditions under which the field
+        changes, so it has to be invalidated by hand. Doing that for the whole
+        stock.picking model also flushes every pending write on it, once per
+        move, which a transfer of a few hundred moves pays in full.
+
+        Only two things can make the field anything other than "no": the
+        transfer's own operation type shows the information, and the state of a
+        move that shares a destination with one of its own has changed. So the
+        transfers to invalidate are the one holding this move and the ones
+        holding the moves around it, kept to those whose operation type shows
+        the information at all.
+        """
+        type_ids = self.env["stock.picking.type"]._completion_info_type_ids()
+        if not type_ids:
+            # Nothing shows the information, so the field is "no" everywhere
+            # and there is nothing cached that could have gone stale.
+            return
+        pickings = self.picking_id | self.common_dest_move_ids.picking_id
+        stale = pickings.filtered(lambda p: p.picking_type_id.id in type_ids)
+        if stale:
+            stale.invalidate_recordset(fnames=["completion_info"])

OCA/stock-logistics-warehouse

Assisted-by: Claude Code:claude-opus-5-5

stock_picking_completion_info/models/stock_picking.py
@@ -1,6 +1,6 @@
 # Copyright 2019 Camptocamp SA
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
-from odoo import api, fields, models
+from odoo import api, fields, models, tools
 
 
 class PickingType(models.Model):
@@ -10,6 +10,41 @@ class PickingType(models.Model):
         help="Inform operator of a completed operation at processing and at"
         " completion"
     )
+
+    @api.model
+    @tools.ormcache()
+    def _completion_info_type_ids(self):
+        """The operation types that show the completion information.
+
+        Archived operation types count as well: their transfers still compute
+        the field, and archiving does not go through the flag write below.
+        """
+        return tuple(
+            self.sudo()
+            .with_context(active_test=False)
+            .search([("display_completion_info", "=", True)])
+            .ids
+        )
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        records = super().create(vals_list)
+        if any(vals.get("display_completion_info") for vals in vals_list):
+            self.env.registry.clear_cache()
+        return records
+
+    def write(self, vals):
+        res = super().write(vals)
+        if "display_completion_info" in vals:
+            self.env.registry.clear_cache()
+        return res
+
+    def unlink(self):
+        flagged = any(self.mapped("display_completion_info"))
+        res = super().unlink()
+        if flagged:
+            self.env.registry.clear_cache()
+        return res
 
 
 class StockPicking(models.Model):
@@ -75,9 +110,32 @@ class StockMove(models.Model):
     _inherit = "stock.move"
 
     def write(self, vals):
-        super().write(vals)
+        res = super().write(vals)
         if "state" in vals:
-            # invalidate cache, the api.depends do not allow to find all
-            # the conditions to invalidate the field
-            self.env["stock.picking"].invalidate_model(fnames=["completion_info"])
-        return True
+            self._invalidate_completion_info()
+        return res
+
+    def _invalidate_completion_info(self):
+        """Drop the completion information of the transfers this move reaches.
+
+        The api.depends cannot express the conditions under which the field
+        changes, so it has to be invalidated by hand. Doing that for the whole
+        stock.picking model also flushes every pending write on it, once per
+        move, which a transfer of a few hundred moves pays in full.
+
+        Only two things can make the field anything other than "no": the
+        transfer's own operation type shows the information, and the state of a
+        move that shares a destination with one of its own has changed. So the
+        transfers to invalidate are the one holding this move and the ones
+        holding the moves around it, kept to those whose operation type shows
+        the information at all.
+        """
+        type_ids = self.env["stock.picking.type"]._completion_info_type_ids()
+        if not type_ids:
+            # Nothing shows the information, so the field is "no" everywhere
+            # and there is nothing cached that could have gone stale.
+            return
+        pickings = self.picking_id | self.common_dest_move_ids.picking_id
+        stale = pickings.filtered(lambda p: p.picking_type_id.id in type_ids)
+        if stale:
+            stale.invalidate_recordset(fnames=["completion_info"])